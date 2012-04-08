fd0bda55797c12ce3b2bd2545a983c1415ca632f
Martin Malmsten <martin.malmsten@gmail.com>
2012-04-08 22:58:57 +0200

    Made Document immutable

--- src/whelks-core/src/main/java/se/kb/libris/whelks/Document.java
+++ src/whelks-core/src/main/java/se/kb/libris/whelks/Document.java
@@ -9,12 +9,7 @@ public interface Document {
     public List<? extends Link> getLinks();
     public List<? extends Key> getKeys();
     public byte[] getData();
-    public Document setData(byte[] data);
-    public Document setData(InputStream data);
-    public InputStream getDataAsStream();
     public String getContentType();
-    public Document setContentType();
     public String getFormat();
-    public Document setFormat(String format);
     public long getSize();
 }
