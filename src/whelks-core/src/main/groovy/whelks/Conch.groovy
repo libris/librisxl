a5313132b846ff4af7aff46e16a5a6848d776871
Markus Sköld <markus.skold@kb.se>
2012-04-17 15:38:02 +0200

    Added File

--- src/whelks-core/src/main/groovy/whelks/Conch.groovy
+++ src/whelks-core/src/main/groovy/whelks/Conch.groovy
@@ -3,9 +3,19 @@ package se.kb.libris.conch
 import se.kb.libris.whelks.Document
 import se.kb.libris.whelks.storage.Storage
 import se.kb.libris.whelks.basic.BasicWhelk
+import se.kb.libris.whelks.basic.BasicDocument
 import java.net.URI
 
 class MyDocument extends BasicDocument {
+    def type
+    URI identifier
+
+    MyDocument() {}
+
+    MyDocument(def uri) {
+        this.identifier = new URI(uri)
+        
+    }
 }
 
 class DiskStorage { //implements Storage {
@@ -31,9 +41,9 @@ class DiskStorage { //implements Storage {
 
     void store(MyDocument d) {
         def filename = (d.identifier ? d.identifier.toString() : _create_filename())
-        File file = new File("$storageDir/$filename")
-        file.write(new String(d.data))
-        d.identifier = new URI(filename)
+            File file = new File("$storageDir/$filename")
+            file.write(new String(d.data))
+            d.identifier = new URI(filename)
     }
 
     MyDocument retrieve(URI u) {
@@ -54,16 +64,25 @@ class DiskStorage { //implements Storage {
     }
 }
 
-class Index {}
+interface Index {
+    def index(Document d, def indexName, def type)
+}
+class BasicIndex implements Index {
+    def index(Document d, def indexName, def type) {
+        throw new UnsupportedOperationException("Not supported yet.")
+    }
+
+}
 class TripleStore {}
 
 class Whelk extends BasicWhelk {
     DiskStorage storage
     Index index
     TripleStore ts
+    def name
 
     Whelk(Storage _s, Index _i, TripleStore _ts) { storage = _s; index = _i; ts = _ts}
-    Whelk(Storage _s, Index _i) {storage = _s; index = _i}
+    Whelk(DiskStorage _s, Index _i) {storage = _s; index = _i}
     Whelk(DiskStorage _s) {storage = _s}
 
     def query(def q) {
@@ -72,12 +91,19 @@ class Whelk extends BasicWhelk {
 
     def ingest(MyDocument d, boolean bulk = false) {
         storage.store(d)
+        index.index(d, this.name, d.type)
         return d.identifier
     }
 
     def retrieve(identifier) {
         identifier = new URI(identifier)
         storage.retrieve(identifier)
+    }
+
+    def find(identifier) {
+        def response = index.find(this.name, "marc21", identifier)
+        println "Located document from elastic search"
+        println response
     }
 }
 
@@ -101,7 +127,9 @@ class App {
         def env = System.getenv()
         def whelk_storage = (env['PROJECT_HOME'] ? env['PROJECT_HOME'] : System.getProperty('user.home')) + "/whelk_storage"
         def storage = new DiskStorage(whelk_storage)
-        def whelk = new Whelk(storage)
+        def index = new ElasticSearchIndex()
+        def whelk = new Whelk(storage, index)
+        whelk.name = "test"
 
         def api = new API()
         api.addWhelk(whelk)
@@ -116,13 +144,21 @@ class App {
             println "File:"
             print new String(ldoc.data)
         }
+        else if (args.length > 1 && args[0] == 'find') {
+            whelk.find(args[1])
+        } 
         else if (args.length > 0) {
+
             def file = new File(args[0])
-            println "Storing file ${file}"
+            println "Storing file ${file} (${file.name})"
             println "${file.text}"
-            def doc = new MyDocument(file.text)
+
+            def doc = new MyDocument(file.name).withData(file.text.getBytes('UTF-8'))
+            doc.type = "marc21"
             def identifier = whelk.ingest(doc)
             println "Stored document with identifier ${identifier}"
+            println "Now trying to fetch it ..."
+            whelk.find(file.name)
         }
     }
 }
