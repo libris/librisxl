09c4f55abe15967efaf64a1eb1ef81bdc13e4468
Markus Sköld <markus.skold@kb.se>
2012-06-05 10:01:23 +0200

    Changes in suggest-conversion

--- src/whelks-core/src/main/resources/sug_json.py
+++ src/whelks-core/src/main/resources/sug_json.py
@@ -13,7 +13,7 @@ def transform(a_json, rtype):
 
     should_add = True
 
-    for f in a_json['fields']:
+    for f in a_json['data']['fields']:
         for k, v in f.items():
             if k == '001':
                 link = "/%s/%s" % (rtype, v)
@@ -24,7 +24,7 @@ def transform(a_json, rtype):
                         if sk == '0' and rtype == 'bib':
                             should_add = False
                         sug_json[k][sk] = sv
-            elif k in ['400', '678', '856']:
+            elif k in get_fields(rtype):
                 f_list = sug_json.get(k, [])
                 d = {}
                 for sf in v['subfields']:
@@ -37,12 +37,17 @@ def transform(a_json, rtype):
         f_100 = ' '.join(sug_json['100'].values())
         if link:
             sug_json['link'] = link
+        sug_json['authorized'] = True if rtype == 'auth' else False
         sug_json = get_records(f_100, sug_json)
         
         return sug_json
     else:
         return 0
  
+def get_fields(rtype):
+    if rtype == 'auth':
+        return ['400','500','678','856']
+    return ['400','678']
 
 
 def get_records(f_100, sug_json):
@@ -79,27 +84,31 @@ try:
     data = document.getDataAsString()
 except:
     data = sys.stdin.read()
+    whelk = None
     _in_console = True
 
 
 #print "console mode", _in_console
 
 in_json = json.loads(data)
-rtype = record_type(in_json['leader'])
+rtype = record_type(in_json['data']['leader'])
 suggest_source = rtype 
 if (rtype == 'bib'):
     suggest_source = 'name'
 
-identifier = "/%s/%s/%s" % (whelk.name, suggest_source, document.identifier.toString().split("/")[-1])
+w_name = whelk.name if whelk else "test"
+
+identifier = "/%s/%s/%s" % (w_name, suggest_source, document.identifier.toString().split("/")[-1])
 sug_json = transform(in_json, rtype)
 if sug_json:
     sug_json['identifier'] = identifier
     r = json.dumps(sug_json)
 
-    mydoc = whelk.createDocument().withIdentifier(identifier).withData(r).withContentType("application/json")
+    if whelk:
+        mydoc = whelk.createDocument().withIdentifier(identifier).withData(r).withContentType("application/json")
 
-    #print "Sparar dokument i whelken daaraa"
-    uri = whelk.store(mydoc)
+        #print "Sparar dokument i whelken daaraa"
+        uri = whelk.store(mydoc)
 else:
     print "Record %s has no usable auth information. (i.e. no 100-field)" % document.identifier
 
