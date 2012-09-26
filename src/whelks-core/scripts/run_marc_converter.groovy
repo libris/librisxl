4049699be3c575025c439fdc21587ae5d89caf18
Niklas Lindström <lindstream@gmail.com>
2012-09-26 18:35:39 +0200

    Add groovy task to tools and scripts for (dev) utilities

---
+++ src/whelks-core/scripts/run_marc_converter.groovy
@@ -0,0 +1,12 @@
+import se.kb.libris.whelks.basic.*
+import se.kb.libris.whelks.plugin.*
+
+def path = args[0]
+def slug = (path =~ /\/(\w+\/\w+)\.json$/)[0][1]
+def id = "tag:data.kb.se,2012:/${slug}"
+def source = new BasicDocument().withIdentifier(id).withData(new File(path).text)
+
+def conv = new MarcCrackerIndexFormatConverter()
+def result = conv.convert(source)
+
+println groovy.json.JsonOutput.prettyPrint(result.dataAsString)
