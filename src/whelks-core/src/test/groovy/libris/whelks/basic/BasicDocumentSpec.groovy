7ca6a277a7e45444fd9d026f9f5c965a442efcd8
mskold <markus.skold@kb.se>
2012-10-03 19:01:34 +0200

    Simplified test

--- src/whelks-core/src/test/groovy/libris/whelks/basic/BasicDocumentSpec.groovy
+++ src/whelks-core/src/test/groovy/libris/whelks/basic/BasicDocumentSpec.groovy
@@ -9,26 +9,13 @@ class BasicDocumentSpec extends Specification {
         def doc = new BasicDocument().withIdentifier("/bib/123").withData("foo").withContentType("text/plain")
         when:
         def newdoc = new BasicDocument(doc)
-        then:
-        newdoc.identifier == doc.identifier
-        newdoc.dataAsString == doc.dataAsString
-        newdoc.contentType == doc.contentType
-        newdoc.version == doc.version
-        newdoc.size == doc.size
-    }
-
-    def "should copy document without linking"() {
-        given:
-        def doc = new BasicDocument().withIdentifier("/bib/123").withData("foo").withContentType("text/plain")
-        when:
-        def newdoc = new BasicDocument(doc)
         doc.identifier = new URI("/bib/234")
+        doc.version = "2"
         then:
         newdoc.identifier == new URI("/bib/123")
         newdoc.dataAsString == doc.dataAsString
         newdoc.contentType == doc.contentType
-        newdoc.version == doc.version
+        newdoc.version == "1"
         newdoc.size == doc.size
     }
-
 }
