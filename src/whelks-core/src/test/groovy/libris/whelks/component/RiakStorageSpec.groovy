b2072da259b7d33eb43236b3cdaca10b9d1b68db
Katarina Stempel <ina.stempel@gmail.com>
2012-10-18 16:16:22 +0200

    added spock spec test for riak storage

---
+++ src/whelks-core/src/test/groovy/libris/whelks/component/RiakStorageSpec.groovy
@@ -0,0 +1,48 @@
+package se.kb.libris.whelks.component
+
+import spock.lang.*
+import se.kb.libris.whelks.basic.BasicDocument
+import java.net.URI
+
+class RiakStorageSpec extends Specification {
+
+    def rs = new RiakStorage("bib")
+    def doc = new BasicDocument().withIdentifier("/bib/777").withData("test").withContentType("text/plain")
+    
+    def "should store document in riak"(){
+        when:
+        rs.store(doc)
+
+        then:
+        notThrown(Exception)
+    }
+
+    def "should extract id from uri"(){
+        given:
+        URI uri = new URI("/bib/777")
+
+        expect:
+        rs.extractIdFromURI(uri) == "777"
+    }
+
+
+    /*def "should get document from riak"(){
+        given:
+        URI uri = new URI("/bib/777")
+
+        when:
+        def d = rs.get(uri)
+
+        then:
+        !d.empty
+    }
+
+    def "should delete document from riak"(){
+        when:
+        rs.delete(new URI("/bib/777"))
+
+        then:
+        notThrown(Exception)
+    }*/
+}
+
