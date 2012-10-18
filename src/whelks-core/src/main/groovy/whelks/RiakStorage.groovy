421c99f719cd8ed9703b8a6a32eab74c0c60f7b9
Katarina Stempel <ina.stempel@gmail.com>
2012-10-18 15:13:57 +0200

    implemented riakstorage get and delete

--- src/whelks-core/src/main/groovy/whelks/RiakStorage.groovy
+++ src/whelks-core/src/main/groovy/whelks/RiakStorage.groovy
@@ -2,29 +2,29 @@ package se.kb.libris.whelks.component
 
 import se.kb.libris.whelks.Document
 import se.kb.libris.whelks.basic.BasicDocument
+import se.kb.libris.whelks.basic.BasicPlugin
 import se.kb.libris.whelks.Key
 import se.kb.libris.whelks.LookupResult
-import se.kb.libris.whelks.exception.WhelkException
+import se.kb.libris.whelks.exception.*
 import groovy.util.logging.Slf4j as Log
 import java.net.URL
-import java.net.HttpURLConnection
-import com.basho.riak.client.IRiakClient
-import com.basho.riak.client.bucket.Bucket
-import com.basho.riak.client.RiakFactory
-import com.basho.riak.client.operations.StoreObject
+import java.net.URI
+import com.basho.riak.client.http.RiakClient
 import com.basho.riak.client.RiakException
+import com.basho.riak.client.http.RiakObject
+import com.basho.riak.client.http.response.FetchResponse
 
 @Log
-class RiakStorage implements Storage {
-    private IRiakClient riakClient
+class RiakStorage extends BasicPlugin implements Storage {
+    private RiakClient riakClient
     private byte[] clientId
-    private Bucket riakBucket
+    private String bucket_name
     private int riak_port
     private String plugin_id = "riakstore"
     private boolean enabled = true
 
-    //public String getId(){ return this.clientId }
-    def id
+    String getId(){ return this.clientId }
+    int order
     boolean isEnabled(){ return this.enabled }
     void enable(){ this.enabled = true }
     void disable(){ this.enabled = false }
@@ -37,23 +37,31 @@ class RiakStorage implements Storage {
             def riak_host = properties.getProperty("riakhost")
             def riak_port = Integer.parseInt(properties.getProperty("riakport"))
             def url = "http://" + riak_host + ":" + riak_port + "/riak/"
-            riakClient = RiakFactory.httpClient(url)
+            bucket_name = bucket
+            riakClient = new RiakClient(url)
+            clientId = riakClient.clientId
             log.info("Loading RiakClient " + url)
-            clientId = riakClient.generateAndSetClientId()
-            riakBucket = riakClient.createBucket(bucket).execute()
          } catch(RiakException re) {
-            log.debug("Could not intialize riak httpclient for: " + url + " Bucket: " + bucket + " " + e.getMessage())
+            log.debug("Could not initialize riak httpclient for: " + url + " " + e.getMessage())
+            throw new WhelkRuntimeException("Could not connect to RiakStorage. Url: " + url + " " + e.printStackTrace())
         }
     }
 
     void store(Document d){
-        try {
-            final String id = UUID.randomUUID().toString()
-            this.riakBucket.store(id, d.data).execute()
-            log.debug("Stored object with id: " + id)
-        } catch (Exception e) {
-            log.debug("Exception in store: " + e.printStackTrace())
-        }
+        String key = extractIdFromURI(d.identifier)
+        RiakObject riakObject = new RiakObject(riakClient, bucket_name, key, d.data, d.contentType)
+        def storeResponse = riakClient.store(riakObject)
+        log.info("Stored object with key: " + key + " to bucket: " + bucket_name + " response status: " + storeResponse.getStatusCode())
+    }
+
+    String extractIdFromURI(URI uri) {
+        def path_fragments = uri.path.split("/")
+        return path_fragments[path_fragments.length-1]
+    }
+
+    String extractBucketNameFromURI(URI uri) {
+        def path_fragments = uri.path.split("/")
+        return path_fragments[path_fragments.length-2]
     }
 
     void store(Iterable<Document> d){
@@ -62,8 +70,18 @@ class RiakStorage implements Storage {
         }
     }
 
-    Document get(URI uri){
-        throw new UnsupportedOperationException("Not supported yet.")
+    String createURIFromId(String id){
+        return "/" + bucket_name + "/" + id
+    }
+
+    Document get(URI uri) {
+        String key = extractIdFromURI(d.identifier)
+        String bucket = extractBucketNameFromURI(d.identifier)
+        FetchResponse fetchResponse = riakClient.fetch(bucket, key)
+        if (fetchResponse.getStatusCode().toString().trim().substring(0,1) == "2") {
+            return new BasicDocument().withIdentifier(key).withData(fetchResponse.getObject().getValue()).withContentType(fetchResponse.getObject().getContentType())
+        }
+        return null
     }
 
     Iterable<Document> getAll(){
@@ -71,7 +89,9 @@ class RiakStorage implements Storage {
     }
 
     void delete(URI uri){
-        throw new UnsupportedOperationException("Not supported yet.")
+        String key = extractIdFromURI(d.identifier)
+        String bucket = extractBucketNameFromURI(d.identifier)
+        FetchResponse response = new FetchResponse(riakClient.delete(bucket, key))
     }
 
     LookupResult<? extends Document> lookup(Key key){
