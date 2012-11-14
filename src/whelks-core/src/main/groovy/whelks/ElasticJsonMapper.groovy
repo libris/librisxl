68d95a5a652c48b0d7e54bf1434ea170a9a916fe
mskold <markus.skold@kb.se>
2012-11-14 14:03:25 +0100

    Added file.

---
+++ src/whelks-core/src/main/groovy/whelks/ElasticJsonMapper.groovy
@@ -0,0 +1,21 @@
+package se.kb.libris.whelks.basic
+
+import org.codehaus.jackson.*
+import org.codehaus.jackson.map.*
+import org.codehaus.jackson.map.ser.CustomSerializerFactory
+import org.elasticsearch.common.text.*
+
+class ElasticJsonMapper extends ObjectMapper {
+    ElasticJsonMapper() {
+        super();
+        CustomSerializerFactory sf = new CustomSerializerFactory();
+        this.setSerializerFactory(sf);
+
+        sf.addGenericMapping(StringAndBytesText.class, new JsonSerializer<StringAndBytesText>() {
+            @Override
+            public void serialize(StringAndBytesText value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+                jgen.writeObject(value.string())
+            }
+        });
+    }
+}
