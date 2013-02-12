49c0869745bb8a56c228bc234f6e60f3ea0cd374
Katarina Stempel <ina.stempel@gmail.com>
2013-02-12 17:16:53 +0100

    Started work on jsonld2marcconverter

---
+++ whelk-core/src/main/groovy/whelks/JsonLD2MarcConverter.groovy
@@ -0,0 +1,28 @@
+package se.kb.libris.whelks.plugin
+
+import groovy.util.logging.Slf4j as Log
+
+import org.codehaus.jackson.map.ObjectMapper
+
+@Log
+class  JsonLD2MarcConverter extends MarcCrackerAndLabelerIndexFormatConverter implements FormatConverter {
+
+    String requiredContentType = "application/json"
+    def marcref
+
+    JsonLD2MarcConverter() {
+        mapper = new ObjectMapper()
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("marc_refs.json").getText("utf-8")
+        this.marcref = mapper.readValue(is, Map)
+    }
+
+    def mapDocument(def injson) {
+        def fields = [:]
+        def idstr = injson?.get("@id").split("/")
+        if (idstr) {
+         fields["001"] = idstr[idstr.length - 1]
+        }
+        fields["005"] = injson?.get("dateAndTimeOfLatestTransaction").replaceAll("^\\d.", "")
+    }
+
+}
