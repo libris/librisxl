0404324cc565b180329252df223eae8e030e68a9
mskold <markus.skold@kb.se>
2013-02-13 15:46:31 +0100

    Added more tests and fixed typo in test author.

--- whelk-core/src/test/groovy/libris/whelks/plugin/Marc2JsonLDConverterSpec.groovy
+++ whelk-core/src/test/groovy/libris/whelks/plugin/Marc2JsonLDConverterSpec.groovy
@@ -33,13 +33,21 @@ class Marc2JsonLDConverterSpec extends Specification {
             ["ind1":"1","ind2":" ","subfields":[["a": "Svensson, Sven"], ["z": "foo"]]]       | [(Marc2JsonLDConverter.RAW_LABEL):["100":["ind1":"1","ind2":" ","subfields":[["a": "Svensson, Sven"], ["z": "foo"]]]]]
     }
 
+    def "should map multiple authors"() {
+        expect:
+            conv.createJson(new URI("/bib/1234"), marc)["describes"] == jsonld
+        where:
+            marc                                                                                                                               | jsonld
+            ["fields":[["100":["ind1":"1","subfields":[["a": "Svensson, Sven"]]]],["700":["ind1":"1","subfields":[["a":"Karlsson, Karl,"]]]]]] | ["expressionManifested":["authorList":[["preferredNameForThePerson" : "Svensson, Sven", "surname":"Svensson", "givenName":"Sven", "name": "Sven Svensson"],["preferredNameForThePerson" : "Karlsson, Karl", "surname":"Karlsson", "givenName":"Karl", "name": "Karl Karlsson"]]]]
+    }
+
     def "should map title"() {
         expect:
             conv.mapDefault("245", marc) == jsonld
         where:
             marc                                                                                            | jsonld
-            [ "ind1":" ", "ind2": " ", "subfields":[["a":"Bokens titel", "c": "Kalle Kula"]]]               | ["describes":["expressionManifested":["titleProper" : "Bokens titel", "statementOfResponsibilityRelatingToTitleProper" : "Kalle Kula"]]]
-            [ "ind1":" ", "ind2": " ", "subfields":[["a":"Bokens titel", "c": "Kalle Kula", "z":"foo"]]]    | [(Marc2JsonLDConverter.RAW_LABEL):["245":[ "ind1":" ", "ind2": " ", "subfields":[["a":"Bokens titel", "c": "Kalle Kula", "z":"foo"]]]]]
+            [ "ind1":" ", "ind2": " ", "subfields":[["a":"Bokens titel"], ["c": "Kalle Kula"]]]               | ["describes":["expressionManifested":["titleProper" : "Bokens titel", "statementOfResponsibilityRelatingToTitleProper" : "Kalle Kula"]]]
+            [ "ind1":" ", "ind2": " ", "subfields":[["a":"Bokens titel"], ["c": "Kalle Kula"],["z":"foo"]]]    | [(Marc2JsonLDConverter.RAW_LABEL):["245":[ "ind1":" ", "ind2": " ", "subfields":[["a":"Bokens titel", "c": "Kalle Kula", "z":"foo"]]]]]
     }
 
     def "should map isbn"() {
@@ -58,10 +66,12 @@ class Marc2JsonLDConverterSpec extends Specification {
         expect:
             conv.mergeMap(o, n) == m
         where:
-            o                              | n                          | m
-            ["foo":"bar"]                  | ["foo":"beer"]             | ["foo": ["bar", "beer"]]
-            ["key":["v1","v2"]]            | ["key":"v3"]               | ["key": ["v1","v2","v3"]]
-            ["key":["subkey":["v1","v2"]]] | ["key":["subkey":"v3"]]    | ["key": ["subkey":["v1","v2","v3"]]]
+            o                              | n                                | m
+            ["foo":"bar"]                  | ["foo":"beer"]                   | ["foo": ["bar", "beer"]]
+            ["key":["v1","v2"]]            | ["key":"v3"]                     | ["key": ["v1","v2","v3"]]
+            ["key":["subkey":["v1","v2"]]] | ["key":["subkey":"v3"]]          | ["key": ["subkey":["v1","v2","v3"]]]
+            ["key":["subkey":["v1":"v2"]]] | ["key":["subkey":["v3":"v4"]]]   | ["key": ["subkey":["v1":"v2","v3":"v4"]]]
+            ["key":["subkey":["v1":"v2"]]] | ["key":["subkey":["v1":"v4"]]]   | ["key": ["subkey":[["v1":"v2"],["v1":"v4"]]]]
     }
 
     /*
