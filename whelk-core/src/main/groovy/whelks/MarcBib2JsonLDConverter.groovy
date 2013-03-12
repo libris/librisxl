3c0bef209bcd2db4f08b4f268c116f9edcc024ef
mskold <markus.skold@kb.se>
2013-03-12 15:31:14 +0100

    Mapping most fields using restructured marc mapper.

---
+++ whelk-core/src/main/groovy/whelks/MarcBib2JsonLDConverter.groovy
@@ -0,0 +1,83 @@
+package se.kb.libris.whelks.plugin
+
+import se.kb.libris.whelks.exception.*
+
+import groovy.util.logging.Slf4j as Log
+
+@Log
+class MarcBib2JsonLDConverter extends BasicMarc2JsonLDConverter {
+    MarcBib2JsonLDConverter(String rt) {
+        super(rt)
+    }
+    @Override
+    def mapPerson(outjson, code, fjson, marcjson) {
+        def person = super.mapPerson(outjson, code, fjson, marcjson)
+        log.trace("HIJACKED PERSON: $person")
+        return person
+    }
+
+    def mapIsbn(outjson, code, fjson, marcjson) {
+        return getMarcValueFromField("a", fjson).split(/\s+/, 2)[0].replaceAll("-", "")
+    }
+
+    def mapIdentifier(outjson, code, fjson, marcjson) {
+        def scheme = "isbn"
+        def id = ["@type":"Identifier","identifierScheme":scheme]
+        if (scheme == "isbn") {
+            def iv = fjson.subfields[0]["a"].split(/\s+/, 2)
+            id["identifiedValue"] = iv[0].replaceAll("-", "")
+            if (iv.length > 1) {
+                id["identifierNote"] = iv[1]
+            }
+            log.trace("bnode other identifier: $id")
+        } else {
+            id["identifierValue"] = fjson.subfields[0]["a"]
+        }
+        return id
+    }
+
+    def mapSubject(code, json) {
+        def baseurl = "http://libris.kb.se/"
+        def system = "sab"
+        def subjectcode = ""
+        def out = []
+        boolean complete = true
+        log.trace("map subject for $json")
+        json["subfields"].each {
+            it.each { key, value ->
+                switch(key) {
+                    case "a":
+                    subjectcode = value
+                    break;
+                    case "2":
+                    system = value
+                    break;
+                    default:
+                    complete = false
+                    break;
+                }
+            }
+        }
+        if (!complete) {
+            return false
+        }
+        if (marcref.get(recordType).subjects.get(system)?.containsKey(subjectcode)) {
+            out << ["@id":new String(marcref.get(recordType).subjects[system][subjectcode])]
+        }
+        if (system.startsWith("kssb/")) {
+            if (subjectcode =~ /\s/) {
+                def (maincode, restcode) = subjectcode.split(/\s+/, 2)
+                subjectcode = maincode+"/"+URLEncoder.encode(restcode)
+            } else {
+                if (marcref.get(recordType).subjects.get("sab")?.containsKey(subjectcode)) {
+                    out << ["@id":new String(marcref.get(recordType).subjects[system][subjectcode])]
+                }
+                subjectcode = URLEncoder.encode(subjectcode)
+            }
+            out << ["@id":new String("http://libris.kb.se/sab/$subjectcode")]
+        }
+        log.trace("map subject out: $out")
+        return (out.size() > 0 ? out : false)
+    }
+}
+
