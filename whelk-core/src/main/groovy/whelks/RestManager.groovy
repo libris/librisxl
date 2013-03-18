5fb41b86df0461bd2751ee124db0fb347cc73894
Katarina Stempel <ina.stempel@gmail.com>
2013-03-18 16:36:44 +0100

    Added RootRouteRestlet to RestManager

--- whelk-core/src/main/groovy/whelks/RestManager.groovy
+++ whelk-core/src/main/groovy/whelks/RestManager.groovy
@@ -36,21 +36,33 @@ class RestManager extends Application {
         def router = new Router(ctx)
 
 
-        router.attach("/", new Restlet() {
+        /*router.attach("/", new Restlet() {
             ObjectMapper mapper = new ObjectMapper()
 
             void handle(Request request, Response response) {
                 if (request.method == Method.POST) {
                     // TODO: Handle uploaded unnamed document
                 }
+                if (request.method == Method.GET) {
+                    def discoveryAPI = new DiscoveryAPI(this.whelk)
+                    def uri = new URI(discoveryAPI.path)
+                    log.info "Handling inbound route to " + uri
+                    reponse.serverRedirect(discoveryAPI, new Reference(uri), request, response)
+            } else if (request.method == Method.PUT || request.method == Method.POST) {
+                /*def documentAPI = new DocumentRestlet(this.whelk)
+                response.serverRedirect(documentAPI, new Reference(new URI(documentAPI.path)), request, response)
+            }
                 def wlist = ["available_whelks":whelks.collect { it.prefix }]
                 response.setEntity(mapper.writeValueAsString(wlist), MediaType.APPLICATION_JSON)
             }
-        })
+        })*/
 
         log.debug("Looking for suitable APIs to attach")
 
         whelks.each {
+            log.debug("Attaching RootRoute API")
+            def rapi = new RootRouteRestlet(it)
+            router.attach(rapi.path, rapi)
             log.debug("Attaching Discovery API")
             def dapi = new DiscoveryAPI(it)
             router.attach(dapi.path, dapi)
@@ -64,7 +76,7 @@ class RestManager extends Application {
                 log.debug("Attaching ${api.class.name} at ${api.path}")
                 if (api.varPath) {
                     router.attach(api.path, api).template.variables.put("identifier", new Variable(Variable.TYPE_URI_PATH))
-                }
+                } 
             }
         }
 
