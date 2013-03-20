980fe948d99fb89e7e1d03fb256d91844282ea8c
Niklas Lindström <lindstream@gmail.com>
2013-03-20 01:52:59 +0100

    Use relatorcodes.json for relators in MarcBib2JsonLDConverter

--- whelk-core/src/main/groovy/whelks/MarcBib2JsonLDConverter.groovy
+++ whelk-core/src/main/groovy/whelks/MarcBib2JsonLDConverter.groovy
@@ -6,8 +6,14 @@ import groovy.util.logging.Slf4j as Log
 
 @Log
 class MarcBib2JsonLDConverter extends BasicMarc2JsonLDConverter {
+
+    Map relators
+
     MarcBib2JsonLDConverter(String rt) {
         super(rt)
+        relators = getClass().classLoader.getResourceAsStream("relatorcodes.json").withStream {
+            mapper.readValue(it, List).collectEntries { [it.code, it] }
+        }
     }
 
     @Override
@@ -36,7 +42,7 @@ class MarcBib2JsonLDConverter extends BasicMarc2JsonLDConverter {
         if (relcode) {
             section = []
             relcode.split().each {
-                section << ["relator":(marcref.bib.relators[it] ?: "creator")]
+                section << ["relator":(relators[it].term ?: "creator")]
             }
         } else {
             section = [["relator": "authorList"]]
