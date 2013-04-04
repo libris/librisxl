d9e0e3b8a785af0f45784de1f3588d603fe46af5
mskold <markus.skold@kb.se>
2013-04-04 08:26:25 +0200

    Made select parts of whelk statically compiled to improve stability and performance. URLEncoding values for identifier from import, since that has proven to be flaky.

--- whelk-core/src/main/java/se/kb/libris/whelks/component/Index.java
+++ whelk-core/src/main/java/se/kb/libris/whelks/component/Index.java
@@ -8,7 +8,7 @@ import se.kb.libris.whelks.SearchResult;
 
 public interface Index extends Component {
     public void index(Document d, String indexName);
-    public void index(Iterable<Document> d, String indexName);
+    public void index(List<Document> d, String indexName);
     public SearchResult query(Query query, String indexName);
 
 }
