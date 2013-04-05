18a5933b86267e71cbaad7b3d2eac0b194277b5f
mskold <markus.skold@kb.se>
2013-04-05 10:58:16 +0200

    Updated the script

--- datatools/oaipmh.sh
+++ datatools/oaipmh.sh
@@ -3,21 +3,21 @@
 RESOURCE=$1
 GOBACK=$2
 if [[ "$RESOURCE" == "" ]] || [[ $GOBACK == "" ]]; then
-    echo "Usage $(basename $0) <OAIPMH resource> <time back in days (d) or hours (H)>"
+    echo "Usage $(basename $0) <OAIPMH resource> <time back hours>"
     echo ""
-    echo "  Example: Start an OAIPMH import from bib since $(date -v-1d +'%Y-%m-%d')"
-    echo "  \$ $(basename $0) bib 1d"
+    echo "  Example: Start an OAIPMH import from bib since $(date -d '-24 hour' +'%Y-%m-%d')"
+    echo "  \$ $(basename $0) bib 24"
     echo ""
     exit
 fi
 
 WEBAPPS=/var/lib/tomcat6/webapps/whelk-core
-CONFIG=file:/data/librisxl/whelks.json
+SETENV=/usr/share/tomcat6/bin/setenv.sh
 
 CLASSES=$WEBAPPS/WEB-INF/classes
 LIBS=$WEBAPPS/WEB-INF/lib
-DATE=$(date -v-$GOBACK +"%Y-%m-%dT%H:%M:%SZ")
+SINCE=$(date -d "-$GOBACK hour" +"%Y-%m-%dT%H:%M:%SZ")
 
-echo "get OAIPMH for $RESOURCE since $DATE"
+source $SETENV
 
-#java -Dwhelk.config.uri=$CONFIG -cp $CLASSES:$LIBS/* se.kb.libris.whelks.WhelkOperator import auth auth
+java $JAVA_OPTS -cp $CLASSES:$LIBS/* se.kb.libris.whelks.WhelkOperator import $RESOURCE $RESOURCE $SINCE
