f62086f26f734204a5c83779bb4342c9594c9c81
mskold <markus.skold@kb.se>
2013-04-15 10:35:34 +0200

    Removed crap

--- whelk-core/src/main/java/se/kb/libris/whelks/storage/Storage.java
+++
@@ -1,9 +0,0 @@
-package se.kb.libris.whelks.storage;
-
-import java.net.URI;
-import se.kb.libris.whelks.Document;
-
-public interface Storage {
-    public void store(Document d);
-    public Document retrieve(URI uri);
-}
