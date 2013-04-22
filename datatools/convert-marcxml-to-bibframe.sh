e82de87a4f556b6fbf8978ef9be4d91b6ae0b070
Niklas Lindström <lindstream@gmail.com>
2013-04-22 19:00:51 +0200

    Simpler convert-marcxml-to-bibframe.sh

--- datatools/convert-marcxml-to-bibframe.sh
+++ datatools/convert-marcxml-to-bibframe.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 SAXON_JAR=$1
-BIBFRAME_REPO=$2
+QUERY=$2
 SOURCE=$(pwd)/$3
 
-QUERY=$BIBFRAME_REPO/xquery/saxon.xqy
 BASE=http://libris.kb.se/bib/
 java -cp $SAXON_JAR net.sf.saxon.Query $QUERY marcxmluri=$SOURCE baseuri=$BASE serialization=rdfxml
