9f11e8f1a184be9025f27bd5478c55617898503a
mskold <markus.skold@kb.se>
2013-04-24 13:09:43 +0200

    Fatjar builds into dedicated jar-file. Using that for scripts.

--- datatools/convert-iso2709-to-json.sh
+++ datatools/convert-iso2709-to-json.sh
@@ -11,4 +11,4 @@ SOURCE_MARC=$(cd $(dirname $SOURCE_MARC); pwd)/$(basename $SOURCE_MARC)
 SCRIPT_DIR=$(dirname $0)
 cd $SCRIPT_DIR
 WHELKS_BUILD=../whelk-extensions/build
-java -cp $WHELKS_BUILD/classes/main/:$WHELKS_BUILD/libs/whelk-extensions-0.1.1.jar se.kb.libris.conch.converter.MarcJSONConverter $SOURCE_MARC
+java -cp $WHELKS_BUILD/classes/main/:$WHELKS_BUILD/libs/librisxl.jar se.kb.libris.conch.converter.MarcJSONConverter $SOURCE_MARC
