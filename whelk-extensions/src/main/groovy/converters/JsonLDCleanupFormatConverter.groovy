23ea7c7eeb59a25b3b200a1706d68ffca9e998e0
Katarina Stempel <ina.stempel@gmail.com>
2013-04-25 17:05:48 +0200

    JsonLDCleanuptFormatConverter cleaning title and titleRemainder

--- whelk-extensions/src/main/groovy/converters/JsonLDCleanupFormatConverter.groovy
+++ whelk-extensions/src/main/groovy/converters/JsonLDCleanupFormatConverter.groovy
@@ -14,16 +14,27 @@ class JsonLDCleanupFormatConverter extends BasicFormatConverter {
 
     Document doConvert(Document doc) {
         def json = mapper.readValue(doc.dataAsString, Map)
-        def title = json.about?.instanceOf?.get("title")
+        def title = json.about.instanceOf?.get("title")
+        def titleRemainder = json.about.instanceOf?.get("titleRemainder")
+        def statementOfResponsibility = json.about.instanceOf?.get("statementOfResponsibility")
+        def cleaned_title = title
+        def cleaned_titleRemainder = titleRemainder
         if (title) {
-           def cleaned_title
-           if (title instanceof String) {
-              if (title[-1].equals("/")) {
-                  cleaned_title = title[0..-2].trim()
-              }
-              json["about"]["instanceOf"]["title"] = cleaned_title
+           if (titleRemainder && title[-1].equals(":")) {
+               cleaned_title = title[0..-2].trim()
            }
+           if (statementOfResponsibility) {
+               if (title[-1].equals("/")) {
+                    cleaned_title = title[0..-2].trim()
+               }
+               if (titleRemainder && titleRemainder[-1].equals("/")) {
+                    cleaned_titleRemainder = titleRemainder[0..-2].trim()
+               }
+           }
+           json["about"]["instanceOf"]["title"] = cleaned_title
+           json["about"]["instanceOf"]["titleRemainder"] = cleaned_titleRemainder
         }
+        
         doc = doc.withData(mapper.writeValueAsBytes(json))
         //TODO: clean up interpunction 260, 300
         return doc
