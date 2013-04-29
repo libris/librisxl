a8375bd4636d080708cc0bb9aa21e972eb4f3a65
Katarina Stempel <ina.stempel@gmail.com>
2013-04-29 13:44:28 +0200

    Interpunction removed from publication properties

--- whelk-extensions/src/main/groovy/converters/JsonLDCleanupFormatConverter.groovy
+++ whelk-extensions/src/main/groovy/converters/JsonLDCleanupFormatConverter.groovy
@@ -14,6 +14,7 @@ class JsonLDCleanupFormatConverter extends BasicFormatConverter {
 
     Document doConvert(Document doc) {
         def json = mapper.readValue(doc.dataAsString, Map)
+
         def title = json.about.instanceOf?.get("title")
         def titleRemainder = json.about.instanceOf?.get("titleRemainder")
         def statementOfResponsibility = json.about.instanceOf?.get("statementOfResponsibility")
@@ -24,7 +25,7 @@ class JsonLDCleanupFormatConverter extends BasicFormatConverter {
                cleaned_title = title[0..-2].trim()
            }
            if (statementOfResponsibility) {
-               if (title[-1].equals("/")) {
+               if (title.trim()[-1].equals("/")) {
                     cleaned_title = title[0..-2].trim()
                }
                if (titleRemainder && titleRemainder[-1].equals("/")) {
@@ -34,7 +35,34 @@ class JsonLDCleanupFormatConverter extends BasicFormatConverter {
            json["about"]["instanceOf"]["title"] = cleaned_title
            json["about"]["instanceOf"]["titleRemainder"] = cleaned_titleRemainder
         }
-        
+        def publisher = json.about?.get("publisher")
+        def placeOfPublication_name = json.about?.get("placeOfPublication")?.get("name")
+        def dateOfPublication = json.about.get("dateOfPublication")
+        def placeOfManufacture_name = json.about?.get("placeOfManufacture")?.get("name")
+        if (dateOfPublication) {
+            if (dateOfPublication.size() > 1 && (dateOfPublication[-1].equals(";") || dateOfPublication[-1].equals(","))) {
+                json["about"]["dateOfPublication"] = dateOfPublication[0..-2].trim()
+            }
+            if (placeOfPublication_name && placeOfPublication_name.size() > 1) {
+                if ((json.about?.get("publisher") && placeOfPublication_name[-1].equals(":")) || placeOfPublication_name[-1].equals(",") || placeOfPublication_name[-1].equals(";")) {
+                    json["about"]["placeOfPublication"]["name"] = placeOfPublication_name[0..-2].trim()
+                }
+            } 
+            if (placeOfManufacture_name) {
+                if (placeOfManufacture_name[-1].equals(";")) {
+                    json["about"]["placeOfManufacture"]["name"] = placeOfManufacture_name[0..-2].trim().replaceAll("[()]","")
+                } else {
+                    json["about"]["placeOfManufacture"]["name"] = placeOfManufacture_name.replaceAll("[()]","")
+                }
+            }
+            if (publisher && publisher instanceof List) {
+                publisher.eachWithIndex() { it, i ->
+                    if (it["name"] && it["name"].size() > 1 && (it["name"][-1].equals(",") || it["name"][-1].equals(":"))) {
+                        json["about"]["publisher"][i]["name"] = it["name"][0..-2].trim()
+                    }
+                }
+            }
+        }
         doc = doc.withData(mapper.writeValueAsBytes(json))
         //TODO: clean up interpunction 260, 300
         return doc
