dea5b159b58df00740c282c4fe3606fb3c8abafd
Katarina Stempel <ina.stempel@gmail.com>
2013-05-10 12:05:16 +0200

    interpunction removal for extent and edition

--- whelk-extensions/src/main/groovy/converters/JsonLDCleanupFormatConverter.groovy
+++ whelk-extensions/src/main/groovy/converters/JsonLDCleanupFormatConverter.groovy
@@ -15,23 +15,23 @@ class JsonLDCleanupFormatConverter extends BasicFormatConverter {
     Document doConvert(Document doc) {
         def json = mapper.readValue(doc.dataAsString, Map)
 
-        def title = json.about?.instanceOf?.get("title")
-        def titleRemainder = json.about?.instanceOf?.get("titleRemainder")
-        def statementOfResponsibility = json.about?.instanceOf?.get("statementOfResponsibility")
+        def title = json.about.instanceOf?.get("title")
+        def titleRemainder = json.about.instanceOf?.get("titleRemainder")
+        def statementOfResponsibility = json.about.instanceOf?.get("statementOfResponsibility")
         def publisher = json.about?.get("publisher")
-        def placeOfPublication_name, placeOfManufacture_name
-        try {
-            placeOfPublication_name = json.about?.get("placeOfPublication")?.get("name")
-            placeOfManufacture_name = json.about?.get("placeOfManufacture")?.get("name")
-        } catch (Exception e) {
-            // TODO: Handle better
-            log.error("Tried to get array data as map: ${e.message}")
-        }
-        def dateOfPublication = json.about?.get("dateOfPublication")
+        def placeOfPublication_name = json.about?.get("placeOfPublication")?.get("name")
+        def dateOfPublication = json.about.get("dateOfPublication")
+        def placeOfManufacture_name = json.about?.get("placeOfManufacture")?.get("name")
         def extent = json.about?.get("extent")
         def physicalDetails = json.about?.get("physicalDetails")
         def dimensions = json.about?.get("dimensions")
+        def isbn = json.about?.get("isbn")
+        def edition = json.about?.get("edition")
 
+
+        if (isbn && isbn.size() > 1 && (isbn[-1].equals(":") || isbn[-1].equals(";"))) {
+            json["about"]["isbn"] = isbn[0..-2].trim()
+        }
         if (title && title.size() > 1) {
            if (titleRemainder && title[-1].equals(":")) {
                json["about"]["instanceOf"]["title"] = title[0..-2].trim()
@@ -45,7 +45,7 @@ class JsonLDCleanupFormatConverter extends BasicFormatConverter {
                }
            }
         }
-        if (dateOfPublication && dateOfPublication.size() > 1 && (dateOfPublication[-1].equals(";") || dateOfPublication[-1].equals(","))) {
+        if (dateOfPublication.size() > 1 && (dateOfPublication[-1].equals(";") || dateOfPublication[-1].equals(","))) {
             json["about"]["dateOfPublication"] = dateOfPublication[0..-2].trim()
         }
         if (placeOfPublication_name && placeOfPublication_name.size() > 1) {
@@ -55,9 +55,9 @@ class JsonLDCleanupFormatConverter extends BasicFormatConverter {
         }
         if (placeOfManufacture_name) {
             if (placeOfManufacture_name[-1].equals(";")) {
-                    json["about"]["placeOfManufacture"]["name"] = placeOfManufacture_name[0..-2].trim().replaceAll("[()]","")
+                json["about"]["placeOfManufacture"]["name"] = placeOfManufacture_name[0..-2].trim().replaceAll("[()]","")
              } else {
-                    json["about"]["placeOfManufacture"]["name"] = placeOfManufacture_name.replaceAll("[()]","")
+                 json["about"]["placeOfManufacture"]["name"] = placeOfManufacture_name.replaceAll("[()]","")
              }
         }
         if (publisher && publisher instanceof List) {
@@ -67,7 +67,7 @@ class JsonLDCleanupFormatConverter extends BasicFormatConverter {
                 }
             }
         }
-        if (extent && extent.size() > 1 && (extent[-1].equals("+") || extent[-1].equals(":"))) {
+        if (extent.size() > 1 && (extent[-1].equals("+") || extent[-1].equals(":"))) {
             json["about"]["extent"] = extent[0..-2].trim()
         }
         if (physicalDetails && physicalDetails.size() > 1 && (physicalDetails[-1].equals(";") || physicalDetails[-1].equals("+"))) {
@@ -76,7 +76,12 @@ class JsonLDCleanupFormatConverter extends BasicFormatConverter {
         if (dimensions && dimensions.size() > 1 && (dimensions[-1].equals(";") || dimensions[-1].equals("+"))) {
             json["about"]["dimensions"] = dimensions[0..-2].trim()
         }
-        
+        if (edition && edition.size() > 1) {
+            if (edition[-1].equals("/") || edition[-1].equals("=") || edition[-1].equals(",")) {
+                  json["about"]["edition"] = edition[0..-2].trim()
+            }
+        }
+
         doc = doc.withData(mapper.writeValueAsBytes(json))
         return doc
     }
