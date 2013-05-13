8b8b48dc661d2bd44821ab9a8eb3b8cd75cc4384
Niklas Lindström <lindstream@gmail.com>
2013-05-13 18:33:11 +0200

    Add initial marcframe converter

---
+++ whelk-extensions/src/main/groovy/converters/MarcFrameConverter.groovy
@@ -0,0 +1,220 @@
+package se.kb.libris.whelks.plugin
+
+import org.codehaus.jackson.map.ObjectMapper
+
+
+class MarcFrameConverter {
+
+    MarcFrame mf
+    def mapper = new ObjectMapper()
+
+    MarcFrameConverter() {
+        def loader = getClass().classLoader
+        def config = loader.getResourceAsStream("marcframe.json").withStream {
+            mapper.readValue(it, Map)
+        }
+        def relators = loader.getResourceAsStream("relatorcodes.json").withStream {
+            mapper.readValue(it, List).collectEntries { [it.code, it] }
+        }
+        mf = new MarcFrame(config, relators)
+    }
+
+    Map createFrame(Map marcSource) {
+        return mf.createFrame(marcSource)
+    }
+
+    String getMarcType(Map marcSource) {
+        return mf.getMarcType(marcSource)
+    }
+
+    public static void main(String[] args) {
+        def converter = new MarcFrameConverter()
+        def source = converter.mapper.readValue(new File(args[0]), Map)
+        def frame = converter.createFrame(source)
+        converter.mapper.writeValue(System.out, frame)
+    }
+
+}
+
+
+class MarcFrame {
+    Map marcTypeMap = [:]
+    Map marcConversions = [:]
+    Map relators
+    MarcFrame(Map config, Map relators) {
+        marcTypeMap = config.marcTypeFromTypeOfRecord.clone()
+        this.relators = relators
+        marcConversions["bib"] = new MarcBibConversion(config["bib"])
+        //marcConversions["auth"] = new MarcAuthConversion(config["auth"])
+        //marcConversions["hold"] = new MarcHoldingsConversion(config["hold"])
+    }
+
+    Map createFrame(marcSource) {
+        def marcType = getMarcType(marcSource)
+        return marcConversions[marcType].createFrame(marcSource)
+    }
+
+    String getMarcType(marcSource) {
+        def typeOfRecord = marcSource.leader.substring(6, 7)
+        return marcTypeMap[typeOfRecord] ?: marcTypeMap['*']
+    }
+
+}
+
+class BaseMarcConversion {
+
+    def fieldHandlers = [:]
+    //def entityHandlers = [:]
+
+    BaseMarcConversion(data) {
+        data.each { tag, fieldDfn ->
+            def handler = null
+            def m = null
+            if (fieldDfn.inherit) {
+                fieldDfn = data[fieldDfn.inherit] + fieldDfn
+            }
+            if (fieldDfn.ignored || fieldDfn.size() == 0) {
+                return
+            }
+            fieldDfn.each { key, obj ->
+                if ((m = key =~ /^\[(\d+):(\d+)\]$/)) {
+                    if (handler == null) {
+                        handler = new MarcFixedFieldHandler()
+                    }
+                    def start = m[0][1].toInteger()
+                    def end = m[0][2].toInteger()
+                    handler.addColumn(obj.id, start, end)
+                } else if ((m = key =~ /^\$(\w+)$/)) {
+                    if (handler == null) {
+                        handler = new MarcFieldHandler(fieldDfn)
+                    }
+                    def code = m[0][1]
+                    handler.addSubfield(code, obj)
+                }
+            }
+            if (handler == null) {
+                handler = new MarcSimpleFieldHandler(fieldDfn)
+            }
+            fieldHandlers[tag] = handler
+        }
+    }
+}
+
+class MarcBibConversion extends BaseMarcConversion {
+
+    MarcBibConversion(data) {
+        super(data)
+    }
+
+    Map createFrame(marcSource) {
+        def record = ["@type": "Record"]
+        def work = ["@type": "Work"]
+        def unknown = []
+        def instance = [
+            "@type": "Instance",
+            instanceOf: work,
+            describedby: record,
+        ]
+        def entityMap = [:]
+        [record, instance, work].each {
+            entityMap[it["@type"]] = it
+        }
+        fieldHandlers["000"].convert(marcSource, marcSource.leader, entityMap)
+        marcSource.fields.each { row ->
+            def ok = false
+            row.each { tag, value ->
+                def handler = fieldHandlers[tag]
+                if (handler) {
+                    ok = handler.convert(marcSource, value, entityMap)
+                }
+            }
+            if (!ok) {
+                unknown << row
+            }
+        }
+        if (unknown) {
+            instance.unknown = unknown
+        }
+        return instance
+    }
+}
+
+class MarcFixedFieldHandler {
+    def columns = []
+    void addColumn(property, start, end) {
+        columns << new Column(property: property, start: start, end: end)
+    }
+    boolean convert(marcSource, value, entityMap) {
+        // TODO
+        return false
+    }
+    class Column {
+        String property
+        int start
+        int end
+    }
+}
+
+class MarcSimpleFieldHandler {
+    String property
+    String domainEntityName
+    MarcSimpleFieldHandler(fieldDfn) {
+        property = fieldDfn.property
+        domainEntityName = fieldDfn.domainEntity ?: 'Instance'
+    }
+    boolean convert(marcSource, value, entityMap) {
+        assert property, value
+        entityMap[domainEntityName][property] = value
+        return true
+    }
+}
+
+class MarcFieldHandler {
+    String ind1
+    String ind2
+    String domainEntityName
+    String link
+    String rangeEntityName
+    String property
+    //Map partitionRules
+    Map subfields = [:]
+    MarcFieldHandler(fieldDfn) {
+        ind1 = fieldDfn.i1
+        ind2 = fieldDfn.i2
+        domainEntityName = fieldDfn.domainEntityName ?: 'Instance'
+        link = fieldDfn.link
+        rangeEntityName = fieldDfn.rangeEntity
+        property = fieldDfn.property
+        //partitionRules = fieldDfn.partition
+    }
+    void addSubfield(code, obj) {
+        // TODO
+        subfields[code] = obj
+    }
+    boolean convert(marcSource, value, entityMap) {
+        // TODO:
+        //def partitions = [:]
+        def entity = entityMap[domainEntityName]
+        if (!entity) return false
+        if (rangeEntityName) {
+            entity = entity[link] = ["@type": rangeEntityName]
+        }
+        value.subfields.each {
+            it.each { code, subVal ->
+                def subDfn = subfields[code]
+                if (subDfn) {
+                    def ent = (subDfn.domainEntity)? entityMap[subDfn.domainEntity] : entity
+                    if (subDfn.link) {
+                        ent = ent[subDfn.link] = ["@type": subDfn.rangeEntity]
+                    }
+                    assert subDfn.property
+                    ent[subDfn.property] = subVal
+                    if (subDfn.defaults) {
+                        ent += subDfn.defaults
+                    }
+                }
+            }
+        }
+        return true
+    }
+}
---
+++ whelk-extensions/src/test/groovy/MarcFrameConverterSpec.groovy
@@ -0,0 +1,121 @@
+package se.kb.libris.whelks.plugin
+
+import spock.lang.*
+
+
+class MarcFrameConverterSpec extends Specification {
+
+    def converter = new MarcFrameConverter()
+
+    @Unroll
+    def "should detect marc type"() {
+        expect:
+        converter.getMarcType(marc) == type
+        where:
+        marc                                    | type
+        [leader: "01113cz  a2200421n  4500"]    | "auth"
+        [leader: "00887cam a2200277 a 4500"]    | "bib"
+        [leader: "00187nx  a22000971n44500"]    | "hold"
+    }
+
+    def "should convert a simple bib post"() {
+        given:
+        def marc = [
+            leader: "00887cam a2200277 a 4500",
+            fields: [
+                ["001": "7149593"],
+                ["020": ["ind1": " ", "ind2": " ", "subfields": [
+                    ["a": "91-0-056322-6 (inb.)"],
+                    ["c": "310:00"]
+                ]]],
+                ["100": ["ind1": "1", "ind2": " ", "subfields": [
+                    ["a": "Jansson, Tove,"],
+                    ["d": "1914-2001"]
+                ]]],
+                ["245": ["ind1": "0", "ind2": "0", "subfields": [
+                    ["a": "Anteckningar från en ö"],
+                    ["c": "Tove Jansson, Tuulikki Pietilä"]
+                ]]],
+                ["260": ["ind1": " ", "ind2": " ", "subfields": [
+                    ["a": "Stockholm"],
+                    ["b": "Bonnier"],
+                    ["c": "1996"],
+                    ["e": "(Finland)"]
+                ]]],
+                ["700": ["ind1": "1", "ind2": " ", "subfields": [
+                    ["a": "Pietilä, Tuulikki,"],
+                    ["d": "1917-"]
+                ]]],
+            ]
+        ]
+        when:
+        def frame = converter.createFrame(marc)
+        then:
+        frame == [
+            "@type": "Instance",
+            "title": [
+                "titleValue": "Anteckningar från en ö"
+            ],
+            "responsibilityStatement": "Tove Jansson, Tuulikki Pietilä",
+            "publication": [
+                "@type": "ProviderEvent",
+                "providerPlace": "Stockholm",
+                "providerName": "Bonnier",
+                "providerDate": "1996"
+            ],
+            "manufacture": [
+                "@type": "ProviderEvent",
+                "providerPlace": "Finland",
+            ],
+            "identifier": [
+                "identifierValue": "91-0-056322-6",
+                "identifierScheme": "ISBN",
+                "identifierNote": "inb."
+            ],
+            "instanceOf": [
+                "@type": "Work",
+                "authorList": [
+                    [
+                        "@type": "Person",
+                        "authorizedAccessPoint": "Jansson, Tove, 1914-2001",
+                        "birthYear": "1914",
+                        "deathYear": "2001",
+                        "familyName": "Jansson",
+                        "givenName": "Tove"
+                    ],
+                    [
+                        "@type": "Person",
+                        "authorizedAccessPoint": "Pietil\u00e4, Tuulikki, 1917-",
+                        "birthYear": "1917",
+                        "familyName": "Pietil\u00e4",
+                        "givenName": "Tuulikki"
+                    ]
+                ],
+            ],
+            "describedby": [
+                "@type": "Record",
+                "controlNumber": "7149593"
+            ]
+        ]
+    }
+
+    def "should handle complex 260 fields"() {
+    /*
+
+    260	_	_	#a London ; #a New York : #b Routledge Falmer ; #a [London] : #b Open University, #c 2002
+
+    260#3
+    Upprepade utgivarbyten för fortlöpande resurser:
+
+    260	_	_	#3 Sammanfattad utgivningstid: #a Lund : #b Svenska Clartésektionen, #c 1924- #e (Stockholm : #f Fram)
+    260	_	_	#a Lund : #b Svenska Clartésektionen, #c 1924-1925
+    260	2	_	#a Lund : #b Svenska Clartéavdelningen, #c 1926-1927
+    260	2	_	#a Stockholm : #b Svenska Clartéavdelningen, #c 1928-1931
+    260	2	_	#a Stockholm : #b Svenska Clartéförbundet, #c 1932-1953
+    260	2	_	#a Hägersten : #b Clarté, #c 1991-1995
+    260	3	_	#a Stockholm : #b Clarté, #c 1953-1991, 1995-
+
+    */
+    }
+
+}
