eec682fc64ecebbfbc1a990b34d25fdda510ef6b
Katarina Stempel <ina.stempel@gmail.com>
2013-06-07 15:35:56 +0200

    SubjectList term added to entity extraction of concept

--- whelk-extensions/src/main/groovy/converters/JsonLDEntityExtractorIndexFormatConverter.groovy
+++ whelk-extensions/src/main/groovy/converters/JsonLDEntityExtractorIndexFormatConverter.groovy
@@ -17,8 +17,6 @@ class JsonLDEntityExtractorIndexFormatConverter extends BasicIndexFormatConverte
         def json = mapper.readValue(doc.dataAsString, Map)
         def indexTypeLists = ["person": json.about?.instanceOf?.authorList, "concept": json.about?.instanceOf?.subject]
         def entityLinks = ["person": "authorOf", "concept": "subject"]
-        log.debug("Author list: " + json.about?.instanceOf?.authorList)
-        log.debug("Subject list: " + json.about?.instanceOf?.subject)
         indexTypeLists.each { k, entities ->
             if (entities) {
                 if (entities instanceof List) {
@@ -40,12 +38,24 @@ class JsonLDEntityExtractorIndexFormatConverter extends BasicIndexFormatConverte
             json.remove("unknown")
             doclist << new BasicDocument().withData(mapper.writeValueAsBytes(json)).withContentType("application/ld+json").withIdentifier(doc.identifier).tag("entityType", json["@type"])
         }
+        if (json.about?.instanceOf?.subjectList) {
+            json.about.instanceOf.subjectList.each {
+                if (it.get("term", null)) {
+                    doclist << createEntityDoc("concept", it, doc.identifier, entityLinks["concept"])
+                }
+            }
+        }
         log.debug("Extraction results: $doclist")
         return doclist
     }
 
     Document createEntityDoc(def type, def entityJson, def docId, def linkType) {
-        String pident = slugify(entityJson["authorizedAccessPoint"], docId, type)
+        String pident
+        if (entityJson.get("authorizedAccessPoint", null)) {
+            pident = slugify(entityJson.get("authorizedAccessPoint"), docId, type)
+        } else if (entityJson.get("term", null)) {
+            pident = slugify(entityJson.get("term"), docId, type)
+        }
         entityJson["@id"] = pident
         entityJson["recordPriority"] = 0
         return new BasicDocument().withData(mapper.writeValueAsBytes(entityJson)).withContentType("application/ld+json").withIdentifier(pident).tag("entityType", type).withLink(docId, linkType)
