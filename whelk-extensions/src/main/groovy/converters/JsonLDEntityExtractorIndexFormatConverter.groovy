d45060863d42e3dcbbb35028b3f1e9407e6983ed
Katarina Stempel <ina.stempel@gmail.com>
2013-06-12 19:04:57 +0200

    Updated EntityExtractor according to marcframe

--- whelk-extensions/src/main/groovy/converters/JsonLDEntityExtractorIndexFormatConverter.groovy
+++ whelk-extensions/src/main/groovy/converters/JsonLDEntityExtractorIndexFormatConverter.groovy
@@ -15,19 +15,34 @@ class JsonLDEntityExtractorIndexFormatConverter extends BasicIndexFormatConverte
     List<IndexDocument> doConvert(Resource doc) {
         List<IndexDocument> doclist = [new IndexDocument(doc)]
         def json = mapper.readValue(doc.dataAsString, Map)
-        def indexTypeLists = ["person": json.about?.instanceOf?.authorList, "concept": json.about?.instanceOf?.subject]
-        def entityLinks = ["person": "authorOf", "concept": "subject"]
+        def indexTypeLists = [
+            "creator": json.about?.instanceOf?.creator,
+            "contributor" : json.about?.instanceOf?.contributor,
+            "subject" : json.about?.instanceOf?.subject
+        ]
+
         indexTypeLists.each { k, entities ->
             if (entities) {
                 if (entities instanceof List) {
                     for (entity in entities) {
-                        if (entity.get("authorizedAccessPoint", null)) {
-                            doclist << createEntityDoc(k, entity, doc.identifier, entityLinks[(k)])
+                        if (k == "creator" && entity.get("controlledLabel", null)) {
+                            doclist << createEntityDoc("person", entity, entity["controlledLabel"], doc.identifier, "creatorOf")
+                        } else if (k == "contributor" && entity.get("controlledLabel", null)) {
+                            doclist << createEntityDoc("person", entity, entity["controlledLabel"], doc.identifier, "contributor")
+                        } else if (k == "concept" && entity.get("prefLabel", null)) {
+                            doclist << createEntityDoc("concept", entity, entity["prefLabel"], doc.identifier, "subject")
                         }
                     }
                 } else if (entities instanceof Map) {
-                    if (entities.get("authorizedAccessPoint", null)) {
-                        doclist << createEntityDoc(k, entities, doc.identifier, entityLinks[(k)])
+                    /*if (entities.get("controlledLabel", null)) {
+                    doclist << createEntityDoc(k, entities, doc.identifier, entityLinks[(k)])
+                    }*/
+                    if (k == "creator" && entity.get("controlledLabel", null)) {
+                        doclist << createEntityDoc("person", entity, entity["controlledLabel"], doc.identifier, "creatorOf")
+                    } else if (k == "contributor" && entity.get("controlledLabel", null)) {
+                        doclist << createEntityDoc("person", entity, entity["controlledLabel"], doc.identifier, "contributor")
+                    } else if (k == "concept" && entity.get("prefLabel", null)) {
+                        doclist << createEntityDoc("concept", entity, entity["prefLabel"], doc.identifier, "subject")
                     }
                 }
             }
@@ -38,24 +53,12 @@ class JsonLDEntityExtractorIndexFormatConverter extends BasicIndexFormatConverte
             json.remove("unknown")
             doclist << new IndexDocument(doc).withData(mapper.writeValueAsBytes(json)).withContentType("application/ld+json").withType(json["@type"])
         }
-        if (json.about?.instanceOf?.subjectList) {
-            json.about.instanceOf.subjectList.each {
-                if (it.get("term", null)) {
-                    doclist << createEntityDoc("concept", it, doc.identifier, entityLinks["concept"])
-                }
-            }
-        }
         log.debug("Extraction results: $doclist")
         return doclist
     }
 
-    IndexDocument createEntityDoc(def type, def entityJson, def docId, def linkType) {
-        String pident
-        if (entityJson.get("authorizedAccessPoint", null)) {
-            pident = slugify(entityJson.get("authorizedAccessPoint"), docId, type)
-        } else if (entityJson.get("term", null)) {
-            pident = slugify(entityJson.get("term"), docId, type)
-        }
+    IndexDocument createEntityDoc(def type, def entityJson, def authPoint, def docId, def linkType) {
+        String pident = slugify(authPoint, docId, type)
         entityJson["@id"] = pident
         entityJson["recordPriority"] = 0
         return new IndexDocument().withData(mapper.writeValueAsBytes(entityJson)).withContentType("application/ld+json").withIdentifier(pident).withType(type)
