97cb9250e873f23dd3dc7f08cbe39d7a3e3ff23b
Katarina Stempel <ina.stempel@gmail.com>
2013-09-25 15:37:49 +0200

    Added linkenhancer

---
+++ whelk-extensions/src/main/groovy/converters/JsonLDLinkEnhancerFormatConverter.groovy
@@ -0,0 +1,81 @@
+package se.kb.libris.whelks.plugin
+
+import se.kb.libris.whelks.*
+import se.kb.libris.whelks.basic.*
+import se.kb.libris.whelks.component.ElasticQuery
+
+import org.codehaus.jackson.map.ObjectMapper
+import groovy.util.logging.Slf4j as Log
+
+@Log
+class JsonLDLinkEnhancerFormatConverter extends BasicFormatConverter implements WhelkAware {
+
+    String requiredContentType = "application/ld+json"
+    ObjectMapper mapper = new ObjectMapper()
+    def whelk
+
+    public void setWhelk(Whelk whelk) {
+        this.whelk = whelk
+    }
+
+    Document doConvert(Document doc) {
+        def changedData = false
+        def json, authDataJson
+
+        for (link in doc.links) {
+            if (link.type == "auth") {
+                def authDoc = whelk.get(link.identifier)
+                if (authDoc) {
+                    log.debug("*********************Document ${doc.identifier.toString()} has a link to ${link.identifier.toString()}, type ${link.type}. Found document")
+                    authDataJson = mapper.readValue(authDoc.dataAsString, Map)
+                    switch (authDataJson["about"]["@type"] as String) {
+                        case "Person":
+                            json = mapper.readValue(doc.dataAsString, Map)
+                            if (json["about"]["instanceOf"].containsKey("creator")) {
+                                def creatorProp = json["about"]["instanceOf"]["creator"]
+                                if (creatorProp instanceof List) {
+                                    creatorProp.eachWithIndex { c, ind ->
+                                        if (c["@type"] == "Person" && c["controlledLabel"] == authDataJson["about"]["controlledLabel"]) {
+                                            json["about"]["instanceOf"]["creator"][ind]["@id"] = authDataJson["about"]["@id"]
+                                            changedData = true
+                                        }
+                                    }
+                                } else if (creatorProp instanceof Map) {
+                                    if (creatorProp["@type"] == "Person" && creatorProp["controlledLabel"] == authDataJson["about"]["controlledLabel"]) {
+                                        json["about"]["instanceOf"]["creator"]["@id"] = authDataJson["about"]["@id"]
+                                        changedData = true
+                                    }
+                                }
+
+                            }
+                            break
+                        case "Concept":
+                            json = mapper.readValue(doc.dataAsString, Map)
+                            if (json["about"]["instanceOf"].containsKey("subject")) {
+                                for (subj in json["about"]["instanceOf"]["creator"]) {
+                                    if (subj["@type"] == "Concept") {
+                                        subj.eachWithIndex { it, index ->
+                                            it["broader"].eachWithIndex { br, i ->
+                                                if (br["prefLabel"] && br["prefLabel"] == authDataJson["about"]["prefLabel"]) {
+                                                    json["about"]["instanceOf"]["subject"][index]["broader"][i]["@id"] = authDataJson["about"]["@id"]
+                                                    changedData = true
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                    }
+                }
+            }
+
+        }
+        if (changedData) {
+             return doc.withData(mapper.writeValueAsString(json))
+        }
+        return doc
+    }
+}
+
+
+
