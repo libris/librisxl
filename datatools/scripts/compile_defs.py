5f64f45fcbb99bfad26ad961bd1e6542b689b3a9
Niklas Lindström <lindstream@gmail.com>
2013-10-23 00:50:35 +0200

    Small update to compile_defs.py

--- datatools/scripts/compile_defs.py
+++ datatools/scripts/compile_defs.py
@@ -2,10 +2,19 @@ import json
 from rdflib import *
 from rdflib.namespace import SKOS
 
+
+BASE = "http://libris.kb.se/"
+
+datasets = {}
+def dataset(f):
+    datasets[f.__name__] = f
+
+
 def deref(iri):
     return Graph().parse(iri).resource(iri)
 
-def get_languages():
+@dataset
+def languages():
     base = "/def/%s/" % dataset
     with open('whelk-core/src/main/resources/langcodes.json') as f:
         lang_code_map = json.load(f)
@@ -13,10 +22,11 @@ def get_languages():
     items = {}
     result = {
         '@context': {
-            '@base': "http://libris.kb.se/",
+            '@base': BASE,
             '@vocab': SKOS,
             'dc': 'http://purl.org/dc/terms/',
-            'iso-639-2': {'@id': 'notation', '@type': 'dc:ISO639-2'},
+            'code': {'@id': 'notation', '@type': 'dc:ISO639-2'},
+            'tag': {'@id': 'notation', '@type': 'dc:ISO639-1'},
             'byCode': {'@id': '@graph', '@container': '@index'},
             'matches': {'@id': 'exactMatch', '@type': '@id'},
             'labelByLang': {'@id': 'prefLabel', '@container': '@language'}
@@ -26,20 +36,17 @@ def get_languages():
     for lang_concept in map(source.resource, source.subjects(RDF.type, SKOS.Concept)):
         # TODO: separate codes for ISO 639-2 and ISO 639-1 (described in deref:ed data)
         #deref(lang_concept.identifier)
-        code = unicode(lang_concept.value(SKOS.notation))
+        code = lang_concept.value(SKOS.notation)
         langobj = items[code] = {'@id': base + code, '@type': 'Concept', 'iso-639-2': code}
         langobj['matches'] = lang_concept.identifier
         prefLabels = langobj['labelByLang'] = {}
         for label in lang_concept.objects(SKOS.prefLabel):
-            prefLabels[label.language] = unicode(label)
+            prefLabels[label.language] = label
         label_sv = lang_code_map.get(code)
         if label_sv:
             prefLabels['sv'] = label_sv
-    return json.dumps(result, indent=2, sort_keys=True, ensure_ascii=False).encode('utf-8')
+    return result
 
-datasets = {
-    'languages': get_languages
-}
 
 if __name__ == '__main__':
     import sys
@@ -47,4 +54,5 @@ if __name__ == '__main__':
     if not args:
         sys.exit()
     dataset = args.pop(0)
-    print datasets[dataset]()
+    result = datasets[dataset]()
+    print json.dumps(result, indent=2, sort_keys=True, ensure_ascii=False).encode('utf-8')
