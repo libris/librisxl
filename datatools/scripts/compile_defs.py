08f78cafe170dad3719295114380c9a5583207dd
Niklas Lindström <lindstream@gmail.com>
2013-10-25 19:15:28 +0200

    Add datasets to compile_defs.py and improve output control

--- datatools/scripts/compile_defs.py
+++ datatools/scripts/compile_defs.py
@@ -1,17 +1,35 @@
+import os
 import json
 from rdflib import *
-from rdflib.namespace import SKOS
+from rdflib.namespace import SKOS, DCTERMS
 
-
-BASE = "http://libris.kb.se/"
 
 datasets = {}
 def dataset(f):
     datasets[f.__name__] = f
+    return f
 
 
-def deref(iri):
-    return Graph().parse(iri).resource(iri)
+BASE = "http://libris.kb.se/"
+
+
+@dataset
+def terms():
+    source = Graph().parse('datatools/def/terms.ttl', format='turtle')
+    return source.serialize(format="json-ld", auto_compact=True)
+
+
+@dataset
+def schemes():
+    source = Graph().parse('datatools/def/schemes.ttl', format='turtle')
+    return source.serialize(format="json-ld",
+            context="datatools/def/schemes-context.jsonld")
+
+
+@dataset
+def relators():
+    pass
+
 
 @dataset
 def languages():
@@ -24,7 +42,7 @@ def languages():
         '@context': {
             '@base': BASE,
             '@vocab': SKOS,
-            'dc': 'http://purl.org/dc/terms/',
+            'dc': DCTERMS,
             'langCode': {'@id': 'notation', '@type': 'dc:ISO639-2'},
             'langTag': {'@id': 'notation', '@type': 'dc:ISO639-1'},
             'byCode': {'@id': '@graph', '@container': '@index'},
@@ -48,14 +66,64 @@ def languages():
     return result
 
 
+@dataset
+def countries():
+    pass
+
+
+@dataset
+def nationalities():
+    pass
+
+
+def deref(iri):
+    return Graph().parse(iri).resource(iri)
+
+
+def serialize(data):
+    if isinstance(data, (list, dict)):
+        data = json.dumps(data, indent=2, sort_keys=True, ensure_ascii=False)
+    if isinstance(data, unicode):
+        data = data.encode('utf-8')
+    return data
+
+
 if __name__ == '__main__':
-    import sys
-    args = sys.argv[1:]
+
+    from optparse import OptionParser
+    op = OptionParser("Usage: %prog [-h] [-o OUTPUT_DIR] [DATASET..]",
+            description="Available datasets: " + ", ".join(datasets))
+    op.add_option('-o', '--output', type=str, help="Output directory")
+    opts, args = op.parse_args()
+
     if not args:
-        print "Available datasets:"
-        for k in datasets:
-            print "  %s" % k
-        sys.exit()
-    dataset = args.pop(0)
-    result = datasets[dataset]()
-    print json.dumps(result, indent=2, sort_keys=True, ensure_ascii=False).encode('utf-8')
+        if opts.output:
+            args = list(datasets)
+        else:
+            op.print_usage()
+            op.exit()
+
+    import sys
+
+    for dataset in args:
+        if len(args) > 1:
+            print "Dataset:", dataset
+        result = datasets[dataset]()
+        data = serialize(result)
+        if data and opts.output:
+            outdir = os.path.join(opts.output, dataset)
+            if not os.path.isdir(outdir):
+                os.makedirs(outdir)
+            outfile = os.path.join(outdir, "data.jsonld")
+            fp = open(outfile, 'w')
+        else:
+            fp = sys.stdout
+        try:
+            if data:
+                fp.write(data)
+            else:
+                print "N/A"
+            print
+        finally:
+            if fp is not sys.stdout:
+                fp.close()
