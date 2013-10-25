c431912ca7258b9da970389461c207cc4e3a57ea
Niklas Lindström <lindstream@gmail.com>
2013-10-26 00:34:30 +0200

    Add rdf cache and iso-639-1 lang tag lookup to compile_defs.py

--- datatools/scripts/compile_defs.py
+++ datatools/scripts/compile_defs.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 from rdflib import *
 from rdflib.namespace import SKOS, DCTERMS
@@ -28,7 +29,8 @@ def schemes():
 
 @dataset
 def relators():
-    pass
+    source = cached_rdf('http://id.loc.gov/vocabulary/relators')
+    return source.serialize(format="json-ld", auto_compact=True)
 
 
 @dataset
@@ -36,7 +38,7 @@ def languages():
     base = "/def/%s/" % dataset
     with open('whelk-core/src/main/resources/langcodes.json') as f:
         lang_code_map = json.load(f)
-    source = Graph().parse('http://id.loc.gov/vocabulary/iso639-2')
+    source = cached_rdf('http://id.loc.gov/vocabulary/iso639-2')
     items = {}
     result = {
         '@context': {
@@ -51,14 +53,18 @@ def languages():
         },
         'byCode': items
     }
+    ISO639_1Lang = u"http://id.loc.gov/vocabulary/iso639-1/iso639-1_Language"
     for lang_concept in map(source.resource, source.subjects(RDF.type, SKOS.Concept)):
-        # TODO: separate codes for ISO 639-2 and ISO 639-1 (described in deref:ed data)
-        #deref(lang_concept.identifier)
         code = lang_concept.value(SKOS.notation)
         langobj = items[code] = {'@id': base + code, '@type': 'Concept', 'langCode': code}
         langobj['matches'] = lang_concept.identifier
+        langdef = deref(lang_concept.identifier)
+        for variant in langdef[SKOS.exactMatch]:
+            if any(o.identifier == ISO639_1Lang for o in variant[RDF.type]):
+                iso639_1 = variant.value(SKOS.notation)
+                langobj['tag'] = iso639_1
         prefLabels = langobj['labelByLang'] = {}
-        for label in lang_concept.objects(SKOS.prefLabel):
+        for label in lang_concept[SKOS.prefLabel]:
             prefLabels[label.language] = label
         label_sv = lang_code_map.get(code)
         if label_sv:
@@ -68,7 +74,8 @@ def languages():
 
 @dataset
 def countries():
-    pass
+    source = cached_rdf('http://id.loc.gov/vocabulary/countries')
+    return source.serialize(format="json-ld", auto_compact=True)
 
 
 @dataset
@@ -77,7 +84,29 @@ def nationalities():
 
 
 def deref(iri):
-    return Graph().parse(iri).resource(iri)
+    return cached_rdf(iri).resource(iri)
+
+
+CACHEDIR = None
+
+def cached_rdf(fpath):
+    source = Graph()
+    http = 'http://'
+    if not CACHEDIR:
+        print >> sys.stderr, "No cache directory configured"
+    elif fpath.startswith(http):
+        remotepath = fpath
+        fpath = os.path.join(CACHEDIR, remotepath[len(http):]) + '.ttl'
+        if not os.path.isfile(fpath):
+            fdir = os.path.dirname(fpath)
+            if not os.path.isdir(fdir):
+                os.makedirs(fdir)
+            source.parse(remotepath)
+            source.serialize(fpath, format='turtle')
+            return source
+        else:
+            return source.parse(fpath, format='turtle')
+    return source.parse(fpath)
 
 
 def serialize(data):
@@ -94,7 +123,11 @@ if __name__ == '__main__':
     op = OptionParser("Usage: %prog [-h] [-o OUTPUT_DIR] [DATASET..]",
             description="Available datasets: " + ", ".join(datasets))
     op.add_option('-o', '--output', type=str, help="Output directory")
+    op.add_option('-c', '--cache', type=str, help="Cache directory")
     opts, args = op.parse_args()
+
+    if opts.cache:
+        CACHEDIR = opts.cache
 
     if not args:
         if opts.output:
@@ -102,8 +135,6 @@ if __name__ == '__main__':
         else:
             op.print_usage()
             op.exit()
-
-    import sys
 
     for dataset in args:
         if len(args) > 1:
