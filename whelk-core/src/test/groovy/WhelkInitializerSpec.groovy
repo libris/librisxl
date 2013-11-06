a2294157a947cc68bf9561531fe124ae09660c47
Niklas Lindström <lindstream@gmail.com>
2013-11-06 11:24:10 +0100

    Change whelk config for plugins to be a Map

--- whelk-core/src/test/groovy/WhelkInitializerSpec.groovy
+++ whelk-core/src/test/groovy/WhelkInitializerSpec.groovy
@@ -1,22 +1,46 @@
 package se.kb.libris.whelks
 
-import se.kb.libris.whelks.basic.*
-import se.kb.libris.whelks.plugin.*
-
-import spock.lang.Specification
+import spock.lang.*
 
 class WhelkInitializerSpec extends Specification {
 
-    def wi = new WhelkInitializer(
-            new ByteArrayInputStream("""{"_whelks": [ {
-                    "default": {
-                        "_class" : "se.kb.libris.whelks.StandardWhelk"
-                    } } ] }""".bytes),
-            new ByteArrayInputStream("[]".bytes))
+    @Shared wi = new WhelkInitializer(
+            new ByteArrayInputStream("""{
+                "_whelks": [
+                    {
+                        "default": {
+                            "_class" : "se.kb.libris.whelks.StandardWhelk",
+                            "_plugins": ["basic1", "basic2"]
+                        }
+                    }
+                ]
+            }""".bytes),
+            new ByteArrayInputStream("""{
+                "basic1": {
+                    "_class" : "se.kb.libris.whelks.plugin.BasicPlugin"
+                },
+                "basic2": {
+                    "_class" : "se.kb.libris.whelks.plugin.BasicPlugin"
+                }
+            }""".bytes))
+
+    @Shared whelks
+
+    def setupSpec() {
+        whelks = wi.getWhelks() // force reading of whelks...
+    }
+
+    def "should get plugins"() {
+        when:
+        def plugin = wi.getPlugin(name, "default")
+        then:
+        name == plugin.id
+        where:
+        name << ["basic1", "basic2"]
+    }
 
     def "should expand parameter variables"() {
         given:
-        def whelks = wi.getWhelks() // force reading of whelks...
         def params = wi.translateParams("_whelk:default, other", "default")
         expect:
         params == [whelks[0], "other"]
