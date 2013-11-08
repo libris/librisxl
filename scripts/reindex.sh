4ed7c646fe876f154083318148a28e6ee80e5402
mskold <markus.skold@kb.se>
2013-11-08 05:37:02 +0100

    Creating proper documents from getAll()

--- scripts/reindex.sh
+++ scripts/reindex.sh
@@ -11,11 +11,11 @@ if [[ "$RESOURCE" == "" ]] || [[ $ORIGIN == "" ]]; then
 fi
 
 WEBAPPS=/var/lib/tomcat6/webapps/whelk
-SETENV=/etc/tomcat6/tomcat6.conf
+SETENV=%(envscript)s
 
 CLASSES=$WEBAPPS/WEB-INF/classes
 LIBS=$WEBAPPS/WEB-INF/lib
 
 source $SETENV
 
-java $JAVA_OPTS $EXTRA_OPTS -cp $CLASSES:$LIBS/* se.kb.libris.whelks.WhelkOperator reindex libris $RESOURCE $ORIGIN
+java $JAVA_OPTS $EXTRA_OPTS -cp $CLASSES:$LIBS/* se.kb.libris.whelks.WhelkOperator reindex libris $RESOURCE
