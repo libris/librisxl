857ed2cef21b490faf538e4ba041c51075f50e56
Katarina Stempel <ina.stempel@gmail.com>
2013-11-21 16:13:03 +0100

    Refactored JsonLDLinkEnhancer

--- whelk-extensions/src/main/groovy/converters/JsonLDLinkEnhancerFormatConverter.groovy
+++ whelk-extensions/src/main/groovy/converters/JsonLDLinkEnhancerFormatConverter.groovy
@@ -18,118 +18,126 @@ class JsonLDLinkEnhancerFormatConverter extends BasicFormatConverter implements 
         this.whelk = whelk
     }
 
+    def searchLabel = ["Person" : "controlledLabel", "Concept" : "prefLabel"]
+
     Document doConvert(Document doc) {
         def changedData = false
-        def authDataJson
+        def entityType, entity, labelKey, searchStr, esQuery, result
 
         def json = mapper.readValue(doc.dataAsString, Map)
+        def work = json.about?.instanceOf ? json.about.instanceOf : json.about
 
-        for (link in doc.links) {
-            if (link.type == "auth") {
-                def authDoc = whelk.get(new URI(link.identifier))
-                if (authDoc == null) {
-                    continue
+        //For each property in incoming document
+        work.each { propKey, propValue ->
+
+            //Find entity
+            if (propValue instanceof List) {
+                propValue.each {
+                    if (entity instanceof Map) {
+                        entity = it
+                        entityType = it.get("@type")
+                    }
                 }
-                authDataJson = mapper.readValue(authDoc.dataAsString, Map)
-                def work = json["about"]["instanceOf"]
-                def authItem = authDataJson["about"]
-                switch (authItem["@type"] as String) {
-                    case "Person":
-                        if (work.containsKey("creator")) {
-                            def creator = work["creator"]
-                            if (creator instanceof List) {
-                                creator.each {
-                                    if (updatePersonId(it, authItem))
-                                        changedData = true
-                                }
-                            } else if (creator instanceof Map) {
-                                if (updatePersonId(creator, authItem))
-                                    changedData = true
-                            }
-                        }
-                        if (work.containsKey("contributorList")) {
-                            def contributors = work["contributorList"]
-                            if (contributors instanceof List) {
-                                contributors.each {
-                                    if (updatePersonId(it, authItem))
-                                        changedData = true
-                                }
-                            }
-                        }
-                        break
-                    case "Concept":
-                        if (work.containsKey("subject")) {
-                            def concepts = work["subject"]
-                            concepts.each {
-                                if (it["@type"] == "Concept") {
-                                    if (updateConceptId(it, authItem))
-                                        changedData = true
-                                    it["broader"].each { broader ->
-                                        if (updateConceptId(broader, authItem))
-                                            changedData = true
-                                    }
-                                }
-                            }
-                        }
-                        if (work.containsKey("class")) {
-                            def concepts = work["class"]
-                            concepts.each {
-                                if (it["@type"] == "Concept") {
-                                    if (updateConceptId(it, authItem))
-                                        changedData = true
-                                }
-                            }
-                        }
-                        break
-                    case "Work":
-                        if (work.containsKey("subject")) {
-                            def concepts = work["subject"]
-                            concepts.each {
-                                if (it["@type"] == "Work") {
-                                    if (updateWorkId(it, authItem))
-                                        changedData = true
-                                }
-                            }
-                        }
+            }  else if (propValue instanceof Map) {
+                entity = propValue
+                entityType = propValue.get("@type")
+            }
+
+            log.info("$entityType")
+            log.info("$entity")
+
+            //Try to update entity.@id with matching linked documents
+            if (entityType && !entity.containsKey("@value"))  {
+
+                changedData = updatePropertyWithLinks(entity, doc.links)
+
+                //Or using search
+                if (!changedData) {
+
+                    labelKey = searchLabel.get(entityType)
+                    searchStr = "$labelKey:${entity.get(labelKey)}"
+                    esQuery = new ElasticQuery(searchStr)
+                    esQuery.indexType = entityType
+
+                    log.info("Performing search on: $searchStr ...") //or search auth with @type?
+                    result = whelk.search(esQuery)
+
+                    log.info("Number of hits: ${result.numberOfHits}")
+
+                    if (result.numberOfHits == 1) {
+                        entity["@id"] = "/resource" + result.hits[0]["identifier"]
+                    }  //TODO: else if more than 1 hit?
+
                 }
+
             }
 
         }
+
         if (changedData) {
-             return doc.withData(mapper.writeValueAsString(json))
+            return doc.withData(mapper.writeValueAsString(json))
         }
+
         return doc
     }
 
-    boolean updatePersonId(item, authItem) {
-        if (item["@type"] == "Person" && item["controlledLabel"] == authItem["controlledLabel"]) {
-            item["@id"] = authItem["@id"]
+    boolean updatePropertyWithLinks(property, links) {
+        boolean updated = false
+        def relatedDoc, relatedDocJson, relatedItem, updateAction
+        if (links.size() > 0) {
+            for (link in links) {
+                log.debug("Link type: ${link.type}")
+                log.debug("Trying to get document with identifier ${link.identifier}")
+                relatedDoc = whelk.get(new URI(link.identifier))
+                if (relatedDoc == null) {
+                    continue
+                }
+                relatedDocJson = mapper.readValue(relatedDoc.dataAsString, Map)
+                relatedItem = relatedDocJson.about?.instanceOf ?: relatedDocJson.about
+                if (relatedItem.get("@type") == property.get("@type")) {
+                    try {
+                        updateAction = "update" + property["@type"] + "Id"
+                        updated = "$updateAction"(property, relatedItem)
+                        log.debug("$updated")
+                    } catch (Exception e) {
+                        log.debug("Could not update property of type ${property["@type"]}")
+                        updated = false
+                    }
+                }
+            }
+        }
+        return updated
+    }
+
+    boolean updatePersonId(item, relatedItem) {
+        if (item["controlledLabel"] == relatedItem["controlledLabel"]) {
+            item["@id"] = relatedItem["@id"]
             return true
         }
         return false
     }
 
-    boolean updateConceptId(item, authItem) {
-        def authItemSameAs = authItem.get("sameAs")?.get("@id")
+    boolean updateConceptId(item, relatedItem) {
+        def authItemSameAs = relatedItem.get("sameAs")?.get("@id")
         if (item["@id"] && item["@id"] == authItemSameAs) {
             item["sameAs"] = ["@id": item["@id"]]
-            item["@id"] = authItem["@id"]
+            item["@id"] = relatedItem["@id"]
             return true
         }
         def same = item.get("sameAs")?.get("@id") == authItemSameAs
-        if (same || (item["prefLabel"] && item["prefLabel"] == authItem["prefLabel"])) {
-            item["@id"] = authItem["@id"]
+        if (same || (item["prefLabel"] && item["prefLabel"] == relatedItem["prefLabel"])) {
+            item["@id"] = relatedItem["@id"]
             return true
         }
         return false
     }
 
-    boolean updateWorkId(item, authItem) {
-        if (item["@type"] == "Work" && item["uniformTitle"] == authItem["uniformTitle"]) {
+    boolean updateWorkId(item, relatedItem) {
+        if (item["uniformTitle"] == relatedItem["uniformTitle"]) {
             def creator = item.creator
             if (creator instanceof List)
                 creator = creator[0]
-            def authCreator = authItem.creator
+            def authCreator = relatedItem.creator
             if (authCreator instanceof List)
                 authCreator = authCreator[0]
             if (
@@ -138,11 +146,10 @@ class JsonLDLinkEnhancerFormatConverter extends BasicFormatConverter implements 
                      (creator["@type"] == "Person" && authCreator["@type"] == "Person" &&
                       creator["controlledLabel"] == authCreator["controlledLabel"]))
                ) {
-                item["@id"] = authItem["@id"]
+                item["@id"] = relatedItem["@id"]
                 return true
             }
         }
         return false
     }
-
 }
