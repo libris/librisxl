6a38cd2c3ce28273ae8958c5cd2a3033838ce083
Katarina Stempel <ina.stempel@gmail.com>
2013-12-13 14:06:46 +0100

    Finding links for work.uniformtitle

--- whelk-extensions/src/main/groovy/converters/IndexLinkFinder.groovy
+++ whelk-extensions/src/main/groovy/converters/IndexLinkFinder.groovy
@@ -15,7 +15,7 @@ class IndexLinkFinder extends BasicPlugin implements LinkFinder, WhelkAware {
     ObjectMapper mapper
     def whelk
 
-    def searchLabel = ["Person" : "controlledLabel", "Concept" : "prefLabel", "Organization" : ["name", "label"]]
+    def searchLabel = ["Person": "controlledLabel", "Concept": "prefLabel", "Organization": ["name", "label"], "Work": ["uniformTitle", "attributedTo.controlledLabel"]]
 
     public IndexLinkFinder(def whelk) {
         this.whelk = whelk
@@ -40,53 +40,71 @@ class IndexLinkFinder extends BasicPlugin implements LinkFinder, WhelkAware {
         return links as Set
     }
 
-    def performSearch(labelKey, prop, propValue) {
+    def performSearch(labelKeyOrKeys, prop, propType) {
         def searchStr, esQuery, searchTerm
+        def operand = "OR"
 
-        log.trace("Property entity type: $propValue")
+        log.trace("Property entity type: $propType")
 
-        if (labelKey instanceof List) {
-            labelKey.each { x ->
-                searchTerm = prop.get(x)
-            }
-        } else searchTerm = prop.get(labelKey)
+        if (labelKeyOrKeys instanceof List) {
 
-        if (searchTerm) {
-            if (labelKey instanceof List) {
-                labelKey.eachWithIndex() { it, i ->
+            if (propType == "Work") operand = "AND"
+
+            labelKeyOrKeys.eachWithIndex() { label, i ->
+                searchTerm = prop.get(label)
+                if (searchTerm) {
                     if (i == 0) {
-                        searchStr = "$it:" + URLEncoder.encode(searchTerm, "UTF-8")
+                        searchStr = "$label:" + URLEncoder.encode(searchTerm, "UTF-8")
                     } else {
-                        searchStr = searchStr + " OR $it:" + URLEncoder.encode(searchTerm, "UTF-8")
+                        searchStr = searchStr + " $operand $label:" + URLEncoder.encode(searchTerm, "UTF-8")
                     }
                 }
-                log.trace("labelKey is List, resulting in searchStr: ${searchStr}")
-            } else {
-                searchStr = "$labelKey:" + URLEncoder.encode(searchTerm, "UTF-8")
             }
+            log.trace("labelKey is List, resulting in searchStr: ${searchStr}")
+
+        } else {
+
+            searchTerm = prop.get(labelKeyOrKeys)
+            if (searchTerm) {
+                searchStr = "$labelKeyOrKeys:" + URLEncoder.encode(searchTerm, "UTF-8")
+            }
+        }
+
+        if (searchStr) {
             esQuery = new ElasticQuery(searchStr)
-            esQuery.indexType = propValue.toLowerCase() //or search auth with @type?
+            esQuery.indexType = propType.toLowerCase() //or search auth with @type?
 
             log.trace("Performing search on: $searchStr ... using indextype ${esQuery.indexType}")
             return whelk.search(esQuery)
         }
     }
 
-    def collectIds(prop, type, selfId) {
+
+    def collectIds(prop, relationType, selfId) {
         def ids = []
-        def labelKey, result, resultJson
+        def labelKey, result, resultJson, entityType
 
         if (prop instanceof Map) {
 
             prop.each { propKey, propValue ->
 
-                if ((propKey == "@type" || (propKey == "@id" && (type == "broader" || type == "narrower"))) && !prop.containsKey("@value")) {
-                    if (type == "broader" || type == "narrower") {
-                        type = propValue = "Concept"
-                    }
-                    labelKey = searchLabel.get(propValue)
+                if ((propKey == "@type" || (propKey == "@id" && (relationType == "broader" || relationType == "narrower"))) && !prop.containsKey("@value")) {
 
-                    result = performSearch(labelKey, prop, propValue)
+                    switch (relationType) {
+                        case "broader":
+                        case "narrower":
+                            entityType = "Concept"
+                            break
+                        case "relation":
+                            entityType = "Work"
+                            break
+                        default:
+                            entityType = propValue
+                    }
+
+                    labelKey = searchLabel.get(entityType)
+
+                    result = performSearch(labelKey, prop, entityType)
                 }
 
                 if (result) {
@@ -95,8 +113,8 @@ class IndexLinkFinder extends BasicPlugin implements LinkFinder, WhelkAware {
                     if (resultJson.hits > 0) {
                         for (r in resultJson.list) {
                             if (r["identifier"] != selfId && r["identifier"] != "/resource" + selfId)  {
-                                ids << new Link(new URI(r["identifier"]), type)
-                                log.debug("Added link of type: $type to ${r["identifier"]} to doc with id $selfId")
+                                ids << new Link(new URI(r["identifier"]), relationType)
+                                log.debug("Added link of type: $relationType to ${r["identifier"]} to doc with id $selfId")
                             }
                         }
                         log.trace("Result data: " + mapper.writeValueAsString(resultJson))
@@ -121,4 +139,5 @@ class IndexLinkFinder extends BasicPlugin implements LinkFinder, WhelkAware {
 
         return ids
     }
+
 }
