1b1cd109bb8e0271b2affc353451e19aca5132c5
mskold <markus.skold@kb.se>
2014-03-27 17:01:52 +0100

    Enabled marcconverter

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -14,4 +14,4 @@ LIBS=$WEBAPPS/WEB-INF/lib
 
 source $SETENV
 
-java $JAVA_OPTS -Ddisable.plugins="indexingprawn,marcconverter" $EXTRA_OPTS -cp $CLASSES:$LIBS/* se.kb.libris.whelks.WhelkOperator -o reindex --fromStorage oaipmhstorage -w libris $DATASET
+java $JAVA_OPTS -Ddisable.plugins="indexingprawn" -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false $EXTRA_OPTS -cp $CLASSES:$LIBS/* se.kb.libris.whelks.WhelkOperator -o reindex --fromStorage oaipmhstorage -w libris $DATASET
