496424d3d5304d3915761497fb27e7902d3ad20d
Niklas Lindström <lindstream@gmail.com>
2014-06-30 17:04:22 +0200

    Add 'valid' method to LinkExpander interface

--- whelk-core/src/main/java/se/kb/libris/whelks/plugin/LinkExpander.java
+++ whelk-core/src/main/java/se/kb/libris/whelks/plugin/LinkExpander.java
@@ -3,5 +3,6 @@ package se.kb.libris.whelks.plugin;
 import se.kb.libris.whelks.Document;
 
 public interface LinkExpander extends Plugin {
+    boolean valid(Document document);
     Document expand(Document document);
 }
