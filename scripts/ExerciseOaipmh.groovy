502ec36b6b74fbfb5bcdd66cc49fd55e8024a70c
Niklas Lindström <lindstream@gmail.com>
2014-09-04 16:00:03 +0200

    Add net/parse time ration measurement to ExerciseOaipmh.groovy

--- scripts/ExerciseOaipmh.groovy
+++ scripts/ExerciseOaipmh.groovy
@@ -9,13 +9,20 @@ class ExerciseOaipmh {
         def recordCount = 0
         while (true) {
             def url = makeNextUrl(startUrl, resumptionToken)
+
+            def netTimeStart = System.currentTimeMillis()
             def xmlString = new URL(url).text // NOTE: might be bad UTF-8
+            def netTime = System.currentTimeMillis() - netTimeStart
+
+            def parseTimeStart = System.currentTimeMillis()
             def doc = new XmlSlurper(false,false).parseText(xmlString)
+            def parseTime = System.currentTimeMillis() - parseTimeStart
+
             resumptionToken = doc.ListRecords.resumptionToken?.text()
 
             recordCount += doc.ListRecords.record.size()
             def elapsed = (System.currentTimeMillis() - startTime) / 1000
-            println "Record count: $recordCount. Got resumption token: $resumptionToken. Elapsed time: $elapsed. Records/second: ${recordCount / elapsed}"
+            println "Record count: $recordCount. Got resumption token: $resumptionToken. Elapsed time: $elapsed. Records/second: ${recordCount / elapsed}. Net/net+parse time ratio: ${netTime/(netTime + parseTime)}"
             if (!resumptionToken)
                 break
         }
