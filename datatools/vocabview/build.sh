105a763fc188af5b4c7b83fa023154697fb1d12c
Niklas Lindström <lindstream@gmail.com>
2014-11-03 12:53:58 +0100

    Add external vocabulary cache to vocabview

--- datatools/vocabview/build.sh
+++ datatools/vocabview/build.sh
@@ -9,13 +9,10 @@ mkdir -p $BUILD_DIR
 D3JS=$BUILD_DIR/d3.v3.min.js
 curl http://d3js.org/d3.v3.min.js -o $D3JS -z $D3JS
 
-# TODO: download dependent vocabs
-VOCAB_DIR=BUILD_DIR
-EXTVOCABS=
-#EXTVOCABS="$VOCAB_DIR/schema_org_rdfa.ttl $VOCAB_DIR/dcterms.rdfs $VOCAB_DIR/dcmitype.rdf $VOCAB_DIR/skos.rdf $VOCAB_DIR/prov.ttl $VOCAB_DIR/bibo.owl $VOCAB_DIR/bibframe.ttl"
+VOCAB_DIR=$BUILD_DIR/vocab-cache
 
 pushd $(dirname $0)
-    python vocab_to_html.py $VOCAB -- $EXTVOCABS > $BUILD_DIR/index.html
+    python vocab_to_html.py $VOCAB_DIR $VOCAB > $BUILD_DIR/index.html
     cp graph.* $BUILD_DIR/
     cp vocab.* $BUILD_DIR/
 popd
