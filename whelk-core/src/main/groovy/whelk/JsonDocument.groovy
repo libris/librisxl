26a102de53a0db9d196c8bbbca65d509474f40b0
mskold <markus.skold@kb.se>
2014-11-25 10:57:13 +0100

    Perform jsonld document modifications only on whelk.add()

--- whelk-core/src/main/groovy/whelk/JsonDocument.groovy
+++ whelk-core/src/main/groovy/whelk/JsonDocument.groovy
@@ -1,9 +1,5 @@
 package whelk
 
-import java.time.ZoneId
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-
 import groovy.util.logging.Slf4j as Log
 
 import org.codehaus.jackson.map.*
@@ -15,26 +11,9 @@ import whelk.util.Tools
 @Log
 class JsonDocument extends DefaultDocument {
 
-    static final DateTimeFormatter DT_FORMAT =
-            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss[.n]XXX")
-
-    @JsonIgnore
-    def timeZone = ZoneId.systemDefault()
-
     // store serialized data
     @JsonIgnore
     protected Map serializedDataInMap
-
-    @JsonIgnore
-    Map metaExtractions = null
-
-    JsonDocument() {
-        super()
-    }
-
-    JsonDocument(Map metaMapping) {
-        this.metaExtractions = metaMapping
-    }
 
     JsonDocument fromDocument(Document otherDocument) {
         setEntry(otherDocument.getEntry())
@@ -60,34 +39,6 @@ class JsonDocument extends DefaultDocument {
     void setData(byte[] data) {
         this.serializedDataInMap = null
         super.setData(data)
-        if (getContentType() == "application/ld+json") {
-            extractMetaFieldsFromData()
-        }
-    }
-
-    private void extractMetaFieldsFromData() {
-        def d = getDataAsMap()
-        if (metaExtractions) {
-            metaExtractions.each { dset, rules ->
-                if (getDataset() == dset) {
-                    rules.each { jsonldpath ->
-                        try {
-                            def value = Eval.x(d, "x.$jsonldpath")
-                            if (value) {
-                                meta = Tools.insertAt(meta, jsonldpath, value)
-                            }
-                        } catch (MissingPropertyException mpe) {
-                            log.trace("Unable to set meta property $jsonldpath : $mpe")
-                        } catch (NullPointerException npe) {
-                            log.warn("Failed to set $jsonldpath for $meta")
-                        } catch (Exception e) {
-                            log.error("Failed to extract meta info: $e", e)
-                            throw e
-                        }
-                    }
-                }
-            }
-        }
     }
 
     @JsonIgnore
@@ -97,19 +48,5 @@ class JsonDocument extends DefaultDocument {
             this.serializedDataInMap = mapper.readValue(new String(getData(), "UTF-8"), Map)
         }
         return serializedDataInMap
-    }
-
-    @Override
-    void setModified(long mt) {
-        log.trace("Updating modified for ${this.identifier} to ${mt}")
-        this.entry[MODIFIED_KEY] = mt
-        if (getContentType() == "application/ld+json") {
-            log.trace("Setting modified in document data.")
-            def map = getDataAsMap()
-            def time = ZonedDateTime.ofInstant(new Date(mt).toInstant(), timeZone)
-            def timestamp = time.format(DT_FORMAT)
-            map.put("modified", timestamp)
-            withData(map)
-        }
     }
 }
