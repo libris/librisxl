6ae8fd52ba35fe510c2597afe46726bb2483fcb3
Niklas Lindström <lindstream@gmail.com>
2015-01-19 16:46:46 +0100

    Add more controlled structure to marcframeview

--- librisxl-tools/datatools/marcframeview/render.py
+++ librisxl-tools/datatools/marcframeview/render.py
@@ -9,6 +9,20 @@ marcframe_path = args.pop(0)
 with open(marcframe_path) as fp:
     marcframe = json.load(fp)
 
+def marc_categories():
+    for cat in MARC_CATEGORIES:
+        yield cat, marcframe[cat]
+
+def tags(catdfn):
+    for tag, dfn in sorted(catdfn.items()):
+        if tag.isdigit() and dfn:
+            yield tag, dfn
+
+def codes(dfn):
+    for code, subdfn in sorted(dfn.items()):
+        if code.startswith('$') and subdfn:
+            yield code, subdfn
+
 env = Environment(loader=PackageLoader(__name__, '.'),
         variable_start_string='${', variable_end_string='}',
         line_statement_prefix='%')
