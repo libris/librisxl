79ffbada03e3638482062e259524ec07e50f0ec7
lisaw <lisa.winberg@kb.se>
2015-03-24 09:22:37 +0100

    main loader for cherry load

--- src/main/groovy/whelk/camel/XInfoRouteProcessor.groovy
+++ src/main/groovy/whelk/camel/XInfoRouteProcessor.groovy
@@ -7,6 +7,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClientBuilder
+import whelk.Whelk
 import whelk.plugin.BasicPlugin
 
 /**
@@ -16,8 +17,14 @@ class XInfoRouteProcessor extends BasicPlugin implements Processor {
 
     XInfoRouteProcessor(Map settings) {
         cherryElasticUrl = settings.get("cherryElasticUrl")
+        elasticCluster = settings.get("elasticCluster")
+        elasticHost = settings.get("elasticHost")
+        elasticPort = settings.get("elasticPort")
+        indexType = settings.get("indexType")
+        indexName = settings.get("indexName")
     }
-    String cherryElasticUrl = null
+    String cherryElasticUrl, elasticCluster, elasticHost, indexType, indexName = null
+    int elasticPort = 9300
 
 
 
@@ -34,7 +41,21 @@ class XInfoRouteProcessor extends BasicPlugin implements Processor {
             idField = "isbn"
             bookId = bookId.substring(9)
         }
-
+        String operation = message.getHeader("whelk:operation")
+        if (operation == Whelk.ADD_OPERATION) {
+            operation = "INDEX"
+        }
+        if (operation == Whelk.BULK_ADD_OPERATION) {
+            operation = "BULK_INDEX"
+        }
+        def parentId = findParentId(idField, bookId)
+        if (parentId){
+            exchange.getIn().setHeader("elasticDestination", "elasticsearch://${elasticCluster}?ip=${elasticHost}&port=${elasticPort}&operation=${operation}&indexType=${indexType}&indexName=${indexName}")
+            exchange.getIn().setHeader("indexId", identifier)
+            exchange.getIn().setHeader("parentId", parentId)
+        } else {
+            exchange.getIn().setHeader("elasticDestination", "stub:discard")
+        }
 
 
     }
@@ -49,5 +70,7 @@ class XInfoRouteProcessor extends BasicPlugin implements Processor {
         ]
         post.setEntity(new StringEntity(mapper.writeValueAsString(query)))
         def response = client.execute(post)
+        def json = mapper.readValue(response.entity.getContent(), Map.class)
+        return json.get('hits').get('hits').first().get('_id')
     }
 }
