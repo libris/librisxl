af18dcce8556ec19ee65eacfb38f18e540766a8f
mskold <markus.skold@kb.se>
2015-04-16 15:07:01 +0200

    use cursors for loading mysql data

--- src/main/groovy/whelk/importer/libris/MySQLImporter.groovy
+++ src/main/groovy/whelk/importer/libris/MySQLImporter.groovy
@@ -20,6 +20,7 @@ class MySQLImporter extends BasicPlugin implements Importer {
 
     MarcFrameConverter marcFrameConverter
     JsonLDLinkCompleterFilter enhancer
+    LinkFinder linkfinder
 
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver"
 
@@ -46,6 +47,7 @@ class MySQLImporter extends BasicPlugin implements Importer {
     void bootstrap() {
         marcFrameConverter = plugins.find { it instanceof MarcFrameConverter }
         enhancer = plugins.find { it instanceof JsonLDLinkCompleterFilter }
+        linkfinder = plugins.find { it instanceof LinkFinder }
         assert marcFrameConverter
     }
 
@@ -82,65 +84,61 @@ class MySQLImporter extends BasicPlugin implements Importer {
 
             log.debug("Connecting to database...")
             conn = connectToUri(serviceUrl)
+            conn.setAutoCommit(false)
 
             if (dataset == "bib") {
                 log.info("Creating bib load statement.")
-                statement = conn.prepareStatement("SELECT bib.bib_id, bib.data, auth.auth_id FROM bib_record bib LEFT JOIN auth_bib auth ON bib.bib_id = auth.bib_id WHERE bib.bib_id > ? AND bib.deleted = 0 ORDER BY bib.bib_id LIMIT $sqlLimit")
+                statement = conn.prepareStatement("SELECT bib.bib_id, bib.data, auth.auth_id FROM bib_record bib LEFT JOIN auth_bib auth ON bib.bib_id = auth.bib_id WHERE bib.bib_id > ? AND bib.deleted = 0 ORDER BY bib.bib_id", java.sql.ResultSet.TYPE_FORWARD_ONLY, java.sql.ResultSet.CONCUR_READ_ONLY)
             }
             if (dataset == "hold") {
                 log.info("Creating hold load statement.")
-                statement = conn.prepareStatement("SELECT mfhd_id, data, bib_id, shortname FROM mfhd_record WHERE mfhd_id > ? AND deleted = 0 ORDER BY mfhd_id LIMIT $sqlLimit")
+                statement = conn.prepareStatement("SELECT mfhd_id, data, bib_id, shortname FROM mfhd_record WHERE mfhd_id > ? AND deleted = 0 ORDER BY mfhd_id", java.sql.ResultSet.TYPE_FORWARD_ONLY, java.sql.ResultSet.CONCUR_READ_ONLY)
             }
 
             if (!statement) {
                 throw new Exception("No valid dataset selected")
             }
+            statement.setFetchSize(Integer.MIN_VALUE)
 
             int recordId = startAt
             log.info("Starting loading at ID $recordId")
 
             MarcRecord record = null
 
-            for (;;) {
-                statement.setInt(1, recordId)
-                log.debug("Reset statement with $recordId")
-                resultSet = statement.executeQuery()
+            statement.setInt(1, recordId)
+            resultSet = statement.executeQuery()
 
-                log.debug("Query executed. Starting processing ...")
-                int lastRecordId = recordId
-                while(resultSet.next()) {
-                    recordId  = resultSet.getInt(1)
-                    record = Iso2709Deserializer.deserialize(normalizeString(new String(resultSet.getBytes("data"), "UTF-8")).getBytes("UTF-8"))
+            log.debug("Query executed. Starting processing ...")
+            int lastRecordId = recordId
+            while(resultSet.next()) {
+                recordId = resultSet.getInt(1)
+                record = Iso2709Deserializer.deserialize(normalizeString(new String(resultSet.getBytes("data"), "UTF-8")).getBytes("UTF-8"))
 
-                    buildDocument(record, dataset, null)
+                buildDocument(record, dataset, null)
 
-                    if (dataset == "bib") {
-                        int auth_id = resultSet.getInt("auth_id")
-                        if (auth_id > 0) {
-                            log.trace("Found auth_id $auth_id for $recordId Adding to oaipmhSetSpecs")
-                            buildDocument(record, dataset, "authority:"+auth_id)
-                        }
-                    } else if (dataset == "hold") {
-                        int bib_id = resultSet.getInt("bib_id")
-                        String sigel = resultSet.getString("shortname")
-                        if (bib_id > 0) {
-                            log.trace("Found bib_id $bib_id for $recordId Adding to oaipmhSetSpecs")
-                            buildDocument(record, dataset, "bibid:" + bib_id)
-                        }
-                        if (sigel) {
-                            log.trace("Found sigel $sigel for $recordId Adding to oaipmhSetSpecs")
-                            buildDocument(record, dataset, "location:" + sigel)
-                        }
+                if (dataset == "bib") {
+                    int auth_id = resultSet.getInt("auth_id")
+                    if (auth_id > 0) {
+                        log.trace("Found auth_id $auth_id for $recordId Adding to oaipmhSetSpecs")
+                        buildDocument(record, dataset, "authority:"+auth_id)
+                    }
+                } else if (dataset == "hold") {
+                    int bib_id = resultSet.getInt("bib_id")
+                    String sigel = resultSet.getString("shortname")
+                    if (bib_id > 0) {
+                        log.trace("Found bib_id $bib_id for $recordId Adding to oaipmhSetSpecs")
+                        buildDocument(record, dataset, "bibid:" + bib_id)
+                    }
+                    if (sigel) {
+                        log.trace("Found sigel $sigel for $recordId Adding to oaipmhSetSpecs")
+                        buildDocument(record, dataset, "location:" + sigel)
                     }
                 }
-
                 if (nrOfDocs > 0 && recordCount > nrOfDocs) {
                     log.info("Max docs reached. Breaking.")
                     break
                 }
-
-                if (cancelled || lastRecordId == recordId) {
-                    log.info("Same id. Breaking.")
+                if (cancelled) {
                     break
                 }
             }
@@ -182,7 +180,7 @@ class MySQLImporter extends BasicPlugin implements Importer {
             tickets.acquire()
             */
             log.debug("Doclist has reached batch size. Sending it to bulkAdd (open the trapdoor)")
-            queue.execute(new ConvertAndStoreRunner(whelk, marcFrameConverter, enhancer, documentList, tickets))
+            queue.execute(new ConvertAndStoreRunner(whelk, marcFrameConverter, enhancer, linkfinder, documentList, tickets))
             log.debug("     Current poolsize: ${queue.poolSize}")
             log.debug("------------------------------")
             log.debug("queuedSubmissionCount: ${queue.queuedSubmissionCount}")
@@ -220,12 +218,13 @@ class MySQLImporter extends BasicPlugin implements Importer {
         private Whelk whelk
         private MarcFrameConverter converter
         private Filter filter
+        private LinkFinder linkfinder
 
         private List recordList
 
         private Semaphore tickets
 
-        ConvertAndStoreRunner(Whelk w, FormatConverter c, Filter f, final List recList, Semaphore t) {
+        ConvertAndStoreRunner(Whelk w, FormatConverter c, Filter f, LinkFinder linkfinder, final List recList, Semaphore t) {
             this.whelk = w
             this.converter = c
             this.filter = f
@@ -243,6 +242,9 @@ class MySQLImporter extends BasicPlugin implements Importer {
                     Document doc = converter.doConvert(it.record, ["entry":it.entry,"meta":it.meta])
                     if (filter) {
                         doc = filter.filter(doc)
+                    }
+                    if (linkfinder) {
+                        doc = linkfinder.findLinks(doc)
                     }
                     convertedDocs << doc
                 }
