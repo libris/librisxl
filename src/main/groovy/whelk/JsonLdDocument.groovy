8396765cef2d1d546b85ee2cdd0d11fe3780f842
mskold <markus.skold@kb.se>
2015-05-21 16:24:21 +0200

    added jsonlddocument

---
+++ src/main/groovy/whelk/JsonLdDocument.groovy
@@ -0,0 +1,33 @@
+package whelk
+
+import groovy.util.logging.Slf4j as Log
+
+@Log
+class JsonLdDocument extends JsonDocument {
+
+
+    final static String ID_KEY = "@id"
+
+    protected List<String> findLinks(Map dataMap) {
+        List<String> ids = []
+        for (entry in dataMap) {
+            if (entry.key == ID_KEY && entry.value != identifier) {
+                ids.add(entry.value)
+            } else if (entry.value instanceof Map) {
+                ids.addAll(findLinks(entry.value))
+            } else if (entry.value instanceof List) {
+                for (l in entry.value) {
+                    if (l instanceof Map) {
+                        ids.addAll(findLinks(l))
+                    }
+                }
+            }
+        }
+        return ids
+    }
+
+    void setData(byte[] data) {
+        super.setData(data)
+        this.entry['links'] = findLinks(getDataAsMap())
+    }
+}
