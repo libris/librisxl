b16dfcbd9e3ff74b4a174e9b7d51548937ebac3a
Niklas Lindström <lindstream@gmail.com>
2015-09-15 23:52:52 +0200

    Adapt LoadJsonLdDump script to remade API

--- librisxl-tools/scripts/LoadJsonLdDump.groovy
+++ librisxl-tools/scripts/LoadJsonLdDump.groovy
@@ -1,6 +1,6 @@
 import org.codehaus.jackson.map.ObjectMapper
-import whelk.JsonDocument
-import whelk.component.PostgreSQLStorage
+import whelk.Document
+import whelk.component.PostgreSQLComponent
 
 
 def sourceDump = args[0]
@@ -8,21 +8,13 @@ def databaseUrl = args[1]
 
 def mapper = new ObjectMapper()
 
-def storage = new PostgreSQLStorage(
-        ["databaseUrl": databaseUrl, "tableName": "lddb"])
-
-storage.onStart()
-storage.componentBootstrap(null)
+def storage = new PostgreSQLComponent(databaseUrl, "lddb", null, null)
 
 def i = 1
 new File(sourceDump).eachLine('UTF-8') {
     def data = mapper.readValue(it, Map)
     def id = data.descriptions.entry['@id']
-    def doc = new JsonDocument()
-        .withIdentifier(id)
-        .withData(data)
-        .withModified(new Date().time)
-        .withDataset('auth')
+    def doc = new Document(id, data, [dataset: 'auth'])
     storage.store(doc)
     println "Stored #${i++} as ${id}"
 }
