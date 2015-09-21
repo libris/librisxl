bf7d512911aa11f59e08aee14b511e98d59c83db
mskold <markus.skold@kb.se>
2015-09-21 13:24:12 +0200

    moved link expander

--- converters/src/main/groovy/whelk/converter/JsonLDLinkExpander.groovy
+++ converters/src/main/groovy/whelk/converter/JsonLDLinkExpander.groovy
@@ -1,21 +1,12 @@
 package whelk.converter
 
+import groovy.transform.Synchronized
 import groovy.util.logging.Slf4j as Log
 
+import org.codehaus.jackson.map.ObjectMapper
 import whelk.*
+import whelk.component.*
 import whelk.exception.*
-
-import org.apache.http.client.*
-import org.apache.http.client.methods.*
-import org.apache.http.client.utils.*
-import org.apache.http.entity.*
-import org.apache.http.impl.client.DefaultHttpClient
-import org.apache.http.util.EntityUtils
-import org.apache.http.client.entity.UrlEncodedFormEntity
-import org.apache.http.message.BasicNameValuePair
-import org.apache.http.impl.conn.*
-import org.apache.http.impl.client.*
-import org.apache.http.client.protocol.*
 
 
 @Log
@@ -23,24 +14,24 @@ class JsonLDLinkExpander {
 
     Map nodesToExpand = null
     private Map cachedDocuments = [:]
-    Map settings
+    Storage storage
 
-    PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager()
-    HttpClient client = HttpClients.custom().setConnectionManager(cm).build()
+    static final ObjectMapper mapper = new ObjectMapper()
 
-    private static final JsonLDLinkExpander instance = new JsonLDLinkExpander()
-    private JsonLDLinkExpander() {}
-    public static JsonLDLinkExpander getInstance() { return instance }
-
-    public void setSettings(Map settings) {
-        this.nodesToExpand = settings.get('nodesToExpand').asImmutable()
-
+    JsonLDLinkExpander(Storage s) {
+        storage = s
+        try {
+            nodesToExpand = mapper.readValue(this.getClass().getClassLoader().getResourceAsStream("jsonLdNodesToExpand.json"), Map)
+        } catch (Exception e) {
+            log.error("Unable to find file \"jsonLdNodesToExpand.json\" in classpath.")
+            throw e
+        }
     }
 
     /**
      * Keys to preserve from linked document.
      */
-    private List getRelevantKeys(String identifier) {
+    private static  List getRelevantKeys(String identifier) {
         if (identifier.startsWith("/def/enum/")) {
             return ["@id", "@type", "prefLabel"]
         }
@@ -48,17 +39,17 @@ class JsonLDLinkExpander {
     }
 
     boolean valid(Document doc) {
-        if (doc && doc.isJson() && doc.contentType == "application/ld+json" && this.nodesToExpand.containsKey(doc.manifest.dataset)) {
+        if (doc && doc.contentType == "application/ld+json" && this.nodesToExpand.containsKey(doc.dataset)) {
             return true
         }
         return false
     }
 
-    @groovy.transform.Synchronized
+    @Synchronized
     void loadCachedDocuments() {
         log.debug("Caching def-documents.")
-        for (doc in whelk.loadAll("def")) {
-            def dataMap = (doc.dataAsMap.containsKey("about") ? doc.dataAsMap.get("about") : doc.dataAsMap)
+        for (doc in storage.loadAll("def")) {
+            Map dataMap = (doc.data.containsKey("about") ? doc.data.get("about") : doc.data)
             dataMap.put("@id", doc.identifier)
             if (getRelevantKeys(doc.identifier)) {
                 def newMap = [:]
@@ -75,9 +66,9 @@ class JsonLDLinkExpander {
         log.debug("Cached ${cachedDocuments.size()} docs.")
     }
 
-    Document doFilter(Document doc) {
+    Document filter(Document doc) {
         log.debug("Expanding ${doc.identifier}")
-        def dataMap = doFilter(doc.dataAsMap, doc.dataset)
+        def dataMap = doFilter(doc.data, doc.dataset)
         return doc.withData(dataMap)
     }
 
@@ -108,71 +99,24 @@ class JsonLDLinkExpander {
         return dataMap
     }
 
-    /* TODO: replace with storage
-    Index getIndex() {
-        if (!index) {
-            index = plugins.find { it instanceof Index }
-        }
-        return index
-    }
-    */
-
-    Map expandNode(Map node, Map instructions) {
-        log.error("TODO: rewrite node expansion using storage instead of index.")
-        return node
-    }
-
-    /*
     Map expandNode(Map node, Map instructions) {
         if (cachedDocuments.containsKey(node['@id'])) {
             return cachedDocuments.get(node['@id'])
-        } else if (node['@id'] && !node['@id'].startsWith("/def/")) {
-            if (instructions['method'] == "query") {
-                Map queryMap = [ 'terms': [ instructions['field']+":"+node['@id'] ] ]
-                if (instructions['queryProperties']) {
-                    if (instructions['queryProperties']['_source.include'] && !instructions['queryProperties']['_source.include'].contains(instructions['field'])) {
-                        instructions['queryProperties']['_source.include'] << instructions['field']
-                    }
-                    queryMap.putAll(instructions['queryProperties'])
-                }
-                def query = new ElasticQuery(queryMap)
-                def result = getIndex().query(query, this.whelk.index.getIndexName(), instructions['dataset'])
-                if (result.numberOfHits) {
-                    log.trace("data is : " + result.hits[0].dataAsString)
-                    log.trace("ct is : " + result.hits[0].contentType)
-
-                    def resultMap = (instructions['resultKey'] ? result.hits[0].dataAsMap.get(instructions['resultKey']) : result.hits[0].dataAsMap)
-                    def bastards = (instructions['resultKey'] ? instructions['queryProperties']['_source.include'].findAll { it != "@id" && !it.startsWith(instructions['resultKey'])} : [])
-                    if (bastards.size() > 0) {
-                        bastards.each {
-                            resultMap.put("@reverse", [(instructions['resultKey']): [(it): result.hits[0].dataAsMap.get(it)]])
-                        }
-                    }
-
-                    return resultMap
-                } else {
-                    log.trace("No results in query.")
-                    return node
-                }
-            }
-            if (instructions['method'] == "get") {
-                log.debug("getting from whelk for node $node")
-                def doc = whelk.get(node['@id'], Storage.RAW_VERSION)
-                return (doc ? doc.dataAsMap : node)
-            }
-            if (instructions['method'] == "http") {
-                HttpGet get = new HttpGet(new URI(instructions['url']+node['@id']))
-                def response = client.execute(get)
-                log.trace("response: ${response.getEntity().getContent().getText()}")
-                // TODO: implement
+        } else if (node['@id'] && !((String)node['@id']).startsWith("/def/")) {
+            Location loc = storage.locate(node['@id'])
+            if (!loc) {
+                return node
+            } else if (loc.getDocument()) {
+                return loc.document.data
+            } else {
+                return storage.load(loc.uri.toString())?.data
             }
         }
         return node
     }
-    */
 
-    def getNestedObject(key, map) {
-        def m = map
+    def getNestedObject(String key, Map map) {
+        Map m = map
         for (k in key.split(/\./)) {
             if (m && m.containsKey(k)) {
                 m = m[k]
@@ -195,3 +139,5 @@ class JsonLDLinkExpander {
         }
     }
 }
+
+
