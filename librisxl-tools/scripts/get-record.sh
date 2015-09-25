190ecab4474db6d358faa7c29eb58ce222dc961b
Niklas Lindström <lindstream@gmail.com>
2015-09-25 16:32:34 +0200

    Simplify get-record.sh a bit

--- librisxl-tools/scripts/get-record.sh
+++ librisxl-tools/scripts/get-record.sh
@@ -6,8 +6,8 @@ if [[ "$TYPE" == "" ]] || [[ "$ID" == "" ]]; then
     exit
 fi
 
-MARCPATH=/tmp/$TYPE-$ID.iso2709
-JSONPATH=/tmp/$TYPE-$ID.json
-curl -s http://libris.kb.se/data/$TYPE/$ID?format=ISO2709 -o $MARCPATH
-
-$(dirname $0)/convert-iso2709-to-json.sh $MARCPATH
+#MARCPATH=/tmp/$TYPE-$ID.iso2709
+#JSONPATH=/tmp/$TYPE-$ID.json
+#curl -s http://libris.kb.se/data/$TYPE/$ID?format=ISO2709 -o $MARCPATH
+#$(dirname $0)/convert-iso2709-to-json.sh $MARCPATH
+curl -s http://libris.kb.se/data/$TYPE/$ID?format=application/marcxml #| python $(dirname $0)/librisxl-tools/scripts/marcxml_to_marcjson.py
