913b112e4899e2e1bbfde7752cf15b2f3b1b897f
mskold <markus.skold@kb.se>
2015-10-08 15:37:29 +0200

    detect flat json with description

--- core/src/test/groovy/JsonLdSpec.groovy
+++ core/src/test/groovy/JsonLdSpec.groovy
@@ -10,48 +10,24 @@ import whelk.JsonLd
 class JsonLdSpec extends Specification {
 
     static final ObjectMapper mapper = new ObjectMapper()
-    static List<String> flatfiles, framefiles
-
-    def setup() {
-        flatfiles = IOUtils.readLines(this.getClass().getClassLoader().getResourceAsStream("flatfiles/"), Charsets.UTF_8);
-        framefiles = IOUtils.readLines(this.getClass().getClassLoader().getResourceAsStream("framefiles/"), Charsets.UTF_8);
-    }
+    static List<String> flatfiles = IOUtils.readLines(JsonLdSpec.class.getClassLoader().getResourceAsStream("flatfiles/"), Charsets.UTF_8);
+    static List<String> framefiles = IOUtils.readLines(JsonLdSpec.class.getClassLoader().getResourceAsStream("framefiles/"), Charsets.UTF_8);
 
     def "should frame flat jsonld"() {
-        given:
-        def flatJson = mapper.readValue(loadJsonLdFile("flatfiles/bib_13531679_flat.jsonld"), Map)
-        def framedJson = mapper.readValue(loadJsonLdFile("framefiles/bib_13531679_framed.jsonld"), Map)
-        when:
-        def resultJson = JsonLd.frame("/bib/13531679", flatJson)
-        then:
-        resultJson.get("about").get("identifier")[0].get("identifierValue") == "9789174771107"
-        resultJson.get("about").get("genre")[0].get("prefLabel") == "E-böcker"
-        resultJson.equals(framedJson)
-
-    }
-
-
-    def "test many files"() {
         expect:
         JsonLd.frame(ids, flatJson).equals(framedJson)
         where:
-
         ids << flatfiles.collect { "/"+it.replace("_flat.jsonld", "").replace("_", "/") }
         flatJson << flatfiles.collect { mapper.readValue(loadJsonLdFile("flatfiles/${it}"), Map) }
         framedJson << framefiles.collect { mapper.readValue(loadJsonLdFile("framefiles/${it}"), Map) }
-
     }
 
-
-
     def "should flatten framed jsonld"() {
-        given:
-        def flatJson = mapper.readValue(loadJsonLdFile("flatfiles/bib_13531679_flat.jsonld"), Map)
-        def framedJson = mapper.readValue(loadJsonLdFile("framefiles/bib_13531679_framed.jsonld"), Map)
-        when:
-        def resultJson = JsonLd.flatten(framedJson)
-        then:
-        flatJson.size() == resultJson.size()
+        expect:
+        JsonLd.flatten(framedJson).equals(flatJson)
+        where:
+        flatJson << flatfiles.collect { mapper.readValue(loadJsonLdFile("flatfiles/${it}"), Map) }
+        framedJson << framefiles.collect { mapper.readValue(loadJsonLdFile("framefiles/${it}"), Map) }
     }
 
     def "should detect flat jsonld"() {
