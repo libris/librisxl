c3b6d4b9877234142348a91c69a946327af1a1a0
Niklas Lindström <lindstream@gmail.com>
2015-12-17 23:08:23 +0100

    Enable indexing again during vcopy import

--- importers/src/main/groovy/whelk/importer/ImporterMain.groovy
+++ importers/src/main/groovy/whelk/importer/ImporterMain.groovy
@@ -42,7 +42,7 @@ class ImporterMain {
 
     void goMysql(String collection) {
         def importer = pico.getComponent(MySQLImporter.class)
-        importer.storageOnly = true
+        //importer.storageOnly = true
         importer.doImport(collection)
         //println("Starting LinkFinder for collection $collection")
         //goLinkFind(collection)
