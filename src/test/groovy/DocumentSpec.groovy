0395924b3ffee6b10e6a6eee6f2c7abac2135cb5
Markus Sköld <markus.skold@kb.se>
2016-01-25 13:47:23 +0100

    handle weird @ids for sameas

--- src/test/groovy/DocumentSpec.groovy
+++ src/test/groovy/DocumentSpec.groovy
@@ -11,6 +11,10 @@ class DocumentSpec extends Specification {
                 "@type": "ObjectProperty",
                 "sameAs": [
                     ["@id": "https://libris.kb.se/fnrblrghr1234567"],
+                    ["@id": "ftp://ppftpuser:welcome@@ftp01.penguingroup.com/Booksellers and Media/Covers/2008@03322009@0332New@0332Covers/9780399168123.jpg"],
+                    ["@id": "http://kulturarvsdata.se/raa/samla/html/6743 |z Fritt tillgänglig via Riksantikvarieämbetets webbplats"],
+                    ["@id": "http://images.contentreserve.com/ImageType-10\n" +
+                                "0/1559-1/{C07B6BAA-9D1B-4FC5-B724-42628F4BBF59}Img100.jpg"]
                 ],
                 "rangeIncludes": [
                     ["@id": "https://id.kb.se/marc/AuthorityCharacterCodingType"],
@@ -37,8 +41,14 @@ class DocumentSpec extends Specification {
             ]
         ]
 
-    static identifiers = ["https://id.kb.se/marc/characterCoding",
-                          "https://libris.kb.se/fnrblrghr1234567"]
+    static identifiers = ["ftp://ppftpuser:welcome@@ftp01.penguingroup.com/Booksellers%20and%20Media/Covers/2008@03322009@0332New@0332Covers/9780399168123.jpg",
+                          "http://images.contentreserve.com/ImageType-100/1559-1/{C07B6BAA-9D1B-4FC5-B724-42628F4BBF59}Img100.jpg",
+                          "http://kulturarvsdata.se/raa/samla/html/6743",
+                          "https://id.kb.se/marc/characterCoding",
+                          "https://libris.kb.se/fnrblrghr1234567"
+            ]
+
+
 
     static quoted = ["https://id.kb.se/marc/AuthorityCharacterCodingType",
                      "https://id.kb.se/marc/CharacterCodingType",
