8639772cddf5fbb26eacc64cb5585c6bd37a098a
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2016-02-08 13:06:46 +0100

    Initial scaffolding for apix_export.

---
+++ apix_export/src/main/java/whelk/apix/servlet/ApixExportUI.java
@@ -0,0 +1,30 @@
+package whelk.apix.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ApixExportUI extends HttpServlet
+{
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+    {
+        //handleRequest(req, res);
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException
+    {
+        //handleRequest(req, res);
+    }
+
+    public void init()
+    {
+        //configuration = PropertyLoader.loadProperties("secret");
+        //DataBase.init();
+    }
+
+    public void destroy()
+    {
+        //DataBase.destroy();
+    }
+}
