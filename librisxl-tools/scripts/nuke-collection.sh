e5fb78bfa1bbe862478f211237de06e9d16997bf
Niklas Lindström <lindstream@gmail.com>
2016-03-15 14:42:01 +0100

    Provide whelk as argument to nuke-collection.sh

--- librisxl-tools/scripts/nuke-collection.sh
+++ librisxl-tools/scripts/nuke-collection.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-COLLECTION_ID=$1
-[[ "$COLLECTION_ID" == '' ]] && echo "USAGE $0 COLLECTION_ID" && exit 1
+WHELK_ID=$1
+COLLECTION_ID=$2
+[[ "$COLLECTION_ID" == '' ]] && echo "USAGE $0 WHELK_ID COLLECTION_ID" && exit 1
 set -x
-psql whelk -c "delete from lddb where manifest->>'collection' = '$COLLECTION_ID';"
-curl -XDELETE "http://localhost:9200/whelk/$COLLECTION_ID/_query" -d '{"query":{"match_all": {}}}'
+psql $WHELK_ID -c "delete from lddb where manifest->>'collection' = '$COLLECTION_ID';"
+curl -XDELETE "http://localhost:9200/$WHELK_ID/$COLLECTION_ID/_query" -d '{"query":{"match_all": {}}}'
