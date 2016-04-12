c88ea8de6e3a6e2fe9dcbdcb3f4675ef31a73aef
Kai Pöykiö <kai.poykio@kb.se>
2016-04-12 14:07:59 +0200

    Tentative synopsis.

--- src/main/java/whelk/importer/Import.java
+++ src/main/java/whelk/importer/Import.java
@@ -1,9 +1,20 @@
 package whelk.importer;
 
+/* lxl_import, Synopsis
+
+0. Setup configuration variables for channel
+1. Choose reader for channel
+2. For each record in stream:
+3. Select whelk-id's (for duplicate check)
+4. Depending on id's and recordtype
+5. Transform (if necessary)
+6. Action=delete,merge,add (from marc-leader?)
+
+*/
+
 public class Import
 {
 	public static void main(String[] args)
 	{
-	// test	
 	}
 }
