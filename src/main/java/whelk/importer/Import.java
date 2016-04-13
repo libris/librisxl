ad92a086e0b454ecbca746fff16a0b285695891e
Kai Pöykiö <kai.poykio@kb.se>
2016-04-13 09:29:04 +0200

    added replace action to synopsis

--- src/main/java/whelk/importer/Import.java
+++ src/main/java/whelk/importer/Import.java
@@ -4,11 +4,11 @@ package whelk.importer;
 
 0. Setup configuration variables for channel
 1. Choose reader for channel
-2. For each record in stream:
-3. Select whelk-id's (for duplicate check)
-4. Depending on id's and recordtype
-5. Transform (if necessary)
-6. Action=delete,merge,add (from marc-leader?)
+2. Transform (if necessary)
+3. For each record in (transformed) stream:
+4. Select whelk-id's (for duplicate check)
+5. Depending on id's and recordtype
+6. Action=delete,merge,add,replace (from marc-leader?)
 
 */
 
