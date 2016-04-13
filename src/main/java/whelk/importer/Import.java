7a93f71980f2e9d80e1dac1959b1093a55338dcb
Kai Pöykiö <kai.poykio@kb.se>
2016-04-13 10:37:35 +0200

    more synopsis work

--- src/main/java/whelk/importer/Import.java
+++ src/main/java/whelk/importer/Import.java
@@ -3,12 +3,12 @@ package whelk.importer;
 /* lxl_import, Synopsis
 
 0. Setup configuration variables for channel
-1. Choose reader for channel
-2. Transform (if necessary)
+1. Choose reader for channel (xml, iso2709, json, json-ld,tsv)
+2. Transform (if necessary, possibly to several records)
 3. For each record in (transformed) stream:
 4. Select whelk-id's (for duplicate check)
-5. Depending on id's and recordtype
-6. Action=delete,merge,add,replace (from marc-leader?)
+5. Depending on id's (what if multiple?) and recordtype (bib,hold,auth)
+6. Action=delete,merge,add,replace (from marc-leader? or config?)
 
 */
 
