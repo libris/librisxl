356e09a46a882fbd4ca76e6e1d9ea19f8997fee5
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2016-04-14 10:35:09 +0200

    First stab at traversing some indata, no actual importing being done yet.

--- src/main/java/whelk/importer/Import.java
+++ src/main/java/whelk/importer/Import.java
@@ -12,9 +12,103 @@ package whelk.importer;
 
 */
 
+import org.apache.commons.io.IOUtils;
+import se.kb.libris.util.marc.MarcRecord;
+import se.kb.libris.util.marc.io.Iso2709Deserializer;
+import se.kb.libris.util.marc.io.Iso2709Serializer;
+import se.kb.libris.util.marc.io.MarcRecordReader;
+import se.kb.libris.util.marc.io.MarcXmlRecordReader;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Import
 {
 	public static void main(String[] args)
+            throws Exception
 	{
-	}
+		Parameters parameters = new Parameters(args);
+
+        if (parameters.getPath() == null)
+            importStream(System.in, parameters);
+
+        else // A path was speficied
+        {
+            File file = new File(parameters.getPath().toString());
+            if (file.isDirectory())
+            {
+                for (File subFile : file.listFiles())
+                {
+                    if (!subFile.isDirectory())
+                        importFile(subFile.toPath(), parameters);
+                }
+            }
+            else // regular file
+            {
+                importFile(file.toPath(), parameters);
+            }
+        }
+    }
+
+    private static void importFile(Path path, Parameters parameters)
+            throws Exception
+    {
+        System.out.println("Doing file: " + path.toString());
+        try (ExclusiveFile file = new ExclusiveFile(path);
+             InputStream fileInStream = file.getInputStream())
+        {
+            importStream(fileInStream, parameters);
+        }
+    }
+
+    private static void importStream(InputStream inputStream, Parameters parameters)
+            throws Exception
+    {
+        MarcRecord marcRecord = null;
+
+        switch (parameters.getFormat())
+        {
+            case FORMAT_XML:
+            {
+                // Apply any transforms
+                List<Transformer> transformers = parameters.getTransformers();
+                if (!transformers.isEmpty())
+                {
+                    for (Transformer transformer : transformers)
+                        inputStream = transform(transformer, inputStream);
+                }
+
+                marcRecord = new MarcXmlRecordReader(inputStream).readRecord();
+                break;
+            }
+
+            case FORMAT_ISO2709:
+            {
+                byte[] iso2709record = IOUtils.toByteArray(inputStream);
+                marcRecord = Iso2709Deserializer.deserialize(iso2709record);
+            }
+        }
+
+        importISO2709(marcRecord, parameters);
+    }
+
+    private static InputStream transform(Transformer transformer, InputStream inputStream)
+            throws TransformerException
+    {
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        StreamResult result = new StreamResult(buf);
+        transformer.transform( new StreamSource(inputStream), result );
+        return new ByteArrayInputStream(buf.toByteArray());
+    }
+
+    private static void importISO2709(MarcRecord marcRecord, Parameters parameters)
+            throws Exception
+    {
+        System.out.println("Would now import iso2709 record: " + marcRecord.toString());
+    }
 }
---
+++ src/main/java/whelk/importer/Parameters.java
@@ -0,0 +1,106 @@
+package whelk.importer;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parameters
+{
+    private Path path;
+    private INPUT_FORMAT format;
+    private boolean readOnly = true;
+    private List<Transformer> transformers = new ArrayList<Transformer>();
+
+    public Path getPath() { return path; }
+    public INPUT_FORMAT getFormat() { return format; }
+    public boolean getReadOnly() { return readOnly; }
+    public List<Transformer> getTransformers() { return transformers; }
+
+    public enum INPUT_FORMAT
+    {
+        FORMAT_ISO2709,
+        FORMAT_XML,
+    }
+
+    public Parameters(String[] args)
+            throws Exception
+    {
+        for (String arg : args)
+        {
+            int separatorIndex= arg.indexOf('=');
+            if (separatorIndex > 1 && arg.length() > 2)
+            {
+                String param = arg.substring(0, separatorIndex);
+                String value = arg.substring(separatorIndex+1);
+                System.out.println(param + " -> " + value);
+                interpretBinaryParameter(param, value);
+            }
+            else
+            {
+                interpretUnaryParameter(arg);
+            }
+        }
+
+        // Check required parameters
+        if (format == null)
+        {
+            printUsage();
+            System.exit(-1);
+        }
+    }
+
+    private void printUsage()
+    {
+        System.out.println("Usage: ..");
+    }
+
+    private void interpretBinaryParameter(String parameter, String value)
+            throws Exception
+    {
+        switch (parameter)
+        {
+            case "--path":
+                path = Paths.get(value);
+                break;
+
+            case "--format":
+                switch (value)
+                {
+                    case "iso2709":
+                        format = INPUT_FORMAT.FORMAT_ISO2709;
+                        break;
+                    case "xml":
+                        format = INPUT_FORMAT.FORMAT_XML;
+                        break;
+                    default:
+                        throw new IllegalArgumentException(value + " is not a valid indata format.");
+                }
+                break;
+
+            case "--transformer":
+                transformers.add( TransformerFactory.newInstance().newTransformer(
+                        new StreamSource(new File(value))) );
+                break;
+
+            default:
+                throw new IllegalArgumentException(parameter);
+        }
+    }
+
+    private void interpretUnaryParameter(String parameter)
+    {
+        switch (parameter)
+        {
+            case "--live":
+                readOnly = false;
+                break;
+            default:
+                throw new IllegalArgumentException(parameter);
+        }
+    }
+}
