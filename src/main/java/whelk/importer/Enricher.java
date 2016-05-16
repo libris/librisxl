e1bbbe2dc7e752ab3d66b7762e0b1858b112b310
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2016-05-16 15:32:35 +0200

    refactored merge. Also now enriching with bnode ids that cant be mapped as new bnode ids in target graph.

--- src/main/java/whelk/importer/Enricher.java
+++ src/main/java/whelk/importer/Enricher.java
@@ -15,43 +15,13 @@ public class Enricher
         Graph originalGraph = new Graph(originalTriples);
         Graph withGraph = new Graph(withTriples);
 
-        Map<String, String> bNodeMapping = withGraph.generateBNodeMapFrom(originalGraph);
-        Set<String> mergedTriples = new HashSet<>(); // Just to make sure we don't introduce doubles of triples.
-        for (String[] triple : originalTriples)
-            mergedTriples.add(triple[0]+triple[1]+triple[2]);
-        for (String[] triple : withTriples)
-        {
-            String subject = getTranslatedNodeId(triple[0], bNodeMapping);
-            String predicate = triple[1];
-            String object = getTranslatedNodeId(triple[2], bNodeMapping);
+        //System.out.println(originalGraph);
+        //System.out.println(withGraph);
 
-            if (subject != null && object != null)
-            {
-                String setRepresentation = subject+predicate+object;
-                if (!mergedTriples.contains(setRepresentation))
-                {
-                    mergedTriples.add(setRepresentation);
-                    originalTriples.add(new String[]{subject, predicate, object});
-                }
-            }
-        }
+        originalGraph.enrichWith(withGraph);
 
-        Map enrichedData = JsonldSerializer.serialize(originalTriples);
+        Map enrichedData = JsonldSerializer.serialize(originalGraph.getTriples());
         JsonldSerializer.normalize(enrichedData, mutableDocument.getId());
         mutableDocument.setData(enrichedData);
-    }
-
-    /**
-     * Given a node id, get the corresponding node id in this graph (which is the same unless it is a blank node id).
-     */
-    private static String getTranslatedNodeId(String naiveNodeId, Map<String, String> bNodeMapping)
-    {
-        if (naiveNodeId.startsWith(JsonldSerializer.BLANKNODE_PREFIX))
-        {
-            if (bNodeMapping.keySet().contains(naiveNodeId))
-                return bNodeMapping.get(naiveNodeId);
-            return null;
-        }
-        return naiveNodeId;
     }
 }
--- src/main/java/whelk/importer/Graph.java
+++ src/main/java/whelk/importer/Graph.java
@@ -9,44 +9,161 @@ public class Graph
      *
      * Mapping node IDs (A) to lists of predicate-subject pairs (edges) that connect (A) with other nodes
      */
-    private Map<String, List<String[]>> edgesFromId;
+    private Map<String, List<String[]>> m_edgesFromId;
+
+    /**
+     * Keep track of the highest currently assigned BNode id in the graph (so that new BNode ids can be assigned)
+     */
+    private int m_nextBNodeIDNumber = 0;
 
     public Graph(List<String[]> triples)
     {
-        edgesFromId = new HashMap<>();
+        m_edgesFromId = new HashMap<>();
 
         for (String[] triple : triples)
         {
-            // for clarity:
-            String subject = triple[0];
-            String predicate = triple[1];
-            String object = triple[2];
-
-            // get the edge list for this node (subject)
-            List<String[]> edges = edgesFromId.get(subject);
-            if (edges == null)
-            {
-                edges = new ArrayList<>();
-                edgesFromId.put(subject, edges);
-            }
-
-            // add the edge represented by this triple
-            edges.add(new String[]{predicate, object});
+            addTriple(triple);
         }
+    }
+
+    /**
+     * Add the given triple to this graph
+     */
+    public void addTriple(String[] triple)
+    {
+        // for clarity:
+        String subject = triple[0];
+        String predicate = triple[1];
+        String object = triple[2];
+
+        // get the edge list for this node (subject)
+        List<String[]> edges = m_edgesFromId.get(subject);
+        if (edges == null)
+        {
+            edges = new ArrayList<>();
+            m_edgesFromId.put(subject, edges);
+        }
+
+        // check for duplicates
+        for (String[] edge : edges)
+        {
+            if (edge[0].equals(predicate) && edge[1].equals(object)) // We already have this edge!
+                return;
+        }
+
+        // add the edge represented by this triple
+        edges.add(new String[]{predicate, object});
+
+        // keep track of the highest BNode id
+        if (subject.startsWith(JsonldSerializer.BLANKNODE_PREFIX))
+        {
+            // There may be BNode ids that are formatted in other ways and may fail the below check. But Such IDs are
+            // of no consequence as they will for the same reason not interfere with new BNode IDs we assign later on.
+            try
+            {
+                int bnodeNumber = Integer.parseInt(subject.substring( JsonldSerializer.BLANKNODE_PREFIX.length() ));
+                if (bnodeNumber >= m_nextBNodeIDNumber)
+                    m_nextBNodeIDNumber = bnodeNumber + 1;
+            }
+            catch (Exception e) { /* ignore */ }
+        }
+    }
+
+    public void enrichWith(Graph otherGraph)
+    {
+        Map<String, String> bNodeMapping = generateBNodeMapFrom(otherGraph);
+
+        for (String subject : otherGraph.m_edgesFromId.keySet())
+        {
+            List<String[]> otherEdges = otherGraph.m_edgesFromId.get(subject);
+            subject = getTranslatedNodeId(subject, bNodeMapping);
+            for (String[] otherEdge: otherEdges)
+            {
+                String predicate = otherEdge[0];
+                String object = getTranslatedNodeId(otherEdge[1], bNodeMapping);
+
+                addTriple(new String[]{subject, predicate, object});
+            }
+        }
+    }
+
+    /**
+     * Get all triples in this graph
+     */
+    public List<String[]> getTriples()
+    {
+        List<String[]> triples = new ArrayList<>();
+
+        for (String subject : m_edgesFromId.keySet())
+        {
+            List<String[]> edges = m_edgesFromId.get(subject);
+            for (String[] edge: edges)
+            {
+                String predicate = edge[0];
+                String object = edge[1];
+                triples.add(new String[]{subject, predicate, object});
+            }
+        }
+
+        return triples;
+    }
+
+    public String toString()
+    {
+        StringBuilder result = new StringBuilder();
+        for ( String subject : m_edgesFromId.keySet() )
+        {
+            List<String[]> edges = m_edgesFromId.get(subject);
+            if (edges == null)
+                continue;
+
+            for (String[] edge : edges)
+            {
+                result.append(subject);
+                result.append(" [");
+                result.append(edge[0]);
+                result.append(" ->] ");
+                result.append(edge[1]);
+                result.append("\n");
+            }
+        }
+
+        return result.toString();
+    }
+
+
+    /**
+     * Given a node id in the graph from which bNodeMapping is derived, get the corresponding node id in this graph
+     * (which is usually the same unless it is a blank node id).
+     */
+    private String getTranslatedNodeId(String naiveNodeId, Map<String, String> bNodeMapping)
+    {
+        if (naiveNodeId.startsWith(JsonldSerializer.BLANKNODE_PREFIX))
+        {
+            if (bNodeMapping.keySet().contains(naiveNodeId))
+                return bNodeMapping.get(naiveNodeId);
+
+            // A blank node without mapping? Give it a new blank node id in this graph
+            // and add the mapping, so the linked web remains intact
+            String newBNodeId = JsonldSerializer.BLANKNODE_PREFIX + m_nextBNodeIDNumber++;
+            bNodeMapping.put(naiveNodeId, newBNodeId);
+            return newBNodeId;
+        }
+        return naiveNodeId;
     }
 
     /**
      * Generates a map where BNode ids in otherGraph are mapped to BNode ids in this map (where the mapping can be
      * done with surety)
      */
-    public Map<String, String> generateBNodeMapFrom(Graph otherGraph)
+    private Map<String, String> generateBNodeMapFrom(Graph otherGraph)
     {
         Map<String, String> BNodeMap = new HashMap<>();
-        for (String otherSubject : otherGraph.edgesFromId.keySet())
+        for (String otherSubject : otherGraph.m_edgesFromId.keySet())
         {
             if (otherSubject.startsWith(JsonldSerializer.BLANKNODE_PREFIX))
             {
-                for (String subject : edgesFromId.keySet())
+                for (String subject : m_edgesFromId.keySet())
                 {
                     if (subject.startsWith(JsonldSerializer.BLANKNODE_PREFIX))
                     {
@@ -79,10 +196,10 @@ public class Graph
         otherCycleGuard.put(otherSubject, subject);
 
         // Normal recursive base cases
-        List<String[]> otherEdges = otherGraph.edgesFromId.get(otherSubject);
+        List<String[]> otherEdges = otherGraph.m_edgesFromId.get(otherSubject);
         if (otherEdges == null)
             return true; // nothing more to check
-        List<String[]> edges = edgesFromId.get(subject);
+        List<String[]> edges = m_edgesFromId.get(subject);
         if (edges == null)
             return false; // no more edges in (possibly) containing graph to compare with
 
@@ -110,28 +227,5 @@ public class Graph
                 return false;
         }
         return true;
-    }
-
-    public String toString()
-    {
-        StringBuilder result = new StringBuilder();
-        for ( String subject : edgesFromId.keySet() )
-        {
-            List<String[]> edges = edgesFromId.get(subject);
-            if (edges == null)
-                continue;
-
-            for (String[] edge : edges)
-            {
-                result.append(subject);
-                result.append(" [");
-                result.append(edge[0]);
-                result.append(" ->] ");
-                result.append(edge[1]);
-                result.append("\n");
-            }
-        }
-
-        return result.toString();
     }
 }
