21d8626ae17b077848460d3910388d2850582635
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2016-05-20 15:39:38 +0200

    More tests.

--- src/test/java/whelk/importer/EnrichTest.java
+++ src/test/java/whelk/importer/EnrichTest.java
@@ -33,15 +33,74 @@ public class EnrichTest
         Map enrichedData = JsonldSerializer.serialize(originalGraph.getTriples(), new HashSet<>());
         JsonldSerializer.normalize(enrichedData, MAIN_ID);
 
-        if (!enrichedData.equals(expectedResultData))
+        if ( ! rdfEquals( enrichedData, expectedResultData ) )
         {
             System.out.println("not equal:");
-            System.out.println(enrichedData);
-            System.out.println(expectedResultData);
+            System.out.println("enriched result:\n"+enrichedData);
+            System.out.println("expected result:\n"+expectedResultData);
             Assert.assertTrue( false );
         }
     }
 
+    private boolean rdfEquals(Map m1, Map m2)
+    {
+        if (m1.size() != m2.size())
+            return false;
+
+        for (Object key : m1.keySet())
+        {
+            Object value = m1.get(key);
+
+            if (!m2.containsKey(key))
+                return false;
+
+            if (value instanceof Map)
+            {
+                if ( ! rdfEquals( (Map) value, (Map) m2.get(key)) )
+                    return false;
+            }
+            else if (value instanceof List)
+            {
+                if ( ! rdfEquals( (List) value, (List) m2.get(key)) )
+                    return false;
+            }
+            else if ( ! value.equals(m2.get(key)) )
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean rdfEquals(List l1, List l2)
+    {
+        if (l1.size() != l2.size())
+            return false;
+
+        Set s1 = new HashSet(l1);
+        Set s2 = new HashSet(l2);
+
+        for (Object entry1 : s1)
+        {
+            boolean existsAMatch = false;
+            for (Object entry2 : s2)
+            {
+                if (entry1 instanceof Map && entry2 instanceof Map)
+                {
+                    if ( rdfEquals( (Map) entry1, (Map) entry2) )
+                        existsAMatch = true;
+                }
+                else if (entry1 instanceof List && entry2 instanceof List)
+                {
+                    if ( rdfEquals( (List) entry1, (List) entry2) )
+                        existsAMatch = true;
+                }
+            }
+            if (!existsAMatch)
+                return false;
+        }
+
+        return true;
+    }
 
 
     @Test
@@ -128,6 +187,52 @@ public class EnrichTest
                 "}\n";
 
         testEnrich(original, incoming, incoming);
+    }
+
+    @Test
+    public void extraInfoNestedBNode() throws Exception
+    {
+        // Adding an extra key value pair in a BNode, containing another nested BNode that must be matched
+
+        String original = "{\n" +
+                "    \"@graph\": [\n" +
+                "        {\n" +
+                "            \"some subobject\": {\n" +
+                "                \"some literal\": \"some value\"\n" +
+                "            }\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"this bnode\": \"should not match\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}\n";
+
+        String incoming = "{\n" +
+                "    \"@graph\": [\n" +
+                "        {\n" +
+                "            \"other literal\": \"other value\",\n" +
+                "            \"some subobject\": {\n" +
+                "                \"some literal\": \"some value\"\n" +
+                "            }\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}\n";
+
+        String expectedResult = "{\n" +
+                "    \"@graph\": [\n" +
+                "        {\n" +
+                "            \"other literal\": \"other value\",\n" +
+                "            \"some subobject\": {\n" +
+                "                \"some literal\": \"some value\"\n" +
+                "            }\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"this bnode\": \"should not match\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}\n";
+
+        testEnrich(original, incoming, expectedResult);
     }
 
     @Test
