b1cba161a4f2c2daf8b3f828159a95198083e714
Markus Sköld <markus.skold@kb.se>
2016-06-02 13:50:02 +0200

    use jsonld2marcxml-converter directly

--- rest/src/main/groovy/whelk/rest/api/ConverterAPI.groovy
+++ rest/src/main/groovy/whelk/rest/api/ConverterAPI.groovy
@@ -6,37 +6,23 @@ import org.apache.http.entity.ContentType
 import whelk.Whelk
 import whelk.Document
 import whelk.util.Tools
-import whelk.converter.marc.MarcFrameConverter
+import whelk.converter.marc.JsonLD2MarcXMLConverter
 import whelk.util.PropertyLoader
-
-import org.picocontainer.Characteristics
-import org.picocontainer.PicoContainer
 
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-/**
- * Created by markus on 2016-06-02.
- */
 @Log
 class ConverterAPI extends HttpServlet {
 
-    PicoContainer pico
-    MarcFrameConverter marcFrameConverter
+    JsonLD2MarcXMLConverter marcConverter
+
 
     public ConverterAPI() {
         log.info("Starting converterAPI ...")
 
-        Properties props = PropertyLoader.loadProperties("secret")
-
-        pico = Whelk.getPreparedComponentsContainer(props)
-
-        pico.addComponent(new MarcFrameConverter())
-
-        pico.start()
-
-        marcFrameConverter = pico.getComponent(MarcFrameConverter.class)
+        marcConverter = new JsonLD2MarcXMLConverter()
 
         log.info("Started ...")
     }
@@ -48,10 +34,10 @@ class ConverterAPI extends HttpServlet {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "No content received.")
         } else {
             String ctype = ContentType.parse(request.getContentType()).getMimeType()
-            Document doc = new Document([(Document.NON_JSON_CONTENT_KEY): Tools.normalizeString(request.getInputStream().getText("UTF-8"))], [(Document.CONTENT_TYPE_KEY): ctype])
+            Document doc = new Document("nodoc", [(Document.NON_JSON_CONTENT_KEY): Tools.normalizeString(request.getInputStream().getText("UTF-8"))], [(Document.CONTENT_TYPE_KEY): ctype])
             if (!requestedContentType || requestedContentType == "application/x-marcxml") {
                 log.info("Constructed document. Converting to $requestedContentType")
-                doc = marcFrameConverter.convert(doc)
+                doc = marcConverter.convert(doc)
             } else if (requestedContentType) {
                 log.info("Can not convert to $requestedContentType.")
             } else {
