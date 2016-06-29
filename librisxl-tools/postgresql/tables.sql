4a51ee80542439a41289d7d423c65bcaf2f74114
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2016-06-29 13:35:34 +0200

    Manifest column removed. Necessary components moved out to separate columns.

--- librisxl-tools/postgresql/tables.sql
+++ librisxl-tools/postgresql/tables.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS lddb (
     id text not null unique primary key,
     data jsonb not null,
-    manifest jsonb not null,
     quoted jsonb,
+    collection text not null,
+    changedIn text not null,
+    changedBy text,
+    checksum text not null,
     created timestamp with time zone not null default now(),
     modified timestamp with time zone not null default now(),
     deleted boolean default false
@@ -17,9 +20,11 @@ CREATE TABLE IF NOT EXISTS lddb__identifiers (
 CREATE TABLE IF NOT EXISTS lddb__versions (
     pk serial,
     id text not null,
-    checksum char(32) not null,
     data jsonb not null,
-    manifest jsonb not null,
+    collection text not null,
+    changedIn text not null,
+    changedBy text,
+    checksum text not null,
     modified timestamp with time zone not null default now(),
     unique (id, checksum, modified)
     );
