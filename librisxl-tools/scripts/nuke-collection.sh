4f2524726a1177433437534ace1e1c6873e5c850
Niklas Lindström <lindstream@gmail.com>
2016-08-16 15:04:04 +0200

    Adapt nuke-collection.sh to lddb changes

--- librisxl-tools/scripts/nuke-collection.sh
+++ librisxl-tools/scripts/nuke-collection.sh
@@ -3,5 +3,5 @@ WHELK_ID=$1
 COLLECTION_ID=$2
 [[ "$COLLECTION_ID" == '' ]] && echo "USAGE $0 WHELK_ID COLLECTION_ID" && exit 1
 set -x
-psql $WHELK_ID -c "delete from lddb where manifest->>'collection' = '$COLLECTION_ID';"
+psql $WHELK_ID -c "delete from lddb where collection = '$COLLECTION_ID';"
 curl -XDELETE "http://localhost:9200/$WHELK_ID/$COLLECTION_ID/_query" -d '{"query":{"match_all": {}}}'
