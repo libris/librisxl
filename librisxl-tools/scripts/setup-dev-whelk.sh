c148d07ac6232b23e0ca72bd0c0e9e260a51dac2
Matthias Nilsson <matthias.nilsson@kb.se>
2016-08-26 09:59:43 +0200

    Update whelk setup script
    * Make sure the different steps work
    * Make the db setup work on stricter systems (i.e. use sudo when necessary)
    * Add command line arguments
    * Set up/use virtualenvs for the Python parts

--- librisxl-tools/scripts/setup-dev-whelk.sh
+++ librisxl-tools/scripts/setup-dev-whelk.sh
@@ -2,53 +2,177 @@
 TOOLDIR=$(dirname $(dirname $0))
 WORKDIR=$(pwd)/work
 
-WHELKNAME=$1
+usage () {
+    cat <<EOF
+Usage: ./setup-dev-whelk.sh -n <NAME> -O <OAIPMH CREDENTIALS> [-D <DATABASE USER>] [-C <CREATEDB USER>]
 
-if [ "$WHELKNAME" == "" ]; then
-    echo "Provide a whelk name (to be used for the local psql db AND elasticsearch index)"
+    -n, --whelk-name
+        Instance name. Used as database name and ElasticSearch index.
+
+    -O, --oaipmh-credentials
+        Credentials for OAIPMH. On the form "<USER>:<PASSWORD>".
+
+    -D, --db-user
+        PostgreSQL database user.
+
+        If unset, no username will be explicitly supplied to PostgreSQL.
+
+    -C, --createdb-user
+        User to run 'createdb' as ('sudo -u <CREATEDB USER> createdb ...').
+
+        If unset, runs 'createdb' without 'sudo'.
+
+    -F, --force-rebuild
+        If set, rebuild definitions, OAIPMH files, and import JAR.
+EOF
+}
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -n|--whelk-name)
+            WHELKNAME="$2"
+            shift
+            ;;
+        -O|--oaipmh-credentials)
+            OAIPMH_CREDENTIALS="$2"
+            shift
+            ;;
+        -D|--db-user)
+            DBUSER="$2"
+            shift
+            ;;
+        -C|--createdb-user)
+            CREATEDB_USER="$2"
+            shift
+            ;;
+        -F|--force-rebuild)
+            FORCE_REBUILD=true
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+
+# Verify that mandatory arguments have been supplied
+if [ -z "$WHELKNAME" ]; then
+    echo "ERROR: Whelk name not specified!"
+    echo ""
+    usage
     exit 1
 fi
 
-set -x
+if [ -z "$OAIPMH_CREDENTIALS" ]; then
+    echo "ERROR: OAIPMH credentials not specified!"
+    echo ""
+    usage
+    exit 1
+fi
 
-# Drop and create PostgreSQL DB
-dropdb $WHELKNAME
-createdb $WHELKNAME
-psql $WHELKNAME < $TOOLDIR/postgresql/tables.sql
-psql $WHELKNAME < $TOOLDIR/postgresql/indexes.sql
 
-# Drop and create ES index
+# Set up PostgreSQL database
+echo "Setting up PostgreSQL..."
+echo ""
+
+if [ -z "$CREATEDB_USER" ]; then
+    dropdb $WHELKNAME
+    createdb $WHELKNAME
+else
+    sudo -u $CREATEDB_USER dropdb $WHELKNAME
+    sudo -u $CREATEDB_USER createdb $WHELKNAME
+fi
+
+if [ -z "$DBUSER" ]; then
+    psql $WHELKNAME < $TOOLDIR/postgresql/tables.sql
+    psql $WHELKNAME < $TOOLDIR/postgresql/indexes.sql
+else
+    psql -U $DBUSER -h localhost $WHELKNAME < \
+         $TOOLDIR/postgresql/tables.sql
+    psql -U $DBUSER -h localhost $WHELKNAME < \
+         $TOOLDIR/postgresql/indexes.sql
+fi
+
+
+# Set up ElasticSearch
+echo ""
+echo "Setting up ElasticSearch..."
+echo ""
+
 curl -XDELETE http://localhost:9200/$WHELKNAME
-curl -XPOST http://localhost:9200/$WHELKNAME -d@$TOOLDIR/elasticsearch/libris_config.json
+curl -XPOST http://localhost:9200/$WHELKNAME \
+     -d@$TOOLDIR/elasticsearch/libris_config.json
+
 
 # Create OAI-PMH files from examples unless they exist
-if [ ! -f $WORKDIR/oaidump/bib/oaipmh ]; then
+if [ "$FORCE_REBUILD" = true ] || [ ! -f $WORKDIR/oaidump/bib/oaipmh ]; then
+    echo ""
+    echo "Setting up OAIPMH dump..."
+    echo ""
+
     mkdir -p $WORKDIR/oaidump
-    OAIPMH_PASS=work/oaipmh.txt
-    if [ ! -f $OAIPMH_PASS ]; then
-        echo "Missing $OAIPMH_PASS"
-        exit 1
+    if [ ! -d .venv ]; then
+        virtualenv .venv
+        source .venv/bin/activate
+        pip install -r librisxl-tools/scripts/requirements.txt
+    else
+	source .venv/bin/activate
     fi
-    python librisxl-tools/scripts/assemble_oaipmh_records.py $(cat $OAIPMH_PASS) librisxl-tools/scripts/example_records.tsv $WORKDIR/oaidump/
+    python librisxl-tools/scripts/assemble_oaipmh_records.py \
+           $OAIPMH_CREDENTIALS \
+           librisxl-tools/scripts/example_records.tsv $WORKDIR/oaidump/
+    deactivate
 fi
+
 
 # Create definitions dataset unless it exists
 DEFS_FILE=../definitions/build/definitions.jsonld.lines
-if [ ! -f $DEFS_FILE ]; then
+if [ "$FORCE_REBUILD" = true ] || [ ! -f $DEFS_FILE ]; then
+    echo ""
+    echo "Creating definitions dataset..."
+    echo ""
+
     pushd ../definitions
-        python datasets.py -l
+    if [ ! -d .venv ]; then
+        virtualenv .venv
+        source .venv/bin/activate
+        pip install -r requirements.txt
+    else
+        source .venv/bin/activate
+    fi
+    python datasets.py -l
+    deactivate
     popd
 fi
 
+
+# Import OAIPMH data
+echo ""
+echo "Importing OAIPMH data..."
+echo ""
+
 pushd importers
-    JAR=build/libs/importers.jar
-    if [ ! -f $JAR ]; then
-        gradle jar
-    fi
+JAR=build/libs/importers.jar
+if [ "$FORCE_REBUILD" = true ] || [ ! -f $JAR ]; then
+    gradle jar
+fi
 
-    java -jar $JAR defs ../$DEFS_FILE
+java -Dxl.secret.properties=../secret.properties -jar $JAR \
+     defs ../$DEFS_FILE
 
-    for source in auth bib hold; do
-        java -jar build/libs/importers.jar harvest file:///$WORKDIR/oaidump/$source/oaipmh
-    done
+for source in auth bib hold; do
+    java -Dxl.secret.properties=../secret.properties \
+         -jar build/libs/importers.jar \
+         harvest file:///$WORKDIR/oaidump/$source/oaipmh
+done
 popd
+
+
+echo ""
+echo "Done!"
+echo ""
