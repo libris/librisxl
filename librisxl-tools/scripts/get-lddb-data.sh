30e0f246599eeb298701bc2521287037f227aee7
Niklas Lindström <lindstream@gmail.com>
2016-08-31 14:20:48 +0200

    Cleanup get-lddb-data.sh and use Python to pretty-print JSON

--- librisxl-tools/scripts/get-lddb-data.sh
+++ librisxl-tools/scripts/get-lddb-data.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-usage() {
-    echo "usage $0 DB ID"
-}
-
 DB=$1
 ID=$2
-
-if [ -z "$DB" ]; then
-    usage
+if [ -z "$DB" ] || [ -z "$ID" ]; then
+    echo "usage: $0 DB ID"
     exit 1
 fi
-if [ -z "$ID" ]; then
-    usage
-    exit 1
-fi
-psql $DB -tc "select data from lddb where id in (select id from lddb__identifiers where identifier = '$ID');"
+psql $DB -tc "select data from lddb where id in (select id from lddb__identifiers where identifier = '$ID');" | python -mjson.tool
