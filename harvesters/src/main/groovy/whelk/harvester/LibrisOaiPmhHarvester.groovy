25163af78031977cde102790e90685dd0f54c810
Theodor Tolstoy <github.teddes@tolstoy.se>
2017-03-21 10:13:38 +0100

    Remove handling of suppressed records from importers

--- harvesters/src/main/groovy/whelk/harvester/LibrisOaiPmhHarvester.groovy
+++ harvesters/src/main/groovy/whelk/harvester/LibrisOaiPmhHarvester.groovy
@@ -12,24 +12,9 @@ import se.kb.libris.util.marc.io.*
 @Log
 class LibrisOaiPmhHarvester extends OaiPmhHarvester {
 
-    static String SUPPRESSED_RECORD = "SUPPRESSRECORD"
-
     LibrisOaiPmhHarvester(){}
 
     LibrisOaiPmhHarvester(Whelk w, MarcFrameConverter mfc) {
         super(w, mfc)
-    }
-
-    @Override
-    boolean okToSave(OaiPmhRecord oaiPmhRecord) {
-        if (oaiPmhRecord.record) {
-            MarcRecord marcRecord = MarcXmlRecordReader.fromXml(oaiPmhRecord.record)
-            def aList = marcRecord.getDatafields("599").collect { it.getSubfields("a").data }.flatten()
-            if (aList.contains(SUPPRESSED_RECORD)) {
-                log.debug("Record ${oaiPmhRecord.identifier} is suppressed.")
-                return false
-            }
-        }
-        return true
     }
 }
