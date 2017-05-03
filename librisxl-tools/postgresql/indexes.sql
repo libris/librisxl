6d9cd15b7e249fd25d4a888c19da12e55ba77a2e
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2017-05-03 15:15:01 +0200

    Readjust index path, after format changes.

--- librisxl-tools/postgresql/indexes.sql
+++ librisxl-tools/postgresql/indexes.sql
@@ -2,7 +2,7 @@ CREATE INDEX idx_lddb_alive ON lddb (id) WHERE deleted IS NOT true;
 CREATE INDEX idx_lddb_modified ON lddb (modified);
 
 CREATE INDEX idx_lddb_graph ON lddb USING GIN ((data->'@graph') jsonb_path_ops);
-CREATE INDEX idx_lddb_holding_for on lddb ((data#>>'{@graph,1,holdingFor,@id}'));
+CREATE INDEX idx_lddb_holding_for on lddb ((data#>>'{@graph,1,itemOf,@id}'));
 CREATE INDEX idx_lddb_systemnumber on lddb using gin ((data#>'{@graph,0,systemNumber}'));
 CREATE INDEX idx_lddb_thing_identifiers on lddb using gin ((data#>'{@graph,1,identifier}'));
 
