8881bad66a66665c7bb953147f93fb49dee87734
Matthias Nilsson <matthias.nilsson@kb.se>
2017-06-09 14:37:55 +0200

    Use cache when reindexing

--- importers/src/main/groovy/whelk/reindexer/ElasticReindexer.groovy
+++ importers/src/main/groovy/whelk/reindexer/ElasticReindexer.groovy
@@ -18,6 +18,7 @@ class ElasticReindexer {
     }
 
     void reindex(String suppliedCollection) {
+        boolean useDocumentCache = true
         int counter = 0
         long startTime = System.currentTimeMillis()
         List<String> collections = suppliedCollection ? [suppliedCollection] : whelk.storage.loadCollections()
@@ -30,13 +31,13 @@ class ElasticReindexer {
                 if (counter % BATCH_SIZE == 0) {
                     long indexTime = System.currentTimeMillis()
                     print("Elapsed time: ${(System.currentTimeMillis() - startTime) / 1000} seconds. Loaded $counter documents. Bulk indexing ${documents.size()} documents ...")
-                    whelk.elastic.bulkIndex(documents, collection, whelk)
+                    whelk.elastic.bulkIndex(documents, collection, whelk, useDocumentCache)
                     println(" In ${(System.currentTimeMillis() - indexTime)} milliseconds.")
                     documents = []
                 }
             }
             if (documents.size() > 0) {
-                whelk.elastic.bulkIndex(documents, collection, whelk)
+                whelk.elastic.bulkIndex(documents, collection, whelk, useDocumentCache)
             }
         }
         println("Done! $counter documents reindexed in ${(System.currentTimeMillis() - startTime) / 1000} seconds.")
