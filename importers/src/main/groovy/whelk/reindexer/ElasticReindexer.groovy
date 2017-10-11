0e25d877ea9028a9389e8d0371055e00bce393a0
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2017-10-11 13:54:26 +0200

    Revert "Perform elasticsearch indexing in parallell."
    This reverts commit 7cb66e867ff0c0abf94f5c8920d57fe71f653eb1.

--- importers/src/main/groovy/whelk/reindexer/ElasticReindexer.groovy
+++ importers/src/main/groovy/whelk/reindexer/ElasticReindexer.groovy
@@ -3,7 +3,6 @@ package whelk.reindexer
 import groovy.util.logging.Log4j2 as Log
 import whelk.Document
 import whelk.Whelk
-import whelk.util.ThreadPool
 import whelk.util.Tools
 
 /**
@@ -15,19 +14,16 @@ class ElasticReindexer {
     static final int BATCH_SIZE = 5000
     Whelk whelk
 
-    final static boolean useDocumentCache = true
-    long startTime
-
     ElasticReindexer(Whelk w) {
         this.whelk = w
         this.whelk.loadCoreData()
     }
 
-    synchronized void reindex(String suppliedCollection) {
+    void reindex(String suppliedCollection) {
+        boolean useDocumentCache = true
         int counter = 0
-        startTime = System.currentTimeMillis()
+        long startTime = System.currentTimeMillis()
         List<String> collections = suppliedCollection ? [suppliedCollection] : whelk.storage.loadCollections()
-        ThreadPool threadPool = new ThreadPool(Runtime.getRuntime().availableProcessors())
         collections.each { collection ->
             List<Document> documents = []
             for (document in whelk.storage.loadAll(collection)) {
@@ -35,8 +31,10 @@ class ElasticReindexer {
                 Tools.printSpinner("Elapsed time: ${(System.currentTimeMillis() - startTime) / 1000} seconds. Loaded $counter documents.", counter)
                 counter++
                 if (counter % BATCH_SIZE == 0) {
+                    long indexTime = System.currentTimeMillis()
                     print("Elapsed time: ${(System.currentTimeMillis() - startTime) / 1000} seconds. Loaded $counter documents. Bulk indexing ${documents.size()} documents ...")
-                    threadPool.executeOnThread(new Batch(documents, collection), new BatchHandler())
+                    whelk.elastic.bulkIndex(documents, collection, whelk, useDocumentCache)
+                    println(" In ${(System.currentTimeMillis() - indexTime)} milliseconds.")
                     documents = []
                 }
             }
@@ -48,24 +46,6 @@ class ElasticReindexer {
                      "stale cache reads: ${whelk.cacheStaleCount()} " +
                      "(after ${collection})")
         }
-        threadPool.joinAll()
         println("Done! $counter documents reindexed in ${(System.currentTimeMillis() - startTime) / 1000} seconds.")
-    }
-
-    private class Batch {
-        Batch(List<Document> documents, String collection) {
-            this.documents = documents
-            this.collection = collection
-        }
-        List<Document> documents
-        String collection
-    }
-
-    private class BatchHandler implements ThreadPool.Worker<Batch> {
-        void doWork(Batch batch, int threadIndex) {
-            long indexTime = System.currentTimeMillis()
-            whelk.elastic.bulkIndex(batch.documents, batch.collection, whelk, useDocumentCache)
-            println(" In ${(System.currentTimeMillis() - indexTime)} milliseconds.")
-        }
     }
 }
