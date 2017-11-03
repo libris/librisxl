6715b6c9fbfa641f946371ccce7f06c78b8913d4
Niklas Lindström <lindstream@gmail.com>
2017-11-03 13:27:00 +0100

    Extract static utility specs
    from MarcFrameConverterSpec to MarcFrameConverterUtilsSpec.

--- src/test/groovy/MarcFrameConverterSpec.groovy
+++ src/test/groovy/MarcFrameConverterSpec.groovy
@@ -71,16 +71,6 @@ class MarcFrameConverterSpec extends Specification {
         }
     }
 
-    def "should extract #token from #uri"() {
-        expect:
-        MarcSimpleFieldHandler.extractToken(tplt, uri) == token
-        where:
-        tplt            | uri               | token
-        "/item/{_}"     | "/item/thing"     | "thing"
-        "/item/{_}/eng" | "/item/thing/eng" | "thing"
-        "/item/{_}/swe" | "/item/thing/eng" | null
-    }
-
     def "should detect marc #type type in leader value #marc.leader"() {
         expect:
         converter.conversion.getMarcCategory(marc.leader) == type
@@ -89,27 +79,6 @@ class MarcFrameConverterSpec extends Specification {
         [leader: "01113cz  a2200421n  4500"]    | "auth"
         [leader: "00887cam a2200277 a 4500"]    | "bib"
         [leader: "00187nx  a22000971n44500"]    | "hold"
-    }
-
-    def "should parse formatted #date"() {
-        expect:
-        MarcSimpleFieldHandler.parseDate(date)
-        where:
-        date << ['2014-06-10T12:05:05.0+02:00', '2014-06-10T12:05:05.0+0200']
-    }
-
-    def "should treat arrays as sets of objects"() {
-        given:
-        def obj = [:]
-        def prop = "label"
-        when:
-        2.times {
-            BaseMarcFieldHandler.addValue(obj, prop, value, true)
-        }
-        then:
-        obj[prop] == [value]
-        where:
-        value << ["Text", ["@id": "/link"]]
     }
 
     def "should convert field spec for #fieldSpec.marcType #fieldSpec.tag (#fieldSpec.name) [#fieldSpec.i]"() {
@@ -339,33 +308,6 @@ class MarcFrameConverterSpec extends Specification {
 
         then:
         resultWithoutDollars != null
-    }
-
-    def "should get as list"() {
-        expect:
-        Util.asList('1') == ['1']
-        Util.asList(['1']) == ['1']
-        Util.asList('') == ['']
-        Util.asList(null) == []
-    }
-
-    def "should get by path"() {
-        expect:
-        Util.getAllByPath(entity, path) == values
-        where:
-        entity                          | path              | values
-        [key: '1']                      | 'key'             | ['1']
-        [key: ['1', '2']]               | 'key'             | ['1', '2']
-        [item: [[key: '1']]]            | 'item.key'        | ['1']
-        [item: [[key: '1'],
-                [key: '2']]]            | 'item.key'        | ['1', '2']
-        [part: [[item: [[key: '1']]],
-                [item: [key: '2']]]]    | 'part.item.key'   | ['1', '2']
-    }
-
-    def "should process includes"() {
-        expect:
-        MarcRuleSet.processInclude([patterns: [a: [a:1]]], [include: 'a', b:2]) == [a:1, b:2]
     }
 
     void assertJsonEquals(result, expected) {
