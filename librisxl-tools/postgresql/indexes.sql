d93259dffbf4f8d09e80097cf2ad1a43aafe78f2
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2017-11-06 13:38:59 +0100

    Fixed indices for typed ID searches.

--- librisxl-tools/postgresql/indexes.sql
+++ librisxl-tools/postgresql/indexes.sql
@@ -7,7 +7,8 @@ CREATE INDEX idx_lddb_graph ON lddb USING GIN ((data->'@graph') jsonb_path_ops);
 CREATE INDEX idx_lddb_holding_for on lddb ((data#>>'{@graph,1,itemOf,@id}'));
 CREATE INDEX idx_lddb_held_by on lddb ((data#>>'{@graph,1,heldBy,@id}'));
 CREATE INDEX idx_lddb_systemnumber on lddb using gin ((data#>'{@graph,0,systemNumber}'));
-CREATE INDEX idx_lddb_thing_identifiers on lddb using gin ((data#>'{@graph,1,identifier}'));
+CREATE INDEX idx_lddb_thing_identifiers on lddb using gin ((data#>'{@graph,1,identifiedBy}'));
+CREATE INDEX idx_lddb_record_identifiers on lddb using gin ((data#>'{@graph,0,identifiedBy}'));
 
 CREATE INDEX idx_lddb__identifiers_id ON lddb__identifiers (id);
 CREATE INDEX idx_lddb__identifiers_iri ON lddb__identifiers (iri);
