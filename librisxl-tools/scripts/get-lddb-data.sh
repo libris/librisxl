4cc833396eec303d1c6d3960971224a143dc112d
Niklas Lindström <lindstream@gmail.com>
2017-11-22 17:21:16 +0100

    Fix get-lddb-data.sh

--- librisxl-tools/scripts/get-lddb-data.sh
+++ librisxl-tools/scripts/get-lddb-data.sh
@@ -5,4 +5,4 @@ if [ -z "$DB" ] || [ -z "$ID" ]; then
     echo "usage: $0 DB ID"
     exit 1
 fi
-psql $DB -tc "select data from lddb where id in (select id from lddb__identifiers where identifier = '$ID');" | python -mjson.tool
+psql $DB -tc "select data from lddb where id in (select id from lddb__identifiers where iri = '$ID');" | python -mjson.tool
