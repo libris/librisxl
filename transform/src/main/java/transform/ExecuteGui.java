a4f4274eebd8a10a5a5e097126715c27d28510bd
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2018-01-10 13:22:51 +0100

    Some layout changes.

--- transform/src/main/java/transform/ExecuteGui.java
+++ transform/src/main/java/transform/ExecuteGui.java
@@ -15,16 +15,21 @@ public class ExecuteGui extends JFrame
         JButton b1 = new JButton("Load script");
         this.getContentPane().add( makeLeftAligned(b1) );
 
-        JComponent jc = makeLeftAligned(getTextArea("SELECT * from lddb", 5, 40, false));
-        jc.setPreferredSize(new Dimension(30, 0));
+        JComponent jc = makeLeftAligned(getTextArea("SELECT * from lddb", 4, 40, false, true));
         this.getContentPane().add( makeLeftAligned(new JLabel("Run on:")) );
         this.getContentPane().add( jc );
+
+        JComponent scriptArea = makeLeftAligned(getTextArea("# SCRIPT GOES HERE", 20, 40, true, true));
+        JComponent editPanel = makeLeftAligned(new JPanel());
+        editPanel.setLayout(new BorderLayout());
+        editPanel.add( makeLeftAligned(new JLabel("Transformation Script:")), BorderLayout.NORTH );
+        editPanel.add( scriptArea, BorderLayout.CENTER );
+        this.getContentPane().add( editPanel );
 
         JPanel jsonDisplay = new JPanel();
         jsonDisplay.setLayout(new GridLayout(1, 2));
 
         JPanel buttonPanel = new JPanel();
-        //buttonPanel.setLayout();
         this.getContentPane().add(makeLeftAligned(buttonPanel));
         buttonPanel.add(new JButton("Next"));
         buttonPanel.add(new JButton("Run"));
@@ -32,12 +37,12 @@ public class ExecuteGui extends JFrame
         JPanel before = new JPanel();
         before.setLayout(new BorderLayout(10, 10));
         before.add( new JLabel("Before execution:"), BorderLayout.NORTH );
-        before.add( getTextArea("TEMP 2                     sdfnksdjngggggggggggggggggggggggggggggggggggggggg", 40, 40, true), BorderLayout.CENTER );
+        before.add( getTextArea("", 20, 40, true, false), BorderLayout.CENTER );
 
         JPanel after = new JPanel();
         after.setLayout(new BorderLayout(10, 10));
         after.add( new JLabel("After execution:"), BorderLayout.NORTH );
-        after.add( getTextArea("TEMP 1", 40, 40, true), BorderLayout.CENTER );
+        after.add( getTextArea("", 20, 40, true, false), BorderLayout.CENTER );
 
         jsonDisplay.add( makeLeftAligned(before), 0 );
         jsonDisplay.add( makeLeftAligned(after), 1 );
@@ -48,12 +53,11 @@ public class ExecuteGui extends JFrame
         this.setVisible(true);
     }
 
-    private JComponent getTextArea(String s, int lines, int columns, boolean scrolls)
+    private JComponent getTextArea(String s, int lines, int columns, boolean scrolls, boolean editable)
     {
-        if (s == null)
-            s = "";
         JTextArea area = new JTextArea(s, lines, columns);
         area.setLineWrap(true);
+        area.setEditable(editable);
         if (scrolls)
         {
             JScrollPane sp = new JScrollPane(area);
