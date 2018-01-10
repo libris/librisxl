2f4dce7f8919fd526e294cf255a1ba0d0538fdbc
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2018-01-10 15:14:12 +0100

    Add Open/Save etc menu items.

--- transform/src/main/java/transform/ExecuteGui.java
+++ transform/src/main/java/transform/ExecuteGui.java
@@ -2,18 +2,43 @@ package transform;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.*;
+import java.util.Properties;
 
 public class ExecuteGui extends JFrame
 {
     public ExecuteGui()
     {
+        ActionResponse actionResponse = new ActionResponse(this);
+
         this.setTitle("Libris XL data transform");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         BoxLayout boxLayout = new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS);
         this.getContentPane().setLayout(boxLayout);
 
-        JButton b1 = new JButton("Load script");
-        this.getContentPane().add( makeLeftAligned(b1) );
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        this.setJMenuBar(menuBar);
+
+        JMenuItem loadItem = new JMenuItem("Open");
+        loadItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.ALT_MASK));
+        loadItem.addActionListener(actionResponse);
+        fileMenu.add(loadItem);
+        JMenuItem saveItem = new JMenuItem("Save");
+        saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.ALT_MASK));
+        saveItem.addActionListener(actionResponse);
+        fileMenu.add(saveItem);
+        JMenuItem saveAsItem = new JMenuItem("SaveAs");
+        saveAsItem.addActionListener(actionResponse);
+        fileMenu.add(saveAsItem);
+        JMenuItem loadEnvironment = new JMenuItem("LoadEnvironmentFile");
+        loadEnvironment.addActionListener(actionResponse);
+        fileMenu.add(loadEnvironment);
 
         JComponent jc = makeLeftAligned(getTextArea("SELECT * from lddb", 4, 40, false, true));
         this.getContentPane().add( makeLeftAligned(new JLabel("Run on:")) );
@@ -70,5 +95,96 @@ public class ExecuteGui extends JFrame
     {
         c.setAlignmentX(Component.LEFT_ALIGNMENT);
         return c;
+    }
+
+    private class ActionResponse implements ActionListener
+    {
+        private Component m_parent;
+        private JFileChooser m_fileChooser = new JFileChooser();
+        private File m_currentFile = null;
+        private Properties m_envProps = null;
+
+        public ActionResponse(Component parent)
+        {
+            m_parent = parent;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent ae)
+        {
+            int result;
+
+            switch(ae.getActionCommand())
+            {
+                case "Open":
+                    result = m_fileChooser.showOpenDialog(m_parent);
+                    if (result == JFileChooser.APPROVE_OPTION)
+                    {
+                        m_currentFile = m_fileChooser.getSelectedFile();
+                        load();
+                    }
+                    break;
+                case "Save":
+                    if (m_currentFile == null)
+                    {
+                        result = m_fileChooser.showOpenDialog(m_parent);
+                        if (result == JFileChooser.APPROVE_OPTION)
+                        {
+                            m_currentFile = m_fileChooser.getSelectedFile();
+                        }
+                    }
+                    if (m_currentFile != null)
+                        save();
+                    break;
+                case "SaveAs":
+                    result = m_fileChooser.showOpenDialog(m_parent);
+                    if (result == JFileChooser.APPROVE_OPTION)
+                    {
+                        m_currentFile = m_fileChooser.getSelectedFile();
+                        save();
+                    }
+                    break;
+                case "LoadEnvironmentFile":
+                    result = m_fileChooser.showOpenDialog(m_parent);
+                    if (result == JFileChooser.APPROVE_OPTION)
+                    {
+                        File envFile = m_fileChooser.getSelectedFile();
+                        System.out.println("Load env-file from: " + envFile);
+                        try
+                        {
+                            InputStream propStream = new FileInputStream(envFile);
+                            m_envProps.load(propStream);
+                        } catch (IOException e){/* ignore */}
+                    }
+                    break;
+            }
+
+        }
+
+        private void load()
+        {
+            try
+            {
+                BufferedReader reader = new BufferedReader(new FileReader(m_currentFile));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ( (line = reader.readLine()) != null)
+                {
+                    sb.append(line);
+                    sb.append("\n");
+                }
+                System.out.println(sb.toString());
+            } catch (IOException ioe)
+            {
+                JOptionPane.showMessageDialog(m_parent, ioe.toString());
+            }
+
+            System.out.println("Load from: " + m_currentFile);
+        }
+
+        private void save()
+        {
+            System.out.println("Save to: " + m_currentFile);
+        }
     }
 }
