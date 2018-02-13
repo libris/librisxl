d1d229b501fb8ae086e924d77653f6be08373ebc
Niklas Lindström <lindstream@gmail.com>
2018-02-13 18:25:33 +0100

    Add script for fetching and building local definitions

---
+++ librisxl-tools/scripts/build-local-definitions.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+defsdir=$1
+shift
+buildargs=$@
+
+if [[ "$defsdir" == "" ]]; then
+    echo "Please provide your local definitions directory."
+    exit 1
+fi
+
+ensurevenv() {
+    if [[ ! -d .venv ]]; then
+        virtualenv .venv -p python2.7
+    fi
+}
+
+updatereqs() {
+    ensurevenv
+    .venv/bin/pip install -r requirements.txt
+}
+
+builddefs() {
+    ensurevenv
+    .venv/bin/python datasets.py $buildargs
+}
+
+if [[ ! -d "$defsdir" ]]; then
+    mkdir -p $(dirname $defsdir)
+    pushd $(dirname $defsdir)
+        git clone https://github.com/libris/definitions
+    popd
+    pushd $defsdir
+        updatereqs
+        builddefs
+    popd
+else
+    pushd $defsdir
+        local=$(git rev-parse @)
+        remote=$(git rev-parse "@{u}")
+        git fetch
+        if [[ $local != $remote ]]; then
+            git pull
+            if [[ requirements.txt -nt .venv ]]; then
+                touch -r requirements.txt .venv
+                updatereqs
+            fi
+            builddefs
+        fi
+    popd
+fi
