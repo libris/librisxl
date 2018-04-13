a7118441f2b10b1c36f073485697208df93311cc
Niklas Lindström <lindstream@gmail.com>
2018-04-13 15:15:56 +0200

    Use Whelk.embellish when calling revert in ConverterAPI

--- rest/src/main/groovy/whelk/rest/api/ConverterAPI.groovy
+++ rest/src/main/groovy/whelk/rest/api/ConverterAPI.groovy
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse
 @Log
 class ConverterAPI extends HttpServlet {
 
+    Whelk whelk
     MarcFrameConverter marcFrameConverter
 
     public ConverterAPI() {
@@ -30,7 +31,7 @@ class ConverterAPI extends HttpServlet {
     void init() {
         log.info("Starting converterAPI")
         if (!marcFrameConverter) {
-            Whelk whelk = Whelk.createLoadedCoreWhelk()
+            whelk = Whelk.createLoadedCoreWhelk()
             marcFrameConverter = whelk.createMarcFrameConverter()
         }
         log.info("Started ...")
@@ -67,7 +68,11 @@ class ConverterAPI extends HttpServlet {
             String jsonText = Tools.normalizeString(request.getInputStream().getText("UTF-8"))
             Map json = marcFrameConverter.mapper.readValue(jsonText, Map)
             log.info("Constructed document. Converting to $requestedContentType")
-            json = marcFrameConverter.runRevert(json)
+            Document doc = new Document(json)
+            if (whelk) {
+                whelk.embellish(doc)
+            }
+            json = marcFrameConverter.runRevert(doc.data)
             def framedText = marcFrameConverter.mapper.writeValueAsString(json)
             HttpTools.sendResponse(response, framedText, requestedContentType)
         }
