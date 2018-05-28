6bb51c310582ed726030b4ed6bd42be51ddab758
root <root@import.libris.kb.se>
2018-05-28 10:33:49 +0200

    localhost deploy script of batchimport.jar

---
+++ batchimport/batchimport_deploy_localhost.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+BATCHIMPORT=/appl/src/librisxl/batchimport
+GRADLE=/opt/gradle/bin/gradle
+DEPLOY=/appl/import/lib/
+
+bailout ()
+{
+	mailx -s "deploy batchimport failed" kai.poykio@kb.se <<-EOF
+	$1
+EOF
+	exit 1
+}
+
+
+if [ ! -f $GRADLE ]; then
+	bailout "fatal: install gradle."
+fi
+
+if [ -d $BATCHIMPORT ]; then
+	cd $BATCHIMPORT
+	git pull
+	if [ $? -ne 0 ]; then
+		bailout "fatal: git pull failed."
+	fi 
+	$GRADLE jar --refresh-dependencies
+	if [ $? -ne 0 ]; then
+		bailout "fatal: gradle build failed."
+	fi 
+	cp -f $BATCHIMPORT/build/libs/batchimport.jar $DEPLOY
+	if [ $? -ne 0 ]; then
+		bailout "fatal: cp batchimport.jar failed."
+	fi 
+else
+	bailout "fatal: clone repo"
+	# TODO:
+	# mkdir 
+	# git clone
+fi
