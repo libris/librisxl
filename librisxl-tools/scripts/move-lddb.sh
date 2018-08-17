1afc19b45bdd223540f603eb81b11509b679dbbd
Matthias Nilsson <matthias.nilsson@kb.se>
2018-08-17 14:13:11 +0200

    Add argument handling to import script

--- librisxl-tools/scripts/move-lddb.sh
+++ librisxl-tools/scripts/move-lddb.sh
@@ -1,27 +1,133 @@
 #!/bin/bash
-sourcehost=$1
-destenv=$2
-desthost=$3
+set -e
 
-if [ -z $sourcehost ] || [ -z $destenv ]; then
-    echo "USAGE: $0 SOURCEHOST DESTENV DESTHOST"
+TOOLDIR=$(dirname $(dirname $0))
+
+usage () {
+    cat <<EOF
+
+Usage: $0 -s <SOURCEHOST> -u <SOURCEUSER> -e <DESTENV> [-p <SOURCEPASSWORD>]
+           [-d <DESTHOST>] [-n <DESTUSER>] [-w <DESTPASSWORD>]
+           [-S <DESTDB>] [-D <DESTDB>]
+
+    -s, --sourcehost
+        PostgreSQL host to read from.
+
+    -u, --sourceuser
+        Source database username.
+
+    -p, --sourcepassword
+        Source database password.
+
+    -e, --destenv
+        Destination environement ({dev|qa|stg}).
+
+    -d, --desthost
+        PostgreSQL host to write to. Defaults to localhost.
+
+    -n, --destuser
+        Destination database username. Defaults to source database username.
+
+    -w, --destpassword
+        Destination database password. Defaults to source database password.
+
+    -S, --sourcedb
+        Source database name. Defaults to 'whelk'.
+
+    -D, --destdb
+        Destination database name. Default to 'whelk'.
+
+EOF
+}
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -s|--sourcehost)
+            SOURCEHOST="$2"
+            shift
+            ;;
+        -d|--desthost)
+            DESTHOST="$2"
+            shift
+            ;;
+        -e|--destenv)
+            DESTENV="$2"
+            shift
+            ;;
+        -u|--sourceuser)
+            SOURCEUSER="$2"
+            shift
+            ;;
+        -p|--sourcepassword)
+            SOURCEPASSWORD="$2"
+            shift
+            ;;
+        -n|--destuser)
+            DESTUSER="$2"
+            shift
+            ;;
+        -w|--destpassword)
+            DESTPASSWORD="$2"
+            shift
+            ;;
+        -S|--sourcedb)
+            SOURCEDB="$2"
+            shift
+            ;;
+        -D|--destdb)
+            DESTDB="$2"
+            shift
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+
+if [ -z $SOURCEHOST ] || [ -z $SOURCEUSER ] || [ -z $SOURCEPASSWORD ] || [ -z $DESTENV ]; then
+    usage
     exit 1
 fi
 
-if [ -z $desthost ]; then
+if [ -z "$SOURCEDB" ]; then
+    SOURCEDB="whelk"
+fi
+
+if [ -z "$DESTDB" ]; then
+    DESTDB="$SOURCEDB"
+fi
+
+if [ -z "$DESTUSER" ]; then
+    DESTUSER="$SOURCEUSER"
+fi
+
+if [ -z "$DESTPASSWORD" ]; then
+    DESTPASSWORD="$SOURCEPASSWORD"
+fi
+
+if [ -z $DESTHOST ]; then
     handle_sql_dump() {
         cat -
     }
 else
+    $TOOLDIR/postgresql/drop-tables-and-indexes-sql.sh | PGPASSWORD=$DESTPASSWORD psql -h $DESTHOST -U $DESTUSER $DESTDB
+
     handle_sql_dump() {
-        psql -h $desthost -U whelk -W whelk
+        PGPASSWORD=$DESTPASSWORD psql -h $DESTHOST -U $DESTUSER $DESTDB
     }
 fi
 
-replaceid="s!\(\(\"@id\": \"\|\t\)https://libris\)\(-\w\+\)\?\(.kb.se/[bcdfghjklmnpqrstvwxz0-9]\+\)\>!\1-$destenv\4!g"
+REPLACEID="s!\(\(\"@id\": \"\|\t\)https://libris\)\(-\w\+\)\?\(.kb.se/[bcdfghjklmnpqrstvwxz0-9]\+\)\>!\1-$DESTENV\4!g"
 
+# Using `--schema=public` excludes e.g. extensions.
 #-T lddb__versions
-pg_dump -h $sourcehost -U whelk whelk -W |
+PGPASSWORD=$SOURCEPASSWORD pg_dump --schema=public -h $SOURCEHOST -U $SOURCEUSER $SOURCEDB |
     ( read; cat - |
-    sed "$replaceid" |
+    sed "$REPLACEID" |
     handle_sql_dump )
