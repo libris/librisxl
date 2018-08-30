59c5e3292321c7ea7d2cc199d6d585444e08865d
Niklas Lindström <lindstream@gmail.com>
2018-08-30 16:19:43 +0200

    Remove unused scripts and move some to examples

--- librisxl-tools/scripts/make_elastic_stats.sh
+++
@@ -1,5 +0,0 @@
-ES_HOST=$1
-TYPE=$2
-OUT_DIR=$3
-curl -s http://$ES_HOST/libris/_mapping | python -mjson.tool > /tmp/es_mapping.json
-python scripts/elastic_stats.py /tmp/es_mapping.json ext-libris/src/main/resources/marcframe.json -f $TYPE -s $ES_HOST | bash > $OUT_DIR/es-stats-$TYPE.json
