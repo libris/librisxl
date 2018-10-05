06e5183722d29ad75baaa4611cf340f2bae0a8f6
Sara-Lena Wiktor <sara-lena.wiktor@kb.se>
2018-10-05 11:55:04 +0200

    Add script (skeleton-ish) for converting termComponentList
    Keep the first term in termComponent as it is, and convert remaining
    types to subclasses of Subdivision.
    Probably not possible to be sure that the first term is the main term.
    If not, possible to use prefLabel to get the order of the terms?

--- whelktool/scripts/examples/mainbatchcontrol.groovy
+++ whelktool/scripts/examples/mainbatchcontrol.groovy
@@ -1,3 +1,4 @@
 selectByCollection('bib') {
     script('set-auth-role.groovy')(it)
+    //script('make-termComponentList-subdivision.groovy')(it)
 }
---
+++ whelktool/scripts/examples/make-termComponentList-subdivision.groovy
@@ -0,0 +1,26 @@
+def subdivisionTypes = [
+        'GenreForm' : 'GenreSubdivision',
+        'Temporal' : 'TemporalSubdivision',
+        'Geographic' : 'GeographicSubdivision'
+]
+
+def (record, instance, work) = data.graph
+
+if (!work) return
+assert work['@id'] == instance.instanceOf['@id']
+
+work.subject.findAll {
+    it['@type'] == 'ComplexSubject'
+}.each {
+    it.termComponentList.eachWithIndex { term, i ->
+        //Can we know for sure that the first term always is the main term?
+        // If not, how can we determine which one the main term is?
+        if (i != 0) {
+            if (subdivisionTypes[term['@type']]){
+                term['@type'] = subdivisionTypes[term['@type']]
+            }
+        }
+    }
+    data.scheduleSave()
+}
+
