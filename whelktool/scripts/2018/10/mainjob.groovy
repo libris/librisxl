dcff5a0b654dc9dfa21084f6bcc4cdbca485e079
Sara-Lena Wiktor <sara-lena.wiktor@kb.se>
2018-10-11 16:58:41 +0200

    Refactor scripts for remodeling termComponentList to one script
    One script for both bib and auth data

--- whelktool/scripts/2018/10/mainjob.groovy
+++ whelktool/scripts/2018/10/mainjob.groovy
@@ -1,8 +1,8 @@
 selectByCollection('bib') {
     script('fix-unbalanced-brackets.groovy')(it)
-    script('remodel-termComponentList-bib.groovy')(it)
+    script('remodel-termComponentList.groovy')(it)
 }
 
 selectByCollection('auth') {
-    script('remodel-termComponentList-auth.groovy')(it)
+    script('remodel-termComponentList.groovy')(it)
 }
---
+++ whelktool/scripts/2018/10/remodel-termComponentList.groovy
@@ -0,0 +1,50 @@
+subdivisionTypes = [
+        'GenreForm' : 'GenreSubdivision',
+        'Temporal' : 'TemporalSubdivision',
+        'Geographic' : 'GeographicSubdivision'
+]
+
+COMPLEX_SUBJECT_TYPE = 'ComplexSubject'
+
+boolean changeToSubdivision(it, subdivisionTypes) {
+    boolean changed = false
+    it.termComponentList.eachWithIndex { term, i ->
+        if (i != 0) {
+            if (subdivisionTypes[term['@type']]) {
+                term['@type'] = subdivisionTypes[term['@type']]
+                changed = true
+            }
+        }
+    }
+    return changed
+}
+
+process { data ->
+    // bib
+    if (data.graph[1].containsKey('instanceOf')) {
+        def (record, instance, work) = data.graph
+
+        if (!work) return
+        assert work['@id'] == instance.instanceOf['@id']
+
+        work.subject.findAll {
+            it['@type'] == COMPLEX_SUBJECT_TYPE
+        }.each {
+            if (changeToSubdivision(it, subdivisionTypes)) {
+                data.scheduleSave()
+            }
+        }
+    } else {
+        // auth
+        def (record, authdata) = data.graph
+
+        if (!authdata) return
+
+        if (authdata['@type'] == COMPLEX_SUBJECT_TYPE) {
+            if (changeToSubdivision(authdata, subdivisionTypes)) {
+                data.scheduleSave()
+            }
+        }
+    }
+
+}
