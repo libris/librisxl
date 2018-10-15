9c195511de47d3c7de14a07d031183ea02224475
Niklas Lindström <lindstream@gmail.com>
2018-10-15 12:32:09 +0200

    Add delete by id example to whelktool

---
+++ whelktool/scripts/examples/deletebyid.groovy
@@ -0,0 +1,11 @@
+selectByIds([
+    'https://libris-qa.kb.se/3mffslbf5r9snhq',
+    'https://libris-qa.kb.se/k3ww822w3f5631r',
+    'https://libris-qa.kb.se/6qjjvcvj2fddtjz',
+    'https://libris-qa.kb.se/7rkkxr6k07n94zh',
+    'http://libris.kb.se/bib/7149593',
+    'https://id.kb.se/term/rda/Unmediated',
+    'p711q7q11nf01k0'
+]) {
+    it.scheduleDelete()
+}
