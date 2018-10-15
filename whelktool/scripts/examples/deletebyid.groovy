abdd247d944a600b6b8d7ab158e974f906675973
Niklas Lindström <lindstream@gmail.com>
2018-10-15 16:39:52 +0200

    Don't delete special record from definitions in example
    It lacks controlNumber, which the deletion API requires at the moment.

--- whelktool/scripts/examples/deletebyid.groovy
+++ whelktool/scripts/examples/deletebyid.groovy
@@ -4,7 +4,6 @@ selectByIds([
     'https://libris-qa.kb.se/6qjjvcvj2fddtjz',
     'https://libris-qa.kb.se/7rkkxr6k07n94zh',
     'http://libris.kb.se/bib/7149593',
-    'https://id.kb.se/term/rda/Unmediated',
     'p711q7q11nf01k0'
 ]) {
     it.scheduleDelete()
