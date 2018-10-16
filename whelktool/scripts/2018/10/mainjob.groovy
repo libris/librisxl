7d384f2f681983722716ddd75f3bb441f8cd78d7
Niklas Lindström <lindstream@gmail.com>
2018-10-16 17:12:52 +0200

    Run mainjon on auth, bib and hold collections

--- whelktool/scripts/2018/10/mainjob.groovy
+++ whelktool/scripts/2018/10/mainjob.groovy
@@ -1,8 +1,12 @@
+selectByCollection('auth') {
+    script('remodel-termComponentList.groovy')(it)
+}
+
 selectByCollection('bib') {
     script('fix-unbalanced-brackets.groovy')(it)
     script('remodel-termComponentList.groovy')(it)
 }
 
-selectByCollection('auth') {
-    script('remodel-termComponentList.groovy')(it)
+selectByCollection('hold') {
+    script('fix-unbalanced-brackets.groovy')(it)
 }
