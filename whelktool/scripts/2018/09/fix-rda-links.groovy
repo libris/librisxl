6f6ed99319e35ebfe6d5d5b8f85b25704d4371f0
Niklas Lindström <lindstream@gmail.com>
2018-11-09 17:27:17 +0100

    Improve fix-rda-links whelktool script
    - Eliminate duplicate references.
    - Fix MediaType missing URI (another original mapping error).

--- whelktool/scripts/2018/09/fix-rda-links.groovy
+++ whelktool/scripts/2018/09/fix-rda-links.groovy
@@ -7,20 +7,35 @@ String findCorrectId(String id) {
     if (id in correctIdMap) {
         return correctIdMap[id]
     }
-    def correctId = correctIdMap[id] = findCanonicalId(id)
+    String correctId = correctIdMap[id] = findCanonicalId(id)
     return correctId
 }
 
+List asList(o) {
+    return (o instanceof List) ? (List) o : o != null ? [o] : []
+}
+
 boolean fixRdaLink(ref) {
-    def brokenRef = ref.sameAs?.find { it[ID].startsWith(brokenBase) }
+    Map brokenRef = ref.sameAs?.find { it[ID].startsWith(brokenBase) }
+    String fixedId = null
+
     if (brokenRef) {
         def brokenId = brokenRef[ID]
-        def fixedId = correctBase + brokenId.substring(brokenBase.size())
-        def correctId = findCorrectId(fixedId)
+        fixedId = correctBase + brokenId.substring(brokenBase.size())
+    } else if (ref[TYPE] == 'MediaType') {
+        def tokens = asList(ref.code) ?: asList(ref.label)
+        if (tokens) {
+            fixedId = correctBase + 'media/' + tokens[0]
+        }
+    }
+
+    if (fixedId) {
+        String correctId = findCorrectId(fixedId)
         if (correctId) {
             ref.clear()
             ref[ID] = correctId
         } else { // Not a correct term, but shape of URI is correct
+            // TODO: really though? We could just remove it...
             brokenRef[ID] = fixedId
         }
         return true
@@ -28,7 +43,27 @@ boolean fixRdaLink(ref) {
     return false
 }
 
-selectByCollection('bib') { data ->
+boolean fixRdaLinks(List <Map> references) {
+    boolean fixed = false
+    references.each {
+        if (fixRdaLink(it)) {
+            fixed = true
+        }
+    }
+    if (fixed) {
+        // Eliminate duplicate references:
+        Set seenRefs = new HashSet()
+        references.removeAll { !seenRefs.add(it[ID]) }
+        return true
+    }
+    return false
+}
+
+//selectByIds(['h1t5mmst1k95p7w', 'fzrs2qjr3mb4fkk']) { data ->
+selectBySqlWhere('''
+    data#>'{@graph,1}' ??| array['carrierType', 'mediaType'] OR
+    data#>'{@graph,2}' ?? 'contentType'
+''') { data ->
     def (record, instance, work) = data.graph
 
     if (!isInstanceOf(instance, 'Instance')) {
@@ -44,11 +79,8 @@ selectByCollection('bib') { data ->
         instance.carrierType,
         work.contentType
     ].each {
-        it.each {
-            if (fixRdaLink(it)) {
-                // TODO: when correct, eliminate possible duplicate in List
-                data.scheduleSave(loud: false)
-            }
+        if (fixRdaLinks(it)) {
+            data.scheduleSave(loud: false)
         }
     }
 }
