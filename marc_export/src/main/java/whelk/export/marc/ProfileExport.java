9a9e5c8c33f8dbf488f68621356483dba3aa8d7c
Matthias Nilsson <matthias.nilsson@kb.se>
2018-11-14 15:48:51 +0100

    Handle carrierType w/o @id

--- marc_export/src/main/java/whelk/export/marc/ProfileExport.java
+++ marc_export/src/main/java/whelk/export/marc/ProfileExport.java
@@ -244,7 +244,7 @@ public class ProfileExport
             {
                 for (Map map : carrierTypes)
                 {
-                    if ( map.get("@id").equals("https://id.kb.se/marc/OnlineResource") )
+                    if ( map.containsKey("@id") && map.get("@id").equals("https://id.kb.se/marc/OnlineResource") )
                         onlineResource = true;
                 }
             }
