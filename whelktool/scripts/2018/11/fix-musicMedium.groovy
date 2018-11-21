c0ce67c33689f8a3e4f302044f83beda45fbbc21
Sara-Lena Wiktor <sara-lena.wiktor@kb.se>
2018-11-21 13:51:10 +0100

    Improvements of script fix-musicMedium

--- whelktool/scripts/2018/11/fix-musicMedium.groovy
+++ whelktool/scripts/2018/11/fix-musicMedium.groovy
@@ -2,24 +2,24 @@ MUSICMEDIUM_TYPE = 'MusicMedium'
 MUSICMEDIUM_PROPERTY = 'musicMedium'
 
 
-boolean remodelToStructuredValue(key, term) {
+boolean remodelToStructuredValue(data, obj) {
 
     //Guard against updating definitions data or musicMedium with correct structure (added via the viewer)
-    //musicMedium shall consist of a list of strings, otherwise ignore.
-    if (key != MUSICMEDIUM_PROPERTY || !(term[MUSICMEDIUM_PROPERTY] instanceof List))
-        return false
+    if (!(obj[MUSICMEDIUM_PROPERTY] instanceof List))
+        return
 
-    if (term[MUSICMEDIUM_PROPERTY].any { !(it instanceof String)}) {
-        println("${MUSICMEDIUM_PROPERTY} is not a list of strings. Skipping term...")
-        return false
+    //musicMedium shall consist of a list of strings, otherwise ignore.
+    if (obj[MUSICMEDIUM_PROPERTY].any { !(it instanceof String)}) {
+        println("${MUSICMEDIUM_PROPERTY} is not a list of strings in post ${data.graph[0][ID]}. Skipping term...")
+        return
     }
 
     //Create musicMedium object and add to list
-    term[MUSICMEDIUM_PROPERTY] = term[MUSICMEDIUM_PROPERTY].collect {
+    obj[MUSICMEDIUM_PROPERTY] = obj[MUSICMEDIUM_PROPERTY].collect {
         ['@type': MUSICMEDIUM_TYPE, 'label': it]
     }
-    return true
 
+    data.scheduleSave()
 }
 
 void findAndFixValuesInData(data, obj) {
@@ -27,23 +27,16 @@ void findAndFixValuesInData(data, obj) {
         obj.each {
             findAndFixValuesInData(data, it)
         }
-    }
-    else if (obj instanceof Map) {
+    } else if (obj instanceof Map) {
         obj.each { key, value ->
-            checkValueInData(data, obj, key, value)
+            if (key == MUSICMEDIUM_PROPERTY) {
+                remodelToStructuredValue(data, obj)
+            } else {
+                findAndFixValuesInData(data, value)
+            }
         }
     }
 }
-
-void checkValueInData(data, container, key, value) {
-
-    if (remodelToStructuredValue(key, container))
-        data.scheduleSave()
-    else {
-        findAndFixValuesInData(data, value)
-    }
-}
-
 
 selectBySqlWhere('''
         data::text LIKE '%"musicMedium"%'
