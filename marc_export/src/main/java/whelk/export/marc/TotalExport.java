bd1b96889a02b020b2731a7420f038bd204aa9af
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2018-11-30 15:43:31 +0100

    Partial commit.

--- marc_export/src/main/java/whelk/export/marc/TotalExport.java
+++ marc_export/src/main/java/whelk/export/marc/TotalExport.java
@@ -2,61 +2,71 @@ package whelk.export.marc;
 
 import org.apache.commons.lang3.StringUtils;
 import se.kb.libris.export.ExportProfile;
+import se.kb.libris.util.marc.MarcRecord;
+import se.kb.libris.util.marc.io.Iso2709MarcRecordWriter;
+import se.kb.libris.util.marc.io.MarcRecordWriter;
+import se.kb.libris.util.marc.io.MarcXmlRecordWriter;
+import whelk.Document;
 import whelk.Whelk;
 import whelk.converter.marc.JsonLD2MarcXMLConverter;
+import whelk.util.MarcExport;
+import whelk.util.ThreadPool;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Vector;
 
 public class TotalExport
 {
+    private final int BATCH_SIZE = 200;
     private JsonLD2MarcXMLConverter m_toMarcXmlConverter;
     private Whelk m_whelk;
+
     public TotalExport(Whelk whelk)
     {
         m_whelk = whelk;
         m_toMarcXmlConverter = new JsonLD2MarcXMLConverter(whelk.createMarcFrameConverter());
     }
 
-    public static void main(String[] args)
-            throws IOException, SQLException
+    class Batch
     {
-        // The idea is to choose holdings based on profile locations, and order them by created-date.
-        // This will require a btree index on created (we would use IDs if they were sequential).
-        // This will enable us to do total dumps, with predictable start/offsets, meaning total dumps
-        // can be made as sequences of parts where necessary.
+        public Batch(ExportProfile profile, MarcRecordWriter output)
+        {
+            bibUrisToConvert = new ArrayList<>(BATCH_SIZE);
+            this.profile = profile;
+            this.output = output;
+        }
+        List<String> bibUrisToConvert;
+        ExportProfile profile;
+        MarcRecordWriter output;
+    }
 
-        // Necessary parameters: A profile, a 'number' of records to include (possibly infinite), and a
-        // part number. The result will be all 'number' of records after/offset part*number.
-
-        // Using export.properties, get record 1001 -> 2000
-        // java -jar marc_export.jar export.properties 1000 1
-
+    public static void main(String[] args)
+            throws IOException, SQLException, InterruptedException
+    {
         if (args.length != 4)
             printUsageAndExit();
 
         ExportProfile profile = new ExportProfile(new File(args[2]));
         long size = Long.parseLong(args[3]);
         long segment = Long.parseLong(args[4]);
-    }
 
-    private void dump(ExportProfile profile, long size, long segment)
-            throws SQLException
-    {
-        try (Connection connection = getConnection();
-             PreparedStatement statement = getAllHeldURIs(profile, size, size*segment, connection);
-             ResultSet resultSet = statement.executeQuery())
-        {
-            while (resultSet.next())
-            {
-                String bibMainEntityUri = resultSet.getString(1);
-                // BATCH THAT SHIT AND DUMP IT CONCURRENTLY
-            }
+        String encoding = profile.getProperty("characterencoding");
+        if (encoding.equals("Latin1Strip")) {
+            encoding = "ISO-8859-1";
         }
+        MarcRecordWriter output = null;
+        if (profile.getProperty("format", "ISO2709").equalsIgnoreCase("MARCXML"))
+            output = new MarcXmlRecordWriter(System.out, encoding);
+        else
+            output = new Iso2709MarcRecordWriter(System.out, encoding);
+
+        new TotalExport(Whelk.createLoadedCoreWhelk()).dump(profile, size, segment, output);
     }
 
     private static void printUsageAndExit()
@@ -71,7 +81,64 @@ public class TotalExport
         System.out.println(" java -jar marc_export.jar export.properties 1000 1");
         System.out.println("Would generate the second segment (each consisting of 1000 records) of all records held by whatever");
         System.out.println("is in location=[] in export.properties.");
+        System.out.println("");
+        System.out.println("To not use segmentation, both SEGMENT and SEGMENT-SIZE should be '0'.");
         System.exit(1);
+    }
+
+    private void dump(ExportProfile profile, long size, long segment, MarcRecordWriter output)
+            throws SQLException, InterruptedException
+    {
+        ThreadPool threadPool = new ThreadPool(4 * Runtime.getRuntime().availableProcessors());
+        Batch batch = new Batch(profile, output);
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = getAllHeldURIsStatement(profile, size, size*segment, connection);
+             ResultSet resultSet = statement.executeQuery())
+        {
+            while (resultSet.next())
+            {
+                String bibMainEntityUri = resultSet.getString(1);
+                batch.bibUrisToConvert.add(bibMainEntityUri);
+
+                if (batch.bibUrisToConvert.size() >= BATCH_SIZE)
+                {
+                    threadPool.executeOnThread(batch, this::executeBatch);
+                    batch = new Batch(profile, output);
+                }
+            }
+            if (!batch.bibUrisToConvert.isEmpty())
+                threadPool.executeOnThread(batch, this::executeBatch);
+        }
+
+        threadPool.joinAll();
+    }
+
+    private void executeBatch(Batch batch, int threadIndex)
+    {
+        Connection connection = m_whelk.getStorage().getConnection();
+        for (String bibUri : batch.bibUrisToConvert)
+        {
+            String systemID = m_whelk.getStorage().getSystemIdByIri(bibUri, connection);
+            Document document = m_whelk.getStorage().loadEmbellished(systemID, m_whelk.getJsonld());
+
+            Vector<MarcRecord> result = MarcExport.compileVirtualMarcRecord(batch.profile, document, m_whelk, m_toMarcXmlConverter);
+            if (result == null) // A conversion error will already have been logged.
+                continue;
+
+
+            for (MarcRecord mr : result)
+            {
+                try
+                {
+                    batch.output.writeRecord(mr);
+                } catch (Exception e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
     }
 
     private Connection getConnection()
@@ -82,23 +149,29 @@ public class TotalExport
         return connection;
     }
 
-    private PreparedStatement getAllHeldURIs(ExportProfile profile, long limit, long offset, Connection connection)
+    private PreparedStatement getAllHeldURIsStatement(ExportProfile profile, long limit, long offset, Connection connection)
             throws SQLException
     {
         String locations = profile.getProperty("locations", "");
 
-        if (isObviouslyBadSql(locations)) // Not watertight, but this is not user-input, it's admin-input.
+        if (isObviouslyBadSql(locations)) // Not watertight, but this is not user-input. It's admin-input.
             throw new RuntimeException("SQL INJECTION SUSPECTED.");
 
         List<String> libraryUriList = Arrays.asList(locations.split(" "));
         String stringList = "'" + String.join("', '", libraryUriList) + "'";
 
-        String sql = "SELECT data#>>'{@graph,1,itemOf,@id}' FROM lddb WHERE collection = 'hold' AND data#>>'{@graph,1,heldBy,@id}' IN (£) ORDER BY created LIMIT ? OFFSET ?";
+        String sql = "SELECT data#>>'{@graph,1,itemOf,@id}' FROM lddb WHERE collection = 'hold' AND data#>>'{@graph,1,heldBy,@id}' IN (£)";
         sql = sql.replace("£", stringList);
 
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setLong(1, limit);
-        preparedStatement.setLong(2, offset);
+
+        if (limit != 0)
+        {
+            sql += " ORDER BY created LIMIT ? OFFSET ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, limit);
+            preparedStatement.setLong(2, offset);
+        }
         preparedStatement.setFetchSize(100);
 
         return preparedStatement;
