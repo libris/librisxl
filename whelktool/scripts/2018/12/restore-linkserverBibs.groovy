1fcb3e1c8c4dc4c615ba8a4266581b45e9628b12
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2018-12-10 15:37:13 +0100

    Add script to restore linkserver records to the state they had before they were 'replaced' by lesser versions.

---
+++ whelktool/scripts/2018/12/restore-linkserverBibs.groovy
@@ -0,0 +1,9 @@
+PrintWriter failedBibIDs = getReportWriter("failed-to-restore-bibIDs")
+File bibIDsFile = new File(scriptDir, "bibIDsToRestore")
+selectByIds( bibIDsFile.readLines() ) { bib ->
+
+    if (bib.doc.encodingLevel == "marc:PartialPreliminaryLevel") {
+        bib.scheduleRevertTo(loud: false, time: "2018-06-27T05:00:00Z", onError:
+                { e -> failedBibIDs.println("Failed to delete ${bib.doc.shortId} due to: $e") })
+    }
+}
