fc2d0b55c744ccbb3fab0ba424dfa69eed0788f0
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2018-12-11 12:55:41 +0100

    Change restore to date.

--- whelktool/scripts/2018/12/restore-linkserverBibs.groovy
+++ whelktool/scripts/2018/12/restore-linkserverBibs.groovy
@@ -3,7 +3,7 @@ File bibIDsFile = new File(scriptDir, "bibIDsToRestore")
 selectByIds( bibIDsFile.readLines() ) { bib ->
 
     if (bib.doc.encodingLevel == "marc:PartialPreliminaryLevel") {
-        bib.scheduleRevertTo(loud: false, time: "2018-06-27T05:00:00Z", onError:
+        bib.scheduleRevertTo(loud: false, time: "2018-07-03T05:00:00Z", onError:
                 { e -> failedBibIDs.println("Failed to restore ${bib.doc.shortId} due to: $e") })
     }
 }
