e206cc2c2fed825408824be81cf271c1d0e9a82b
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2018-12-11 10:35:19 +0100

    Fix error printout in restoration script.

--- whelktool/scripts/2018/12/restore-linkserverBibs.groovy
+++ whelktool/scripts/2018/12/restore-linkserverBibs.groovy
@@ -4,6 +4,6 @@ selectByIds( bibIDsFile.readLines() ) { bib ->
 
     if (bib.doc.encodingLevel == "marc:PartialPreliminaryLevel") {
         bib.scheduleRevertTo(loud: false, time: "2018-06-27T05:00:00Z", onError:
-                { e -> failedBibIDs.println("Failed to delete ${bib.doc.shortId} due to: $e") })
+                { e -> failedBibIDs.println("Failed to restore ${bib.doc.shortId} due to: $e") })
     }
 }
