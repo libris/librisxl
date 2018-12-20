f95b82966913f33baffe4ea8160bbbaff3a5fb95
Matthias Nilsson <matthias.nilsson@kb.se>
2018-12-20 11:11:15 +0100

    Add script to delete holdings for Frescati
    (+ a file with specified bib IDs.)

---
+++ whelktool/scripts/2019/01/delete-fresc-holdings.groovy
@@ -0,0 +1,25 @@
+/*
+ * This deletes holdings for a specified sigel.
+ *
+ * `BIB_ID_FILE` must contain one bib ID per row.
+ */
+String SIGEL_TO_DELETE = 'H'
+String BIB_ID_FILE = 'fresc_ids.txt'
+
+PrintWriter failedHoldIDs = getReportWriter("failed-to-delete-holdIDs")
+PrintWriter scheduledForDeletion = getReportWriter("scheduled-for-deletion")
+File bibIDsFile = new File(scriptDir, BIB_ID_FILE)
+
+selectByIds( bibIDsFile.readLines() ) { bib ->
+    String query = "id in (select id from lddb__dependencies " +
+        "where dependsonid = '${bib.doc.shortId}' and relation = 'itemOf')"
+
+    selectBySqlWhere(query, silent: false) { hold ->
+        if (hold.doc.sigel == SIGEL_TO_DELETE) {
+            scheduledForDeletion.println("${hold.doc.getURI()}")
+            hold.scheduleDelete(onError: { e ->
+                failedHoldIDs.println("Failed to delete ${hold.doc.shortId} due to: $e")
+            })
+        }
+    }
+}
