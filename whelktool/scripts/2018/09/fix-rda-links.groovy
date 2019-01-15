eca631dc275f9b3f0f7c61acc2aac1096d94d22b
Niklas Lindström <lindstream@gmail.com>
2019-01-15 18:01:08 +0100

    Also fix RDA links in parts of instance or work

--- whelktool/scripts/2018/09/fix-rda-links.groovy
+++ whelktool/scripts/2018/09/fix-rda-links.groovy
@@ -1,3 +1,9 @@
+rdaTypeSlugMap = [
+    'ContentType': 'content',
+    'MediaType': 'media',
+    'CarrierType': 'carrier'
+]
+
 brokenBase = 'http://id.kb.se/term/rda/'
 correctBase = 'https://id.kb.se/term/rda/'
 
@@ -22,10 +28,13 @@ boolean fixRdaLink(ref) {
     if (brokenRef) {
         def brokenId = brokenRef[ID]
         fixedId = correctBase + brokenId.substring(brokenBase.size())
-    } else if (ref[TYPE] == 'MediaType') {
+    } else {
+        def rdatype = rdaTypeSlugMap[ref[TYPE]]
         def tokens = asList(ref.code) ?: asList(ref.label)
-        if (tokens) {
-            fixedId = correctBase + 'media/' + tokens[0]
+        boolean otherTermList = ref.termGroup &&
+                ref.termGroup.toLowerCase().indexOf('rda') == -1
+        if (rdatype && !otherTermList && tokens) {
+            fixedId = correctBase + rdatype + '/' + tokens[0]
         }
     }
 
@@ -34,7 +43,8 @@ boolean fixRdaLink(ref) {
         if (correctId) {
             ref.clear()
             ref[ID] = correctId
-        } else { // Not a correct term, but shape of URI is correct
+        } else if (brokenRef) {
+            // Not a correct term, but shape of URI is correct
             // TODO: really though? We could just remove it...
             brokenRef[ID] = fixedId
         }
@@ -43,9 +53,10 @@ boolean fixRdaLink(ref) {
     return false
 }
 
-boolean fixRdaLinks(List <Map> references) {
+boolean fixRdaLinks(Map owner, String link, isPart = false) {
+    List<Map> references = owner[link]
     boolean fixed = false
-    references.each {
+    references?.each {
         if (fixRdaLink(it)) {
             fixed = true
         }
@@ -54,12 +65,22 @@ boolean fixRdaLinks(List <Map> references) {
         // Eliminate duplicate references:
         Set seenRefs = new HashSet()
         references.removeAll { !seenRefs.add(it[ID]) }
+
+        // Move to hasPart
+        def ownerParts = owner.hasPart
+        if (ownerParts && !(ownerParts instanceof List)) {
+            ownerParts = owner.hasPart = ownerParts ? [ownerParts] : []
+        }
+
+        ownerParts.each {
+            fixRdaLinks(it, link, true)
+        }
+
         return true
     }
     return false
 }
 
-//selectByIds(['h1t5mmst1k95p7w', 'fzrs2qjr3mb4fkk']) { data ->
 selectBySqlWhere('''
     data#>'{@graph,1}' ??| array['carrierType', 'mediaType'] OR
     data#>'{@graph,2}' ?? 'contentType'
@@ -74,13 +95,13 @@ selectBySqlWhere('''
     }
     assert work['@id'] == instance.instanceOf['@id']
 
-    [
-        instance.mediaType,
-        instance.carrierType,
-        work.contentType
-    ].each {
-        if (fixRdaLinks(it)) {
-            data.scheduleSave(loud: false)
-        }
+    if (fixRdaLinks(instance, 'mediaType')) {
+        data.scheduleSave(loud: false)
+    }
+    if (fixRdaLinks(instance, 'carrierType')) {
+        data.scheduleSave(loud: false)
+    }
+    if (fixRdaLinks(work, 'contentType')) {
+        data.scheduleSave(loud: false)
     }
 }
