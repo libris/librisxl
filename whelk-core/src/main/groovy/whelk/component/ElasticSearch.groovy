ff97430fb41a0c4b8ec013fb178aac30cbb6b937
Matthias Nilsson <matthias.nilsson@kb.se>
2019-01-15 14:52:22 +0100

    Create nested sort for hasTitle.mainTitle

--- whelk-core/src/main/groovy/whelk/component/ElasticSearch.groovy
+++ whelk-core/src/main/groovy/whelk/component/ElasticSearch.groovy
@@ -326,13 +326,28 @@ class ElasticSearch {
         List result = []
         List sortClauses = sortBy.split(',')
         sortClauses.each { field ->
-            if (field.startsWith('-')) {
-                result << [(field.substring(1)): ['order': 'desc']]
-            } else {
-                result << [(field): ['order': 'asc']]
-            }
+            result << buildSortClause(field)
         }
         return result
+    }
+
+    private static Map buildSortClause(String fieldParam) {
+        def (String field, String sortOrder) = getFieldAndSortOrder(fieldParam)
+        Map clause = [(field): ['order': sortOrder]]
+        // We only handle .keyword right now, the other is left as placeholder
+        if (field == 'hasTitle.mainTitle' || field == 'hasTitle.mainTitle.keyword') {
+            clause[field]['nested_path'] = 'hasTitle'
+            clause[field]['nested_filter'] = ['term': ['hasTitle.@type': 'Title']]
+        }
+        return clause
+    }
+
+    private static Tuple2<String, String> getFieldAndSortOrder(String field) {
+        if (field.startsWith('-')) {
+            return new Tuple2(field.substring(1), 'desc')
+        } else {
+            return new Tuple2(field, 'asc')
+        }
     }
 
     /*
