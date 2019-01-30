b70e0a1bbd4f11bc7b671cb496c57a1e18b3635a
Matthias Nilsson <matthias.nilsson@kb.se>
2019-01-30 16:08:28 +0100

    Add example script for deleting holdings

---
+++ whelktool/scripts/examples/delete-holdings.groovy
@@ -0,0 +1,25 @@
+/*
+ * This deletes holdings for a specified sigel.
+ *
+ * `BIB_ID_FILE` must contain one bib ID per row.
+ */
+String SIGEL_TO_DELETE = 'SEK'
+String BIB_ID_FILE = 'delete_holdings_ids.txt'
+
+PrintWriter failedHoldIDs = getReportWriter("failed-to-delete-holdIDs")
+PrintWriter scheduledForDeletion = getReportWriter("scheduled-for-deletion")
+File bibIDsFile = new File(scriptDir, BIB_ID_FILE)
+
+selectByIds( bibIDsFile.readLines() ) { bib ->
+    String query = "id in (select id from lddb " +
+        "where data#>>'{@graph,1,itemOf,@id}' = '${bib.graph[1][ID]}')"
+
+    selectBySqlWhere(query, silent: false) { hold ->
+        if (hold.doc.sigel == SIGEL_TO_DELETE) {
+            scheduledForDeletion.println("${hold.doc.getURI()}")
+            hold.scheduleDelete(onError: { e ->
+                failedHoldIDs.println("Failed to delete ${hold.doc.shortId} due to: $e")
+            })
+        }
+    }
+}
