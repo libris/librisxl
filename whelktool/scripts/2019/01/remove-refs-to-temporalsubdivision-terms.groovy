9bd36a29c8440504558fee6a6fe70687e4968ede
Sara-Lena Wiktor <sara-lena.wiktor@kb.se>
2019-02-04 13:48:45 +0100

    Secure to only add links to existing uri:s

--- whelktool/scripts/2019/01/remove-refs-to-temporalsubdivision-terms.groovy
+++ whelktool/scripts/2019/01/remove-refs-to-temporalsubdivision-terms.groovy
@@ -1,13 +1,21 @@
-import java.net.URI
-
 TERMS_TO_CHANGE = ["medeltiden", "antiken", "forntiden", "renässansen"]
 COMPLEX_SUBJECT_TYPE = 'ComplexSubject'
 SAO_URI = 'https://id.kb.se/term/sao'
+PREFLABEL = 'prefLabel'
+TEMPSUB_TYPE = 'TemporalSubdivision'
 
 
 String termToUri(term) {
     return SAO_URI + '/' + URLEncoder.encode(term.capitalize(), "UTF-8").replaceAll("\\+", "%20")
 }
+/*
+String getIdOfTerm(term) {
+    selectBySqlWhere("""
+            collection = 'auth' AND data#>>'{@graph,1,@type}' = 'Geographic' AND data#>>'{@graph,1,prefLabel}' = '${term}'
+    """) { data ->
+        return data[1][ID]
+    }
+}*/
 
 boolean updateReference(work) {
     def extractedTerms = []
@@ -20,59 +28,62 @@ boolean updateReference(work) {
     while (iterSubj.hasNext()) {
         subj = iterSubj.next()
 
-        if (subj['@type'] == COMPLEX_SUBJECT_TYPE && subj['inScheme'] && subj['inScheme']['@id'] == SAO_URI) {
+        if (subj[TYPE] == COMPLEX_SUBJECT_TYPE && subj['inScheme'] && subj['inScheme'][ID] == SAO_URI
+            && subj.termComponentList.any{ it[TYPE] == TEMPSUB_TYPE} &&
+                subj.termComponentList.any{ TERMS_TO_CHANGE.contains(it[PREFLABEL])}) {
 
-            if (subj.termComponentList.any{ it['@type'] == 'TemporalSubdivision'}
-                    && subj.termComponentList.any{ TERMS_TO_CHANGE.contains(it['prefLabel'])}) {
+            ListIterator iter = subj.termComponentList.listIterator()
 
-                ListIterator iter = subj.termComponentList.listIterator()
+            while(iter.hasNext()) {
+                cpx_term = iter.next()
 
-                while(iter.hasNext()) {
-                    cpx_term = iter.next()
-
-                    if (cpx_term['@type'] == 'TemporalSubdivision' && TERMS_TO_CHANGE.contains(cpx_term['prefLabel'].toLowerCase())) {
-                        if (!extractedTerms.contains(termToUri(cpx_term['prefLabel']))) {
-                            extractedTerms << termToUri(cpx_term['prefLabel'])
-                        }
-                        iter.remove()
+                if (cpx_term[TYPE] == TEMPSUB_TYPE && TERMS_TO_CHANGE.contains(cpx_term[PREFLABEL].toLowerCase())) {
+                    if (!extractedTerms.contains(termToUri(cpx_term[PREFLABEL]))) {
+                        extractedTerms << termToUri(cpx_term[PREFLABEL])
                     }
+                    iter.remove()
                 }
             }
 
             //Extract remaining entity in ComplexSubject, if only one remains
             if (subj.termComponentList.size() == 1) {
-                def prefLabel = ""
-
-                if (subj.termComponentList.get(0)['@id']) {
-                    if (!extractedTerms.contains(subj.termComponentList.get(0)['@id'])) {
-                        extractedTerms << subj.termComponentList.get(0)['@id']
+                if (subj.termComponentList.get(0)[ID]) {
+                    if (!extractedTerms.contains(subj.termComponentList.get(0)[ID])) {
+                        extractedTerms << subj.termComponentList.get(0)[ID]
                     }
-                } else if (subj.termComponentList.get(0)['prefLabel']) {
-                    prefLabel = subj.termComponentList.get(0)['prefLabel']
+                } else if (subj.termComponentList.get(0)[PREFLABEL]) {
+                    newUri = termToUri(subj.termComponentList.get(0)[PREFLABEL])
+                    if (findCanonicalId(newUri)) {
+                        if (!extractedTerms.contains(newUri))
+                            extractedTerms << newUri
+                    } //else if (subj.termComponentList.get(0)[TYPE] == 'Geographic') {
+                        //geoUri = getIdOfTerm(subj.termComponentList.get(0)[PREFLABEL])
+                        //if (!extractedTerms.contains(geoUri))
+                        //    extractedTerms << geoUri
+                    //}
+                    else {
+                        if (!entitiesToMove.size() || entitiesToMove.any{ it[TYPE] != subj.termComponentList.get(0)[TYPE] ||
+                                it['label'] != subj.termComponentList.get(0)[PREFLABEL]})
+                            entitiesToMove << [(TYPE): subj.termComponentList.get(0)[TYPE],
+                                           'label': subj.termComponentList.get(0)[PREFLABEL]]
+                    }
                 } else {
                     entitiesToMove << subj.termComponentList.get(0)
                 }
-
-                if (!prefLabel.isEmpty() && !extractedTerms.contains(termToUri(prefLabel)))
-                    extractedTerms << termToUri(prefLabel)
-
                 iterSubj.remove()
             } else {
                 // Update existing prefLabel and sameAs uri of ComplexSubject
                 def prefLabelTerms = []
 
                 subj.termComponentList.each {
-                    if (it['@id']) {
-                        def path = it['@id'].toURL().getPath()
-                        prefLabelTerms << URLDecoder.decode(path.substring(path.lastIndexOf('/') + 1), "UTF-8")
-
-                    } else if (it['prefLabel']) {
-                        prefLabelTerms << it['prefLabel']
+                    if (it[ID]) {
+                        doc = load(it[ID])
+                        if (doc && doc[GRAPH][1].containsKey(PREFLABEL)) {
+                            prefLabelTerms << doc[GRAPH][1].prefLabel
+                        }
+                    } else if (it[PREFLABEL]) {
+                        prefLabelTerms << it[PREFLABEL]
                     }
-                    //TODO: What if there is np termComponentList.prefLabel? When creating subject.prefLabel and uris.
-                    // Should probably not find a ComplexSubject with (main) prefLabel and
-                    // sameAs if main term is Work/Person/Organization etc
-                    //prefLabelTerms << it['prefLabel'] ?: it['name']
                 }
 
                 if (subj.prefLabel) {
@@ -80,7 +91,7 @@ boolean updateReference(work) {
                 }
 
                 if (subj['sameAs']) {
-                    subj['sameAs'].get(0)['@id'] = termToUri(prefLabelTerms.join("--"))
+                    subj['sameAs'].get(0)[ID] = termToUri(prefLabelTerms.join("--"))
                 }
             }
 
@@ -88,20 +99,19 @@ boolean updateReference(work) {
     }
 
 
-
     if (extractedTerms.size() || entitiesToMove.size()) {
         def existingUris = []
 
         work.subject.each {
-            if (it['@id']) {
-                existingUris << it['@id']
+            if (it[ID]) {
+                existingUris << it[ID]
             }
         }
 
         if (extractedTerms.size()) {
             extractedTerms.each {
                 if (!existingUris.contains(it))
-                    work.subject << ['@id': it]
+                    work.subject << [(ID): it]
             }
         }
 
@@ -122,7 +132,6 @@ selectBySqlWhere("data::text LIKE '%termComponentList%' and (data::text LIKE '%m
         return
     }
     // bib and hold
-
     if (data.graph[1].containsKey('instanceOf')) {
         def (record, instance, work) = data.graph
 
