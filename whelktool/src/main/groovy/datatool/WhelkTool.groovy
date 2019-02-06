03cf3e43527eadf91c896e144902d760608c32a5
Niklas Lindström <lindstream@gmail.com>
2019-02-06 16:23:45 +0100

    Set errorDetected only and directly when catching exception
    Also saves the exception, not just the detection.

--- whelktool/src/main/groovy/datatool/WhelkTool.groovy
+++ whelktool/src/main/groovy/datatool/WhelkTool.groovy
@@ -50,7 +50,7 @@ class WhelkTool {
 
     boolean allowLoud
 
-    private boolean errorDetected
+    private Throwable errorDetected
 
     private def jsonWriter = new ObjectMapper().writerWithDefaultPrettyPrinter()
 
@@ -249,13 +249,12 @@ class WhelkTool {
                 if (executorService) {
                     executorService.submit {
                         if (!processBatch(process, batchToProcess, counter)) {
-                            errorDetected = true
                             executorService.shutdownNow()
                         }
                     }
                 } else {
                     if (!processBatch(process, batchToProcess, counter)) {
-                        errorDetected = true
+                        log "Aborted selection: ${counter.summary}. Done in ${counter.elapsedSeconds} s."
                         return
                     }
                 }
@@ -306,6 +305,7 @@ class WhelkTool {
                 err.printStackTrace errorLog
                 errorLog.println "-" * 20
                 errorLog.flush()
+                errorDetected = err
                 return false
             }
             if (!doContinue) {
