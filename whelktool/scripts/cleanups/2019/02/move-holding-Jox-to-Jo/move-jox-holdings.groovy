2e2dfae4d658e7800735c800f3ba87be745863a9
Matthias Nilsson <matthias.nilsson@kb.se>
2019-02-19 13:53:15 +0100

    Add script for moving Jox holdings to Jo

---
+++ whelktool/scripts/cleanups/2019/02/move-holding-Jox-to-Jo/move-jox-holdings.groovy
@@ -0,0 +1,35 @@
+/*
+ * This moves all holdings for Jox to Jo.
+ *
+ * See LXL-2248 for more info.
+ *
+ */
+String CURRENT_SIGEL = 'https://libris.kb.se/library/Jox'
+String NEW_SIGEL = 'https://libris.kb.se/library/Jo'
+
+PrintWriter scheduledForMove = getReportWriter("scheduled-for-move")
+PrintWriter notMoved = getReportWriter("refused-to-move")
+
+selectBySqlWhere("""
+        collection = 'hold' AND
+        data#>>'{@graph,1,heldBy,@id}' = '${CURRENT_SIGEL}'
+    """, silent: false) { hold ->
+    def (record, thing) = hold.graph
+    boolean wouldCreateDupe = false
+
+    selectBySqlWhere("""
+        collection = 'hold' AND
+        data#>>'{@graph,1,itemOf,@id}' = '${thing["itemOf"][ID]}' AND
+        data#>>'{@graph,1,heldBy,@id}' = '$NEW_SIGEL'
+    """, silent: true) { other_hold ->
+        wouldCreateDupe = true
+    }
+
+    if (wouldCreateDupe) {
+        notMoved.println("Not moving ${hold.doc.getURI()}, since it would create a duplicate.")
+    } else {
+        thing['heldBy']['@id'] = NEW_SIGEL
+        scheduledForMove.println("${hold.doc.getURI()}")
+        hold.scheduleSave()
+    }
+}
