e5a83da4b941e5a9945e18bded4606eabcc2ff1c
Matthias Nilsson <matthias.nilsson@kb.se>
2019-02-19 16:14:20 +0100

    Use supplied IDs for Li cleanup

--- whelktool/scripts/cleanups/2019/02/delete-weeded-holdings-for-Li/delete-weeded-holdings-for-Li.groovy
+++ whelktool/scripts/cleanups/2019/02/delete-weeded-holdings-for-Li/delete-weeded-holdings-for-Li.groovy
@@ -4,17 +4,23 @@
  * See LXL-2275 for more info.
  *
  */
+String SIGEL_TO_DELETE = 'Li'
+String BIB_ID_FILE = 'lxl-2275-ids.txt'
+
 PrintWriter failedHoldIDs = getReportWriter("failed-to-delete-holdIDs")
 PrintWriter scheduledForDeletion = getReportWriter("scheduled-for-deletion")
+File bibIDsFile = new File(scriptDir, BIB_ID_FILE)
 
-selectBySqlWhere("""
-        collection = 'hold' AND
-        data#>>'{@graph,1,heldBy,@id}' = 'https://libris.kb.se/library/Li' AND
-        data#>>'{@graph,1,availability,0,@type}' = 'ItemAvailability' AND
-        data#>>'{@graph,1,availability,0,label}' = 'UTGALLRAD'
-    """, silent: false) { hold ->
-    scheduledForDeletion.println("${hold.doc.getURI()}")
-    hold.scheduleDelete(onError: { e ->
-        failedHoldIDs.println("Failed to delete ${hold.doc.shortId} due to: $e")
-    })
+selectByIds( bibIDsFile.readLines() ) { bib ->
+    String query = "id in (select id from lddb " +
+        "where data#>>'{@graph,1,itemOf,@id}' = '${bib.graph[1][ID]}')"
+
+    selectBySqlWhere(query, silent: false) { hold ->
+        if (hold.doc.sigel == SIGEL_TO_DELETE) {
+            scheduledForDeletion.println("${hold.doc.getURI()}")
+            hold.scheduleDelete(onError: { e ->
+                failedHoldIDs.println("Failed to delete ${hold.doc.shortId} due to: $e")
+            })
+        }
+    }
 }
