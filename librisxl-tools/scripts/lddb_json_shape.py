64532172507f23d9b20a18394ee6705e7e33762d
Niklas Lindström <lindstream@gmail.com>
2019-02-27 14:57:55 +0100

    Improve escaping and add works in lddb_json_shape.py

--- librisxl-tools/scripts/lddb_json_shape.py
+++ librisxl-tools/scripts/lddb_json_shape.py
@@ -11,6 +11,8 @@ def compute_shape(node, index):
         return
 
     rtype = node.get('@type')
+    if isinstance(rtype, list):
+        rtype = '+'.join(rtype)
     shape = index.setdefault(rtype, {})
 
     for k, vs in node.items():
@@ -41,15 +43,29 @@ if __name__ == '__main__':
     for i, l in enumerate(sys.stdin):
         if not l.rstrip():
             continue
-        l = l.replace(b'\\\\"', b'\\"')
+        if isinstance(l, bytes):
+            l = l.decode('utf-8')
+        l = l.replace(r'\\', '\\')
+
+        if i % 100000 == 0:
+            print("At line", i, file=sys.stderr)
+
         try:
             data = json.loads(l)
-            thing = data['@graph'][1]
-            thing['meta'] = data['@graph'][0]
+            graph =  data['@graph']
+            thing =graph[1]
+            thing['meta'] =graph[0]
+
+            if len(graph) > 2 and 'instanceOf' in thing:
+                work = graph[2]
+                assert thing['instanceOf']['@id'] == work['@id']
+                thing['instanceOf'] = work
+
             compute_shape(thing, index)
-        except ValueError as e:
+
+        except (ValueError, AttributeError) as e:
             print("ERROR at", i, "in data:", file=sys.stderr)
             print(l, file=sys.stderr)
             print(e, file=sys.stderr)
 
-    print(json.dumps(index, indent=2)) 
+    print(json.dumps(index, indent=2))
