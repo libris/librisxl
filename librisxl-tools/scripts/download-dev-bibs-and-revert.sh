a167ee6f7662a3990a012fa58e8b260d97f08832
Niklas Lindström <lindstream@gmail.com>
2019-03-21 17:37:18 +0100

    Fix glaring omissions in download-dev-bibs-and-revert.sh

--- librisxl-tools/scripts/download-dev-bibs-and-revert.sh
+++ librisxl-tools/scripts/download-dev-bibs-and-revert.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 BIBDUMP=/tmp/lddb-dev-bib-records
+ENV=${1:-dev}
+LIMIT=${2:-1000}
 
 # Download a temporary dump of (embellished) instance records.
 # Take a couple of minutes.
 if [[ ! -d $BIBDUMP ]]; then
     mkdir -p $BIBDUMP
     pushd $BIBDUMP
-    curl -s -HAccept:application/ld+json 'https://libris-dev.kb.se/find.jsonld?@type=Instance&_limit=1000' | python -c '
-    import sys, json
-    data = json.load(sys.stdin)
-    for item in data["items"]:
-        url = item["@id"].rsplit("#", 1)[0]
-        xlid = url.rsplit("/", 1)[-1]
-        print("curl -s -HAccept:application/ld+json -o {} {}".format(xlid, url))
-    ' | bash
+    curl -s -HAccept:application/ld+json "https://libris-$ENV.kb.se/find.jsonld?@type=Instance&_limit=$LIMIT" | python -c '
+import sys, json
+data = json.load(sys.stdin)
+for item in data["items"]:
+    url = item["@id"].rsplit("#", 1)[0]
+    xlid = url.rsplit("/", 1)[-1]
+    print("curl -s -HAccept:application/ld+json -o {}.jsonld {}".format(xlid, url))
+' | bash
     popd
 fi
 
