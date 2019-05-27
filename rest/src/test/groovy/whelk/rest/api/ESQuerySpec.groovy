da64c0c7676f308012b0cd72ecab0de01841c38d
Niklas Lindström <lindstream@gmail.com>
2019-05-27 14:38:42 +0200

    Add spec for ESQuery.computeBoostFieldsFromLenses

--- rest/src/test/groovy/whelk/rest/api/ESQuerySpec.groovy
+++ rest/src/test/groovy/whelk/rest/api/ESQuerySpec.groovy
@@ -221,4 +221,61 @@ class ESQuerySpec extends Specification {
         emptyExpected == es.hideKeywordFields(emptyEsResponse)
         expected == es.hideKeywordFields(esResponse)
     }
+
+    def "should compute boost fields from lenses"() {
+        given:
+        def context = [
+            '@context': [
+                '@vocab': 'http://example.org/ns/'
+            ]
+        ]
+
+        def display = [
+            "lensGroups": [
+                "chips": [
+                    "lenses": [
+                        "Instance": [
+                            "showProperties": ["hasTitle", "comment"]
+                        ],
+                        "Publication": [
+                            "showProperties": ["agent"]
+                        ]
+                    ]
+                ],
+                "cards": [
+                    "lenses": [
+                        "Instance": [
+                            "showProperties": ["hasTitle", "publication"]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+        def vocab = [
+            "@graph": [
+                ["@id": "http://example.org/ns/Publication",
+                 "subClassOf": ["@id": "http://example.org/ns/QualifiedRole"]],
+                ["@id": "http://example.org/ns/Title",
+                 "subClassOf": ["@id": "http://example.org/ns/StructuredValue"]],
+                ["@id": "http://example.org/ns/hasTitle",
+                 "@type": "ObjectProperty",
+                 "range": ["@id": "http://example.org/ns/Title"]],
+                ["@id": "http://example.org/ns/publication",
+                 "@type": "ObjectProperty",
+                 "range": ["@id": "http://example.org/ns/Publication"]],
+                ["@id": "http://example.org/ns/agent",
+                 "@type": "ObjectProperty",
+                 "range": ["@id": "http://example.org/ns/Agent"]],
+            ]
+        ]
+
+        def jsonld = new JsonLd(context, display, vocab)
+
+        when:
+        def boostFields = ESQuery.computeBoostFieldsFromLenses(jsonld, ["Instance"] as String[])
+
+        then:
+        boostFields == ['_str^100', 'hasTitle._str^200', 'comment^200', 'publication._str^10']
+    }
 }
