955e484aff1643c8e01b90e45d00012023d9e208
Lars Rosenström <lars.rosenstrom@kb.se>
2019-06-03 11:32:38 +0200

    Changed allowed sigels for CTHB

--- whelktool/scripts/cleanups/2019/06/delete-holdings-for-CTHB.groovy
+++ whelktool/scripts/cleanups/2019/06/delete-holdings-for-CTHB.groovy
@@ -1,23 +1,23 @@
-/* This deletes selected holdings for CTHB based on file input.
+/*
+ * This script deletes selected holdings for CTHB based on file input.
  *
  * See LXL-2543 for more info.
  *
  */
-
-String SIGEL_TO_DELETE = "CTHB"
+List SIGEL_FOR_CTHB = ["Z", "Za", "Zl", "Enll"]
 PrintWriter failedHoldIDs = getReportWriter("failed-to-delete-holdIDs")
 PrintWriter scheduledForDeletion = getReportWriter("scheduled-for-deletion")
 String HOLD_ID_FILE = 'lxl-2543-holdIds.txt'
 File holdIds = new File(scriptDir, HOLD_ID_FILE)
 
 selectByIds(holdIds.readLines()) { hold ->
-    if (hold.doc.sigel == SIGEL_TO_DELETE) {
+    if (SIGEL_FOR_CTHB.contains(hold.doc.sigel)) {
         scheduledForDeletion.println("${hold.doc.getURI()}")
         hold.scheduleDelete(onError: { e ->
             failedHoldIDs.println("Failed to delete ${hold.doc.shortId} due to: $e")
         })
     } else {
         failedHoldIDs.println("Failed to delete ${hold.doc.shortId}, " +
-                "item is not held by ${SIGEL_TO_DELETE}")
+                "item is held by ${hold.doc.sigel} not CTHB")
     }
 }
