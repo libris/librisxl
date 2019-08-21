1d730d2824fc0b0e81f425225a6f334caaa918c8
Sara-Lena Wiktor <sara-lena.wiktor@kb.se>
2019-08-21 14:20:54 +0200

    Move collection of required codes outside iteration of subhandlersByEntity
    To be able to keep track of the combination of more than one required.
    subhandlersByEntity does not necessarily contain all subhandlers and
    thus a combination may be missed.

--- whelk-core/src/main/groovy/whelk/converter/marc/MarcFrameConverter.groovy
+++ whelk-core/src/main/groovy/whelk/converter/marc/MarcFrameConverter.groovy
@@ -2293,6 +2293,11 @@ class MarcFieldHandler extends BaseMarcFieldHandler {
 
         orderedAndGroupedSubfields.each { subhandlers ->
 
+            subhandlers.each {
+                if (it.required)
+                    requiredCodes << it.code
+            }
+
             def about = subhandlers[0].about ?: aboutAlias
 
             def subhandlersByEntity
@@ -2310,10 +2315,6 @@ class MarcFieldHandler extends BaseMarcFieldHandler {
                 def code = subhandler.code
                 if (failedRequired)
                     return
-
-                if (subhandler.required) {
-                    requiredCodes << code
-                }
 
                 if (subhandler.requiresI1) {
                     if (i1 == null) {
