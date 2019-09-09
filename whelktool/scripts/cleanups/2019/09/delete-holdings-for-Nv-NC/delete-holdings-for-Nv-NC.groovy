4024829ff024c3070a853b70e64368ff263f8367
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2019-09-09 16:32:13 +0200

    Handle component hold items

--- whelktool/scripts/cleanups/2019/09/delete-holdings-for-Nv-NC/delete-holdings-for-Nv-NC.groovy
+++ whelktool/scripts/cleanups/2019/09/delete-holdings-for-Nv-NC/delete-holdings-for-Nv-NC.groovy
@@ -7,16 +7,43 @@
 
 PrintWriter failedHoldIDs = getReportWriter("failed-to-delete-holdIDs")
 PrintWriter scheduledForDeletion = getReportWriter("scheduled-for-deletion")
+PrintWriter scheduledForUpdate = getReportWriter("scheduled-for-update")
 
 where = """
 collection = 'hold'
 and data #>>'{@graph,1,heldBy,@id}' = 'https://libris.kb.se/library/Nv'
-and data #>>'{@graph,1,shelfMark,label}' like 'NC %'
 """
 
 selectBySqlWhere(where, silent: false) { hold ->
-    scheduledForDeletion.println("${hold.doc.getURI()}")
-    hold.scheduleDelete(onError: { e ->
-        failedHoldIDs.println("Failed to delete ${hold.doc.shortId} due to: $e")
-    })
+    def delete = {
+        scheduledForDeletion.println("${hold.doc.getURI()}")
+        hold.scheduleDelete(onError: { e ->
+            failedHoldIDs.println("Failed to delete ${hold.doc.shortId} due to: $e")
+        })
+    }
+
+    item = hold.doc.data['@graph'][1]
+    if(isNcShelf(item)) {
+        delete()
+    }
+    else if (item['hasComponent']) {
+        List components = item['hasComponent']
+        if (components.removeAll(this.&isNcShelf)) {
+            if (components.isEmpty()) {
+                delete()
+            } else {
+                scheduledForUpdate.println("${hold.doc.getURI()}")
+                hold.scheduleSave()
+            }
+        }
+    }
+}
+
+boolean isNcShelf(holdItem) {
+    (holdItem['shelfMark'] && holdItem['shelfMark']['label'].toString().startsWith('NC')) ||
+    (holdItem['physicalLocation']
+            && holdItem['physicalLocation'].size() == 1
+            && holdItem['physicalLocation'][0].startsWith('NC')
+    )
+
 }
