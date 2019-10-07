03fd20db1eca4eafba1f87a2414ae676c853c9f7
Sara-Lena Wiktor <sara-lena.wiktor@kb.se>
2019-10-07 19:01:31 +0200

    Adjust sql query to speed up performance

--- whelktool/scripts/cleanups/2019/10/lxl-2594-fix-faulty-encodingLevels/set-correct-bib-encodingLevel-value.groovy
+++ whelktool/scripts/cleanups/2019/10/lxl-2594-fix-faulty-encodingLevels/set-correct-bib-encodingLevel-value.groovy
@@ -8,26 +8,27 @@
 
 PrintWriter failedBibIDs = getReportWriter("failed-to-update-bibIDs")
 PrintWriter scheduledForUpdate = getReportWriter("scheduled-for-update")
-VALID_BIB_ENCODING_LEVELS = ["marc:AbbreviatedLevel",
-                             "marc:CoreLevel",
-                             "marc:FullLevel",
-                             "marc:FullLevelMaterialNotExamined",
-                             "marc:LessThanFullLevelMaterialNotExamined",
-                             "marc:MinimalLevel",
-                             "marc:PartialPreliminaryLevel",
-                             "marc:PrepublicationLevel"]
+BIB_ENCODING_LEVEL = "marc:AbbreviatedLevel"
 
 selectBySqlWhere("""
-        collection = 'bib' AND data#>>'{@graph,0,encodingLevel}' IS NOT NULL
+        collection = 'bib' AND 
+        (data#>>'{@graph,0,encodingLevel}' = 'marc:HoldingsLevel1' OR
+        data#>>'{@graph,0,encodingLevel}' = 'marc:HoldingsLevel2' OR
+        data#>>'{@graph,0,encodingLevel}' = 'marc:HoldingsLevel3' OR
+        data#>>'{@graph,0,encodingLevel}' = 'marc:HoldingsLevel4' OR
+        data#>>'{@graph,0,encodingLevel}' = 'marc:HoldingsLevel4WithPieceDesignation' OR
+        data#>>'{@graph,0,encodingLevel}' = 'marc:MixedLevels' OR
+        data#>>'{@graph,0,encodingLevel}' = 'marc:OtherLevel' OR
+        data#>>'{@graph,0,encodingLevel}' = 'marc:CompleteAuthorityRecord' OR
+        data#>>'{@graph,0,encodingLevel}' = 'marc:IncompleteAuthorityRecord') AND 
+        lddb.deleted = FALSE
     """) { data ->
 
     def (record, instance, work) = data.graph
 
-    if (!VALID_BIB_ENCODING_LEVELS.contains(record['encodingLevel'])) {
-        record.put('encodingLevel', VALID_BIB_ENCODING_LEVELS[0])
-        scheduledForUpdate.println("${record[ID]}")
-        data.scheduleSave(onError: { e ->
-            failedBibIDs.println("Failed to save ${record[ID]} due to: $e")
-        })
-    }
+    record.put('encodingLevel', BIB_ENCODING_LEVEL)
+    scheduledForUpdate.println("${record[ID]}")
+    data.scheduleSave(onError: { e ->
+        failedBibIDs.println("Failed to save ${record[ID]} due to: $e")
+    })
 }
