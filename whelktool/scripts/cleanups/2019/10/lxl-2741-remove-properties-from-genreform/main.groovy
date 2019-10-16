ad34224c45bcd8ac7846a6b5a2316af609b730b1
klngwll <fredrik.klingwall@gmail.com>
2019-10-16 17:36:27 +0200

    Update optimized script

--- whelktool/scripts/cleanups/2019/10/lxl-2741-remove-properties-from-genreform/main.groovy
+++ whelktool/scripts/cleanups/2019/10/lxl-2741-remove-properties-from-genreform/main.groovy
@@ -3,28 +3,28 @@ See LXL-2741 & LXL-2395 for more info.
 */
 
 PrintWriter failedIDs = getReportWriter("failed-to-delete-bibIDs")
-PrintWriter scheduledForChange = getReportWriter("scheduledForChange")
+scheduledForChange = getReportWriter("scheduledForChange")
 
 selectBySqlWhere("collection = 'auth' AND data#>>'{@graph,1,@type}' = 'GenreForm'", silent: false) { documentItem ->
     def record = documentItem.doc.data.get('@graph')[0]
 
-    removeFieldFromRecord(record, "descriptionConventions", scheduledForChange)
-    removeFieldFromRecord(record, "marc:catalogingSource", scheduledForChange)
-    removeFieldFromRecord(record, "marc:kindOfRecord", scheduledForChange)
-    removeFieldFromRecord(record, "marc:level", scheduledForChange)
-    removeFieldFromRecord(record, "marc:numberedSeries", scheduledForChange)
-    removeFieldFromRecord(record, "marc:personalName", scheduledForChange)
-    removeFieldFromRecord(record, "marc:reference", scheduledForChange)
-    removeFieldFromRecord(record, "marc:romanization", scheduledForChange)
-    removeFieldFromRecord(record, "marc:transcribingAgency", scheduledForChange)
-    removeFieldFromRecord(record, "marc:typeOfSeries", scheduledForChange)
+    removeFieldFromRecord(record, "descriptionConventions")
+    removeFieldFromRecord(record, "marc:catalogingSource")
+    removeFieldFromRecord(record, "marc:kindOfRecord")
+    removeFieldFromRecord(record, "marc:level")
+    removeFieldFromRecord(record, "marc:numberedSeries")
+    removeFieldFromRecord(record, "marc:personalName")
+    removeFieldFromRecord(record, "marc:reference")
+    removeFieldFromRecord(record, "marc:romanization")
+    removeFieldFromRecord(record, "marc:transcribingAgency")
+    removeFieldFromRecord(record, "marc:typeOfSeries")
 
     documentItem.scheduleSave(onError: { e ->
         failedIDs.println("Failed to save ${record[ID]} due to: $e")
     })
 }
 
-private void removeFieldFromRecord(record, String fieldName, scheduledForChange) {
+private void removeFieldFromRecord(record, String fieldName) {
     if (record.remove(fieldName) == null) {
         scheduledForChange.println "Field $fieldName not present for ${record[ID]}"
     } else {
