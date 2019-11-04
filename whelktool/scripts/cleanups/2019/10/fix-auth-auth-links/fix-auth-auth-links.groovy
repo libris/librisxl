6f62b500667b8ca1ac14d03461833b1d4050418e
Lars Rosenström <lars.rosenstrom@kb.se>
2019-11-04 08:10:00 +0100

    Generalized script to work for all sameAs-links

--- whelktool/scripts/cleanups/2019/10/fix-auth-auth-links/fix-auth-auth-links.groovy
+++ whelktool/scripts/cleanups/2019/10/fix-auth-auth-links/fix-auth-auth-links.groovy
@@ -1,5 +1,5 @@
 /*
- *  This script takes broken links for auth records in 'broader', 'narrower' and 'related' objects such as
+ *  This script takes broken sameAs links for auth records at any level in the tree for objects such as
  *
  *  { "@type" : "Topic", "sameAs" : [{ "@id" : "https://id.kb.se/term/barn/Vardagsliv" }],
  *  "prefLabel" : "Vardagsliv" }
@@ -12,39 +12,45 @@
  *  See LXL-2721 for more info.
  *
  */
+import datatool.util.DocumentUtil
 
 scheduledForChange = getReportWriter("scheduled-for-change")
 
-selectBySqlWhere("collection = 'auth'") { data ->
+selectByCollection('auth') { data ->
 
-    instance = data.graph[1]
+    def instance = data.graph[1]
 
-    repairLinksFor(instance.related, "related", data)
-    repairLinksFor(instance.broader, "broader", data)
-    repairLinksFor(instance.narrower, "narrower", data)
-}
-
-private repairLinksFor(relation, name, data) {
-    relation.eachWithIndex { brokenLink, ix ->
-
-        linkerType = brokenLink.'@type'
-        linkerPrefLabel = brokenLink.'prefLabel'
-        linkerId = brokenLink.sameAs?.first()?.'@id'
-
-        if(!linkerId) {
-            return
-        }
-
-        selectByIds([linkerId]) { linkedData ->
-            auth = linkedData.graph[1]
-            if (auth.'@type' == linkerType && auth.'prefLabel' == linkerPrefLabel) {
-                correctedLink = ['@id': linkerId]
-                scheduledForChange.println "Will replace ${relation[ix]} with $correctedLink for ${data.graph[0].'@id'} and relation $name"
-                relation[ix] = correctedLink
-                data.scheduleSave()
+    boolean modified = DocumentUtil.traverse(instance, { object, path ->
+        if (object instanceof Map && object.sameAs && path.size() > 0) {
+            linkerId = object.sameAs.first()?.'@id' as String
+            if (linkerId && linkedObjectIsValid(object, linkerId)) {
+                def correctedLink = ['@id': linkerId]
+                scheduledForChange.println "Will replace $object with $correctedLink for ${data.graph[0].'@id'} in the path $path"
+                return new DocumentUtil.Replace(correctedLink)
             }
         }
+    })
+    if (modified) {
+        data.scheduleSave()
     }
+}
+
+private boolean linkedObjectIsValid(linker, linkerId) {
+    def linkerType = linker.'@type'
+    def linkerPrefLabel = linker.prefLabel
+
+    boolean isValid = false
+
+    selectByIds([linkerId]) { linkedData ->
+        def linkedObject = linkedData.graph[1]
+        scheduledForChange.println "Linked object: $linkedObject"
+        scheduledForChange.println "LinkerType: ${linkerPrefLabel}"
+        scheduledForChange.println "LinkerPrefLabel: $linkerType"
+
+        isValid = linkedObject.'@type' == linkerType && linkedObject.prefLabel == linkerPrefLabel
+    }
+
+    return isValid
 }
 
 //   Handle closeMatch:
