ae302ef6eb484fb6b471ed3d706973b1c6f58eb2
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2019-12-20 13:10:50 +0100

    Don't wait for MarcFrameConverter on startup

---
+++ rest/src/main/groovy/whelk/rest/api/MarcFrameConverterInitializer.groovy
@@ -0,0 +1,37 @@
+package whelk.rest.api
+
+import groovy.util.logging.Log4j2 as Log
+import whelk.converter.marc.MarcFrameConverter
+import whelk.util.WhelkFactory
+
+import javax.servlet.ServletContextEvent
+import javax.servlet.ServletContextListener
+
+@Log
+class MarcFrameConverterInitializer implements ServletContextListener {
+
+    @Override
+    void contextInitialized(ServletContextEvent sce) {
+        initMarcFrameConverterAsync()
+    }
+
+    @Override
+    void contextDestroyed(ServletContextEvent sce) {
+        //
+    }
+
+    void initMarcFrameConverterAsync() {
+        new Thread(new Runnable() {
+            void run() {
+                try {
+                    WhelkFactory.getSingletonWhelk().getMarcFrameConverter()
+                    log.info("Started {}", MarcFrameConverter.class.getSimpleName())
+                }
+                catch (Exception e) {
+                    log.warn(String.format("Error starting %s: %s", MarcFrameConverter.class.getSimpleName(), e), e)
+                }
+            }
+        }).start()
+    }
+
+}
