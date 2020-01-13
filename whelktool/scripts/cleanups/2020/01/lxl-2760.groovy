57e3ad1ba1c3d381c5c05449545c65f8d84ab5ed
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2020-01-13 13:01:45 +0100

    Add a script for changing complex subjects on holds.

---
+++ whelktool/scripts/cleanups/2020/01/lxl-2760.groovy
@@ -0,0 +1,40 @@
+// A bleak imitation (for hold) of 2018/10/remodel-termComponentList.groovy
+
+subdivisionTypes = [
+        'GenreForm' : 'GenreSubdivision',
+        'Temporal' : 'TemporalSubdivision',
+        'Geographic' : 'GeographicSubdivision'
+]
+
+COMPLEX_SUBJECT_TYPE = 'ComplexSubject'
+
+boolean changeToSubdivision(it, subdivisionTypes) {
+    boolean changed = false
+    it.termComponentList.eachWithIndex { term, i ->
+        if (i != 0) {
+            if (subdivisionTypes[term['@type']]) {
+                term['@type'] = subdivisionTypes[term['@type']]
+                changed = true
+            }
+        }
+    }
+    return changed
+}
+
+selectBySqlWhere('''
+        collection = 'hold' AND data::text LIKE '%"termComponentList"%'
+        ''') { data ->
+    // guard against missing entity
+    if (data.graph.size() < 2) {
+        return
+    }
+
+    def (record, instance) = data.graph
+    instance.subject.findAll {
+        it['@type'] == COMPLEX_SUBJECT_TYPE
+    }.each {
+        if (changeToSubdivision(it, subdivisionTypes)) {
+            data.scheduleSave()
+        }
+    }
+}
