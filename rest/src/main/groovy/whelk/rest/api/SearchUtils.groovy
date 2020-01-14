3531d91256268d163287568b40cb09f7331124aa
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2020-01-14 13:55:50 +0100

    URLencode reverserLinks in search results.

--- rest/src/main/groovy/whelk/rest/api/SearchUtils.groovy
+++ rest/src/main/groovy/whelk/rest/api/SearchUtils.groovy
@@ -201,7 +201,7 @@ class SearchUtils {
                 def item = ld.toCard(it)
                 item['reverseLinks'] = [
                         (JsonLd.TYPE_KEY) : 'PartialCollectionView',
-                        (JsonLd.ID_KEY) : Document.getBASE_URI().toString() + 'find?o=' + it['@id'],
+                        (JsonLd.ID_KEY) : URLEncoder.encode(Document.getBASE_URI().resolve('find?o=' + it['@id']).toString()),
                         'totalItems' : it['meta']['linksHereCount']
                 ]
                 return item
