bd0a70751e5505d82a845b53f49e97a8a373b75c
Sara-Lena Wiktor <sara-lena.wiktor@kb.se>
2020-01-28 15:59:30 +0100

    Only move expressionOf (not removeing legacy properties)

--- whelktool/scripts/2019/11/remodel-link-entries/move-expressionOf.groovy
+++ whelktool/scripts/2019/11/remodel-link-entries/move-expressionOf.groovy
@@ -2,13 +2,18 @@
  * This removes legacy marc properties from link entries
  * and moves expressionOf components to the Work entity.
  *
+ * List of ids are generated with:
+ * String subQueryWork = LINK_FIELDS_WORK.collect {"data#>>'{@graph,2,${it}}' IS NOT NULL"}.join(" OR ")
+ * String query = "collection = 'bib' AND ( ${subQueryWork} )"
+ *
  * See LXL-2833 for more info.
  */
 
 PrintWriter failedIDs = getReportWriter("failed-to-update")
 scheduledForChange = getReportWriter("scheduled-for-change")
+expressionOfpropertiesNotMoved = getReportWriter("properties-not-moved")
+File bibIds = new File(scriptDir, 'linkfields-with-expressionOf')
 
-LINK_FIELDS_INSTANCE = ['hasSeries', 'hasSubseries']
 LINK_FIELDS_WORK = ['translationOf', 'translation', 'supplement', 'supplementTo',
                     'hasPart', 'precededBy', 'continues', 'continuesInPart',
                     'precededInPartBy', 'mergerOf', 'absorbed', 'absorbedInPart',
@@ -17,31 +22,22 @@ LINK_FIELDS_WORK = ['translationOf', 'translation', 'supplement', 'supplementTo'
                     'mergedToForm', 'dataSource', 'relatedTo', 'isPartOf',
                     'otherEdition', 'issuedWith' ]
 
-String subQueryInstance = LINK_FIELDS_INSTANCE.collect {"data#>>'{@graph,1,${it}}' IS NOT NULL"}.join(" OR ")
-String subQueryWork = LINK_FIELDS_WORK.collect {"data#>>'{@graph,2,${it}}' IS NOT NULL"}.join(" OR ")
-String query = "collection = 'bib' AND ( ${subQueryInstance} OR ${subQueryWork} )"
-
-selectBySqlWhere(query) { data ->
+selectByIds(['l4x16knx27bvjvz']) { data ->
     def (record, thing, work) = data.graph
 
-    thing.subMap(LINK_FIELDS_INSTANCE).each {
-        updateProperties(data, it.getKey(), it.getValue(), false)
-    }
-
     work.subMap(LINK_FIELDS_WORK).each {
-        updateProperties(data, it.getKey(), it.getValue(), true)
+        updateProperties(data, it.getKey(), it.getValue())
     }
 }
 
-void updateProperties(data, propertyName, toUpdate, isWork) {
-    boolean someThingWasRemoved
+void updateProperties(data, propertyName, toUpdate) {
     boolean expressionOfWasUpdated
     objectsToUpdate = (toUpdate instanceof List) ? toUpdate : [toUpdate]
 
     objectsToUpdate.each {
         if (propertyName == 'isPartOf' && it[TYPE] != 'Aggregate')
             return
-        expressionOfWasUpdated = isWork ? moveExpressionOf(it) : false
+        expressionOfWasUpdated = moveExpressionOf(it, data)
     }
 
     if (expressionOfWasUpdated) {
@@ -52,12 +48,16 @@ void updateProperties(data, propertyName, toUpdate, isWork) {
     }
 }
 
-boolean moveExpressionOf(object) {
-    valuesToMove = object['expressionOf'] ? object.expressionOf.findAll { it.key != TYPE } : null
-    if (valuesToMove) {
-        object << valuesToMove
-        object.remove('expressionOf')
-        return true
+boolean moveExpressionOf(object, data) {
+    boolean wasChanged = false
+    object.remove('expressionOf')?.findAll { key, value ->
+        if (!object.containsKey(key)) {
+            object[key] = value
+            wasChanged = true
+        } else {
+            if (!object[key] == TYPE)
+                expressionOfpropertiesNotMoved.println "${key} already exists for ${data.graph[0][ID]}"
+        }
     }
-    return false
+    return wasChanged
 }
