b4d96b9e62676be1a2ace7d131ea83c14e711a05
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-02-06 10:24:50 +0100

    Naming things. Add comment about additional normalization

--- whelk-core/src/main/groovy/whelk/util/Unicode.groovy
+++ whelk-core/src/main/groovy/whelk/util/Unicode.groovy
@@ -3,6 +3,12 @@ package whelk.util
 import java.text.Normalizer
 
 class Unicode {
+
+    /**
+     * Typographical ligatures from the "Alphabetic Presentation Forms" unicode block
+     * that are not covered by NFC but we want to normalize.
+     * https://www.unicode.org/charts/PDF/UFB00.pdf
+     */
     private static final List FORBIDDEN_UNICODE_CHARS = [
             'ﬀ', // 'LATIN SMALL LIGATURE FF'
             'ﬃ', // 'LATIN SMALL LIGATURE FFI'
@@ -13,20 +19,20 @@ class Unicode {
             'ﬆ', // 'LATIN SMALL LIGATURE ST'
     ]
 
-    private static final Map FORBIDDEN_UNICODE_MAP
+    private static final Map EXTRA_NORMALIZATION_MAP
 
     static {
-        FORBIDDEN_UNICODE_MAP = FORBIDDEN_UNICODE_CHARS.collectEntries {
+        EXTRA_NORMALIZATION_MAP = FORBIDDEN_UNICODE_CHARS.collectEntries {
             [(it): Normalizer.normalize(it, Normalizer.Form.NFKC)]
         }
     }
 
     static boolean isNormalized(String s) {
-        return Normalizer.isNormalized(s, Normalizer.Form.NFC) && !FORBIDDEN_UNICODE_MAP.keySet().any{ s.contains(it) }
+        return Normalizer.isNormalized(s, Normalizer.Form.NFC) && !EXTRA_NORMALIZATION_MAP.keySet().any{ s.contains(it) }
     }
 
     static String normalize(String s) {
-        return Normalizer.normalize(s, Normalizer.Form.NFC).replace(FORBIDDEN_UNICODE_MAP)
+        return Normalizer.normalize(s, Normalizer.Form.NFC).replace(EXTRA_NORMALIZATION_MAP)
     }
 
 }
