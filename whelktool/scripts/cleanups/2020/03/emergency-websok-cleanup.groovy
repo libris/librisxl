4e30ae7f1438518d2f251b181582e6900f7f7848
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2020-03-17 13:26:00 +0100

    additional clause

--- whelktool/scripts/cleanups/2020/03/emergency-websok-cleanup.groovy
+++ whelktool/scripts/cleanups/2020/03/emergency-websok-cleanup.groovy
@@ -4,6 +4,7 @@ PrintWriter scheduledForDeletion = getReportWriter("scheduled-for-deletion")
 String where =
         "created > '2020-03-13 10:00:00' and\n" +
                 "created < '2020-03-13 16:00:00' and\n" +
+                "changedby = 'SEK' and\n" +
                 "data#>>'{@graph,0,recordStatus}' = 'marc:New' and\n" +
                 "data#>>'{@graph,0,encodingLevel}' = 'marc:PartialPreliminaryLevel' and\n" +
                 "data#>>'{@graph,1,hasTitle}' is null"
