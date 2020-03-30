d0b943bdf7727bda28ac3879f650d0024534e7ac
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2020-03-30 11:22:54 +0200

    Add a FormerTitle cleanup script.

---
+++ whelktool/scripts/cleanups/2020/03/lxl-1828.groovy
@@ -0,0 +1,37 @@
+PrintWriter scheduledForUpdate = getReportWriter("scheduled-for-update")
+PrintWriter failedBibIDs = getReportWriter("failed-to-update-bibIDs")
+
+String where = "collection = 'bib' and data#>'{@graph,1}' ?? 'marc:previousTitle'"
+
+selectBySqlWhere(where) { data ->
+    def (record, mainEntity) = data.graph
+
+    if (mainEntity.hasTitle == null)
+        mainEntity.hasTitle = []
+
+    Map prevTitleEnt = mainEntity.get("marc:previousTitle")
+    boolean changed = false
+    if (prevTitleEnt instanceof List) {
+        for (Object elem : prevTitleEnt)
+            changed |= moveTitle( (Map) elem, mainEntity.hasTitle )
+    } else {
+        changed = moveTitle(prevTitleEnt, mainEntity.hasTitle)
+    }
+    mainEntity.remove("marc:previousTitle")
+
+    if (changed) {
+        scheduledForUpdate.println("${data.doc.getURI()}")
+        data.scheduleSave(onError: { e ->
+            failedBibIDs.println("Failed to update ${data.doc.shortId} due to: $e")
+        })
+    }
+}
+
+boolean moveTitle(Map titleEnt, hasTitleList) {
+    if (titleEnt["@type"] != "Title")
+        return false
+
+    titleEnt["@type"] = "FormerTitle"
+    hasTitleList.add(titleEnt)
+    return true
+}
