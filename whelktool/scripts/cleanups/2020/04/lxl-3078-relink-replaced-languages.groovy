46b79766beffff3bbb0cdd094b9a436b70c91880
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-04-16 10:06:12 +0200

    Add script for replacing links to obsolete language codes

---
+++ whelktool/scripts/cleanups/2020/04/lxl-3078-relink-replaced-languages.groovy
@@ -0,0 +1,32 @@
+/**
+ *  Replace links to obsolete language codes with their replacements (if one-to-one).
+ *
+ *  See LXL-3078 for more info
+ */
+
+import whelk.util.DocumentUtil
+import whelk.util.DocumentUtil.Replace
+
+PrintWriter scheduledForUpdate = getReportWriter('scheduled-for-update')
+
+queryDocs(['@type': ['Language']])
+        .grep{it['isReplacedBy'] instanceof Map || it['isReplacedBy']?.size() == 1}
+        .each { obsolete ->
+            String id = obsolete['@id']
+            String replacedBy = obsolete['isReplacedBy']['@id']
+
+            selectByIds(queryIds(['o': [id]]).collect()) { d ->
+                boolean modified = DocumentUtil.findKey(d.doc.data, '@id') { value, path ->
+                    if (value == id) {
+                        new Replace(replacedBy)
+                    }
+                }
+
+                if (modified) {
+                    String msg = "${d.doc.shortId} : $id --> $replacedBy"
+                    println(msg)
+                    scheduledForUpdate.println(msg)
+                    d.scheduleSave()
+                }
+            }
+        }
