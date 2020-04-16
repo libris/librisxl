d7d3adba7a2a0e65f89cc1297aac5ef8c5a970aa
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-04-16 12:22:51 +0200

    Add script for adding missing translators from responsibilityStatement

--- whelk-core/src/main/groovy/whelk/util/Unicode.groovy
+++ whelk-core/src/main/groovy/whelk/util/Unicode.groovy
@@ -45,4 +45,20 @@ class Unicode {
     static String normalizeForSearch(String s) {
         return Normalizer.normalize(s, Normalizer.Form.NFKC)
     }
+
+    /**
+     * Removes leading and trailing non-"alpha, digit or parentheses".
+     */
+    static String trimNoise(String s) {
+        return trimLeadingNoise(trimLeadingNoise(s).reverse()).reverse()
+    }
+
+    /**
+     * Removes leading non-"alpha, digit or parentheses".
+     */
+    static String trimLeadingNoise(String s) {
+        def w = /\(\)\p{IsAlphabetic}\p{Digit}/
+        def m = s =~ /[^${w}]*(.*)/
+        return m.matches() ? m.group(1) : s
+    }
 }
