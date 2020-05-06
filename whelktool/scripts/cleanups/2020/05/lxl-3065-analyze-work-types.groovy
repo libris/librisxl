9cf4bfdaa9f6247c779efcf246c4da73635b47dc
Lars Rosenström <lars.rosenstrom@kb.se>
2020-05-06 16:24:11 +0200

    Get SAB codes and work types

---
+++ whelktool/scripts/cleanups/2020/05/lxl-3065-analyze-work-types.groovy
@@ -0,0 +1,43 @@
+/* For analysis */
+
+report = getReportWriter("report")
+
+selectByCollection('bib') { bib ->
+    def work = getWork(bib)
+    def recordId = data.graph[0][ID]
+    def sabClassifications = getAllSAB(work)
+    def codes = sabClassifications.collect { getSABcode(it)}
+    report.println("SAB codes: $codes Type: $work Record ID: $recordId")
+}
+
+private List getAllSAB(work) {
+    def classif = work.classification
+    classif = classif instanceof Map ? [classif] : classif
+    return classif?.findAll { c -> isSAB(c) }
+}
+
+boolean isSAB(classification) {
+    if (isInstanceOf(classification, 'Classification')) {
+        def inSchemeCode = classification.inScheme?.code as String
+        return inSchemeCode && inSchemeCode == "kssb" && isInstanceOf(classification.inScheme, 'ConceptScheme')
+    }
+    return false
+}
+
+boolean getSABcode(classification) {
+    return classification?.code instanceof String ? [classification.code] : classification.code
+}
+
+Map getWork(def bib) {
+    def (record, thing, work) = bib.graph
+    if (thing && isInstanceOf(thing, 'Work')) {
+        return thing
+    }
+    else if(thing && thing['instanceOf'] && isInstanceOf(thing['instanceOf'], 'Work')) {
+        return thing['instanceOf']
+    }
+    else if (work && isInstanceOf(work, 'Work')) {
+        return work
+    }
+    return null
+}
