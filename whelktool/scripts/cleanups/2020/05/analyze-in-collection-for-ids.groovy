6efdd804d71f7352e645e15b501a39255c72152f
Lars Rosenström <lars.rosenstrom@kb.se>
2020-05-13 15:17:52 +0200

    add quotes

--- whelktool/scripts/cleanups/2020/05/analyze-in-collection-for-ids.groovy
+++ whelktool/scripts/cleanups/2020/05/analyze-in-collection-for-ids.groovy
@@ -2,7 +2,7 @@
 
 report = getReportWriter("report")
 
-selectByCollection(auth) { auth ->
+selectByCollection('auth') { auth ->
     def recordId = auth.graph[1][ID]
     report.println("Record ID: $recordId InCollection: ${auth.graph[1]['inCollection']}")
 }
