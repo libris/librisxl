fda3615df2267c0bfb272b0b3a7cbf0f7ad549e0
Lars Rosenström <lars.rosenstrom@kb.se>
2020-05-13 15:11:52 +0200

    Check inCollection on auth

--- whelktool/scripts/cleanups/2020/05/analyze-in-collection-for-ids.groovy
+++ whelktool/scripts/cleanups/2020/05/analyze-in-collection-for-ids.groovy
@@ -1,9 +1,8 @@
 /* For analysis */
 
-File bibIDsFile = new File(scriptDir, 'has_broader_ids.txt')
 report = getReportWriter("report")
 
-selectByIds(bibIDsFile.readLines()) { bib ->
-    def recordId = bib.graph[0][ID]
-    report.println("Record ID: $recordId InCollection: ${bib.graph[1]['inCollection']}")
+selectByCollection(auth) { auth ->
+    def recordId = auth.graph[1][ID]
+    report.println("Record ID: $recordId InCollection: ${auth.graph[1]['inCollection']}")
 }
