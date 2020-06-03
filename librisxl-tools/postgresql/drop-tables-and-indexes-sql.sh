08e4d01f9da68a9c55fde25f1bb7f2409a219ec8
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-06-03 10:59:29 +0200

    Fix script for dropping all lddb tables The old version didn't handle new tables created by DB migrations. Drop all tables starting with lddb__ instead.

--- librisxl-tools/postgresql/drop-tables-and-indexes-sql.sh
+++
@@ -1,4 +0,0 @@
-# NO LONGER USED!
-#!/bin/bash
-awk '/^CREATE TABLE IF NOT EXISTS/ { print "DROP TABLE IF EXISTS " $6 ";" }' $(dirname $0)/tables.sql
-awk '/^CREATE INDEX/ { print "DROP INDEX IF EXISTS " $3 ";" }' $(dirname $0)/indexes.sql
