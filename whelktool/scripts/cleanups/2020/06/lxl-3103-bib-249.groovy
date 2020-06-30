142bba30098f7a5c5631054673e20f3c87d2a37e
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-06-30 17:02:43 +0200

    Move 249

--- whelktool/scripts/cleanups/2020/06/lxl-3103-bib-249.groovy
+++ whelktool/scripts/cleanups/2020/06/lxl-3103-bib-249.groovy
@@ -3,15 +3,17 @@ import whelk.util.Statistics
 import org.apache.commons.lang3.StringUtils
 
 //s = new Statistics().printOnShutdown()
-Map MAP_249 = [
-        'marc:originalTitle': 'mainTitle',
-        'marc:titleRemainder': 'subtitle',
-        'marc:titleNumber': 'partNumber',
-        'marc:titlePart': 'partName',
-        'marc:nonfilingChars': 'marc:nonfilingChars'
-]
+class Script {
+    static Map MAP_249 = [
+            'marc:originalTitle' : 'mainTitle',
+            'marc:titleRemainder': 'subtitle',
+            'marc:titleNumber'   : 'partNumber',
+            'marc:titlePart'     : 'partName',
+            'marc:nonfilingChars': 'marc:nonfilingChars'
+    ]
 
-
+    static List TITLE_COMPONENTS = ['mainTitle', 'titleRemainder', 'subtitle', 'hasPart', 'partNumber', 'partName', 'marc:parallelTitle', 'marc:equalTitle']
+}
 selectByCollection('bib') { bib ->
     try {
         process(bib)
@@ -31,50 +33,85 @@ void process(bib) {
         return
     }
 
-    def _249s = listify(instance['marc:hasBib249'])
+    List _249s = listify(instance['marc:hasBib249'])
 
     if(_249s) {
-        println("${bib.doc.shortId}\t$_249s")
+        StringBuilder msg = new StringBuilder()
+        msg.append(bib.doc.shortId).append('\n')
+        msg.append(_249s).append('\n')
+
+        def converted = filterAndConvert(_249s, work, msg)
+
+        if (converted.size() == 1) {
+            def title = converted.first()
+            work['hasTitle'] = listify(work['hasTitle'])
+            if (work['hasTitle'].any{ it['@type'] == 'MainTitle' }) {
+                title['@type'] = 'VariantTitle'
+            }
+
+            work['hasTitle'].add(title)
+
+            msg.append("--> $title\n")
+            msg.append("--> work['hasTitle']: ${work['hasTitle']}\n")
+        }
+        else if (converted.size() > 1) {
+            if(work['hasPart']) {
+                throw new RuntimeException('Already hasPart')
+            }
+            work['hasPart'] = converted.collect {
+                [
+                        '@type': 'Work',
+                        'hasTitle': it
+                ]
+            }
+            msg.append("work['hasPart']: ${work['hasPart']}\n")
+        }
+
+        msg.append('\n')
+        println(msg.toString())
     }
 }
 
-String hasTitle(thing, work) {
-    if (work.hasTitle) {
-        isSameTitle(thing, work) ? "match" : "diff"
-    }
-    else {
-        "no"
+List filterAndConvert(List _249s, Map work, StringBuilder msg) {
+    def workTitles = [:]
+    DocumentUtil.findKey(work, 'hasTitle', { titles, path ->
+        listify(titles).eachWithIndex { title, i ->
+            workTitles.put((path + i).join(', '), title)
+        }
+    })
+
+    _249s.findResults { t ->
+        def c = convert249(t)
+
+        def matches = workTitles.findAll { path, title ->
+            comparisonTitle(title) == comparisonTitle(c)
+        }
+
+        if (!matches.isEmpty()) {
+            msg.append("$t ${comparisonTitle(c)}) already exists: $matches, dropping ")
+            return null
+        }
+        else {
+            return c
+        }
     }
 }
 
-boolean isSameTitle(def thing, def work) {
-
-
-
-    String t = getPathSafe(thing, ['marc:hasBib249', 'marc:originalTitle'], "TT")
-    String w = getPathSafe(work, ['hasTitle', 0, 'mainTitle'], "WT")
-    trim(w.toLowerCase()) == trim(t.toLowerCase())
+Map convert249(Map bib249) {
+    Map<String, String> result = ['@type': 'MainTitle']
+    Script.MAP_249.each { k, v ->
+        if (bib249[k]) {
+            result[v] = bib249[k]
+        }
+    }
+    if (result['mainTitle'] && result['subtitle'] && result['mainTitle'].endsWith(': ')) {
+        result['mainTitle'] = result['mainTitle'].substring(0, result['mainTitle'].length() - 2)
+    }
+    return result
 }
 
 String comparisonTitle(Map title) {
-    
-}
-
-Object maybeList(Object o, Closure c) {
-    o instanceof List
-            ? o.collect(c)
-            : c(o)
-}
-
-private Object getPathSafe(item, path, defaultTo = null) {
-    for (p in path) {
-        if (item[p] != null) {
-            item = item[p]
-        } else {
-            return defaultTo
-        }
-    }
-    return item
+    normalize(Script.TITLE_COMPONENTS.findResults { title.get(it, null) }.join(' '))
 }
 
 private static List listify(Object o) {
@@ -84,7 +121,6 @@ private static List listify(Object o) {
 String normalize(String s) {
     StringUtils.normalizeSpace(s.replaceAll(/[^\p{IsAlphabetic}\p{Digit} ] /, '').toLowerCase().trim())
 }
-
 Map getWork(def bib) {
     def (record, thing, work) = bib.graph
     if(thing && thing['instanceOf'] && thing['instanceOf']['@type']) {
