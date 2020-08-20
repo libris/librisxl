f3d1230852fa974f1cd70f3f4f0e4a60fa252ce6
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-08-20 19:52:10 +0200

    Update bib-976 script

--- whelktool/scripts/analysis/bib-976.groovy
+++ whelktool/scripts/analysis/bib-976.groovy
@@ -1,7 +1,13 @@
 import whelk.util.DocumentUtil
 import whelk.util.Statistics
 
-report = getReportWriter("report.txt")
+class Script {
+    static PrintWriter notIn81 = getReportWriter("not-in-81.txt")
+    static PrintWriter noCode = getReportWriter("no-code.txt")
+}
+Script.notIn81 = getReportWriter("not-in-81.txt")
+Script.noCode = getReportWriter("no-code.txt")
+
 s = new Statistics().printOnShutdown()
 
 selectByCollection('bib') { bib ->
@@ -23,19 +29,40 @@ void process(bib) {
     }
 
     def bib976 = asList(work['marc:hasBib976'])
-    if(bib976) {
-        def bib81 = sab(work)
-        def (in81, notIn81) = bib976.split { bib81.contains(it['marc:bib976-a'] ?: '')  }
+    if(!bib976) {
+        return
+    }
+
+    def (code, noCode) = bib976.split { it['marc:bib976-a'] }
+    def bib81 = sab(work)
+
+    handleWithSabCode(bib, work, bib81, code)
+    handleWithoutSabCode(bib, work, bib81, noCode)
+}
+
+void handleWithSabCode(bib, work, bib81, bib976) {
+
+    bib976.each {
+        def (in81, notIn81) = bib976.split { bib81.contains(it['marc:bib976-a'])  }
 
         in81.each {
-            s.increment('bib976 in bib81', it)
+            s.increment('bib976-a', 'in classification')
         }
 
-        notIn81.each{
-            s.increment('bib976 not in bib81', it)
-            report.println("${bib.doc.getURI()} $notIn81")
+        notIn81.each {
+            s.increment('bib976-a', 'not in classification')
+            Script.notIn81.println("${bib.doc.getURI()} $it")
         }
+    }
+}
 
+void handleWithoutSabCode(bib, work, bib81, bib976) {
+    if (bib976) {
+        s.increment('bib976 without code', bib.graph[0]['descriptionCreator'])
+
+        bib976.each {
+            Script.noCode.println("${bib.doc.getURI()} $it")
+        }
     }
 }
 
