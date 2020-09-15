bae1dce085fa19aa36f9f8904ba769849e547d90
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2020-09-15 14:46:27 +0200

    Add a controlNumber index and have the gui client NOT use IN (big-ass-list), since pgsql does not correctly optimize those queries.

--- gui-whelktool/src/main/resources/templates/create-hold.groovy
+++ gui-whelktool/src/main/resources/templates/create-hold.groovy
@@ -2,38 +2,38 @@ PrintWriter failedHoldIDs = getReportWriter("failed-to-create-holdIDs")
 PrintWriter createdHoldIDs = getReportWriter("attempted-to-create-holdIDs")
 File bibids = new File('INPUT')
 
-String bibidstring = bibids.readLines().join("','")
-
 Vector holdList = new Vector() // Vector because it must be synchronized
 
-selectBySqlWhere("data#>>'{@graph,0,controlNumber}' in ( '$bibidstring' )", silent: false, { bib ->
+for (String bibId : bibids) {
+    selectBySqlWhere("data#>>'{@graph,0,controlNumber}' = '$bibId'", silent: false, { bib ->
 
-    def bibMainEntity = bib.graph[1]["@id"]
+        def bibMainEntity = bib.graph[1]["@id"]
 
-    def holdData =
-            [ "@graph": [
-                    [
-                            "@id": "TEMPID",
-                            "@type": "Record",
-                            "mainEntity" : ["@id": "TEMPID#it"]
-                    ],
-                    [
-                            "@id": "TEMPID#it",
-                            "@type": "Item",
-                            "heldBy": ["@id": "https://libris.kb.se/library/SIGEL"],
-                            "itemOf": ["@id": bibMainEntity],
-                            "hasComponent": [
-                                    [
-                                            "@type": "Item",
-                                            "heldBy": ["@id": "https://libris.kb.se/library/SIGEL"]
-                                    ]
-                            ]
-                    ]
-            ]]
+        def holdData =
+                [ "@graph": [
+                        [
+                                "@id": "TEMPID",
+                                "@type": "Record",
+                                "mainEntity" : ["@id": "TEMPID#it"]
+                        ],
+                        [
+                                "@id": "TEMPID#it",
+                                "@type": "Item",
+                                "heldBy": ["@id": "https://libris.kb.se/library/SIGEL"],
+                                "itemOf": ["@id": bibMainEntity],
+                                "hasComponent": [
+                                        [
+                                                "@type": "Item",
+                                                "heldBy": ["@id": "https://libris.kb.se/library/SIGEL"]
+                                        ]
+                                ]
+                        ]
+                ]]
 
-    holdList.add( create(holdData) )
+        holdList.add( create(holdData) )
 
-})
+    })
+}
 
 selectFromIterable(holdList, { newlyCreatedItem ->
     createdHoldIDs.println(newlyCreatedItem.doc.shortId)
--- gui-whelktool/src/main/resources/templates/delete-bib.groovy
+++ gui-whelktool/src/main/resources/templates/delete-bib.groovy
@@ -2,11 +2,11 @@ PrintWriter failedBibIDs = getReportWriter("failed-to-delete-bibIDs")
 PrintWriter scheduledForDeletion = getReportWriter("scheduled-for-deletion")
 File bibids = new File('INPUT')
 
-String bibidstring = bibids.readLines().join("','")
-
-selectBySqlWhere("collection = 'bib' and data#>>'{@graph,0,controlNumber}' in ( '$bibidstring' )", silent: false, { bib ->
-    scheduledForDeletion.println("${bib.doc.getURI()}")
-    bib.scheduleDelete(onError: { e ->
-        failedBibIDs.println("Failed to delete ${bib.doc.shortId} due to: $e")
+for (String bibId : bibids) {
+    selectBySqlWhere("collection = 'bib' and data#>>'{@graph,0,controlNumber}' = '$bibId' ", silent: false, { bib ->
+        scheduledForDeletion.println("${bib.doc.getURI()}")
+        bib.scheduleDelete(onError: { e ->
+            failedBibIDs.println("Failed to delete ${bib.doc.shortId} due to: $e")
+        })
     })
-})
+}
