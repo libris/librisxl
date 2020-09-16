ca18ccfb26aeaa9c14af1e9ec08886524f7ac60f
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-09-16 13:18:08 +0200

    Fix id in uniformWorkTitle script

--- whelktool/scripts/cleanups/2020/09/lxl-3357-uniformWorkTitle.groovy
+++ whelktool/scripts/cleanups/2020/09/lxl-3357-uniformWorkTitle.groovy
@@ -9,7 +9,7 @@ PrintWriter report = getReportWriter("scheduled-for-update.txt")
 String where = "data#>>'{@graph,1,@type}' = 'Work' and data#>>'{@graph,1,sameAs}' LIKE '%resource/auth%'"
 selectBySqlWhere(where, { auth ->
 
-    auth.graph[1]['inCollection'] = [["@id": "http://id.kb.se/term/uniformWorkTitle"]]
+    auth.graph[1]['inCollection'] = [["@id": "https://id.kb.se/term/uniformWorkTitle"]]
 
     report.println(auth.doc.shortId)
 
