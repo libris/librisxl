39b52d68923acf605caf472709357e4716f2f225
Olov Ylinenpää <51744858+olovy@users.noreply.github.com>
2020-09-16 13:10:55 +0200

    Add script for marking all old MARC works with inCollection uniformWorkTitle (#730)

---
+++ whelktool/scripts/cleanups/2020/09/lxl-3357-uniformWorkTitle.groovy
@@ -0,0 +1,17 @@
+/**
+ * Mark all old MARC works with inCollection to separate the from the new BIBFRAME works.
+ *
+ * See LXL-3357 for more information
+ */
+
+PrintWriter report = getReportWriter("scheduled-for-update.txt")
+
+String where = "data#>>'{@graph,1,@type}' = 'Work' and data#>>'{@graph,1,sameAs}' LIKE '%resource/auth%'"
+selectBySqlWhere(where, { auth ->
+
+    auth.graph[1]['inCollection'] = [["@id": "http://id.kb.se/term/uniformWorkTitle"]]
+
+    report.println(auth.doc.shortId)
+
+    auth.scheduleSave()
+})
