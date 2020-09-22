1d098cc928c8ce53986d0eb63765d517a0140f7a
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2020-09-22 09:40:39 +0200

    Add a script that replaces all links to records with links to the corresponding mainEntities.

---
+++ whelktool/scripts/cleanups/2020/09/lxl-3152-linkOnlyMainEntity.groovy
@@ -0,0 +1,51 @@
+import whelk.Whelk
+
+PrintWriter scheduledForUpdating = getReportWriter("scheduled-updates")
+PrintWriter failedUpdating = getReportWriter("failed-updates")
+PrintWriter linkChanges = getReportWriter("link-changes")
+
+String where = "collection <> 'definitions'"
+
+selectBySqlWhere(where) { data ->
+    boolean changed = traverse(data.graph, data.doc.shortId, data.whelk, linkChanges)
+
+    if (changed) {
+        scheduledForUpdating.println("${data.doc.getURI()}")
+        data.scheduleSave(onError: { e ->
+            failedUpdating.println("Failed to update ${data.doc.shortId} due to: $e")
+        })
+    }
+}
+
+boolean traverse(Object node, String idForLogging, Whelk whelk, PrintWriter linkChanges) {
+    boolean changed = false
+
+    if (node instanceof Map) {
+        Map map = node
+
+        // Correct references are all id.kb.se or ends with #it.
+        if (map.size() == 1 &&
+                map["@id"] &&
+                map["@id"].startsWith(baseUri.toString()) &&
+                !map["@id"].endsWith("#it") ) {
+            String correctLinkTarget = whelk.storage.getThingId(map["@id"])
+            linkChanges.println("In ${idForLogging}, changed a reference ${map["@id"]} , to ${correctLinkTarget}")
+            map["@id"] = correctLinkTarget
+            changed = true
+        }
+
+        for (String key : map.keySet()) {
+            if (key != "generationProcess")
+                changed |= traverse(map[key], idForLogging, whelk, linkChanges)
+        }
+    }
+
+    if (node instanceof List) {
+        List list = node
+        for (Object e : list) {
+            changed |= traverse(e, idForLogging, whelk, linkChanges)
+        }
+    }
+
+    return changed
+}
