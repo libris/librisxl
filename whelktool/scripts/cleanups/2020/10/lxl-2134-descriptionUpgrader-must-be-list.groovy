0334448f4aa96b4362f57b3d556596fffdb0d0a9
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2020-10-15 14:57:16 +0200

    Modify descriptionUpgrader only in record

--- whelktool/scripts/cleanups/2020/10/lxl-2134-descriptionUpgrader-must-be-list.groovy
+++ whelktool/scripts/cleanups/2020/10/lxl-2134-descriptionUpgrader-must-be-list.groovy
@@ -4,25 +4,24 @@
  * See LXL-2134 for more info.
  */
 
-import whelk.util.DocumentUtil
-
 PrintWriter scheduledForUpdating = getReportWriter("scheduled-updates")
 PrintWriter failedUpdating = getReportWriter("failed-updates")
 
 String where = "data#>>'{@graph,0,descriptionUpgrader}' IS NOT NULL"
 
 selectBySqlWhere(where) { data ->
-    boolean changed = DocumentUtil.findKey(data.doc.data, 'descriptionUpgrader') { value, path ->
-        if (value instanceof List) {
-            return DocumentUtil.NOP
-        }
-        return new DocumentUtil.Replace([value])
+    record = data.graph[0]
+
+    if (record["descriptionUpgrader"] instanceof List) {
+        return
+    } else if (record["descriptionUpgrader"] == null) {
+        record["descriptionUpgrader"] = []
+    } else {
+        record["descriptionUpgrader"] = [record["descriptionUpgrader"]]
     }
 
-    if (changed) {
-        scheduledForUpdating.println("${data.doc.getURI()}")
-        data.scheduleSave(onError: { e ->
-            failedUpdating.println("Failed to update ${data.doc.shortId} due to: $e")
-        })
-    }
+    scheduledForUpdating.println("${data.doc.getURI()}")
+    data.scheduleSave(onError: { e ->
+        failedUpdating.println("Failed to update ${data.doc.shortId} due to: $e")
+    })
 }
