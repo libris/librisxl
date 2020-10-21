bc65a2e518cf0e54b038d5159667ef7ce8276514
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-10-21 16:19:42 +0200

    Add script for moving Topic hasVariant to altLabel

---
+++ whelktool/scripts/cleanups/2020/10/lxl-3389-topic-hasVariant-to-altLabel.groovy
@@ -0,0 +1,66 @@
+/**
+ For @type Topic: Move all hasVariant.prefLabel to altLabel
+
+ See LXL-3389
+
+ */
+
+class Script {
+    static PrintWriter modified
+    static PrintWriter errors
+}
+
+Script.modified = getReportWriter("modified.txt")
+Script.errors = getReportWriter("errors.txt")
+
+selectByCollection('auth') { auth ->
+    try {
+        process(auth)
+    }
+    catch(IllegalArgumentException e) {
+        Script.errors.println("${auth.doc.shortId} ${e.getMessage()}")
+    }
+    catch(Exception e) {
+        Script.errors.println("${auth.doc.shortId} $e")
+        e.printStackTrace(Script.errors)
+    }
+}
+
+void process(auth) {
+    Map thing = auth.graph[1]
+
+    if (thing['@type'] != 'Topic') {
+        return
+    }
+
+    if (!thing['hasVariant']) {
+        return
+    }
+
+    def altLabel = asList(thing['altLabel']) as Set
+
+    asList(thing['hasVariant']).each { Map variant ->
+        checkVariantShape(variant)
+        altLabel.add(variant['prefLabel'])
+    }
+
+    thing['altLabel'] = altLabel.toSorted()
+    thing.remove('hasVariant')
+
+    Script.modified.println("${auth.doc.shortId} ${thing['altLabel']}")
+    auth.scheduleSave()
+}
+
+List asList(o) {
+    (o instanceof List) ? (List) o : (o ? [o] : [])
+}
+
+void checkVariantShape(Map variant) {
+    if (variant['@type'] == 'Topic' && (variant.keySet() - ['@type', 'prefLabel', 'marc:controlSubfield']).size() == 0) {
+        return
+    }
+    if (variant['@type'] == 'ComplexSubject' && variant.keySet() == ['@type', 'prefLabel', 'termComponentList'] as Set) {
+        return
+    }
+    throw new IllegalArgumentException("Unhandled shape: $variant")
+}
