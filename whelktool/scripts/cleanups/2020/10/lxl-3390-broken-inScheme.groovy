a0a029e67560139128e2b4dab74e183def4330f8
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-10-22 18:49:40 +0200

    Clean up script

--- whelktool/scripts/cleanups/2020/10/lxl-3390-broken-inScheme.groovy
+++ whelktool/scripts/cleanups/2020/10/lxl-3390-broken-inScheme.groovy
@@ -2,7 +2,6 @@
  Link blank nodes in 'inScheme'
 
  See LXL-3390
-
  */
 
 import whelk.util.Statistics
@@ -14,9 +13,9 @@ substitutions = [
 ]
 
 class Script {
-    static BlankNodeLinker linker
     static PrintWriter modified
     static PrintWriter errors
+    static BlankNodeLinker linker
     static Statistics statistics = new Statistics(5).printOnShutdown()
 }
 
@@ -24,8 +23,8 @@ Script.modified = getReportWriter("modified.txt")
 Script.errors = getReportWriter("errors.txt")
 Script.linker = buildLinker()
 
-println(Script.linker.map)
-println(Script.linker.ambiguousIdentifiers)
+println("Mappings: ${Script.linker.map}")
+println("Ambiguous: ${Script.linker.ambiguousIdentifiers}")
 
 selectByCollection('bib') { bib ->
     try {
@@ -38,10 +37,8 @@ selectByCollection('bib') { bib ->
 }
 
 void process(bib) {
-    Map thing = bib.graph[1]
-
-    Script.statistics.withExample(bib.doc.shortId) {
-        if(Script.linker.linkAll(thing, 'inScheme')) {
+    Script.statistics.withContext(bib.doc.shortId) {
+        if(Script.linker.linkAll(bib.graph[1], 'inScheme')) {
             Script.modified.println("${bib.doc.shortId}")
             bib.scheduleSave()
         }
@@ -53,14 +50,20 @@ def buildLinker() {
     def matchFields = ['code']
     def linker = new BlankNodeLinker(types, matchFields, Script.statistics)
 
+    linker.loadDefinitions(getWhelk())
+    linker.addSubstitutions(substitutions)
+
+    return linker
+}
+
+def getWhelk() {
     // A little hack to get a handle to whelk...
     def whelk = null
     selectByIds(['https://id.kb.se/marc']) { docItem ->
         whelk = docItem.whelk
     }
-
-    linker.loadDefinitions(whelk)
-    linker.addSubstitutions(substitutions)
-
-    return linker
+    if (!whelk) {
+        throw new RuntimeException("Could not get Whelk")
+    }
+    return whelk
 }
