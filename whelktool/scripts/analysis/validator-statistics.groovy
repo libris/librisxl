ada8587b6beb42bf701bb51bce74e77d32301ca7
Niklas Lindström <lindstream@gmail.com>
2020-12-04 23:25:49 +0100

    Add known irregular terms to validator statistics report
    Specifically, collect all terms that are sometimes, but not always used
    as repeatable (set-container) terms from the MarcFrameConverter, and
    list all paths to these.
    
    Also report full paths on validation errors, and mark those that are
    known as problematic (from MarcFrameConverter as per above).
    
    The paths are also annotated with "1", "2+" or "5+" to represent one,
    more than one or more than four list items respectively.

--- whelktool/scripts/analysis/validator-statistics.groovy
+++ whelktool/scripts/analysis/validator-statistics.groovy
@@ -1,12 +1,14 @@
 import whelk.JsonLdValidator
 import whelk.JsonLdValidator.Error as JsonError
 import whelk.util.Statistics
+import whelk.util.DocumentUtil
 
 class Script {
     static PrintWriter report
     static Statistics s = new Statistics(6).printOnShutdown()
     static JsonLdValidator v
     static boolean isInitialized = false
+    static Set<String> expectsSetsWithConflicts = new HashSet<>()
 }
 
 Script.report = getReportWriter("report.txt")
@@ -30,17 +32,68 @@ selectByCollection('bib') { bib ->
 }
 
 void process(bib) {
+    Set<String> pathsToConflictingSetTerms = getPathsToConflictingSetTerms(bib)
+    pathsToConflictingSetTerms.each {
+        Script.s.increment("Term with conflicting repeatability", it, bib.doc.getShortId())
+    }
+
     List<JsonError> errors = Script.v.validateAll(bib.doc.data)
 
     errors.each {
-        def message = "key: " + it.key
+        String valsize = ''
+        if (it.value instanceof List) {
+            valsize = ":${representSize(it.value.size())}"
+        }
+        String message = "path: ${representPath(it.path)}${valsize}"
         if (it.type == JsonError.Type.UNKNOWN_VOCAB_VALUE ||
             it.type == JsonError.Type.UNEXPECTED) {
-            message = message + " value: " + it.value
+            message += " value: ${it.value}"
+        }
+        if (message in pathsToConflictingSetTerms) {
+            message += ' (KNOWN)'
         }
         Script.s.increment(it.getDescription(), message, bib.doc.getShortId())
     }
     if (errors) {
         Script.report.println(bib.doc.shortId)
     }
+}
+
+String representPath(List path) {
+    return path.join('.')
+        .replaceAll(/^@graph\.0/, 'record')
+        .replaceAll(/^@graph\.1/, 'instance')
+        .replaceAll(/^@graph\.2/, 'work')
+        .replaceAll(/\.\d+/, '[]')
+}
+
+String representSize(int size) {
+    size > 4 ? '5+' : size > 1 ? '2+' : size.toString()
+}
+
+Set<String> getPathsToConflictingSetTerms(bib) {
+    if (Script.expectsSetsWithConflicts.size() == 0) {
+        synchronized (Script.expectsSetsWithConflicts) {
+            bib.whelk.marcFrameConverter.conversion.badRepeats.each {
+                Script.expectsSetsWithConflicts << it.term
+            }
+        }
+    }
+    Set<String> pathsToSetsWithList = new HashSet<>()
+    DocumentUtil.traverse(bib.doc.data, { value, path ->
+        if (!path) {
+            return
+        }
+        String key = path.last()
+        if (key in Script.expectsSetsWithConflicts) {
+            String valsize = ''
+            if (value instanceof List) {
+                valsize = ":${representSize(value.size())}"
+            }
+            String message = "path: ${representPath(path)}${valsize}"
+            pathsToSetsWithList << message
+        }
+        return
+    })
+    return pathsToSetsWithList
 }
