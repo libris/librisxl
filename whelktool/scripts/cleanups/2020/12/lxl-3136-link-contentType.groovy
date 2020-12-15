db76aeb50151727fa69e390d0ab7e0b1e6e38f4b
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-12-15 18:17:36 +0100

    Add script for linking contenType

---
+++ whelktool/scripts/cleanups/2020/12/lxl-3136-link-contentType.groovy
@@ -0,0 +1,73 @@
+/**
+ Link blank nodes in 'inScheme'
+
+ See LXL-3390
+ */
+
+import whelk.util.Statistics
+import whelk.filter.BlankNodeLinker
+
+class Script {
+    static PrintWriter modified
+    static PrintWriter errors
+    static BlankNodeLinker linker
+    static Statistics statistics = new Statistics(5).printOnShutdown()
+}
+
+Script.modified = getReportWriter("modified.txt")
+Script.errors = getReportWriter("errors.txt")
+Script.linker = buildLinker()
+
+println("Mappings: ${Script.linker.map}")
+println("Ambiguous: ${Script.linker.ambiguousIdentifiers}")
+
+selectByCollection('bib') { bib ->
+    try {
+        process(bib)
+    }
+    catch(Exception e) {
+        Script.errors.println("${bib.doc.shortId} $e")
+        e.printStackTrace(Script.errors)
+    }
+}
+
+void process(bib) {
+    def thing = bib.graph[1]
+    if(!(thing['instanceOf'] && thing['instanceOf']['contentType'])) {
+        return
+    }
+
+    Script.statistics.withContext(bib.doc.shortId) {
+        if(Script.linker.linkAll(thing['instanceOf'], 'contentType')) {
+            Script.modified.println("${bib.doc.shortId}")
+            bib.scheduleSave()
+        }
+    }
+}
+
+def buildLinker() {
+    def types = ['ContentType']
+    def matchFields = ['code', 'label']
+    def linker = new BlankNodeLinker(types, matchFields, Script.statistics)
+
+    linker.loadDefinitions(getWhelk())
+    linker.addSubstitutions(substitutions())
+
+    return linker
+}
+
+def getWhelk() {
+    // A little hack to get a handle to whelk...
+    def whelk = null
+    selectByIds(['https://id.kb.se/marc']) { docItem ->
+        whelk = docItem.whelk
+    }
+    if (!whelk) {
+        throw new RuntimeException("Could not get Whelk")
+    }
+    return whelk
+}
+
+def substitutions() {
+    [:]
+}
