8b26f502c4916a74088de48ef39d3ec8b1e0d167
kwahlin <kalle.wahlin@kb.se>
2021-01-26 15:29:55 +0100

    State isql path

--- librisxl-tools/scripts/install_namespace_prefixes.sh
+++ librisxl-tools/scripts/install_namespace_prefixes.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Remove predefined prefixes
-DELETE_PREFIXES=$(isql MESSAGE=OFF BANNER=OFF VERBOSE=OFF "EXEC=SELECT NS_PREFIX FROM DB.DBA.SYS_XML_PERSISTENT_NS_DECL" \
+DELETE_PREFIXES=$(/usr/local/bin/isql MESSAGE=OFF BANNER=OFF VERBOSE=OFF "EXEC=SELECT NS_PREFIX FROM DB.DBA.SYS_XML_PERSISTENT_NS_DECL" \
   | awk '{ print "DB.DBA.XML_REMOVE_NS_BY_PREFIX('\''"$0"'\'', 2);"}')
-isql EXEC="$DELETE_PREFIXES"
+/usr/local/bin/isql EXEC="$DELETE_PREFIXES"
 
 # Load custom prefixes
 LOAD_PREFIXES=$(awk '{ print "DB.DBA.XML_SET_NS_DECL('\''"$1"'\'','\''"$2"'\'',2);" }' "$1")
-isql EXEC="$LOAD_PREFIXES"
+/usr/local/bin/isql EXEC="$LOAD_PREFIXES"
