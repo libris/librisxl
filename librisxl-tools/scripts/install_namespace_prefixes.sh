c83b4b8b76e4d1bb8ea3b268011b6a9bd17d2c2d
kwahlin <kalle.wahlin@kb.se>
2021-01-26 14:41:22 +0100

    Add missing quotes

--- librisxl-tools/scripts/install_namespace_prefixes.sh
+++ librisxl-tools/scripts/install_namespace_prefixes.sh
@@ -6,5 +6,5 @@ DELETE_PREFIXES=$(isql MESSAGE=OFF BANNER=OFF VERBOSE=OFF "EXEC=SELECT NS_PREFIX
 isql EXEC="$DELETE_PREFIXES"
 
 # Load custom prefixes
-LOAD_PREFIXES=$(awk '{ print "DB.DBA.XML_SET_NS_DECL('\''"$1"'\'','\''"$2"'\'',2);" }' $1)
+LOAD_PREFIXES=$(awk '{ print "DB.DBA.XML_SET_NS_DECL('\''"$1"'\'','\''"$2"'\'',2);" }' "$1")
 isql EXEC="$LOAD_PREFIXES"
