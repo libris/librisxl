4e3f4c98b07c128d70623dfb7a79942de742c722
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2021-01-27 20:29:31 +0100

    Revert tired change in wrong file "Remove superfluous ReportWriter"
    This reverts commit 1497e1909539a8b91951be3954b081e3aa0ec859.

--- whelktool/scripts/cleanups/2020/10/lxl-3390-link-inScheme.groovy
+++ whelktool/scripts/cleanups/2020/10/lxl-3390-link-inScheme.groovy
@@ -8,11 +8,13 @@ import whelk.util.Statistics
 import whelk.filter.BlankNodeLinker
 
 class Script {
+    static PrintWriter modified
     static PrintWriter errors
     static BlankNodeLinker linker
     static Statistics statistics = new Statistics(5).printOnShutdown()
 }
 
+Script.modified = getReportWriter("modified.txt")
 Script.errors = getReportWriter("errors.txt")
 Script.linker = buildLinker()
 
@@ -39,6 +41,7 @@ void process(bib) {
 
     Script.statistics.withContext(bib.doc.shortId) {
         if(Script.linker.linkAll(subject, 'inScheme')) {
+            Script.modified.println("${bib.doc.shortId}")
             bib.scheduleSave()
         }
     }
