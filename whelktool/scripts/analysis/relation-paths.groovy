ce6e9783ea9c5e6c11d671c5147b1e373beff6b0
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-02-04 17:20:24 +0100

    Add script for analyzing locations of links in documents

--- whelktool/scripts/analysis/relation-paths.groovy
+++ whelktool/scripts/analysis/relation-paths.groovy
@@ -4,7 +4,11 @@ selectBySqlWhere('deleted = false') { doc ->
     try {
         DocumentUtil.findKey(doc.graph, '@id') { String value, List path ->
             if (value.startsWith("https://libris") || value.startsWith("http://libris") || value.startsWith("https://id.kb.se")) {
-                incrementStats('p', path.findAll {!(it instanceof Integer) && it != '@id'}.join('.'))
+                def p = path.findAll {!(it instanceof Integer) && it != '@id'}
+                if (p) {
+                    incrementStats('0 p', p.join('.'))
+                    incrementStats(p.last(), p.join('.'))
+                }
             }
         }    
     } catch (Exception e) {
