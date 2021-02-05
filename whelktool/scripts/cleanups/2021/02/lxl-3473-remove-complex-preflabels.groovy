b562921952550b4b897afaa1fe3bec826451747a
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2021-02-05 10:42:46 +0100

    Add script for removing prefLabel and sameAs on complex subjects.

---
+++ whelktool/scripts/cleanups/2021/02/lxl-3473-remove-complex-preflabels.groovy
@@ -0,0 +1,34 @@
+import whelk.Document;
+import whelk.Whelk;
+
+String where = "collection = 'bib' and data#>'{@graph,1,instanceOf,subject}' @> '[{\"@type\":\"ComplexSubject\"}]'"
+
+selectBySqlWhere(where) { data ->
+    def mainEntity = data.graph[1]
+    def subjectEntities = []
+    if (mainEntity["subject"])
+        subjectEntities.addAll ( asList(mainEntity["subject"]) )
+    if (mainEntity["instanceOf"]["subject"])
+        subjectEntities.addAll ( asList(mainEntity["instanceOf"]["subject"]) )
+
+    def modified = false
+    subjectEntities.each { subject ->
+        if (subject &&
+                subject["@type"] &&
+                subject["@type"] == "ComplexSubject") {
+            modified |= (subject.remove("prefLabel") != null)
+            modified |= (subject.remove("sameAs") != null)
+        }
+    }
+
+    if (modified)
+        data.scheduleSave()
+}
+
+private List asList(Object o) {
+    if (o == null)
+        return []
+    if (o instanceof List)
+        return o
+    return [o]
+}
