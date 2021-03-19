0c28669a9dad3399dcf98a07a9102a4916d36d16
kwahlin <kalle.wahlin@kb.se>
2021-03-19 09:27:43 +0100

    Check also hasNote

--- whelktool/scripts/cleanups/2021/03/lxl-3519-remove-marc-isPrivate-when-false.groovy
+++ whelktool/scripts/cleanups/2021/03/lxl-3519-remove-marc-isPrivate-when-false.groovy
@@ -1,14 +1,25 @@
 String where = """
     collection = 'bib' 
-    AND data#>>'{@graph,1,marc:hasImmediateSourceOfAcquisitionNote}' LIKE '%\"marc:isPrivate\"%'
+    AND (data#>>'{@graph,1,marc:hasImmediateSourceOfAcquisitionNote}' LIKE '%\"marc:isPrivate\"%'
+    OR data#>>'{@graph,1,hasNote}' LIKE '%\"marc:isPrivate\"%')
 """
 
 selectBySqlWhere(where) { data ->
     boolean modified
 
-    asList(data.graph[1]["marc:hasImmediateSourceOfAcquisitionNote"]).each { note ->
-        modified |= note.removeAll{ key, value ->
-            key == "marc:isPrivate" && value == false
+    if (data.graph[1].containsKey("marc:hasImmediateSourceOfAcquisitionNote")) {
+        asList(data.graph[1]["marc:hasImmediateSourceOfAcquisitionNote"]).each { note ->
+            modified |= note.removeAll{ key, value ->
+                key == "marc:isPrivate" && value == false
+            }
+        }
+    }
+
+    if (data.graph[1].containsKey("hasNote")) {
+        asList(data.graph[1]["hasNote"]).each { note ->
+            modified |= note.removeAll{ key, value ->
+                key == "marc:isPrivate" && value == false
+            }
         }
     }
 
