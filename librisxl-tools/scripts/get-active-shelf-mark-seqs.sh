959bb0a7639b59b41bd3b1e0ce366ca69b869b49
kwahlin <72360110+kwahlin@users.noreply.github.com>
2021-03-22 09:55:10 +0100

    Get max control number for last year (#879)

--- librisxl-tools/scripts/get-active-shelf-mark-seqs.sh
+++ librisxl-tools/scripts/get-active-shelf-mark-seqs.sh
@@ -9,11 +9,16 @@ SELECT PrefixBeforeYear,
            END AS PrefixAfterYear,
        LastSerialNumber, Year
 FROM T_CodeType, (
-    SELECT CodeType_FKID, MAX(SerialNumber) as LastSerialNumber, MAX(Year) AS Year
-    FROM T_SerialNumber
-    WHERE ActiveStatus = 2
-    GROUP BY CodeType_FKID
-         ) as Serial
+    SELECT CodeType_FKID, MAX(SerialNumber) as LastSerialNumber, Year
+    FROM T_SerialNumber, (
+        SELECT CodeType_FKID as CodeId, MAX(Year) as maxYear
+        FROM T_SerialNumber
+        WHERE ActiveStatus = 2
+        GROUP BY CodeType_FKID
+    ) as Years
+    WHERE ActiveStatus = 2 AND Year = maxYear AND CodeType_FKID = CodeId
+    GROUP BY CodeType_FKID, Year
+) as Serial
 WHERE CodeType_ID = CodeType_FKID;"
 
 sqlcmd -S mssqlag3.kb.local -d signum -U Signum-read -Q "$QUERY" \
