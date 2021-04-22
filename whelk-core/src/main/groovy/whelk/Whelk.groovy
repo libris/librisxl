64d3259c4ed1cb74c8b5ebab0f1eaa45cd8611de
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-04-22 17:02:05 +0200

    Search: Faster assembly of result by reducing number of DB requests

--- whelk-core/src/main/groovy/whelk/Whelk.groovy
+++ whelk-core/src/main/groovy/whelk/Whelk.groovy
@@ -410,6 +410,23 @@ class Whelk {
 
         e.embellish(document)
     }
+    
+    Map<String, Map> getCards(Iterable<String> iris) {
+        Map<String, Map> result = [:]
+        storage.getCards(iris).each { card ->
+            List<Map> graph = (List<Map>) card['@graph']
+            graph?.each { Map e ->
+                e['@id']?.with { result[(String) it] = card }
+                if (e.sameAs) {
+                    ((List<Map>) (e.sameAs)).each { Map sameAs ->
+                        sameAs['@id']?.with { result[(String) it] = card }
+                    }
+                }
+            }
+        }
+        
+        return result
+    }
 
     Document loadEmbellished(String systemId) {
         return storage.loadEmbellished(systemId, this.&embellish)
