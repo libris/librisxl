33e3900579770f57ec2169ea0382d738c839eb01
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2021-04-30 09:39:54 +0200

    Adapt Jaktskildringar-script after comments

--- whelktool/scripts/cleanups/2021/04/lxl-3536-jaktskildringar.groovy
+++ whelktool/scripts/cleanups/2021/04/lxl-3536-jaktskildringar.groovy
@@ -1,13 +1,45 @@
 String where = """
-    data#>'{@graph,1,instanceOf,genreForm}' @> '[{"@id":"https://id.kb.se/term/sao/Jaktskildringar"}]'
+    data#>'{@graph,1,instanceOf,subject}' LIKE '%https://id.kb.se/term/sao/Jaktskildringar%'
     AND collection = 'bib'
 """
 
 selectBySqlWhere(where) { data ->
-    asList(data.graph[1].instanceOf.genreForm).each { gf ->
-        if (gf["@id"] == "https://id.kb.se/term/sao/Jaktskildringar") {
-            gf["@id"] = "https://id.kb.se/term/saogf/Jaktskildringar"
-            data.scheduleSave()
+    asList(data.graph[1].instanceOf.subject).each { subject ->
+        replaceLinks(subject,
+                "https://id.kb.se/term/saogf/Jaktskildringar",
+                "https://id.kb.se/term/sao/Jaktskildringar")
+    }
+}
+
+void replaceLinks(Object node, String newLinkTarget, String linkToReplace) {
+    if (node instanceof Map) {
+        Map map = node
+
+        if (map.size() == 1 && map["@id"] == linkToReplace ) {
+            map["@id"] = newLinkTarget
+        }
+
+        for (String key : map.keySet()) {
+            replaceLinks(map[key], newLinkTarget, linkToReplace)
+        }
+    }
+
+    if (node instanceof List) {
+        List list = node
+
+        for (Object e : list) {
+            replaceLinks(e, newLinkTarget, linkToReplace)
+        }
+
+        // Check that we've not created a double link (in case there was one before we started)
+        boolean alreadyFound = false
+        Iterator it = list.iterator()
+        while (it.hasNext()) {
+            Object e = it.next()
+            if (e instanceof Map && e.size() == 1 && e["@id"] != null && e["@id"] == newLinkTarget) {
+                if (alreadyFound) { it.remove() }
+                else alreadyFound = true
+            }
         }
     }
 }
