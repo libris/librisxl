b8251e3e9598406df6088d42ce4e73f80025fddf
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2021-05-14 13:14:58 +0200

    Fix bad script.

--- whelktool/scripts/cleanups/2021/04/lxl-3536-jaktskildringar.groovy
+++ whelktool/scripts/cleanups/2021/04/lxl-3536-jaktskildringar.groovy
@@ -1,39 +1,50 @@
 String where = """
-    data#>'{@graph,1,instanceOf,subject}' LIKE '%https://id.kb.se/term/sao/Jaktskildringar%'
+    data#>>'{@graph,1,instanceOf,subject}' LIKE '%https://id.kb.se/term/sao/Jaktskildringar%'
     AND collection = 'bib'
 """
 
 selectBySqlWhere(where) { data ->
-    asList(data.graph[1].instanceOf.subject).each { subject ->
-        replaceLinks(subject,
-                "https://id.kb.se/term/saogf/Jaktskildringar",
-                "https://id.kb.se/term/sao/Jaktskildringar")
-    }
+    replaceLinks( (Map) data.graph[1].instanceOf,
+            "https://id.kb.se/term/saogf/Jaktskildringar",
+            "https://id.kb.se/term/sao/Jaktskildringar")
+    data.scheduleSave()
 }
 
-void replaceLinks(Object node, String newLinkTarget, String linkToReplace) {
-    if (node instanceof Map) {
-        Map map = node
+void replaceLinks(Map work, String newLinkTarget, String linkToReplace) {
 
-        if (map.size() == 1 && map["@id"] == linkToReplace ) {
-            map["@id"] = newLinkTarget
-        }
+    boolean containedBadLink = false
 
-        for (String key : map.keySet()) {
-            replaceLinks(map[key], newLinkTarget, linkToReplace)
+    // Remove bad links
+    if (work.subject instanceof List) {
+        Iterator it = work.subject.iterator()
+        while (it.hasNext()) {
+            Map subject = it.next()
+            if (subject.size() == 1 && subject["@id"] == linkToReplace ) {
+                it.remove()
+                containedBadLink = true
+            }
         }
     }
+    else if (work.subject instanceof Map && work.subject.size() == 1 && work.subject["@id"] == linkToReplace) {
+        work.remove("subject")
+        containedBadLink = true
+    }
 
-    if (node instanceof List) {
-        List list = node
+    // If something was removed, instead set the new one.
+    if (containedBadLink) {
 
-        for (Object e : list) {
-            replaceLinks(e, newLinkTarget, linkToReplace)
-        }
+        // Make sure genreForm is a list
+        if (work.genreForm == null)
+            work.put("genreForm", [])
+        else if ( !(work.genreForm instanceof List) )
+            work.put("genreForm", [work.genreForm])
+
+        // Add the correct link
+        work.genreForm.add(["id" : newLinkTarget])
 
         // Check that we've not created a double link (in case there was one before we started)
         boolean alreadyFound = false
-        Iterator it = list.iterator()
+        Iterator it = work.genreForm.iterator()
         while (it.hasNext()) {
             Object e = it.next()
             if (e instanceof Map && e.size() == 1 && e["@id"] != null && e["@id"] == newLinkTarget) {
