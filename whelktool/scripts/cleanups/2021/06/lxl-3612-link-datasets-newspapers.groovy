fac9adbde48120d829893e98c0d744cb88ad495b
Olov Ylinenpää <51744858+olovy@users.noreply.github.com>
2021-06-03 09:52:14 +0200

    Add script for linking to data.kb.se datasets (#944)
    Co-authored-by: kwahlin <72360110+kwahlin@users.noreply.github.com>

---
+++ whelktool/scripts/cleanups/2021/06/lxl-3612-link-datasets-newspapers.groovy
@@ -0,0 +1,141 @@
+// UPDATE DATASET IDS FOR PROD
+
+import java.util.function.Predicate
+
+def datasets =
+        [
+                [
+                        dataset: 'n2rgzfcklj9zcwlw',
+                        query  :
+                                [
+                                        '@type'                     : ['Electronic'],
+                                        'hasTitle.mainTitle'        : ['AFTONBLADET'],
+                                        'min-publication.year'      : ['1831'],
+                                        'max-publication.year'      : ['1900'],
+                                        '@reverse.itemOf.heldBy.@id': ['https://libris.kb.se/library/APIS'],
+                                        '_sort'                     : ['@id']
+                                ],
+                        predicate: { doc ->
+                            def (record, thing) = doc.graph
+                            
+                            asList(thing.hasTitle).any { Map title ->
+                                title.mainTitle ==~ /AFTONBLADET\s+\d\d\d\d-\d\d\-\d\d/
+                            }
+                        }
+                ],
+                [
+                        dataset: 'm1qfz6d3k163n1ww',
+                        query  :
+                                [
+                                        '@type'                     : ['Electronic'],
+                                        'hasTitle.mainTitle'        : ['DAGENS NYHETER'],
+                                        'min-publication.year'      : ['1864'],
+                                        'max-publication.year'      : ['1900'],
+                                        '@reverse.itemOf.heldBy.@id': ['https://libris.kb.se/library/APIS'],
+                                        '_sort'                     : ['@id']
+                                ],
+                        predicate: { doc ->
+                            def (record, thing) = doc.graph
+
+                            asList(thing.hasTitle).any { Map title ->
+                                title.mainTitle ==~ /DAGENS NYHETER\s+\d\d\d\d-\d\d\-\d\d/
+                            }
+                        }
+                ],
+                [
+                        dataset: 'w90p8vw6tcph9xl3',
+                        query  :
+                                [
+                                        '@type'                     : ['Electronic'],
+                                        'hasTitle.mainTitle'        : ['NORRKÖPINGS WECKOTIDNINGAR'],
+                                        'min-publication.year'      : ['1758'],
+                                        'max-publication.year'      : ['1786'],
+                                        '@reverse.itemOf.heldBy.@id': ['https://libris.kb.se/library/APIS'],
+                                        '_sort'                     : ['@id']
+                                ],
+                        predicate: { doc ->
+                            def (record, thing) = doc.graph
+
+                            asList(thing.hasTitle).any { Map title ->
+                                title.mainTitle ==~ /NORRKÖPINGS WECKOTIDNINGAR\s+\d\d\d\d-\d\d\-\d\d/
+                            }
+                        }
+                ],
+                [
+                        dataset: 'l0pdzslgjcdk19cx',
+                        query  :
+                                [
+                                        '@type'                     : ['Electronic'],
+                                        'hasTitle.mainTitle'        : ['NORRKÖPINGS TIDNINGAR'],
+                                        'min-publication.year'      : ['1787'],
+                                        'max-publication.year'      : ['1895'],
+                                        '@reverse.itemOf.heldBy.@id': ['https://libris.kb.se/library/APIS'],
+                                        '_sort'                     : ['@id']
+                                ],
+                        predicate: { doc ->
+                            def (record, thing) = doc.graph
+
+                            asList(thing.hasTitle).any { Map title ->
+                                title.mainTitle ==~ /NORRKÖPINGS TIDNINGAR\s+\d\d\d\d-\d\d\-\d\d/
+                            }
+                        }
+                ],
+                [
+                        dataset: 'ftj7swkgcxxbm982',
+                        query  :
+                                [
+                                        '@type'                     : ['Electronic'],
+                                        'hasTitle.mainTitle'        : ['SVENSKA DAGBLADET'],
+                                        'min-publication.year'      : ['1884'],
+                                        'max-publication.year'      : ['1900'],
+                                        '@reverse.itemOf.heldBy.@id': ['https://libris.kb.se/library/APIS'],
+                                        '_sort'                     : ['@id']
+                                ],
+                        predicate: { doc ->
+                            def (record, thing) = doc.graph
+
+                            asList(thing.hasTitle).any { Map title ->
+                                title.mainTitle ==~ /SVENSKA DAGBLADET\s+\d\d\d\d-\d\d\-\d\d/
+                            }
+                        }
+                ],
+        ]
+
+datasets.each {
+    run(it)    
+}
+
+void run(Map spec) {
+    String datasetUri = getIri(spec.dataset)
+
+    selectByIds(queryIds(spec.query).collect()) { d ->
+        def (record, _thing) = d.graph
+        Predicate predicate = spec.predicate 
+        if (predicate.test(d) && setDataset(record, datasetUri)) {
+            d.scheduleSave()
+        }
+    }
+}
+
+boolean setDataset(Map record, datasetUri) {
+    Set inDatasets = asList(record.inDataset) as Set
+    def link = [ '@id': datasetUri ]
+    boolean modified = inDatasets.add(link)
+    record.inDataset = inDatasets as List
+    return modified
+}
+
+String getIri(String id) {
+    String iri
+    selectByIds([id]) { d ->
+        iri = d.doc.getThingIdentifiers().first()
+    }
+    if (!iri) {
+        throw new RuntimeException("No iri found for $id")
+    }
+    return iri
+}
+
+List asList(o) {
+    (o instanceof List) ? (List) o : (o ? [o] : [])
+}
