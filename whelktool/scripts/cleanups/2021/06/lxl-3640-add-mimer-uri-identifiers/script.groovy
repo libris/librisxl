56f878658fc327437c6b3350576c5ed3653f656a
kwahlin <kalle.wahlin@kb.se>
2021-06-29 15:05:28 +0200

    Add script for adding identifiers with mimer packet uris

---
+++ whelktool/scripts/cleanups/2021/06/lxl-3640-add-mimer-uri-identifiers/script.groovy
@@ -0,0 +1,57 @@
+List idList = new File(scriptDir, 'idlist.yaml').readLines()
+
+Map mimerUriById = [:]
+
+String id
+
+idList.each { String line ->
+    if (line =~ "libris-id:")
+        id = line.replaceFirst(/.*libris-id:/, "").trim()
+    else if (line =~ /uri:/) {
+        String uri = line.replace("uri:", "").trim()
+
+        if (id.isNumber()) {
+            String where = """
+                collection = 'bib' 
+                AND deleted = 'false' 
+                AND data#>>'{@graph,0,controlNumber}' = '${id}'
+            """
+
+            selectBySqlWhere(where) {
+                id = it.doc.shortId
+            }
+        }
+
+        mimerUriById[id] = uri
+    }
+}
+
+selectByIds(mimerUriById.keySet()) { data ->
+    Map instance = data.graph[1]
+    List identifiedBy = instance.identifiedBy
+
+    Map identifier =
+            [
+                "@type": "Identifier",
+                "typeNote": "uri",
+                "value": mimerUriById[data.doc.shortId]
+            ]
+
+    if (identifiedBy) {
+        if (!(identifier in identifiedBy)) {
+            identifiedBy << identifier
+            data.scheduleSave()
+        }
+    }
+    else {
+        instance["identifiedBy"] = [identifier]
+        data.scheduleSave()
+    }
+}
+
+
+
+
+
+
+
