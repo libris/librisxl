d709af2bd1a56bd562538472fbcb477a55d25791
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-08-27 16:24:02 +0200

    Add Extent TODO

--- whelktool/src/main/groovy/datatool/scripts/mergeworks/compare/Extent.groovy
+++ whelktool/src/main/groovy/datatool/scripts/mergeworks/compare/Extent.groovy
@@ -3,6 +3,7 @@ package datatool.scripts.mergeworks.compare;
 class Extent implements FieldHandler {
 
     // TODO: allow one side missing extent (-1)?
+    // TODO: improve parsing https://metadatabyran.kb.se/beskrivning/materialtyper-arbetsfloden/tryckta-monografier/omfang-for-tryckta-monografier
     @Override
     boolean isCompatible(Object a, Object b) {
         return a * 0.7 < b && a * 1.3 > b
