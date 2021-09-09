c29c8889e197dcf1ce521f3b7291fd62c1cb14a7
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-09-09 12:08:08 +0200

    Add naive contribution linking

--- whelk-core/src/test/groovy/whelk/util/UnicodeSpec.groovy
+++ whelk-core/src/test/groovy/whelk/util/UnicodeSpec.groovy
@@ -68,7 +68,8 @@ class UnicodeSpec extends Specification {
         Unicode.asciiFold(unicode) == ascii
 
         where:
-        unicode     | ascii
-        'Désidéria' | 'Desideria'
+        unicode          | ascii
+        'Désidéria'      | 'Desideria'
+        'Антон Павлович' | 'Антон Павлович'
     }
 }
