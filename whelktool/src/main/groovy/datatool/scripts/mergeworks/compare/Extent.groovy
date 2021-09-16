1e309c19970a94adbc624fa4dd89746002235b18
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-09-16 21:15:25 +0200

    Don't check extent

--- whelktool/src/main/groovy/datatool/scripts/mergeworks/compare/Extent.groovy
+++ whelktool/src/main/groovy/datatool/scripts/mergeworks/compare/Extent.groovy
@@ -5,7 +5,7 @@ class Extent implements FieldHandler {
     // TODO: allow one side missing extent (-1)?
     @Override
     boolean isCompatible(Object a, Object b) {
-        return a * 0.7 < b && a * 1.3 > b
+        return true // a * 0.7 < b && a * 1.3 > b
     }
 
     @Override
