dd5c803388055ca4cc3248bd2f04ab2d714dcb04
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-09-16 18:22:29 +0200

    Print report when merging

--- whelktool/src/main/groovy/datatool/scripts/mergeworks/Html.groovy
+++ whelktool/src/main/groovy/datatool/scripts/mergeworks/Html.groovy
@@ -20,7 +20,7 @@ class Html {
     static def infoFields = ['instance title', 'work title', 'instance type', 'editionStatement', 'responsibilityStatement', 'encodingLevel', 'publication', 'identifiedBy', 'extent']
 
     static String clusterTable(Collection<Doc> cluster) {
-        String id = clusterId(cluster)
+        String id = clusterId(cluster.collect { it.doc.shortId })
         String header = """
             <tr>
                 <th><a id="${id}"><a href="#${id}">${id}</th>
@@ -57,10 +57,6 @@ class Html {
                 : ""
     }
     
-    static String clusterId(Collection<Doc> cluster) {
-        clusterId(cluster.collect { it.doc.shortId })
-    }
-
     private static def fieldRows(Collection<Doc> cluster, String cls) {
         { field ->
             """
