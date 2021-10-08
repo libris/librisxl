ce474b6763df1b4698688a01ecc77d7f5a409798
kwahlin <kalle.wahlin@kb.se>
2021-10-08 17:30:26 +0200

    Update id list and use base uri for dataset

--- whelktool/scripts/cleanups/2021/09/datakbse-377-hollandska-dagstidningar/script.groovy
+++ whelktool/scripts/cleanups/2021/09/datakbse-377-hollandska-dagstidningar/script.groovy
@@ -1,5 +1,5 @@
 List ids = new File(scriptDir, 'hollandska-libris-id.txt').readLines()
-Map dataset = ["@id": "https://libris.kb.se/kz30z57kht9gmjs1#it"]
+Map dataset = ["@id": baseUri.toString() + "kz30z57kht9gmjs1#it"]
 
 selectByIds(ids) { data ->
     data.graph[0]['inDataset'] = data.graph[0]['inDataset'] ?: []
