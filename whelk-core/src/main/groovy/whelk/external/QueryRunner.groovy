0a39b255240cce82cf8ec0a65cb6e59bcfa393e4
kwahlin <kalle.wahlin@kb.se>
2021-10-13 14:04:59 +0200

    Add QueryRunner class

---
+++ whelk-core/src/main/groovy/whelk/external/QueryRunner.groovy
@@ -0,0 +1,77 @@
+package whelk.external
+
+import org.apache.jena.query.ParameterizedSparqlString
+import org.apache.jena.query.Query
+import org.apache.jena.query.QueryExecution
+import org.apache.jena.query.QueryExecutionFactory
+import org.apache.jena.query.ResultSet
+import org.apache.jena.query.ResultSetFactory
+import org.apache.jena.rdf.model.Model
+import org.apache.jena.rdf.model.RDFNode
+import org.apache.jena.shared.PrefixMapping
+
+class QueryRunner {
+    static final Map nsPrefixes =
+            [
+                    "bd"      : "http://www.bigdata.com/rdf#",
+                    "kbv"     : "https://id.kb.se/vocab/",
+                    "rdfs"    : "http://www.w3.org/2000/01/rdf-schema#",
+                    "skos"    : "http://www.w3.org/2004/02/skos/core#",
+                    "wd"      : "http://www.wikidata.org/entity/",
+                    "wdt"     : "http://www.wikidata.org/prop/direct/",
+                    "wikibase": "http://wikiba.se/ontology#"
+            ]
+
+    static PrefixMapping prefixMapping = PrefixMapping.Factory.create().setNsPrefixes(nsPrefixes)
+
+    static ResultSet selectQuery(QueryExecution qe) {
+        ResultSet resultSet
+
+        try {
+            ResultSet results = qe.execSelect()
+            resultSet = ResultSetFactory.copyResults(results)
+        } catch (Exception ex) {
+            println(ex.getMessage())
+        } finally {
+            qe.close()
+        }
+
+        return resultSet
+    }
+
+    static Model constructQuery(QueryExecution qe) {
+        try {
+            return qe.execConstruct()
+        } catch (Exception ex) {
+            println(ex.getMessage())
+        } finally {
+            qe.close()
+        }
+    }
+
+    static boolean askQuery(QueryExecution qe) {
+        try {
+            return qe.execAsk()
+        } catch (Exception ex) {
+            println(ex.getMessage())
+        } finally {
+            qe.close()
+        }
+    }
+
+    static QueryExecution remoteQueryExec(Query query, String sparqlEndpoint) {
+        return QueryExecutionFactory.sparqlService(sparqlEndpoint, query)
+    }
+
+    static QueryExecution localQueryExec(Query query, Model graph) {
+        return QueryExecutionFactory.create(query, graph)
+    }
+
+    static Query prepareQuery(String command, Collection<RDFNode> values = null) {
+        ParameterizedSparqlString paramString = new ParameterizedSparqlString(command, prefixMapping)
+        values?.eachWithIndex { v, i ->
+            paramString.setParam(i, v)
+        }
+        return paramString.asQuery()
+    }
+}
