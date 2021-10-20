8e19ab33e9a30a134672eb84005c99410e006f74
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-10-20 22:16:50 +0200

    Display and index placeholders as actual things if possible

--- whelk-core/src/main/groovy/whelk/component/ElasticSearch.groovy
+++ whelk-core/src/main/groovy/whelk/component/ElasticSearch.groovy
@@ -285,6 +285,12 @@ class ElasticSearch {
     }
 
     String getShapeForIndex(Document document, Whelk whelk) {
+        if (document.isPlaceholder()) {
+            whelk.external.getEphemeral(document.getThingIdentifiers().first()).ifPresent({ ext ->
+                document.setThing(ext.getThing())
+            })
+        }
+        
         Document copy = document.clone()
 
         whelk.embellish(copy, ['chips'])
