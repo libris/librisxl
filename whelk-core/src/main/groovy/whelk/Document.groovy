1e123c65ddf8e72f310b67f5f0c0545651b9c084
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-10-22 23:33:59 +0200

    Handle redirected external URIs when saving

--- whelk-core/src/main/groovy/whelk/Document.groovy
+++ whelk-core/src/main/groovy/whelk/Document.groovy
@@ -908,4 +908,11 @@ class Document {
         jsonLd && key && jsonLd.isSetContainer(key)
     }
 
+    void replaceLinks(Map<String, String> oldToNew) {
+        DocumentUtil.findKey(data, JsonLd.ID_KEY) { value, path ->
+            if (oldToNew.containsKey(value)) {
+                new DocumentUtil.Replace(oldToNew[(String) value])
+            }
+        }
+    }
 }
