14b38572122ced1b1135146e58d052ecb46e61db
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-12-13 14:48:49 +0100

    ES index: Fix missing _str when using alternateProperties in lens

--- whelk-core/src/main/groovy/whelk/JsonLd.groovy
+++ whelk-core/src/main/groovy/whelk/JsonLd.groovy
@@ -925,6 +925,11 @@ class JsonLd {
         }
         if (lens) {
             List propertiesToKeep = (List) lens.get("showProperties")
+            propertiesToKeep = propertiesToKeep.collect { prop ->
+                isAlternateProperties(prop)
+                        ? prop['alternateProperties']
+                        : prop
+            }.flatten()
             for (prop in propertiesToKeep) {
                 def values = object[prop]
                 if (isLangContainer(context[prop]) && values instanceof Map) {
