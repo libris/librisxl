837af7d56c5af436cc173f4068ba31c4714f5665
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2022-01-25 16:49:40 +0100

    Add script for linking blank seeAlso

--- whelktool/scripts/cleanups/2022/01/lxl-3590-see-also.groovy
+++ whelktool/scripts/cleanups/2022/01/lxl-3590-see-also.groovy
@@ -1,7 +1,17 @@
 /*
 Link blank agents in <agent>.seeAlso if an exact match can be found. 
 
-TODO: what to do about marc:controlSubfield:a / marc:controlSubfield:b
+Agents with marc:controlSubfield: a/b ("earlier heading"/"later heading", "tidigare namn"/"senare namn")
+will not match and are not handled. We might want to decide on a more specific relation for them.
+
+   2601 marc:controlSubfield:a
+   2535 marc:controlSubfield:b
+      5 marc:controlSubfield:g
+    292 marc:controlSubfield:i
+      3 marc:controlSubfield:nnnc
+
+https://www.loc.gov/marc/authority/adtracing.html
+https://katalogverk.kb.se/katalogisering/Formathandboken/Auktoritetsformatet/Kontrollkoder-i-auktoritetsformatet/index.html
 
 For more information, see LXL-3590
  */
