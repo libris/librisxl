33b3f01133e042a9030d225afc4d2169686ed87e
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2022-02-17 17:31:55 +0100

    Add script for cleaning up serials using VDD data

---
+++ whelktool/scripts/cleanups/2022/02/lxl-3819-electronic-physical-dagstidningar/lxl-3819-electronic-physical-dagstidningar.groovy
@@ -0,0 +1,96 @@
+/*
+For tidningar.kb.se serials.
+
+Using id list from VDD database:
+- Link electronic/reproduction series to physical series with reproductionOf if missing
+- Add tidningar.kb.se bibliography to electronic series
+
+See LXL-3819 for more information
+ */
+import whelk.Document
+
+notModified = getReportWriter("not-modified.txt")
+
+INPUT_FILE_NAME = 'libris_physical_electronic.tsv'
+
+// TODO: replace test record id with real
+def TIDNINGAR_BIBLIOGRAPHY = 'https://libris-qa.kb.se/katalogisering/k0p5lq17hztn5mng#it'
+
+
+new File(scriptDir, INPUT_FILE_NAME).readLines().each {
+    def (physicalId, electronicId) = it.split('\t')
+    
+    selectByIds([controlNumberToId(electronicId)]) { bib ->
+        def (record, thing) = bib.graph
+        
+        // Sanity check input
+        if (thing.issuanceType != 'Serial') {
+            notModified.println("${bib.doc.shortId} Wrong issuanceType")
+            return
+        }
+
+        if (thing.'@type' != 'Electronic') {
+            notModified.println("${bib.doc.shortId} Not Electronic")
+            return
+        }
+
+        // Link electronic/reproduction to physical if missing
+        if (!getAtPath(bib.graph, [1, 'reproductionOf'])) {
+            def physicalThing = loadThing(controlNumberToId(physicalId))
+            bib.graph[1].reproductionOf = [['@id': physicalThing.'@id']]
+            bib.scheduleSave()
+        }
+
+        // Add tidningar.kb.se bibliography to electronic series
+        if (addLink(record, ['bibliography'], TIDNINGAR_BIBLIOGRAPHY)) {
+            bib.scheduleSave()
+        }
+    }
+}
+
+//---------------------------------
+
+static def controlNumberToId(String controlNumber) {
+    def isXlId = controlNumber.size() > 14
+    isXlId
+            ? controlNumber
+            : 'http://libris.kb.se/resource/bib/' + controlNumber
+}
+
+static Object getAtPath(item, Iterable path, defaultTo = null) {
+    if(!item) {
+        return defaultTo
+    }
+
+    for (int i = 0 ; i < path.size(); i++) {
+        def p = path[i]
+        if (p == '*' && item instanceof Collection) {
+            return item.collect { getAtPath(it, path.drop(i + 1), []) }.flatten()
+        }
+        else if (item[p] != null) {
+            item = item[p]
+        } else {
+            return defaultTo
+        }
+    }
+    return item
+}
+
+Map loadThing(def id) {
+    def thing = [:]
+    selectByIds([id]) { t ->
+        thing = t.graph[1]
+    }
+    return thing
+}
+
+
+boolean addLink(Map data, List path, String uri) {
+    def links = (Document._get(path, data) ?: []) as Set
+    def link = [ '@id': uri ]
+    boolean modified = links.add(link)
+    if (modified) {
+        Document._set(path, links as List, data)
+    }
+    return modified
+}
