ff9e8a07f0666a0ed1ddde14c7d9c751ef654e3b
Olov Ylinenpää <51744858+olovy@users.noreply.github.com>
2022-02-25 17:12:00 +0100

    Feature/lxl 3590 see also (#1076)

---
+++ whelktool/scripts/cleanups/2022/01/lxl-3590-see-also.groovy
@@ -0,0 +1,85 @@
+/*
+Link blank agents in <agent>.seeAlso if an exact match can be found. 
+
+Agents with marc:controlSubfield: a/b ("earlier heading"/"later heading", "tidigare namn"/"senare namn")
+will not match and are not handled. We might want to decide on a more specific relation for them.
+
+   2601 marc:controlSubfield:a
+   2535 marc:controlSubfield:b
+      5 marc:controlSubfield:g
+    292 marc:controlSubfield:i
+      3 marc:controlSubfield:nnnc
+
+https://www.loc.gov/marc/authority/adtracing.html
+https://katalogverk.kb.se/katalogisering/Formathandboken/Auktoritetsformatet/Kontrollkoder-i-auktoritetsformatet/index.html
+
+For more information, see LXL-3590
+ */
+
+import java.util.concurrent.LinkedBlockingQueue
+import whelk.component.ElasticSearch.TooManyResultsException
+
+linked = getReportWriter("linked.tsv")
+notFound = getReportWriter("not-found.tsv")
+multiple = getReportWriter("multiple-matches.tsv")
+variant = getReportWriter("variant.tsv")
+
+selectByCollection('auth') { auth ->
+    def (record, thing) = auth.graph
+    if (!thing.seeAlso) {
+        return
+    }
+    
+    thing.seeAlso.each { seeAlso ->
+        if (seeAlso.'@id') {
+            return 
+        }
+        
+        def ids = findIds(seeAlso).findAll { it != thing.'@id' }
+        if (ids.size() == 1) {
+            linked.println("${thing.'@id'}\t$seeAlso\t${ids.first()}")
+            seeAlso.clear()
+            seeAlso['@id'] = ids.first()
+            auth.scheduleSave()
+        }
+        else {
+            (ids ? multiple : notFound).println("${auth.doc.shortId}\t$seeAlso\t$ids")
+        }
+    }
+}
+
+def findIds(Map seeAlso) {
+    try {
+        find(seeAlso)
+    } catch(TooManyResultsException e) {
+        return []
+    }
+}
+
+def find(Map seeAlso) {
+    def query = [
+            'q' : [seeAlso.values().join(" ")]
+    ]
+
+    LinkedBlockingQueue<String> ids = new LinkedBlockingQueue<>() 
+    selectByIds(queryIds(query).collect()) { candidate -> 
+        def (record, candidateThing) = candidate.graph
+        
+        if (seeAlso.every { key, value -> candidateThing[key] == value}) {
+            ids.add(candidateThing.'@id')
+        }
+        else if (candidateThing.hasVariant) {
+            candidateThing.hasVariant.each {
+                if (seeAlso.every { key, value -> it[key] == value} && it.every { key, value -> seeAlso[key] == value}) {
+                    def allCaps = it.name && it.name.toUpperCase() == it.name
+                    if (!allCaps) {
+                        variant.println("$seeAlso\t$it\t${candidateThing.'@id'}")
+                        //ids.add(candidateThing.'@id') // Skip these for now
+                    }
+                }
+            }
+            
+        }
+    }
+    return ids.collect()
+}
