e656869ead0ae90f467e99a37ea162944dc8f451
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2022-05-02 13:01:56 +0200

    Comment about exceptions without stack traces

--- rest/src/main/groovy/whelk/rest/api/Crud.groovy
+++ rest/src/main/groovy/whelk/rest/api/Crud.groovy
@@ -1066,6 +1066,11 @@ class Crud extends HttpServlet {
         }
     }
 
+    /** "Don't use exceptions for flow control" in part comes from that exceptions in Java are
+     * expensive to create because building the stack trace is expensive. But in the context of 
+     * sending error responses in this API exceptions are pretty useful for flow control. 
+     * This is a base class for stack trace-less exceptions for common error flows.
+     */
     static class NoStackTraceException extends RuntimeException {
         protected NoStackTraceException(String msg) {
             super(msg, null, true, false)
