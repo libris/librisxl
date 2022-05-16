902d940e702f01f99fb749038f69492400ca67c7
Lars Rosenström <lars.rosenstrom@kb.se>
2022-05-16 08:40:11 +0200

    Alternative threading for elastic reindexing

--- importers/src/main/groovy/whelk/reindexer/ElasticReindexer.groovy
+++ importers/src/main/groovy/whelk/reindexer/ElasticReindexer.groovy
@@ -5,6 +5,12 @@ import whelk.Document
 import whelk.Whelk
 import whelk.util.ThreadPool
 
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.stream.Collectors
+
 @Log
 class ElasticReindexer {
 
@@ -65,22 +71,30 @@ class ElasticReindexer {
     }
 
     void reindex(String suppliedCollection) {
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2)
         try {
             int counter = 0
             startTime = System.currentTimeMillis()
             List<String> collections = suppliedCollection ? [suppliedCollection] : whelk.storage.loadCollections()
-            ThreadPool threadPool = new ThreadPool(Runtime.getRuntime().availableProcessors() * 2)
+            println(Runtime.getRuntime().availableProcessors())
             collections.each { collection ->
-                List<Document> documents = []
+                List<Document> documents = new ArrayList<>()
                 for (document in whelk.storage.loadAll(collection)) {
-                    if ( ! document.getDeleted() ) {
+                    if (!document.getDeleted()) {
                         documents.add(document)
                         counter++
                         if (counter % BATCH_SIZE == 0) {
-                            double docsPerSec = ((double) counter) / ((double) ((System.currentTimeMillis() - startTime) / 1000))
-                            println("Indexing $docsPerSec documents per second (running average since process start). Total count: $counter.")
-                            threadPool.executeOnThread(new Batch(documents), new BatchHandler())
-                            documents = []
+
+                            def copy = documents.stream()
+                                    .collect(Collectors.toList())
+
+                            executor.execute( () -> {
+                                double docsPerSec = ((double) counter) / ((double) ((System.currentTimeMillis() - startTime) / 1000))
+                                println("Indexing $docsPerSec documents per second (running average since process start). Total count: $counter.")
+                                bulkIndexWithRetries(copy, whelk)
+                            }
+                            )
+                            documents.clear()
                         }
                     }
                 }
@@ -88,7 +102,8 @@ class ElasticReindexer {
                     bulkIndexWithRetries(documents, whelk)
                 }
             }
-            threadPool.joinAll()
+            executor.shutdown()
+//            threadPool.joinAll()
             println("Done! $counter documents reindexed in ${(System.currentTimeMillis() - startTime) / 1000} seconds.")
             whelk.storage.logStats()
         } catch (Throwable e) {
