29874449108e9dd5f6b0ea01ff44edd719820316
kwahlin <kalle.wahlin@kb.se>
2022-05-18 18:46:28 +0200

    Actually update role property

--- whelktool/src/main/groovy/datatool/scripts/mergeworks/WorkToolJob.groovy
+++ whelktool/src/main/groovy/datatool/scripts/mergeworks/WorkToolJob.groovy
@@ -403,6 +403,8 @@ class WorkToolJob {
                                     }
                                 }
                             }
+
+                            c.role = rolesInContribution
                         }
                     }
 
