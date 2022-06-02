4e26d55b4c4f5e3369dd83b623998a1f7ef81ba4
Niklas Lindström <lindstream@gmail.com>
2022-06-02 17:38:17 +0200

    Add ability to import record-less datasets
    Including predictable minting of XL identifiers.

--- importers/src/main/groovy/whelk/importer/DatasetImporter.groovy
+++ importers/src/main/groovy/whelk/importer/DatasetImporter.groovy
@@ -1,15 +1,37 @@
 package whelk.importer
 
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+import groovy.transform.Immutable
 import groovy.util.logging.Log4j2 as Log
 import whelk.Document
+import whelk.JsonLd
 import whelk.Whelk
+import whelk.IdGenerator
+import whelk.util.DocumentUtil
 import whelk.exception.CancelUpdateException
 
 import static whelk.util.Jackson.mapper
 
 @Log
 class DatasetImporter {
-    static void importDataset(Whelk whelk, String filePath, String dataset) {
+
+    static GRAPH = JsonLd.GRAPH_KEY
+    static ID = JsonLd.ID_KEY
+    static TYPE = JsonLd.TYPE_KEY
+    static REVERSE = JsonLd.REVERSE_KEY
+
+    static String HASH_IT = '#it'
+
+    @Immutable
+    static class DatasetInfo {
+        String uri
+        int createdMs
+        String uriSpace
+    }
+
+    static void importDataset(Whelk whelk, String filePath, String datasetUri) {
 
         if (Runtime.getRuntime().maxMemory() < 2l * 1024l * 1024l * 1024l) {
             log.warn("This application may require substantial amounts of memory, " +
@@ -23,13 +45,13 @@ class DatasetImporter {
         long createdCount = 0
         long lineCount = 0
 
+        DatasetInfo dsInfo = getDatasetInfo(whelk, datasetUri)
+
         File inDataFile = new File(filePath)
         inDataFile.eachLine { line ->
             Map data = mapper.readValue(line.getBytes("UTF-8"), Map)
-            Document incomingDoc = new Document(data)
-            ensureAbsoluteSystemId(incomingDoc)
+            Document incomingDoc = completeRecord(whelk, data, dsInfo)
             idsInInput.add(incomingDoc.getShortId())
-            incomingDoc.addInDataset(dataset)
 
             // This race condition should be benign. If there is a document with
             // the same ID created in between the check and the creation, we'll
@@ -62,22 +84,22 @@ class DatasetImporter {
         }
 
         List<String> needsRetry = []
-        long deletedCount = removeDeleted(whelk, dataset, idsInInput, needsRetry)
+        long deletedCount = removeDeleted(whelk, datasetUri, idsInInput, needsRetry)
 
         System.err.println("Created: " + createdCount +" new,\n" +
                 "updated: " + updatedCount + " existing and\n" +
                 "deleted: " + deletedCount + " old records (should have been: " + (deletedCount + needsRetry.size()) + "),\n" +
-                "out of the: " + idsInInput.size() + " records in dataset: \"" + dataset + "\".\n" +
+                "out of the: " + idsInInput.size() + " records in dataset: \"" + datasetUri + "\".\n" +
                 "Dataset now in sync.")
     }
 
-    protected static long removeDeleted(Whelk whelk, String dataset, Set<String> idsInInput, List<String> needsRetry) {
+    protected static long removeDeleted(Whelk whelk, String datasetUri, Set<String> idsInInput, List<String> needsRetry) {
         // Clear out anything that was previously stored in this dataset, but was not in the in-data now.
         // If faced with "can't delete depended on stuff", retry again later, after more other deletes have
         // succeeded (there may be intra-set dependencies). If the dataset contains circular dependencies,
         // deletions will never be possible until the circle is unlinked/broken somewhere along the chain.
         long deletedCount = 0
-        whelk.storage.doForIdInDataset(dataset, { String storedIdInDataset ->
+        whelk.storage.doForIdInDataset(datasetUri, { String storedIdInDataset ->
             if (!idsInInput.contains(storedIdInDataset)) {
                 if (!remove(whelk, storedIdInDataset)) {
                     needsRetry.add(storedIdInDataset)
@@ -121,6 +143,59 @@ class DatasetImporter {
         }
     }
 
+    static DatasetInfo getDatasetInfo(Whelk whelk, String datasetUri) {
+        Map owningDataset = whelk.storage.getDocumentByIri(datasetUri)?.data[GRAPH][1]
+        assert owningDataset
+        def uriSpace = owningDataset['uriSpace']
+        if (uriSpace == null) {
+            uriSpace = owningDataset[ID]
+            if (!uriSpace.endsWith('/')) {
+                uriSpace += '/'
+            }
+        }
+        int createdMs = parseW3CDateTime(owningDataset['created'])
+
+        return new DatasetInfo(datasetUri, createdMs, uriSpace)
+    }
+
+    static Document completeRecord(Whelk whelk, Map data, DatasetInfo dsInfo) {
+        if (GRAPH !in data) {
+            def givenId = data[ID]
+            def newId = mintNewId(dsInfo, givenId)
+            if (newId != null) {
+                addToSameAs(data, givenId)
+                data[ID] = newId
+            }
+
+            if (REVERSE in data) {
+                Map revs = data.remove(REVERSE)
+            }
+
+            Map record = data.remove('meta') ?: [:]
+            record[ID] = data[ID].replace(HASH_IT, '')
+            record[TYPE] = 'Record'
+
+            record['mainEntity'] = [(ID): data[ID]]
+            data = [(GRAPH): [record, data]]
+        }
+
+        DocumentUtil.findKey(data[GRAPH][1], ID) { id, path ->
+            if ('sameAs' in path) {
+                return
+            }
+            def canonical = whelk.storage.getThingId(id)
+            if (canonical && id != canonical) {
+                return new DocumentUtil.Replace(canonical)
+            }
+        }
+
+        Document doc = new Document(data)
+        ensureAbsoluteSystemId(doc)
+        doc.addInDataset(dsInfo.uri)
+
+        return doc
+    }
+
     private static void ensureAbsoluteSystemId(Document doc) {
         def sysBaseIri = Document.BASE_URI
         // A system id is in place; do nothing and return.
@@ -134,6 +209,36 @@ class DatasetImporter {
             return
         }
         // IDs need to be both reproducible and absolute!
-        throw new RuntimeException("Could not obtain a proper record ID for: " + doc.getURI().toString())
+        throw new RuntimeException("Could not obtain a proper record ID for: " + doc.recordIdentifiers.toString())
     }
+
+    private static long parseW3CDateTime(String dt) {
+        return ZonedDateTime.parse(dt, DateTimeFormatter.ISO_OFFSET_DATE_TIME).toInstant().toEpochMilli()
+    }
+
+    private static String mintNewId(DatasetInfo dsInfo, String givenId) {
+        if (!givenId.startsWith(dsInfo.uriSpace)) {
+            return null
+        }
+        String slug = givenId.substring(dsInfo.uriSpace.length())
+        int timestamp = dsInfo.createdMs + fauxOffset(slug)
+        String xlId = IdGenerator.generate(timestamp, slug)
+        return Document.BASE_URI.resolve(xlId).toString() + HASH_IT
+    }
+
+    private static int fauxOffset(String s) {
+        int n = 0
+        for (int i = 0 ; i < s.size(); i++) {
+            n += s[i].codePointAt(0) * ((i+1) ** 2)
+        }
+        return n
+    }
+
+    private static void addToSameAs(Map entity, String id) {
+        List<Map<String, String>> same = entity.get('sameAs', [])
+        if (!same.find { it[ID] == id }) {
+            same << [(ID): id]
+        }
+    }
+
 }
