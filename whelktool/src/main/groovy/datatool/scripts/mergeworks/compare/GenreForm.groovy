4bcf364fd7bb941a4c999ab2c006d6f9778f3a3e
kwahlin <kalle.wahlin@kb.se>
2022-06-03 13:38:05 +0200

    Fix genreForm merging

--- whelktool/src/main/groovy/datatool/scripts/mergeworks/compare/GenreForm.groovy
+++ whelktool/src/main/groovy/datatool/scripts/mergeworks/compare/GenreForm.groovy
@@ -6,13 +6,17 @@ import datatool.util.DocumentComparator
 class GenreForm extends StuffSet {
     private static final DocumentComparator c = new DocumentComparator()
 
-    // Terms that will be merged, result is the one to right
+    // Terms that will be merged (values precede keys)
     private static def norm = [
-            [['@id': 'https://id.kb.se/marc/NotFictionNotFurtherSpecified'], ['@id': 'https://id.kb.se/marc/FictionNotFurtherSpecified']],
-            [['@id': 'https://id.kb.se/marc/FictionNotFurtherSpecified'], ['@id': 'https://id.kb.se/marc/Novel']],
-            [['@id': 'https://id.kb.se/marc/FictionNotFurtherSpecified'], ['@id': 'https://id.kb.se/marc/Poetry']],
-            [['@id': 'https://id.kb.se/marc/NotFictionNotFurtherSpecified'], ['@id': 'https://id.kb.se/marc/Autobiography']],
-            [['@id': 'https://id.kb.se/marc/NotFictionNotFurtherSpecified'], ['@id': 'https://id.kb.se/marc/Biography']],
+            (['@id': 'https://id.kb.se/marc/NotFictionNotFurtherSpecified']): [
+                    ['@id': 'https://id.kb.se/marc/FictionNotFurtherSpecified'],
+                    ['@id': 'https://id.kb.se/marc/Autobiography'],
+                    ['@id': 'https://id.kb.se/marc/Biography']
+            ],
+            (['@id': 'https://id.kb.se/marc/FictionNotFurtherSpecified']): [
+                    ['@id': 'https://id.kb.se/marc/Poetry'],
+                    ['@id': 'https://id.kb.se/marc/Novel']
+            ],
     ]
 
     @Override
@@ -20,18 +24,13 @@ class GenreForm extends StuffSet {
         return mergeCompatibleElements(super.merge(a, b)) { gf1, gf2 ->
             if (n(gf1, gf2)) {
                 gf2
-            }
-            else if (n(gf2, gf1)) {
+            } else if (n(gf2, gf1)) {
                 gf1
             }
         }
     }
 
     boolean n(a, b) {
-        norm.any {
-            it[0] == a && it[1] == b
-        }
+        norm[a]?.any {it == b || n(it, b) }
     }
-
-
 }
