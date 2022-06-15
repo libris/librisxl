31c48cb40d61fc3b70af306c3e2c6390548cf01d
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2022-06-15 11:07:20 +0200

    Add cleanup script.

---
+++ whelktool/scripts/cleanups/2022/06/lxl-3872-remove-bic.groovy
@@ -0,0 +1,32 @@
+String where = """
+        collection = 'bib' AND deleted = false AND
+        data#>>'{@graph,1,instanceOf,subject}' LIKE '%bicssc%'
+"""
+
+selectBySqlWhere(where) { data ->
+    boolean modified = false
+
+    data.graph[1].instanceOf?.subject?.removeAll { subject ->
+        for (Object o : asList(subject.inScheme)) {
+            Map scheme = (Map) o
+            if (scheme["@id"] == "https://id.kb.se/term/bicssc" || scheme["code"] == "bicssc") {
+                modified = true
+                return true
+            }
+        }
+        return false
+    }
+
+    if (modified) {
+        //System.err.println("result:\n" + data.doc.getDataAsString() + "\n\n")
+        data.scheduleSave()
+    }
+}
+
+private List asList(Object o) {
+    if (o == null)
+        return []
+    if (o instanceof List)
+        return o
+    return [o]
+}
