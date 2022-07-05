923cbfb4a7ae8e671b2fd4e8e44e3b6353b4c79f
kwahlin <kalle.wahlin@kb.se>
2022-07-05 15:24:47 +0200

    Add script for moving summary to instance

---
+++ whelktool/scripts/cleanups/2022/07/lxl-3303-summary-to-instance.groovy
@@ -0,0 +1,45 @@
+def where = """
+  collection = 'bib' 
+  AND data['@graph'][1]['instanceOf']['@type'] = '"Text"'
+  AND data['@graph'][1]['instanceOf']['summary'] IS NOT NULL
+  AND deleted = false
+"""
+
+def badShape = ['@type', 'label', 'language'] as Set
+
+selectByCollection('bib') { bib ->
+    def instance = bib.graph[1]
+    def work = instance.instanceOf
+
+    if (!work || work instanceof List)
+        return
+
+    def normalizedSummary = work.summary.collect { Map s ->
+        if (s.keySet().containsAll(badShape)) {
+            def lang = s.subMap(['@type', 'language'])
+            s.remove('language')
+            modified = true
+            return [lang, s]
+        }
+        s
+    }.flatten()
+
+    def (toInstance, toWork) = normalizedSummary.split { Map s ->
+        s.containsKey('label')
+    }
+
+    if (toInstance) {
+        if (toWork) {
+            work['summary'] = toWork
+        } else {
+            work.remove('summary')
+        }
+
+        instance['summary'] = (instance.summary ?: []) + toInstance
+        bib.scheduleSave()
+    }
+}
+
+def asList(x) {
+    (x ?: []).with {it instanceof List ? it : [it] }
+}
