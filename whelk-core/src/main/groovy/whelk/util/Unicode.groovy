dbec9f1955cfb059ce3d357dba32d9cec3618f8b
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2022-08-18 13:01:43 +0200

    Search: Normalize double quotation marks
    For example Safari on iOs defaults user input to 'Right Double Quotation Mark'
    which breaks phrase search. Replace with regular ".

--- whelk-core/src/main/groovy/whelk/util/Unicode.groovy
+++ whelk-core/src/main/groovy/whelk/util/Unicode.groovy
@@ -33,7 +33,12 @@ class Unicode {
     private static final List STRIP_UNICODE_CHARS = [
             '\ufeff',
     ]
-
+    
+    // U+201C LEFT DOUBLE QUOTATION MARK
+    // U+201D RIGHT DOUBLE QUOTATION MARK
+    private static final Pattern NORMALIZE_DOUBLE_QUOTES = Pattern.compile("[\u201c\u201d]", Pattern.UNICODE_CHARACTER_CLASS)
+    
+    // U+2060 WORD JOINER
     private static final Pattern LEADING_SPACE = Pattern.compile('^[\\p{Blank}\u2060]+', Pattern.UNICODE_CHARACTER_CLASS)
     private static final Pattern TRAILING_SPACE = Pattern.compile('[\\p{Blank}\u2060]+$', Pattern.UNICODE_CHARACTER_CLASS)
     
@@ -54,11 +59,19 @@ class Unicode {
     }
 
     static boolean isNormalizedForSearch(String s) {
-        return Normalizer.isNormalized(s, Normalizer.Form.NFKC)
+        return Normalizer.isNormalized(s, Normalizer.Form.NFKC) && isNormalizedDoubleQuotes(s)
     }
 
     static String normalizeForSearch(String s) {
-        return Normalizer.normalize(s, Normalizer.Form.NFKC)
+        return normalizeDoubleQuotes(Normalizer.normalize(s, Normalizer.Form.NFKC))
+    }
+    
+    static boolean isNormalizedDoubleQuotes(String s) {
+        !NORMALIZE_DOUBLE_QUOTES.matcher(s).find()
+    }
+    
+    static String normalizeDoubleQuotes(String s) {
+        s.replaceAll(NORMALIZE_DOUBLE_QUOTES, '"')
     }
 
     /**
