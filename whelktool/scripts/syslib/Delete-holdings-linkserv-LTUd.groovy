c14651d562b571023ec8a6bd2450f2d86d7d1b5a
poacherone <poacherone@users.noreply.github.com>
2022-10-11 11:15:42 +0200

    Delete Linkserver holdings for LTUd (#1162)

---
+++ whelktool/scripts/syslib/Delete-holdings-linkserv-LTUd.groovy
@@ -0,0 +1,10 @@
+String SIGEL_TO_DELETE = 'LTUd'
+String HOLD_ID_FILE = 'LTUd-diff-ID.txt'
+
+File holdsToRemove = new File(scriptDir, HOLD_ID_FILE)
+
+selectByIds( holdsToRemove.readLines() ) { hold ->
+    if (hold.doc.sigel == SIGEL_TO_DELETE) {
+        hold.scheduleDelete()
+    }
+}
