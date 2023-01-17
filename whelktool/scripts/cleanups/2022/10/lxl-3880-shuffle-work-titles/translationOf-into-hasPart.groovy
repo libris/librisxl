bd57138a22138913d5cb629875856f1d5e59bc8c
kwahlin <kalle.wahlin@kb.se>
2023-01-17 13:00:56 +0100

    Refine hasPart script

--- whelktool/scripts/cleanups/2022/10/lxl-3880-shuffle-work-titles/translationOf-into-hasPart.groovy
+++ whelktool/scripts/cleanups/2022/10/lxl-3880-shuffle-work-titles/translationOf-into-hasPart.groovy
@@ -3,21 +3,24 @@
  *
  * The requirements are:
  *  - Each of the properties translationOf, hasPart and language are present on the work (instanceOf)
- *  - Each part in hasPart has a title and exactly one language**
- *  - The work has exactly language
+ *  - Each part in hasPart has a title*
+ *  - Each part in hasPart has language**
  *  - The language of each part in hasPart matches the work language
  *  - No part in hasPart has translationOf
- *  - work.translationOf has exatly one language
+ *  - work.translationOf has exactly one language
  *  - work.translationOf differs from every hasPart.language
  *
- *  **Except entities that have either of the forms {@type, contentType}, {@type, genreForm, intendedAudience} or {@type, genreForm}
+ *  *Except entities that have either of the forms {@type,language}, {@type,contentType}, {@type,genreForm,intendedAudience} or {@type,genreForm}
  *    These are ignored.
+ *  **With a few exceptions, see code.
  *
  *  Unlink any linked part in hasPart.
  *
  *  Remove work.translationOf after it's been copied to hasPart if there is no instanceOf.hasTitle.
  *
  */
+
+import whelk.filter.LanguageLinker
 
 def unhandled = getReportWriter('unhandled.tsv')
 
@@ -35,16 +38,27 @@ TRANSLATION_OF = 'translationOf'
 TYPE = '@type'
 ID = '@id'
 
-def exceptShapes = [['@type', 'contentType'], ['@type', 'genreForm', 'intendedAudience'], ['@type', 'genreForm']]*.toSet()
+def exceptShapes = [['@type', 'language'], ['@type', 'contentType'], ['@type', 'genreForm', 'intendedAudience'], ['@type', 'genreForm']]*.toSet()
 
 selectBySqlWhere(where) { bib ->
+    def langLinker = bib.whelk.normalizer.normalizers.find { it.linker instanceof LanguageLinker }.linker
     def id = bib.doc.shortId
 
     def work = bib.graph[1][INSTANCE_OF]
     def hasPart = work[HAS_PART]
     def translationOf = work[TRANSLATION_OF]
 
+    if (asList(translationOf).size() > 1) {
+        unhandled.println([id, "multiple translationOf"].join('\t'))
+        return
+    }
+
+    linkLangs(langLinker, work)
+    linkLangs(langLinker, translationOf)
+    linkLangs(langLinker, hasPart, asList(work[LANGUAGE]))
+
     def workLang = asList(work[LANGUAGE])
+    def trlOfLang = asList(asList(translationOf)[0][LANGUAGE])
 
     hasPart.each { p ->
         if (p[ID]) {
@@ -58,15 +72,13 @@ selectBySqlWhere(where) { bib ->
         return
     }
 
-    if (asList(translationOf).size() > 1) {
-        unhandled.println([id, "multiple translationOf"].join('\t'))
+    if (trlOfLang.size() > 1) {
+        unhandled.println([id, "multiple translationOf lang"].join('\t'))
         return
     }
 
-    def trlOfLang = asList(asList(translationOf)[0][LANGUAGE])
-
-    if (workLang.size() != 1 || trlOfLang.size() != 1) {
-        unhandled.println([id, "multiple langs"].join('\t'))
+    if (trlOfLang.size() == 0) {
+        unhandled.println([id, "missing translationOf lang"].join('\t'))
         return
     }
 
@@ -85,13 +97,45 @@ selectBySqlWhere(where) { bib ->
         return
     }
 
-    if (hasPart.any { p -> !(p.keySet() in exceptShapes) && asList(p[LANGUAGE]) != workLang }) {
+    if (hasPart.any { p -> p[LANGUAGE] && !workLang.containsAll(asList(p[LANGUAGE])) }) {
         unhandled.println([id, "language mismatch"].join('\t'))
         return
     }
 
+    def (partsWithLang, partsMissingLang) = hasPart.findAll { p -> !(p.keySet() in exceptShapes) }.split { it[LANGUAGE] }
+    def partLangs = partsWithLang.collect { it[LANGUAGE] }.flatten()
+
+    if (partsWithLang && partsMissingLang) {
+        if (workLang.size() > 1) {
+            def addLang = workLang - partLangs
+            if (addLang) {
+                if (addLang == trlOfLang) {
+                    // There are several work languages
+                    // The parts with language are translations of the parts without language
+                    // Add original language to the parts missing language
+                    hasPart.each { p ->
+                        if (!(p.keySet() in exceptShapes)) {
+                            p[LANGUAGE] = p[LANGUAGE] ?: addLang
+                        }
+                    }
+                } else {
+                    unhandled.println([id, "multiple instanceOf langs"].join('\t'))
+                    return
+                }
+            }
+        }
+        if (partLangs == workLang) {
+            // Add original language to the parts missing language
+            hasPart.each { p ->
+                if (!(p.keySet() in exceptShapes)) {
+                    p[LANGUAGE] = p[LANGUAGE] ?: trlOfLang
+                }
+            }
+        }
+    }
+
     hasPart.each { p ->
-        if (p.keySet() in exceptShapes) {
+        if (p.keySet() in exceptShapes || asList(p[LANGUAGE]) == trlOfLang) {
             return
         }
         p[TRANSLATION_OF] = translationOf
@@ -104,12 +148,13 @@ selectBySqlWhere(where) { bib ->
     bib.scheduleSave()
 }
 
-private List asList(Object o) {
-    if (o == null)
-        return []
-    if (o instanceof List)
-        return o
-    return [o]
+def linkLangs(LanguageLinker linker, Object obj, List disambiguationNodes = []) {
+    asList(obj).each { entity ->
+        entity.subMap('language')?.with {
+            linker.linkLanguages(it, disambiguationNodes)
+            entity['language'] = it['language']
+        }
+    }
 }
 
 Map loadThing(def id) {
