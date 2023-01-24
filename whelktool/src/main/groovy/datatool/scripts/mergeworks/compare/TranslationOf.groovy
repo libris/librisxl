7202c023cef6dd6406c09ac07479aee1fa5ab032
kwahlin <kalle.wahlin@kb.se>
2023-01-24 10:49:10 +0100

    handle titles in translationOf

--- whelktool/src/main/groovy/datatool/scripts/mergeworks/compare/TranslationOf.groovy
+++ whelktool/src/main/groovy/datatool/scripts/mergeworks/compare/TranslationOf.groovy
@@ -1,5 +1,6 @@
 package datatool.scripts.mergeworks.compare
 
+import datatool.scripts.mergeworks.Util
 import datatool.util.DocumentComparator
 
 class TranslationOf implements FieldHandler {
@@ -8,15 +9,23 @@ class TranslationOf implements FieldHandler {
     @Override
     boolean isCompatible(Object a, Object b) {
         // @type is sometimes Work, sometimes Text. Should not matter for comparison
-        (!a && !b) || a && b && a instanceof Map && b instanceof Map && c.isEqual(noType(a), noType(b))
+        (!a && !b) || (a && b
+                && a instanceof Map
+                && b instanceof Map
+                && c.isEqual(noTypeNoTitle(a), noTypeNoTitle(b))
+                && (!a['hasTitle'] && !b['hasTitle']
+                    || !Util.getTitleVariants(a['hasTitle']).intersect(Util.getTitleVariants(b['hasTitle'])).isEmpty()))
+
     }
 
     @Override
     Object merge(Object a, Object b) {
         return a // TODO: prefer one @type over another?
     }
-    
-    Map noType(Map m) {
-        m.findAll { k, v -> k != '@type' }
+
+    Map noTypeNoTitle(Map m) {
+        m.findAll { k, v -> !(k in ['@type', 'hasTitle']) }
     }
+
+
 }
