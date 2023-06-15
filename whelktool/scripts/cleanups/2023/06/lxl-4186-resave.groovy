3930326495d28a3286e86af0b7eb704fa9a40885
Jannis Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2023-06-15 12:52:47 +0200

    No need to use the versions table on second though.

--- whelktool/scripts/cleanups/2023/06/lxl-4186-resave.groovy
+++ whelktool/scripts/cleanups/2023/06/lxl-4186-resave.groovy
@@ -2,7 +2,7 @@
 String where = """
 id in (
  select d.id
- from lddb__versions l inner join lddb__dependencies d on l.id = d.dependsonid
+ from lddb l inner join lddb__dependencies d on l.id = d.dependsonid
  where
   l.collection = 'auth' and
   l.modified > '2023-04-17' and
