0bfa1998c16dcb4788a7a08eebcf638d7f9f6df5
Jannis Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2023-06-15 11:44:01 +0200

    Minor adjustments to cleanup script.

--- whelktool/scripts/cleanups/2023/06/lxl-4186-resave.groovy
+++ whelktool/scripts/cleanups/2023/06/lxl-4186-resave.groovy
@@ -2,12 +2,12 @@
 String where = """
 id in (
  select d.id
- from lddb l inner join lddb__dependencies d on l.id = d.dependsonid
+ from lddb__versions l inner join lddb__dependencies d on l.id = d.dependsonid
  where
   l.collection = 'auth' and
   l.modified > '2023-04-17' and
   l.modified < '2023-05-31'
-) and deleted = false;
+) and deleted = false and collection = 'bib';
 """
 
 selectBySqlWhere(where) { data ->
