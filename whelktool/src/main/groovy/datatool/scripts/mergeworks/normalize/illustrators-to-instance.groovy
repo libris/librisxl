2921926b2d4a71563c8d15b09651749bd7bba3aa
kwahlin <kalle.wahlin@kb.se>
2023-06-21 16:03:21 +0200

    Look for more gf terms when deciding if illustrator should be moved

--- whelktool/src/main/groovy/datatool/scripts/mergeworks/normalize/illustrators-to-instance.groovy
+++ whelktool/src/main/groovy/datatool/scripts/mergeworks/normalize/illustrators-to-instance.groovy
@@ -1,5 +1,6 @@
 import datatool.scripts.mergeworks.Util.Relator
 
+import whelk.Whelk
 import static whelk.JsonLd.ID_KEY
 
 def ids = new File(System.getProperty('clusters')).collect { it.split('\t').collect { it.trim() } }.flatten()
@@ -18,9 +19,8 @@ selectByIds(ids) { bib ->
     contribution.removeAll { c ->
         if (!asList(c.role).contains(ill)) return false
         def has9pu = [(ID_KEY): Relator.PRIMARY_RIGHTS_HOLDER.iri] in asList(c.role)
-        if (has9pu || isPictureBook(work) || isComics(work)) return false
+        if (has9pu || isPictureBook(work) || isComics(work, bib.whelk)) return false
         instance['contribution'] = asList(instance['contribution']) + c.clone().tap { it['role'] = [ill] }
-        println(instance['contribution'])
         c['role'] = asList(c.role) - ill
         modified = true
         return c.role.isEmpty()
@@ -36,10 +36,26 @@ selectByIds(ids) { bib ->
 }
 
 boolean isPictureBook(Map work) {
-    [(ID_KEY): 'https://id.kb.se/term/barngf/Bilderb%C3%B6cker'] in asList(work.genreForm)
+    def picBookTerms = [
+            'https://id.kb.se/term/barngf/Bilderb%C3%B6cker',
+            'https://id.kb.se/term/barngf/Sm%C3%A5barnsbilderb%C3%B6cker'
+    ].collect { [(ID_KEY): it] }
+
+    return asList(work.genreForm).any { it in picBookTerms }
 }
 
-boolean isComics(Map work) {
-    asList(work.genreForm).any { it[ID_KEY] ==~ 'https://id.kb.se/term/(saogf|barngf)/Tecknade%20serier' }
-            || asList(work.classification).any { it.code?.startsWith('Hci') }
+boolean isComics(Map work, Whelk whelk) {
+    def comicsTerms = [
+            'https://id.kb.se/term/saogf/Tecknade%20serier',
+            'https://id.kb.se/term/barngf/Tecknade%20serier',
+            'https://id.kb.se/term/gmgpc/swe/Tecknade%20serier',
+            'https://id.kb.se/marc/ComicOrGraphicNovel',
+            'https://id.kb.se/marc/ComicStrip'
+    ].collect { [(ID_KEY): it] }
+
+    return asList(work.genreForm).any {
+        it in comicsTerms
+                || it[ID_KEY] && whelk.relations.isImpliedBy('https://id.kb.se/term/saogf/Tecknade%20serier', it[ID_KEY])
+                || asList(work.classification).any { it.code?.startsWith('Hci') }
+    }
 }
