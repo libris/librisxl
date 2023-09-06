2a063f493f147beace7dde8025a083ff73a76f08
kwahlin <kalle.wahlin@kb.se>
2023-09-06 16:35:27 +0200

    Exclude works with marc:PreAdolescent in intendedAudience from selection

--- librisworks/scripts/swedish-fiction.groovy
+++ librisworks/scripts/swedish-fiction.groovy
@@ -17,6 +17,7 @@ new File(System.getProperty('clusters')).splitEachLine(~/[\t ]+/) {cluster ->
                 && !doc.isDrama()
                 && !doc.isThesis()
                 && !doc.isInSb17Bibliography()
+                && !doc.intendedForMarcPreAdolescent()
     }
 
     def swedish = { Doc doc ->
