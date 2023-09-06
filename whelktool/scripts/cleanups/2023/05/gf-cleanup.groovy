d4f989b2087e2fbf0248e863f49d99cd9e90c122
kwahlin <kalle.wahlin@kb.se>
2023-09-06 14:44:43 +0200

    Add mapping to barngf/Lättlästa böcker

--- whelktool/scripts/cleanups/2023/05/gf-cleanup.groovy
+++ whelktool/scripts/cleanups/2023/05/gf-cleanup.groovy
@@ -1,19 +1,20 @@
 def substitutions =
         [
-                (['@type': 'GenreForm', 'prefLabel': 'Romaner'])                                                                                                                                              : 'https://id.kb.se/term/saogf/Romaner',
-                (['@type': 'GenreForm', 'prefLabel': 'Barn & Ungdom'])                                                                                                                                        : 'https://id.kb.se/term/barngf/Barn-%20och%20ungdomslitteratur',
-                (['@type': 'GenreForm', 'prefLabel': 'Romantik'])                                                                                                                                             : 'https://id.kb.se/term/saogf/K%C3%A4rleksskildringar',
-                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/sao/Arbetarskildringar']], 'inScheme': ['@id': 'https://id.kb.se/term/sao'], 'prefLabel': 'Arbetarskildringar'])            : 'https://id.kb.se/term/saogf/Arbetarskildringar',
-                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/barn/Mellan%C3%A5ldersb%C3%B6cker']], 'inScheme': ['@id': 'https://id.kb.se/term/barn'], 'prefLabel': 'Mellanåldersböcker']): 'https://id.kb.se/term/barngf/Mellan%C3%A5ldersb%C3%B6cker',
-                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/barn/Ungdomsb%C3%B6cker']], 'inScheme': ['@id': 'https://id.kb.se/term/barn'], 'prefLabel': 'Ungdomsböcker'])               : 'https://id.kb.se/term/barngf/Ungdomsb%C3%B6cker',
-                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/barn/L%C3%A4seb%C3%B6cker']], 'inScheme': ['@id': 'https://id.kb.se/term/barn'], 'prefLabel': 'Läseböcker'])                : 'https://id.kb.se/term/barngf/L%C3%A4seb%C3%B6cker',
-                (['@type': 'GenreForm', 'prefLabel': 'Lyrik'])                                                                                                                                                : 'https://id.kb.se/term/saogf/Poesi',
-                (['@type': 'GenreForm', 'prefLabel': 'Noveller'])                                                                                                                                             : 'https://id.kb.se/term/saogf/Noveller',
-                (['@type': 'GenreForm', 'prefLabel': 'Feelgood'])                                                                                                                                             : 'https://id.kb.se/term/saogf/Feelgood',
-                (['@type': 'GenreForm', 'prefLabel': 'Unga vuxna'])                                                                                                                                           : 'https://id.kb.se/term/barngf/Unga%20vuxna',
-                (['@type': 'GenreForm', 'prefLabel': 'Bilderböcker'])                                                                                                                                         : 'https://id.kb.se/term/barngf/Bilderb%C3%B6cker',
-                (['@type': 'GenreForm', 'prefLabel': 'Punktskrift'])                                                                                                                                          : 'https://id.kb.se/term/saogf/Punktskriftsb%C3%B6cker',
-                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/barn/%C3%84ventyrsb%C3%B6cker']], 'inScheme': ['@id': 'https://id.kb.se/term/barn'], 'prefLabel': 'Äventyrsböcker'])        : 'https://id.kb.se/term/barngf/%C3%84ventyrsb%C3%B6cker'
+                (['@type': 'GenreForm', 'prefLabel': 'Romaner'])                                                                                                                                                 : 'https://id.kb.se/term/saogf/Romaner',
+                (['@type': 'GenreForm', 'prefLabel': 'Barn & Ungdom'])                                                                                                                                           : 'https://id.kb.se/term/barngf/Barn-%20och%20ungdomslitteratur',
+                (['@type': 'GenreForm', 'prefLabel': 'Romantik'])                                                                                                                                                : 'https://id.kb.se/term/saogf/K%C3%A4rleksskildringar',
+                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/sao/Arbetarskildringar']], 'inScheme': ['@id': 'https://id.kb.se/term/sao'], 'prefLabel': 'Arbetarskildringar'])               : 'https://id.kb.se/term/saogf/Arbetarskildringar',
+                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/barn/Mellan%C3%A5ldersb%C3%B6cker']], 'inScheme': ['@id': 'https://id.kb.se/term/barn'], 'prefLabel': 'Mellanåldersböcker'])   : 'https://id.kb.se/term/barngf/Mellan%C3%A5ldersb%C3%B6cker',
+                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/barn/Ungdomsb%C3%B6cker']], 'inScheme': ['@id': 'https://id.kb.se/term/barn'], 'prefLabel': 'Ungdomsböcker'])                  : 'https://id.kb.se/term/barngf/Ungdomsb%C3%B6cker',
+                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/barn/L%C3%A4seb%C3%B6cker']], 'inScheme': ['@id': 'https://id.kb.se/term/barn'], 'prefLabel': 'Läseböcker'])                   : 'https://id.kb.se/term/barngf/L%C3%A4seb%C3%B6cker',
+                (['@type': 'GenreForm', 'prefLabel': 'Lyrik'])                                                                                                                                                   : 'https://id.kb.se/term/saogf/Poesi',
+                (['@type': 'GenreForm', 'prefLabel': 'Noveller'])                                                                                                                                                : 'https://id.kb.se/term/saogf/Noveller',
+                (['@type': 'GenreForm', 'prefLabel': 'Feelgood'])                                                                                                                                                : 'https://id.kb.se/term/saogf/Feelgood',
+                (['@type': 'GenreForm', 'prefLabel': 'Unga vuxna'])                                                                                                                                              : 'https://id.kb.se/term/barngf/Unga%20vuxna',
+                (['@type': 'GenreForm', 'prefLabel': 'Bilderböcker'])                                                                                                                                            : 'https://id.kb.se/term/barngf/Bilderb%C3%B6cker',
+                (['@type': 'GenreForm', 'prefLabel': 'Punktskrift'])                                                                                                                                             : 'https://id.kb.se/term/saogf/Punktskriftsb%C3%B6cker',
+                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/barn/%C3%84ventyrsb%C3%B6cker']], 'inScheme': ['@id': 'https://id.kb.se/term/barn'], 'prefLabel': 'Äventyrsböcker'])           : 'https://id.kb.se/term/barngf/%C3%84ventyrsb%C3%B6cker',
+                (['@type': 'GenreForm', 'sameAs': [['@id': 'https://id.kb.se/term/barn/L%C3%A4ttl%C3%A4sta%20b%C3%B6cker']], 'inScheme': ['@id': 'https://id.kb.se/term/barn'], 'prefLabel': 'Lättlästa böcker']): 'https://id.kb.se/term/barngf/L%C3%A4ttl%C3%A4sta%20b%C3%B6cker'
         ]
 
 selectByCollection('bib') {
@@ -35,7 +36,7 @@ selectByCollection('bib') {
             gf['@id'] = linkableTerm
             modified = true
         }
-        if (isLargePrint && gf == ['@type':'GenreForm', 'prefLabel':'Storstilsbok']) {
+        if (isLargePrint && gf == ['@type': 'GenreForm', 'prefLabel': 'Storstilsbok']) {
             incrementStats('removed from genreForm', gf.toString())
             return modified = true
         }
@@ -45,7 +46,7 @@ selectByCollection('bib') {
     work.genreForm?.unique()
 
     work.subject?.removeAll { s ->
-        if (isLargePrint && s == ['@type':'Topic', 'label':['Storstil']]) {
+        if (isLargePrint && s == ['@type': 'Topic', 'label': ['Storstil']]) {
             incrementStats('removed from subject', s.toString())
             return modified = true
         }
