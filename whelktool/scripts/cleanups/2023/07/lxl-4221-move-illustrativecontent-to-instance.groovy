be82a2d447a221112f57e5f4c0418217226b187d
klngwll <fredrik.klingwall@gmail.com>
2023-09-26 14:22:03 +0200

    Remove illustrativeContent restrictions

--- whelktool/scripts/cleanups/2023/07/lxl-4221-move-illustrativecontent-to-instance.groovy
+++ whelktool/scripts/cleanups/2023/07/lxl-4221-move-illustrativecontent-to-instance.groovy
@@ -2,8 +2,6 @@ def where = """
     collection = 'bib' 
     and deleted = false
     and data#>>'{@graph,1,instanceOf,@type}' = 'Text'
-    and data#>'{@graph,1,instanceOf,language}' @> '[{"@id":"https://id.kb.se/language/swe"}]'
-    and data#>'{@graph,1,instanceOf,genreForm}' @> '[{"@id":"https://id.kb.se/marc/FictionNotFurtherSpecified"}]'
     and data#>'{@graph,1,instanceOf, illustrativeContent}' is not null
 """
 
