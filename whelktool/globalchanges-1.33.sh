9e94ba065f9ed96e96475c680d6353890f40b8a4
klngwll <fredrik.klingwall@gmail.com>
2023-09-28 13:23:00 +0200

    Add lxl-4221 to globalchanges 1.33

--- whelktool/globalchanges-1.33.sh
+++ whelktool/globalchanges-1.33.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 time java -Dxl.secret.properties=$HOME/secret.properties-$ENV -jar build/libs/whelktool.jar --report reports/$ENV-$(date +%Y%m%d-%H%M%S) --skip-index scripts/2023/08/lxl-4243-move-out-solitary-contentType-from-hasPart.groovy
+time java -Dxl.secret.properties=$HOME/secret.properties-$ENV -jar build/libs/whelktool.jar --report reports/$ENV-$(date +%Y%m%d-%H%M%S) --skip-index scripts/cleanups/2023/07/lxl-4221-move-illustrativecontent-to-instance.groovy
