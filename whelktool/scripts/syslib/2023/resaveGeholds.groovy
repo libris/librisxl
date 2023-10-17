1b142633060bb4bb51dec34b277ce90f7dba57ce
poacherone <poacherone@users.noreply.github.com>
2023-10-17 11:02:20 +0200

    Update resaveGeholds.groovy

--- whelktool/scripts/syslib/2023/resaveGeholds.groovy
+++ whelktool/scripts/syslib/2023/resaveGeholds.groovy
@@ -1,6 +1,6 @@
 // MUST be run with --allow-loud
 
-File holdsToResave = new File(scriptDir, "ge_holds_resave.txt")
+File holdsToResave = new File(scriptDir, "ge_holds_resave_hela.txt")
 
 selectByIds( holdsToResave.readLines() ) { hold ->
     hold.scheduleSave(loud: true)
