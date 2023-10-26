644259910cb28c3f4b809e7c9b0ee649e9adf8c1
kwahlin <kalle.wahlin@kb.se>
2023-10-26 11:02:47 +0200

    Remove commented-out code used for testing stuff

--- librisworks/scripts/find-work-clusters.groovy
+++ librisworks/scripts/find-work-clusters.groovy
@@ -7,30 +7,7 @@ import java.util.concurrent.ConcurrentHashMap
 PrintWriter failedQueries = getReportWriter("failed-queries")
 PrintWriter tooLargeResult = getReportWriter("too-large-result")
 
-//def yesterday = new SimpleDateFormat('yyyy-MM-dd').with { sdf ->
-//    Calendar.getInstance().with { c ->
-//        c.add(Calendar.DATE, -1)
-//        sdf.format(c.getTime())
-//    }
-//}
-
-//def where = """
-//    collection = '%s'
-//    AND (modified::date = '$yesterday'
-//        OR (data#>>'{@graph,0,generationDate}')::date = '$yesterday')
-//"""
-
-visited = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>())  // TODO: remove?
-//instancesOfUpdatedLinkedWorks = Collections.synchronizedSet([] as Set)
-//
-//selectBySqlWhere(String.format(where, 'auth')) {
-//    def thing = it.graph[1]
-//    if (Normalizers.isInstanceOf(it.whelk.jsonld, thing, 'Work')) {
-//        selectBySqlWhere("collection = 'bib' and data#>>'{@graph,1,instanceOf,@id}' = '${thing['@id']}'") {
-//            instancesOfUpdatedLinkedWorks.add(it.doc.shortId)
-//        }
-//    }
-//}
+visited = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>())
 
 def process = { bib ->
     if (!visited.add(bib.doc.shortId))
@@ -59,12 +36,6 @@ def process = { bib ->
     }
 }
 
-//selectByIds(instancesOfUpdatedLinkedWorks) {
-//    process(it)
-//}
-
-// TODO: Change when starting to run regularly
-//selectBySqlWhere(String.format(where, 'bib')) {
 selectByCollection('bib') {
     process(it)
 }
