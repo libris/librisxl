d5ead57a8459b6bc0281fbdb96c4610579b1fe0a
Kai Pöykiö <kai.poykio@kb.se>
2023-12-21 14:36:15 +0100

    checks for datatypes...

--- whelktool/scripts/syslib/RanE-fix-links.groovy
+++ whelktool/scripts/syslib/RanE-fix-links.groovy
@@ -9,15 +9,50 @@ String RanE_holds = """
 selectBySqlWhere(RanE_holds) { d ->
 	def hold = d.getGraph()
 	def instance = hold[1]
-	//println("${instance.'@id'}")
-	instance['isPrimaryTopicOf'].each { i ->
-		i['uri'].eachWithIndex { ii, iii ->
-			if ( ii.startsWith("http://\$") ) {
-				i['uri'].set(iii, "https://bibliotek.ranrike.se/digitala-biblioteket")
-				//println(hold)
-				println("Modified ${instance.'@id'}")
-				d.scheduleSave(loud: true)
+	if ( instance.containsKey('isPrimaryTopicOf') ) {
+		if ( instance['isPrimaryTopicOf'] instanceof List ) {
+			instance['isPrimaryTopicOf'].each { i ->
+				if ( i.containsKey('uri') ) {
+					if ( i['uri'] instanceof List ) {
+						i['uri'].eachWithIndex { ii, iii ->
+							if ( ii instanceof String && ii.startsWith("http://\$") ) {
+								i['uri'].set(iii, "https://bibliotek.ranrike.se/digitala-biblioteket")
+								//println(hold)
+								//println("Modified ${instance.'@id'}")
+								d.scheduleSave(loud: true)
+							}
+						}
+					} else if ( i['uri'] instanceof String ) {
+						if ( i['uri'].startsWith("http://\$") ) {
+							i['uri'] = "https://bibliotek.ranrike.se/digitala-biblioteket"
+							//println(hold)
+							//println("Modified ${instance.'@id'}")
+							d.scheduleSave(loud: true)
+						}
+					}
+				}
 			}
+		} else if ( instance['isPrimaryTopicOf'] instanceof Map ) {
+				if ( instance['isPrimaryTopicOf'].containsKey('uri') ) {
+					if ( instance['isPrimaryTopicOf']['uri'] instanceof List ) {
+						instance['isPrimaryTopicOf']['uri'].eachWithIndex { ii, iii ->
+							if ( ii instanceof String && ii.startsWith("http://\$") ) {
+						
+								instance['isPrimaryTopicOf']['uri'].set(iii, "https://bibliotek.ranrike.se/digitala-biblioteket")
+								//println(hold)
+								//println("Modified ${instance.'@id'}")
+								d.scheduleSave(loud: true)
+							}
+						}
+					} else if ( instance['isPrimaryTopicOf']['uri'] instanceof String ) {
+						if ( instance['isPrimaryTopicOf']['uri'].startsWith("http://\$") ) {
+							instance['isPrimaryTopicOf']['uri'] = "https://bibliotek.ranrike.se/digitala-biblioteket"
+							//println(hold)
+							//println("Modified ${instance.'@id'}")
+							d.scheduleSave(loud: true)
+						}
+					}
+				}
 		}
 	}
 }
