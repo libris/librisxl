760195108f4386f56b62a6c63d897c1bd8451a2f
kwahlin <kalle.wahlin@kb.se>
2024-02-16 09:13:00 +0100

    Keep terms if coming from multiple sources

--- whelktool/scripts/cleanups/2024/02/lxl-4441-remove-misleading-intendedAudience-and-genreForm.groovy
+++ whelktool/scripts/cleanups/2024/02/lxl-4441-remove-misleading-intendedAudience-and-genreForm.groovy
@@ -1,10 +1,15 @@
 /**
  * Remove incorrect data originally coming from local works in Elib records and consequently ended up on linked works in
  * the automatic work merging.
+ *
  * The terms to remove are:
- *  - "https://id.kb.se/term/barngf/Barn-%20och%20ungdomslitteratur" from genreForm
- *  - "https://id.kb.se/marc/Juvenile" from intendedAudience
- * unless either coming from a non-Elib record, in that case keep both.
+ *  - Any https://id.kb.se/term/barngf/... term from genreForm
+ *  - Any https://id.kb.se/term/barn/... term from subject
+ *  - https://id.kb.se/marc/Juvenile from intendedAudience
+ *  - Local entities with specific age range (e.g. "9-12 år") from intendedAudience
+ *
+ * Remove only if all these terms came from the same Elib record.
+ * If multiple sources or another source than Elib, keep.
  */
 
 BARN_NS = "https://id.kb.se/term/barn/"
@@ -23,26 +28,26 @@ selectBySqlWhere(where) { workDocItem ->
     def hasMarcJuv = hasMarcJuvenile(work)
 
     if (hasMarcJuvenile(work) || hasBarnGf(work) || hasBarnSubject(work)) {
-        def anyElib = false
-        def anyNonElib = false
+        def fromElib = 0
+        def fromOther = 0
 
         selectByIds(record['derivedFrom'].collect { it['@id'] }) { dfDocItem ->
             def versionBeforeWorkExtraction = dfDocItem.getVersions().reverse().find {
                 it.data['@graph'][1]['instanceOf'].with { it && !it['@id'] }
             }
             def (dfRecord, dfInstance) = versionBeforeWorkExtraction.data['@graph']
-
-            if (isElib(dfRecord)) {
-                anyElib = true
-                return
-            }
-
             def dfWork = dfInstance['instanceOf']
 
-            anyNonElib |= (hasMarcJuvenile(dfWork) || hasBarnGf(dfWork) || hasBarnSubject(dfWork))
+            if (hasMarcJuvenile(dfWork) || hasBarnGf(dfWork) || hasBarnSubject(dfWork)) {
+                if (isElib(dfRecord)) {
+                    fromElib += 1
+                } else {
+                    fromOther += 1
+                }
+            }
         }
 
-        if (anyElib && !anyNonElib) {
+        if (fromElib == 1 && fromOther == 0) {
             work['intendedAudience']?.removeAll {
                 if (it['@id'] == MARC_JUVENILE || it['label'] in ['9-12 år', '6-9 år', '12-15 år', '3-6 år', '15', '0-3 år']) {
                     incrementStats('intendedAudience', it)
