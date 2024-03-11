5a8744fba4d0c0e62ae3fc54fce899bff79f9520
kwahlin <kalle.wahlin@kb.se>
2024-03-11 14:26:59 +0100

    Add script for removing obsolete sab codes from svsk works

---
+++ whelktool/scripts/cleanups/2024/03/svsk-works/lxl-4458-remove-obsolete-sab-codes.groovy
@@ -0,0 +1,44 @@
+removed = getReportWriter('removed.txt')
+changed = getReportWriter('changed.txt')
+
+def ids = new File(scriptDir, 'CREATED-20231212.txt').readLines()
+
+selectByIds(ids) { docItem ->
+    def id = docItem.doc.shortId
+    def work = docItem.graph[1]
+
+    def existsProperSab = hasProperSab(work)
+
+    if (existsProperSab) {
+        if (work['classification'].removeAll {
+            isSab(it) && isBadCode(it.code)
+        }) {
+            docItem.scheduleSave()
+            removed.println(id)
+        }
+    } else {
+        work['classification']?.each {
+            if (isSab(it) && isBadCode(it.code)) {
+                if (it.code.startsWith('Hcb')) {
+                    it['code'] = 'Hc'
+                } else if (it.code.startsWith('Hdab')) {
+                    it['code'] = 'Hda'
+                }
+                docItem.scheduleSave()
+                changed.println(id)
+            }
+        }
+    }
+}
+
+boolean isBadCode(String code) {
+    code =~ /^Hcb|^Hdab/
+}
+
+boolean hasProperSab(Map work) {
+    work['classification']?.any { isSab(it) && !(isBadCode(it.code) || it.code == 'H') }
+}
+
+boolean isSab(Map classification) {
+    classification.inScheme?.code =~ /[Kk]ssb/
+}
