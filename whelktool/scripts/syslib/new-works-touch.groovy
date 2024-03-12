0f165eff46c13c969d91075c971fec6f9a3d2492
Lars Rosenström <lars.rosenstrom@kb.se>
2024-03-12 10:25:16 +0100

    Touch linked instances instead

--- whelktool/scripts/syslib/new-works-touch.groovy
+++ whelktool/scripts/syslib/new-works-touch.groovy
@@ -1,16 +1,16 @@
 // MUST be run with --allow-loud
 
 BATCH_SIZE = 5000
-File allWorksToResave = new File(scriptDir, "new-works-touch-ids.txt")
+File allInstancesToResave = new File(scriptDir, "new-work-instances-touch-ids.txt")
 String touchedIds = System.getProperty("touched-ids-file", '')
 File touchedIdsFile = new File(touchedIds)
-def allWorks = allWorksToResave.readLines() as Set
+def allInstances = allInstancesToResave.readLines() as Set
 def touched = touchedIdsFile.readLines() as Set
-def ids = (allWorks - touched).take(BATCH_SIZE)
+def ids = (allInstances - touched).take(BATCH_SIZE)
 
-selectByIds( ids ) { work ->
-    work.scheduleSave(loud: false)
+selectByIds( ids ) { instance ->
+    instance.scheduleSave(loud: true)
     touchedIdsFile.withWriterAppend { writer ->
-        writer.append(work.doc.shortId + "\n")
+        writer.append(instance.doc.shortId + "\n")
     }
 }
