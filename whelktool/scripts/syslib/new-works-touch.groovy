78890e18012e20367f5c12a8c9550e8ee97be5b5
Lars Rosenström <lars.rosenstrom@kb.se>
2024-03-12 08:32:29 +0100

    Simplify script

--- whelktool/scripts/syslib/new-works-touch.groovy
+++ whelktool/scripts/syslib/new-works-touch.groovy
@@ -1,20 +1,16 @@
 // MUST be run with --allow-loud
 
-BATCH_SIZE = 2000
+BATCH_SIZE = 5000
 File allWorksToResave = new File(scriptDir, "new-works-touch-ids.txt")
 String touchedIds = System.getProperty("touched-ids-file", '')
 File touchedIdsFile = new File(touchedIds)
-List touched = touchedIdsFile.readLines()
-def count = 0
+def allWorks = allWorksToResave.readLines() as Set
+def touched = touchedIdsFile.readLines() as Set
+def ids = (allWorks - touched).take(BATCH_SIZE)
 
-selectByIds( allWorksToResave.readLines() ) { work ->
-    if (touched.contains(work.doc.shortId) || count >= BATCH_SIZE) {
-        return
-    } else {
-        work.scheduleSave(loud: true)
-        count++
-        touchedIdsFile.withWriterAppend { writer ->
-            writer.append(work.doc.shortId + "\n")
-        }
+selectByIds( ids ) { work ->
+    work.scheduleSave(loud: false)
+    touchedIdsFile.withWriterAppend { writer ->
+        writer.append(work.doc.shortId + "\n")
     }
 }
