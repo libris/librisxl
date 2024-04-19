80ca9e5599b85883e1f7378b5332323a0e8760f9
Kalle Wåhlin <72360110+kwahlin@users.noreply.github.com>
2024-04-19 17:13:20 +0200

    Prevent URI's from being encoded twice (#1412)

--- whelk-core/src/test/groovy/whelk/search/XLQLQuerySpec.groovy
+++ whelk-core/src/test/groovy/whelk/search/XLQLQuerySpec.groovy
@@ -3,7 +3,6 @@ package whelk.search
 import spock.lang.Ignore
 import spock.lang.Specification
 import whelk.Whelk
-import whelk.xlql.Operator
 import whelk.xlql.QueryTree
 import whelk.xlql.SimpleQueryTree
 
@@ -292,7 +291,24 @@ class XLQLQuerySpec extends Specification {
         Map mappings = xlqlQuery.toMappings(sqt)
 
         expect:
-        mappings['or'][0]['up']['@id'] =='/find?_q=a b "c:d" f "g h i>j" k l'
-        mappings['or'][1]['up']['@id'] =='/find?_q=hasTitle:"x!"'
+        mappings['or'][0]['up']['@id'] == '/find?_q=a b "c:d" f "g h i>j" k l'
+        mappings['or'][1]['up']['@id'] == '/find?_q=hasTitle:"x!"'
+    }
+
+    def "Encode URI slug"() {
+        expect:
+        XLQLQuery.encodeUri("https://example/" + slug) == "https://example/" + encoded
+
+        where:
+        slug                        | encoded
+        "Hästar"                    | "H%C3%A4star"
+        "H%C3%A4star"               | "H%C3%A4star"
+        "Försvunna personer"        | "F%C3%B6rsvunna%20personer"
+        "F%C3%B6rsvunna%20personer" | "F%C3%B6rsvunna%20personer"
+        "fnrblrghr1234567#it"       | "fnrblrghr1234567#it"
+        "abc+abc"                   | "abc%2Babc"
+        "abc%2Babc"                 | "abc%2Babc"
+        "å/äö"                      | "%C3%A5/%C3%A4%C3%B6"
+        "%C3%A5/%C3%A4%C3%B6"       | "%C3%A5/%C3%A4%C3%B6"
     }
 }
