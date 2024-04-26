libris/librisxl

commit 8d73ac41b60f1aa77fd4465835b76735b1eb60cd

Deterministic sort order for *-search (#1417)
Sort results deterministically when doing *-search. Sort on doc id for now.

rest/src/main/groovy/whelk/rest/api/SearchUtils2.java
@@ -117,7 +117,12 @@ public class SearchUtils2 {
             queryDsl.put("query", xlqlQuery.getEsQuery(queryTree));
             queryDsl.put("size", limit);
             queryDsl.put("from", offset);
-            sortBy.insertSortClauses(queryDsl, xlqlQuery);
+            if (queryTree.isWild()) {
+                // Stable sort order if there is no meaningful relevancy
+                Sort.BY_DOC_ID.insertSortClauses(queryDsl, xlqlQuery);
+            } else {
+                sortBy.insertSortClauses(queryDsl, xlqlQuery);
+            }
             queryDsl.put("aggs", xlqlQuery.getAggQuery(statsRepr, outsetType));
             queryDsl.put("track_total_hits", true);
             return queryDsl;
@@ -327,6 +332,9 @@ public class SearchUtils2 {
     }
 
     static class Sort {
+        public static Sort DEFAULT_BY_RELEVANCY = new Sort("");
+        public static Sort BY_DOC_ID = new Sort("_es_id");
+
         private enum Order {
             asc,
             desc
@@ -349,7 +357,7 @@ public class SearchUtils2 {
 
         List<ParameterOrder> parameters;
 
-        public Sort(String sort) {
+        private Sort(String sort) {
             parameters = Arrays.stream(sort.split(","))
                     .map(String::trim)
                     .filter(s -> !s.isEmpty())
@@ -360,7 +368,9 @@ public class SearchUtils2 {
         }
 
         static Sort fromString(String s) {
-            return new Sort(s);
+            return (s == null || s.isBlank())
+                    ? DEFAULT_BY_RELEVANCY
+                    : new Sort(s);
         }
 
         void insertSortClauses(Map<String, Object> queryDsl, XLQLQuery xlqlQuery) {

whelk-core/src/main/groovy/whelk/xlql/QueryTree.java
@@ -1,16 +1,34 @@
 package whelk.xlql;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static whelk.xlql.Operator.EQUALS;
 
 public class QueryTree {
     public sealed interface Node permits And, Or, Nested, Field, FreeText {
+        default List<Node> children() {
+            return Collections.emptyList();
+        }
     }
 
     public record And(List<Node> conjuncts) implements Node {
+        @Override
+        public List<Node> children() {
+            return conjuncts;
+        }
     }
 
     public record Or(List<Node> disjuncts) implements Node {
+        public List<Node> children() {
+            return disjuncts;
+        }
     }
 
     public record Nested(List<Field> fields, Operator operator) implements Node {
@@ -20,6 +38,9 @@ public class QueryTree {
     }
 
     public record FreeText(Operator operator, String value) implements Node {
+        public boolean isWild() {
+            return operator == EQUALS && "*".equals(value);
+        }
     }
 
     public Node tree;
@@ -30,6 +51,37 @@ public class QueryTree {
 
     public QueryTree(SimpleQueryTree sqt, Disambiguate disambiguate, Disambiguate.OutsetType outsetType) {
         this.tree = sqtToQt(sqt.tree, disambiguate, outsetType);
+    }
+
+    /**
+     * There is no freetext or all freetext nodes are "*"
+     */
+    public boolean isWild() {
+        return StreamSupport.stream(allDescendants(tree).spliterator(), false)
+                .noneMatch(n -> n instanceof FreeText && !((FreeText) n).isWild());
+    }
+
+    private static Iterable<Node> allDescendants(Node node) {
+        Iterator<Node> i = new Iterator<>() {
+            List<Node> nodes;
+            @Override
+            public boolean hasNext() {
+                if (nodes == null) {
+                    nodes = new LinkedList<>();
+                    nodes.add(node);
+                }
+                return !nodes.isEmpty();
+            }
+
+            @Override
+            public Node next() {
+                Node next = nodes.removeFirst();
+                nodes.addAll(next.children());
+                return next;
+            }
+        };
+
+        return () -> i;
     }
 
     private static Node sqtToQt(SimpleQueryTree.Node sqtNode, Disambiguate disambiguate, Disambiguate.OutsetType outset) {

commit 6a1b0929dd777b90e7f1cd8f34d0d4ba8d9c39a8

Fix explicit sort parameter broken by 8d73ac41b

rest/src/main/groovy/whelk/rest/api/SearchUtils2.java
@@ -117,7 +117,7 @@ public class SearchUtils2 {
             queryDsl.put("query", xlqlQuery.getEsQuery(queryTree));
             queryDsl.put("size", limit);
             queryDsl.put("from", offset);
-            if (queryTree.isWild()) {
+            if (sortBy == Sort.DEFAULT_BY_RELEVANCY && queryTree.isWild()) {
                 // Stable sort order if there is no meaningful relevancy
                 Sort.BY_DOC_ID.insertSortClauses(queryDsl, xlqlQuery);
             } else {