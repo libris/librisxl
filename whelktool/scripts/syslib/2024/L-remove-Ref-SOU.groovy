33a1391446a922b05f2992ed268569f266923f51
poacherone <poacherone@users.noreply.github.com>
2024-04-30 12:43:26 +0200

    Remove Ref component from L holdings

---
+++ whelktool/scripts/syslib/2024/L-remove-Ref-SOU.groovy
@@ -0,0 +1,40 @@
+File bibIDs = new File(scriptDir, "L-SOU-ID.txt")
+  
+selectByIds( bibIDs.readLines() ) { hold ->
+def item = hold.graph[1]
+
+if (item["hasComponent"] && item["hasComponent"]?.size() > 1) {
+    List component = item["hasComponent"]
+    boolean change = false
+    boolean modified = false
+
+    component?.each { c ->
+        if (c['shelfMark'] instanceof List) {
+            def removedShelfMark = c['shelfMark'].removeAll { n ->
+                asList(n['label']).any { it.contains('Ref') }
+            }
+            if (c['shelfMark'].isEmpty()) {
+                c.remove('shelfMark')
+                change = true
+            }
+        } else if (c['shelfMark'] instanceof Map && asList(c['shelfMark']['label']).any { it.contains('Ref') }) {
+                c.remove('shelfMark')
+                change = true
+        }
+
+        if (change && !c['shelfMark']) {
+        c.removeAll { it }
+        modified = true
+        }
+    }
+
+    if (modified) {
+        component?.removeAll { !it }
+        if (item["hasComponent"].isEmpty()) {
+            item.remove('hasComponent')
+        }
+        hold.scheduleSave(loud: true)
+    }
+
+}
+}
