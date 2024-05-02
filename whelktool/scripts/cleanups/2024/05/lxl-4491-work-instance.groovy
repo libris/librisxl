4314fe9a8cbaede7b7ea5daac1def3ef45d42e0c
Olov Ylinenpää <51744858+olovy@users.noreply.github.com>
2024-05-02 18:04:20 +0200

    Add script for fixing records with #work as main id (#1426)

---
+++ whelktool/scripts/cleanups/2024/05/lxl-4491-work-instance.groovy
@@ -0,0 +1,20 @@
+import whelk.datatool.DocumentItem
+
+skipped = getReportWriter("skipped")
+
+String where = "id IN (SELECT id FROM lddb__identifiers WHERE graphindex = 1 AND mainid IS true AND iri LIKE '%#work')"
+
+selectBySqlWhere(where) { DocumentItem bib ->
+    def (record, work, instance) = bib.graph;
+
+    if (record && work && instance) {
+        record['mainEntity']['@id'] = instance['@id']
+        work.remove('@id')
+        work.remove('sameAs')
+        instance['instanceOf'] = work
+        bib.doc.data['@graph'] = [record, instance]
+        bib.scheduleSave(loud: true)
+    } else {
+        skipped.println(bib.doc.shortId)
+    }
+}
