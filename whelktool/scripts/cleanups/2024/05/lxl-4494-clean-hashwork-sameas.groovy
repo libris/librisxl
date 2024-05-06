27b1d1315fbe223e40bf50f8944a71fa37395212
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2024-05-06 11:19:42 +0200

    Add script for removing bad sameAs with #work suffix

---
+++ whelktool/scripts/cleanups/2024/05/lxl-4494-clean-hashwork-sameas.groovy
@@ -0,0 +1,7 @@
+String where = "id in (select id from lddb__identifiers where graphindex = 1 AND iri like '%#work' and mainid is false)"
+
+selectBySqlWhere(where) { doc ->
+    if (doc.graph[1]['sameAs'].removeIf{ it['@id'].endsWith('#work') }) {
+        doc.scheduleSave()
+    }
+}
