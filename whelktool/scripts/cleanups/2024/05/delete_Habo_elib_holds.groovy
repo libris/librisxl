9a8d17c755006155f643ad5bee1216c9dea1ce83
Kai Pöykiö <kai.poykio@kb.se>
2024-05-15 13:20:35 +0200

    deletion of extra Habo holds

---
+++ whelktool/scripts/cleanups/2024/05/delete_Habo_elib_holds.groovy
@@ -0,0 +1,18 @@
+//240515 KP, deletes superfluos holds for Habo with 'origin:Elib'
+
+String Habo_elib_holds = """
+        collection = 'hold' 
+        and data#>>'{@graph,1,heldBy,@id}' = 'https://libris.kb.se/library/Habo' 
+        and data#>'{@graph,1,cataloguersNote}' @> '["origin:Elib"]'::jsonb 
+        and not data#>'{@graph,1,cataloguersNote}' @> '["deleted"]'::jsonb
+        and deleted = false
+        """
+
+
+selectBySqlWhere(Habo_elib_holds) { d ->
+	//def hold = d.getGraph()
+	//def id = hold[0].'@id'
+	//println('I: ' + id)
+	//println(hold)
+	d.scheduleDelete(loud: true)
+}
