3008f0ac99150cbd7952150d52524c7317f35ac7
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2024-09-13 18:00:29 +0200

    AccessControl: Make methods that can be static static

--- rest/src/main/groovy/whelk/rest/security/AccessControl.groovy
+++ rest/src/main/groovy/whelk/rest/security/AccessControl.groovy
@@ -13,12 +13,13 @@ class AccessControl {
     static final KAT_KEY = 'cataloger'
 
 
-    boolean checkDocumentToPost(Document newDoc, Map userPrivileges, JsonLd jsonld) {
+    static boolean checkDocumentToPost(Document newDoc, Map userPrivileges, JsonLd jsonld) {
         return checkDocument(newDoc, userPrivileges, jsonld)
     }
 
+    // FIXME Mocking in CrudSpec breaks if this is made static
     boolean checkDocumentToPut(Document newDoc, Document oldDoc,
-                               Map userPrivileges, JsonLd jsonld) {
+                                      Map userPrivileges, JsonLd jsonld) {
         if (oldDoc.isHolding(jsonld)) {
             def newDocSigel = newDoc.getHeldBySigel()
             def oldDocSigel = oldDoc.getHeldBySigel()
@@ -51,11 +52,12 @@ class AccessControl {
                 checkDocument(oldDoc, userPrivileges, jsonld)
     }
 
+    // FIXME Mocking in CrudSpec breaks if this is made static
     boolean checkDocumentToDelete(Document oldDoc, Map userPrivileges, JsonLd jsonld) {
         return checkDocument(oldDoc, userPrivileges, jsonld)
     }
 
-    boolean checkDocument(Document document, Map userPrivileges, JsonLd jsonld) {
+    private static boolean checkDocument(Document document, Map userPrivileges, JsonLd jsonld) {
         if (!isValidActiveSigel(userPrivileges)) {
             return false
         }
@@ -77,7 +79,7 @@ class AccessControl {
         }
     }
 
-    private boolean hasPermissionForSigel(String sigel, Map userPrivileges) {
+    private static boolean hasPermissionForSigel(String sigel, Map userPrivileges) {
         boolean result = false
 
         // redundant, but we want to safeguard against future mishaps
@@ -99,29 +101,29 @@ class AccessControl {
         return result
     }
 
-    private boolean hasCatalogingPermission(Map userPrivileges) {
+    private static boolean hasCatalogingPermission(Map userPrivileges) {
         return userPrivileges.permissions.any { item ->
             item.get(KAT_KEY)
         }
     }
 
-    private boolean hasGlobalRegistrantPermission(Map userPrivileges) {
+    private static boolean hasGlobalRegistrantPermission(Map userPrivileges) {
         return activeSigelPermissions(userPrivileges)
                 .map({p -> p[GLOBALREG_KEY] == true})
                 .orElse(false)
     }
 
-    boolean isValidActiveSigel(Map userPrivileges) {
+    private static boolean isValidActiveSigel(Map userPrivileges) {
         return activeSigelPermissions(userPrivileges).isPresent()
     }
 
-    private Optional<Map> activeSigelPermissions(Map userPrivileges) {
+    private static Optional<Map> activeSigelPermissions(Map userPrivileges) {
         String activeSigel = userPrivileges.get('active_sigel')
         Map permissions = null
         if (activeSigel) {
             permissions = userPrivileges.permissions.find { permission ->
                 return permission.code == activeSigel
-            }
+            } as Map
         }
         return Optional.ofNullable(permissions)
     }
