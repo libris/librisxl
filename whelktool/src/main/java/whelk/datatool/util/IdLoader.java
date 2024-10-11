327f5de6fe45294219b98f1f5712afd3b75c5a73
kwahlin <kalle.wahlin@kb.se>
2024-10-11 13:31:50 +0200

    Fix broken loadAllIds method

--- whelktool/src/main/java/whelk/datatool/util/IdLoader.java
+++ whelktool/src/main/java/whelk/datatool/util/IdLoader.java
@@ -6,15 +6,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.StreamSupport;
 
-import static whelk.component.PostgreSQLComponent.iterateDocuments;
 import static whelk.datatool.WhelkTool.DEFAULT_FETCH_SIZE;
 
 public class IdLoader {
@@ -27,18 +27,30 @@ public class IdLoader {
         this.storage = storage;
     }
 
+    public List<String> fromFile(String fileLocation) {
+        // TODO
+        return Collections.emptyList();
+    };
+
     public List<Id> loadAllIds(Collection<String> shortIds) {
         String where = String.format("id in ( '%s' )", String.join("','", shortIds));
         String q = String.format("""
-                SELECT data
+                SELECT id, data#>>'{@graph,1,@id}' AS thingIri, data#>>'{@graph,0,@id}' AS recordIri
                 FROM lddb
                 WHERE %s
                 """, where);
 
-        Function<ResultSet, List<Id>> collectResults = (rs) ->
-                StreamSupport.stream(iterateDocuments(rs).spliterator(), false)
-                        .map(d -> new Id(d.getShortId(), d.getThingIdentifiers().getFirst(), d.getCompleteSystemId()))
-                        .toList();
+        Function<ResultSet, List<Id>> collectResults = (rs) -> {
+            List<Id> ids = new ArrayList<>();
+            try {
+                while (rs.next()) {
+                    ids.add(new Id(rs.getString("id"), rs.getString("thingIri"), rs.getString("recordIri")));
+                }
+            } catch (SQLException e) {
+                return Collections.emptyList();
+            }
+            return ids;
+        };
 
         try {
             return query(q, collectResults);
@@ -53,6 +65,9 @@ public class IdLoader {
     }
 
     public List<String> collectXlShortIds(Collection<String> ids, String marcCollection) {
+        if (!Arrays.asList("bib", "auth", "hold").contains(marcCollection)) {
+            return collectXlShortIds(ids);
+        }
         Map<String, String> iriToShortId = findShortIdsForIris(ids.stream().filter(id -> id.contains(":")).toList());
         Map<String, String> voyagerIdToXlShortID = findXlShortIdsForVoyagerIds(
                 ids.stream().filter(IdLoader::isVoyagerId).toList(),
