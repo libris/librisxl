2ec5cdf936f71d886a6388950f1123a0db365c3e
Niklas Lindström <lindstream@gmail.com>
2024-10-29 18:54:29 +0100

    Move simplifyAssociatedMedia to mediareprs script

--- whelktool/scripts/typenormalization/algorithm.groovy
+++ whelktool/scripts/typenormalization/algorithm.groovy
@@ -184,8 +184,6 @@ class TypeNormalizer {
       modified |= convertIssuancetype(instance, work)
     }
 
-    // modified |= simplifyAssociatedMedia(instance)  // TODO: move to separate normalization.
-
     return modified
   }
 
@@ -470,52 +468,6 @@ class TypeNormalizer {
       }
     }
     return false
-  }
-
-  static boolean simplifyAssociatedMedia(Map instance) {
-    var modified = false
-
-    for (tuple in [
-      ["associatedMedia", "hasRepresentation"],
-      ["isPrimaryTopicOf", "associatedMedia"]
-    ]) {
-      def (givenRel, mediaRel) = tuple
-      for (Object qualification : asList(instance.get(givenRel))) {
-        def uri = qualification.get("uri")
-        if (uri) {
-          if (uri instanceof Map) {
-            uri = ((Map) uri).get(VALUE)
-          }
-          asList(qualification.getOrDefault("cataloguersNote", "")).each {
-            if (it.toLowerCase().contains("digipic")) mediaRel = "image"
-          }
-          if (uri instanceof List) {
-            assert uri.size() == 1
-            uri = uri[0]
-          }
-          var mediaObj = [(ID): uri]
-          instance.put(mediaRel, mediaObj)
-          var scope = qualification.get("appliesTo")
-          Map annot
-          if ((scope instanceof Map && scope.containsKey("label"))) {
-            if (!mediaObj.containsKey(ANNOTATION)) mediaObj.put(ANNOTATION, [:])
-            annot = (Map) mediaObj.get(ANNOTATION)
-            annot.put("label", ((Map) scope).get("label"))
-          }
-          String note = qualification.get("marc:publicNote")
-          if (note) {
-            if (!mediaObj.containsKey(ANNOTATION)) mediaObj.put(ANNOTATION, [:])
-            annot = (Map) mediaObj.get(ANNOTATION)
-            annot.put("comment", note)
-          }
-        }
-        continue
-      }
-      instance.remove(givenRel)
-      modified = true
-    }
-
-    return modified
   }
 
   static List asList(Object o) {
