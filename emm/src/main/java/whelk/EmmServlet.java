2807edda00b303b850a5416686c00945492ed61f
Jannis Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2024-10-31 11:35:31 +0100

    Progress.

--- emm/src/main/java/whelk/EmmServlet.java
+++ emm/src/main/java/whelk/EmmServlet.java
@@ -1,18 +1,19 @@
 package whelk;
 
-import whelk.Whelk;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 public class EmmServlet extends HttpServlet {
-
+    private static final int TARGET_HITS_PER_PAGE = 100;
     private final Logger logger = LogManager.getLogger(this.getClass());
     private final HashSet<String> availableCategories;
     private final Whelk whelk;
@@ -57,57 +58,89 @@ public class EmmServlet extends HttpServlet {
     }
 
     private void sendChangeSet(Whelk whelk, HttpServletResponse res, String category, String until) throws IOException {
-        final int targetHitsPerPage = 100;
+
+        List<EmmActivity> activitiesOnPage = new ArrayList<>(TARGET_HITS_PER_PAGE+5);
+        Timestamp nextTimeStamp = getPage(whelk, category, until, activitiesOnPage);
+        if (nextTimeStamp == null) {
+            res.sendError(500);
+            return;
+        }
+
+        for (EmmActivity a : activitiesOnPage) {
+            System.err.println(a);
+        }
+    }
+
+    /**
+     * Get a page's worth of items. The results will be added to the 'result'-list. The return value will be "the next timestamp"
+     * to start getting the next page at, or null on failure.
+     */
+    private Timestamp getPage(Whelk whelk, String category, String until, List<EmmActivity> result) {
 
         // Internally 'until' is "milliseconds since epoch".
         long untilNumerical = Long.parseLong(until);
         Timestamp untilTimeStamp = new Timestamp(untilNumerical);
 
-        Timestamp earliestSeenTimeStamp = new Timestamp(System.currentTimeMillis()); // This is how far back in time this page stretches
+        Timestamp earliestSeenTimeStamp = new Timestamp(System.currentTimeMillis()); // This is (will be set to) how far back in time this page stretches
 
         try (Connection connection = whelk.getStorage().getOuterConnection()) {
 
             // Get a page of items
             {
-                String sql =
-                        "SELECT data#>>'{@graph,0,@id}', GREATEST(modified, (data#>>'{@graph,0,generationDate}')::timestamptz) " +
-                                "  FROM lddb WHERE GREATEST(modified, (data#>>'{@graph,0,generationDate}')::timestamptz) <= ? " +
-                                "  ORDER BY GREATEST(modified, (data#>>'{@graph,0,generationDate}')::timestamptz) DESC LIMIT ? ".stripIndent();
+                String sql = "SELECT" +
+                                "  data#>>'{@graph,1,@id}'," +
+                                "  GREATEST(modified, (data#>>'{@graph,0,generationDate}')::timestamptz)," +
+                                "  deleted," +
+                                "  created," +
+                                "  data#>>'{@graph,1,@type}'" +
+                                " FROM" +
+                                "  lddb__versions" +
+                                " WHERE GREATEST(modified, (data#>>'{@graph,0,generationDate}')::timestamptz) <= ? " +
+                                " ORDER BY GREATEST(modified, (data#>>'{@graph,0,generationDate}')::timestamptz) DESC LIMIT ? ".stripIndent();
                 PreparedStatement preparedStatement = connection.prepareStatement(sql);
                 preparedStatement.setTimestamp(1, untilTimeStamp);
-                preparedStatement.setInt(2, targetHitsPerPage);
-                preparedStatement.setFetchSize(targetHitsPerPage + 1);
+                preparedStatement.setInt(2, TARGET_HITS_PER_PAGE);
+                preparedStatement.setFetchSize(TARGET_HITS_PER_PAGE + 1);
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()) {
                         String uri = resultSet.getString(1);
                         Timestamp modificationTime = resultSet.getTimestamp(2);
-
-                        System.err.println("at " + modificationTime + " -> " + uri);
-
+                        boolean deleted = resultSet.getBoolean(3);
+                        Timestamp creationTime = resultSet.getTimestamp(4);
+                        String type = resultSet.getString(5);
+                        result.add(new EmmActivity(uri, type, creationTime, modificationTime, deleted));
                         if (modificationTime.before(earliestSeenTimeStamp))
                             earliestSeenTimeStamp = modificationTime;
                     }
                 }
             }
 
-            // Get any extra records that share an exact modification time with the earliest seen time on this page (above)
+            // Get any extra records that share an exact modification time with the earliest seen time on this page
             {
-                String sql = "SELECT data#>>'{@graph,0,@id}' FROM lddb WHERE GREATEST(modified, (data#>>'{@graph,0,generationDate}')::timestamptz) = ?";
+                String sql = "SELECT" +
+                        "  data#>>'{@graph,1,@id}'," +
+                        "  deleted," +
+                        "  created," +
+                        "  data#>>'{@graph,1,@type}'" +
+                        " FROM lddb__versions WHERE GREATEST(modified, (data#>>'{@graph,0,generationDate}')::timestamptz) = ?".stripIndent();
                 PreparedStatement preparedStatement = connection.prepareStatement(sql);
                 preparedStatement.setTimestamp(1, untilTimeStamp);
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()) {
                         String uri = resultSet.getString(1);
-
-                        System.err.println("at same time! " + uri);
+                        Timestamp modificationTime = resultSet.getTimestamp(2);
+                        boolean deleted = resultSet.getBoolean(3);
+                        Timestamp creationTime = resultSet.getTimestamp(4);
+                        String type = resultSet.getString(5);
+                        result.add(new EmmActivity(uri, type, creationTime, modificationTime, deleted));
                     }
                 }
             }
-
-            System.err.println("Starting point for next page: " + earliestSeenTimeStamp);
-
         } catch (SQLException se) {
-            res.sendError(500);
+            logger.error(se);
+            return null;
         }
+
+        return earliestSeenTimeStamp;
     }
 }
---
+++ librisxl-tools/postgresql/migrations/00000023-index-generationDate-versions.plsql
@@ -0,0 +1,31 @@
+BEGIN;
+
+DO $$DECLARE
+   -- THESE MUST BE CHANGED WHEN YOU COPY THE SCRIPT!
+   
+   -- The version you expect the database to have _before_ the migration
+   old_version numeric := 22;
+   -- The version the database should have _after_ the migration
+   new_version numeric := 23;
+
+   -- hands off
+   existing_version numeric;
+
+BEGIN
+
+   -- Check existing version
+   SELECT version from lddb__schema INTO existing_version;
+   IF ( existing_version <> old_version) THEN
+      RAISE EXCEPTION 'ASKED TO MIGRATE FROM INCORRECT EXISTING VERSION!';
+      ROLLBACK;
+   END IF;
+   UPDATE lddb__schema SET version = new_version;
+
+   -- ACTUAL SCHEMA CHANGES HERE:
+
+   -- This is an ammendment of migration #18, which did this for the lddb-table (including the totstz-function). We now need it for versions as well.
+   CREATE INDEX IF NOT EXISTS idx_lddb__versions_generation_date ON lddb__versions (totstz(data#>>'{@graph,0,generationDate}'));
+   
+END$$;
+
+COMMIT;
