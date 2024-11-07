72e410ff654b2e79083e616f4b3b698d6a5fa98c
kwahlin <kalle.wahlin@kb.se>
2024-11-07 11:48:46 +0100

    Avoid duplicate links when relinking

--- whelktool/src/main/resources/bulk-change-scripts/merge.groovy
+++ whelktool/src/main/resources/bulk-change-scripts/merge.groovy
@@ -5,8 +5,8 @@ import static whelk.JsonLd.ID_KEY
 import static whelk.datatool.bulkchange.BulkJobDocument.DEPRECATE_KEY
 import static whelk.datatool.bulkchange.BulkJobDocument.KEEP_KEY
 
-List<String> deprecate = parameters.get(DEPRECATE_KEY)
-String keep = parameters.get(KEEP_KEY)
+List<String> deprecate = ["https://id.kb.se/term/sao/M%C3%84V1"]//parameters.get(DEPRECATE_KEY)
+String keep = "https://id.kb.se/term/sao/M%C3%84V-NY" //parameters.get(KEEP_KEY)
 
 Set<String> allObsoleteThingUris = synchronizedSet([] as Set<String>)
 selectByIds(deprecate) { obsolete ->
@@ -21,6 +21,13 @@ selectByIds(deprecate) { obsolete ->
         if (modified) {
             depender.scheduleSave(loud: isLoudAllowed)
         }
+        // Remove duplicate links
+        DocumentUtil.traverse(depender.graph) { value, path ->
+            if (value instanceof List) {
+                value.unique(true) { it instanceof Map ? it[ID_KEY] : it }
+                return new DocumentUtil.Nop()
+            }
+        }
     }
     allObsoleteThingUris.addAll(obsoleteThingUris)
 }
@@ -30,7 +37,7 @@ selectByIds(deprecate) {
 }
 
 selectByIds([keep]) {
-    allObsoleteThingUris.each {uri ->
+    allObsoleteThingUris.each { uri ->
         it.doc.addThingIdentifier(uri)
     }
     it.scheduleSave()
