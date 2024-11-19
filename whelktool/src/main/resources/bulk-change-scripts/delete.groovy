1e8c92e224eb98f3346e8b8d7d7207f186b7afb9
kwahlin <kalle.wahlin@kb.se>
2024-11-19 19:11:01 +0100

    Move logic from Specification into scripts

--- whelktool/src/main/resources/bulk-change-scripts/delete.groovy
+++ whelktool/src/main/resources/bulk-change-scripts/delete.groovy
@@ -1,13 +1,21 @@
-import whelk.datatool.bulkchange.Specification
+import whelk.Document
+import whelk.datatool.form.Transform
 
+import static whelk.JsonLd.RECORD_KEY
 import static whelk.datatool.bulkchange.BulkJobDocument.MATCH_FORM_KEY
 
 Map matchForm = parameters.get(MATCH_FORM_KEY)
 
-Specification.Delete delete = new Specification.Delete(matchForm)
+Transform.MatchForm mf = new Transform.MatchForm(matchForm, getWhelk())
 
 selectByForm(matchForm) {
-    if(delete.matches(it.doc, it.whelk)) {
+    if(mf.matches(getFramedThing(it.doc))) {
         it.scheduleDelete(loud: isLoudAllowed)
     }
+}
+
+private static Map getFramedThing(Document doc) {
+    Map<String, Object> thing = doc.clone().getThing();
+    thing.put(RECORD_KEY, doc.getRecord());
+    return thing
 }
