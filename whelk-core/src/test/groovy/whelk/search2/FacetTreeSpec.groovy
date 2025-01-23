be0dda05588eac8f777733e7db0e3be46e3b1705
Lars Rosenström <lars.rosenstrom@kb.se>
2025-01-23 10:53:47 +0100

    Fix tests

--- whelk-core/src/test/groovy/whelk/search2/FacetTreeSpec.groovy
+++ whelk-core/src/test/groovy/whelk/search2/FacetTreeSpec.groovy
@@ -13,69 +13,86 @@ class FacetTreeSpec extends Specification {
         jsonLd.toTermId(_ as String) >> { String s -> s }
     }
 
-//    def "Single observation should return list with one observation"() {
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                    | sorted
-//        [["object": ["@id": "parent"]]] | [["object": ["@id": "parent"]]]
-//    }
-//
-//    def "Sort one parent and one child"() {
-//        given:
-//        jsonLd.getDirectSubclasses("parent") >> ["child"]
-//        jsonLd.getDirectSubclasses("child") >> []
-//
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                                                  | sorted
-//        [["object": ["@id": "parent"]],
-//         ["object": ["@id": "child"]]]             |    [["object": ["@id": "parent"], "_children": [["object": ["@id": "child"]]]]]
-//    }
-//
-//    def "Sort one parent with two children"() {
-//        given:
-//        jsonLd.getDirectSubclasses("parent") >> ["child1", "child2"]
-//        jsonLd.getDirectSubclasses("child1") >> []
-//        jsonLd.getDirectSubclasses("child2") >> []
-//
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                           | sorted
-//        [["object": ["@id": "parent"]],
-//         ["object": ["@id": "child1"]],
-//         ["object": ["@id": "child2"]]]     |   [["object": ["@id": "parent"],
-//                                                  "_children": [["object": ["@id": "child1"]],
-//                                                               ["object": ["@id": "child2"]]]]]
-//    }
-//
-//    def "Sort one parent with one child that has one child"() {
-//        given:
-//        jsonLd.getDirectSubclasses("parent") >> ["child1"]
-//        jsonLd.getDirectSubclasses("child1") >> ["child2"]
-//        jsonLd.getDirectSubclasses("child2") >> []
-//
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                           | sorted
-//        [["object": ["@id": "parent"]],
-//         ["object": ["@id": "child1"]],
-//         ["object": ["@id": "child2"]]]     |   [["object": ["@id": "parent"],
-//                                                  "_children": [["object": ["@id": "child1"],
-//                                                               "_children": [["object": ["@id": "child2"]]]]]]]
-//    }
-//
+    def "Single observation should return list with one observation"() {
+        given:
+        jsonLd.getDirectSubclasses("parent") >> []
+        jsonLd.getSuperClasses("parent") >> []
+
+        expect:
+        def tree = new FacetTree(jsonLd)
+        tree.sortObservationsAsTree(observations) == sorted
+
+        where:
+        observations                    | sorted
+        [["object": ["@id": "parent"]]] | [["object": ["@id": "parent"]]]
+    }
+
+    def "Sort one parent and one child"() {
+        given:
+        jsonLd.getDirectSubclasses("parent") >> ["child"]
+        jsonLd.getDirectSubclasses("child") >> []
+
+        jsonLd.getSuperClasses("child") >> ["parent"]
+        jsonLd.getSuperClasses("parent") >> []
+
+
+        expect:
+        def tree = new FacetTree(jsonLd)
+        tree.sortObservationsAsTree(observations) == sorted
+
+        where:
+        observations                                                  | sorted
+        [["object": ["@id": "parent"]],
+         ["object": ["@id": "child"]]]             |    [["object": ["@id": "parent"], "_children": [["object": ["@id": "child"]]]]]
+    }
+
+    def "Sort one parent with two children, superclasses of root should be ignored"() {
+        given:
+        jsonLd.getDirectSubclasses("root") >> ["child1", "child2"]
+        jsonLd.getDirectSubclasses("child1") >> []
+        jsonLd.getDirectSubclasses("child2") >> []
+
+        jsonLd.getSuperClasses("child1") >> ["root", "Resource"]
+        jsonLd.getSuperClasses("child2") >> ["child1", "root", "Resource"]
+        jsonLd.getSuperClasses("root") >> ["Resource"]
+
+
+        expect:
+        def tree = new FacetTree(jsonLd)
+        tree.sortObservationsAsTree(observations) == sorted
+
+        where:
+        observations                           | sorted
+        [["object": ["@id": "root"]],
+         ["object": ["@id": "child1"]],
+         ["object": ["@id": "child2"]]]     |   [["object": ["@id": "root"],
+                                                  "_children": [["object": ["@id": "child1"]],
+                                                                ["object": ["@id": "child2"]]]]]
+    }
+
+    def "Sort one parent with one child that has one child"() {
+        given:
+        jsonLd.getDirectSubclasses("root") >> ["child1"]
+        jsonLd.getDirectSubclasses("child1") >> ["child2"]
+        jsonLd.getDirectSubclasses("child2") >> []
+
+        jsonLd.getSuperClasses("child1") >> ["root"]
+        jsonLd.getSuperClasses("child2") >> ["child1", "root"]
+        jsonLd.getSuperClasses("root") >> []
+
+        expect:
+        def tree = new FacetTree(jsonLd)
+        tree.sortObservationsAsTree(observations) == sorted
+
+        where:
+        observations                           | sorted
+        [["object": ["@id": "root"]],
+         ["object": ["@id": "child1"]],
+         ["object": ["@id": "child2"]]]     |   [["object": ["@id": "root"],
+                                                  "_children": [["object": ["@id": "child1"],
+                                                               "_children": [["object": ["@id": "child2"]]]]]]]
+    }
+
     def "One parent, two children"() {
         given:
         jsonLd.getDirectSubclasses("root") >> ["child1", "child2"]
@@ -97,148 +114,37 @@ class FacetTreeSpec extends Specification {
          ["object": ["@id": "child2"]]]        |    [["object": ["@id": "root"], "_children" : [["object": ["@id": "child1"]],
                                                                                                 ["object": ["@id": "child2"]]]]]
     }
-//
-//    def "Three root nodes"() {
-//        given:
-//        jsonLd.getDirectSubclasses(_) >> []
-//
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                           | sorted
-//        [["object": ["@id": "root1"]],
-//         ["object": ["@id": "root2"]],
-//         ["object": ["@id": "root3"]]]          |    [["object": ["@id": "root1"]],
-//                                                      ["object": ["@id": "root2"]],
-//                                                      ["object": ["@id": "root3"]]]
-//    }
-//
-//    def "Children should not be considered parents of themselves"() {
-//        given:
-//        jsonLd.getDirectSubclasses(_) >> []
-//
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                                            | sorted
-//        [["object": ["@id": "A"]], ["object": ["@id": "A"]]]    |   [["object": ["@id": "A"]], ["object": ["@id": "A"]]]
-//    }
 
-    /// TESTS CHECKING FOR SUPERCLASS IN OBJECT.SUBCLASSOF
+    def "Three root nodes"() {
+        given:
+        jsonLd.getDirectSubclasses(_) >> []
+        jsonLd.getSuperClasses(_) >> []
 
-//    def "Single observation should return list with one observation"() {
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                    | sorted
-//        [["object": ["@id": "parent"]]] | [["object": ["@id": "parent"]]]
-//    }
-//
-//    def "Sort one parent and one child"() {
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                                                        | sorted
-//        [["object": ["@id": "parent"]],
-//         ["object": ["@id": "child", "subClassOf" : [["@id": "parent"]]]]]    | [["object": ["@id": "parent"], "_children": [["object": ["@id": "child", "subClassOf" : [["@id": "parent"]]]]]]]
-//    }
-//
-//    def "Sort one parent with two children"() {
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations            | sorted
-//        [["object": ["@id": "parent"]],
-//         ["object": ["@id": "child1", "subClassOf" : [["@id": "parent"]]]],
-//         ["object": ["@id": "child2", "subClassOf" : [["@id": "parent"]]]]]    |   [["object": ["@id": "parent"],
-//                                                                                     "_children": [["object" : ["@id": "child1", "subClassOf" : [["@id": "parent"]]]],
-//                                                                                                   ["object" : ["@id": "child2", "subClassOf" : [["@id": "parent"]]]]
-//                                                                                     ]]]
-//    }
-//
-//    def "Sort one parent with one child that has one child"() {
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                           | sorted
-//        [["object": ["@id": "parent"]],
-//         ["object": ["@id": "child1", "subClassOf" : [["@id": "parent"]]]],
-//         ["object": ["@id": "child2", "subClassOf" : [["@id": "child1"]]]]]    |   [["object": ["@id": "parent"],
-//                                                                                     "_children": [["object": ["@id": "child1", "subClassOf" : [["@id": "parent"]]],
-//                                                                                                    "_children": [["object": ["@id": "child2", "subClassOf" : [["@id": "child1"]]]]]]]]]
-//    }
-//
-//    def "One parent, one between, one child"() {
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                                                           | sorted
-//        [["object": ["@id": "parent"]],
-//         ["object": ["@id": "child1", "subClassOf" : [["@id": "parent"]]]]]    |   [["object": ["@id": "parent"],
-//                                                                                     "_children": [["object": ["@id": "child1", "subClassOf" : [["@id": "parent"]]]
-//                                                                                                   ]]]]
-//    }
+        expect:
+        def tree = new FacetTree(jsonLd)
+        tree.sortObservationsAsTree(observations) == sorted
 
+        where:
+        observations                           | sorted
+        [["object": ["@id": "root1"]],
+         ["object": ["@id": "root2"]],
+         ["object": ["@id": "root3"]]]          |    [["object": ["@id": "root1"]],
+                                                      ["object": ["@id": "root2"]],
+                                                      ["object": ["@id": "root3"]]]
+    }
 
-//    def "One parent, two children"() {
-//        given:
-//        jsonLd.getDirectSubclasses("root") >> ["child1", "child2"]
-//        jsonLd.getDirectSubclasses("child1") >> []
-//        jsonLd.getDirectSubclasses("child2") >> []
-//
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                           | sorted
-//        [["object": ["@id": "root"]],
-//         ["object": ["@id": "child1"]],
-//         ["object": ["@id": "child2"]]]        |    [["object": ["@id": "root"], "_children" : [["object": ["@id": "child1"]],
-//                                                                                                ["object": ["@id": "child2"]]]]]
-//    }
-//
-//    def "Three root nodes"() {
-//        given:
-//        jsonLd.getDirectSubclasses(_) >> []
-//
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                           | sorted
-//        [["object": ["@id": "root1"]],
-//         ["object": ["@id": "root2"]],
-//         ["object": ["@id": "root3"]]]          |    [["object": ["@id": "root1"]],
-//                                                      ["object": ["@id": "root2"]],
-//                                                      ["object": ["@id": "root3"]]]
-//    }
-//
-//    def "Children should not be considered parents of themselves"() {
-//        given:
-//        jsonLd.getDirectSubclasses(_) >> []
-//
-//        expect:
-//        def tree = new FacetTree(jsonLd)
-//        tree.sortObservationsAsTree(observations) == sorted
-//
-//        where:
-//        observations                                            | sorted
-//        [["object": ["@id": "A"]], ["object": ["@id": "A"]]]    |   [["object": ["@id": "A"]], ["object": ["@id": "A"]]]
-//    }
+    def "Children should not be considered parents of themselves"() {
+        given:
+        jsonLd.getDirectSubclasses(_) >> []
+        jsonLd.getSuperClasses(_) >> []
+
+        expect:
+        def tree = new FacetTree(jsonLd)
+        tree.sortObservationsAsTree(observations) == sorted
+
+        where:
+        observations                                            | sorted
+        [["object": ["@id": "A"]], ["object": ["@id": "A"]]]    |   [["object": ["@id": "A"]], ["object": ["@id": "A"]]]
+    }
+
 }
