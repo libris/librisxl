5ba691377cfe077624fbc2dd8844700b183e7d0f
branchedelac <lisa.m.sjogren@gmail.com>
2025-02-10 17:17:57 +0100

    restore content

--- whelktool/scripts/typenormalization/main.groovy
+++ whelktool/scripts/typenormalization/main.groovy
@@ -5,11 +5,5 @@ var loadWorkItem = { String workId, Closure process ->
 }
 
 selectByCollection('bib') {
-  incrementStats('1.1 Instance type - before update', it.graph[1]['@type'])
-  incrementStats('2.1 Work type - before update', it.graph[1]['instanceOf']['@type'])
-
   normalizeTypes(it, loadWorkItem)
-  
-  incrementStats('1.2 Instance typ - After update', it.graph[1]['@type'])
-  incrementStats('2.2 Work type - After update', it.graph[1]['instanceOf']['@type'])
 }
