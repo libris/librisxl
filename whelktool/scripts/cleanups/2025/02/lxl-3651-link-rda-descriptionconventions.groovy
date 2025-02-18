77b3a2de7fd4b0a1b5efb407c7a9095eb3d62356
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2025-02-18 16:18:38 +0100

    Add script for linking DescriptionConventions code rda

---
+++ whelktool/scripts/cleanups/2025/02/lxl-3651-link-rda-descriptionconventions.groovy
@@ -0,0 +1,24 @@
+/**
+ * Replace local descriptionConventions code:rda with link
+ *
+ * See https://kbse.atlassian.net/browse/LXL-3651
+ */
+
+// all docs
+selectByCollection(null) { doc ->
+    List rda = asList(doc.graph[0]["descriptionConventions"]).findAll { it.code == "rda" }
+
+    rda.forEach {
+        // fix manual errors in type, e.g. marc:CatalogingRulesType
+        if (it["@type"] != "DescriptionConventions") {
+            incrementStats("@type", it["@type"] ? it["@type"] : "null")
+
+            it["@type"] = "DescriptionConventions"
+        }
+    }
+
+    // heuristicIdentity -> automatically linked based on code on save
+    if (!rda.isEmpty()) {
+        doc.scheduleSave()
+    }
+}
