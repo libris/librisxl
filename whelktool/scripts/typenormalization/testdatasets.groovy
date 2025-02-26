fe0dfc0e2d3ca0688779868094c5e2c744d52119
Niklas Lindström <lindstream@gmail.com>
2025-02-26 19:46:37 +0100

    Add testdatasets typenormalization script

---
+++ whelktool/scripts/typenormalization/testdatasets.groovy
@@ -0,0 +1,9 @@
+Closure normalizeTypes = script('algorithm.groovy')
+
+var loadWorkItem = { String workId, Closure process -> selectByIds([workId], process) }
+
+selectBySqlWhere("""
+    data#>'{@graph,0,inDataset}' @> '[{"@id": "https://libris.kb.se/dataset/instances"}]'::jsonb
+""") {
+  normalizeTypes(it, loadWorkItem)
+}
