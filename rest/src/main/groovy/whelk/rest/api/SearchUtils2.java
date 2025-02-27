88e9151cf13f76fa7dccb7a630230325d192f270
Kalle Wåhlin <kalle.wahlin@kb.se>
2025-02-27 17:18:46 +0100

    (search2): Make stats optional in API response

--- rest/src/main/groovy/whelk/rest/api/SearchUtils2.java
+++ rest/src/main/groovy/whelk/rest/api/SearchUtils2.java
@@ -97,7 +97,10 @@ public class SearchUtils2 {
             }
         }
 
-        queryDsl.put("aggs", buildAggQuery(statsRepr, whelk.getJsonld(), queryTree.collectRulingTypes(whelk.getJsonld()), queryUtil::getNestedPath));
+        if (!queryParams.skipStats) {
+            queryDsl.put("aggs", buildAggQuery(statsRepr, whelk.getJsonld(), queryTree.collectRulingTypes(whelk.getJsonld()), queryUtil::getNestedPath));
+        }
+
         queryDsl.put("track_total_hits", true);
 
         if (queryParams.debug.contains(QueryParams.Debug.ES_SCORE)) {
@@ -130,7 +133,9 @@ public class SearchUtils2 {
         view.put("search", Map.of("mapping", List.of(qt.toSearchMapping(queryParams.getNonQueryParams(0)))));
         view.putAll(Pagination.makeLinks(queryResult.numHits, queryUtil.maxItems(), freeText, fullQuery, queryParams));
         view.put("items", queryResult.collectItems(queryUtil.getApplyLensFunc(queryParams)));
-        view.put("stats", new Stats(whelk.getJsonld(), queryUtil, qt, queryResult, queryParams, appParams).build());
+        if (!queryParams.skipStats) {
+            view.put("stats", new Stats(whelk.getJsonld(), queryUtil, qt, queryResult, queryParams, appParams).build());
+        }
         if (!queryResult.spell.isEmpty()) {
             view.put("_spell", buildSpellSuggestions(queryResult, qt, queryParams.getNonQueryParams(0)));
         }
