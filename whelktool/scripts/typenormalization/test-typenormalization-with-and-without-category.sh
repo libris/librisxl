95d144715f8ec6389257667a5ac73ced9e4018b5
branchedelac <lisa.m.sjogren@gmail.com>
2025-04-24 09:13:03 +0200

    rename reports

--- whelktool/scripts/typenormalization/test-typenormalization-with-and-without-category.sh
+++ whelktool/scripts/typenormalization/test-typenormalization-with-and-without-category.sh
@@ -7,6 +7,6 @@ TESTFOLDER=$2
 gzip -k ../../${TESTFOLDER}/qa-instances.jsonl > ../../${TESTFOLDER}/qa-instances.jsonl.gz
 gzip -k ../../${TESTFOLDER}/qa-works.jsonl > ../../${TESTFOLDER}/qa-works.jsonl.gz
 
-time java -Xmx2G -Dxl.secret.properties=../secret.properties-${LIBRISENV} -Dtypenormalization=simple-types-algorithm -DaddCategory=true -Dnowhelk.datadir=../../${TESTFOLDER} -Dnowhelk.basename=qa -jar build/libs/whelktool.jar --report ../../${TESTFOLDER}/reports/dev2-typenorm-with-category$(date +%Y%m%d) --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
+time java -Xmx2G -Dxl.secret.properties=../secret.properties-${LIBRISENV} -Dtypenormalization=simple-types-algorithm -DaddCategory=true -Dnowhelk.datadir=../../${TESTFOLDER} -Dnowhelk.basename=qa -jar build/libs/whelktool.jar --report ../../${TESTFOLDER}/reports/typenorm-with-category$(date +%Y%m%d) --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
 
-time java -Xmx2G -Dxl.secret.properties=../secret.properties-${LIBRISENV} -Dtypenormalization=simple-types-algorithm -DaddCategory=false -Dnowhelk.datadir=../../${TESTFOLDER} -Dnowhelk.basename=qa -jar build/libs/whelktool.jar --report ../../${TESTFOLDER}/reports/dev2-typenorm-$(date +%Y%m%d) --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
+time java -Xmx2G -Dxl.secret.properties=../secret.properties-${LIBRISENV} -Dtypenormalization=simple-types-algorithm -DaddCategory=false -Dnowhelk.datadir=../../${TESTFOLDER} -Dnowhelk.basename=qa -jar build/libs/whelktool.jar --report ../../${TESTFOLDER}/reports/typenorm-$(date +%Y%m%d) --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
