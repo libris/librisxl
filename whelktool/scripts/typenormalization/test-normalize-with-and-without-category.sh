387505b1faecc611a77ced8fb9b0e4db18dbfae8
branchedelac <lisa.m.sjogren@gmail.com>
2025-05-14 09:22:34 +0200

    overwrite zipped file if it already exists

--- whelktool/scripts/typenormalization/test-normalize-with-and-without-category.sh
+++ whelktool/scripts/typenormalization/test-normalize-with-and-without-category.sh
@@ -10,13 +10,13 @@ TESTFOLDER=$2
 # curl -XGET -H "Accept: application/ld+json" https://libris-qa.kb.se/bvnqr36n2w6jl6f\?embellished\=false
 
 # 2. Create zipped copies of files qa-instance.jsonl and qa-work.jsonl, retaining the original jsnol file
-gzip -k ../../${TESTFOLDER}/qa-instances.jsonl > ../../${TESTFOLDER}/qa-instances.jsonl.gz
-gzip -k ../../${TESTFOLDER}/qa-works.jsonl > ../../${TESTFOLDER}/qa-works.jsonl.gz
+gzip -k -f ../../${TESTFOLDER}/qa-instances.jsonl > ../../${TESTFOLDER}/qa-instances.jsonl.gz
+gzip -k -f ../../${TESTFOLDER}/qa-works.jsonl > ../../${TESTFOLDER}/qa-works.jsonl.gz
 
 # 3. Run the typenormalization script on instances and works to nromalize with category as qa-NORMALIZED-with-category.jsonl
-time java -Xmx2G -Dxl.secret.properties=../secret.properties-${LIBRISENV} -Dtypenormalization=simple-types-algorithm -DaddCategory=true -Dnowhelk.datadir=../../${TESTFOLDER} -Dnowhelk.basename=qa -jar build/libs/whelktool.jar --report ../../${TESTFOLDER}/reports/typenorm-with-category$(date +%Y%m%d) --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
+time java -Xmx2G -Dxl.secret.properties=../secret.properties-${LIBRISENV} -Dtypenormalization=simple-types-algorithm -DaddCategory=true -Dnowhelk.datadir=../../${TESTFOLDER} -Dnowhelk.basename=qa -jar build/libs/whelktool.jar --report ../../${TESTFOLDER}/reports/with-category --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
 
 # 4. Run the typenormalization on instances and works script to nromalize without category as qa-NORMALIZED.jsonl
-time java -Xmx2G -Dxl.secret.properties=../secret.properties-${LIBRISENV} -Dtypenormalization=simple-types-algorithm -DaddCategory=false -Dnowhelk.datadir=../../${TESTFOLDER} -Dnowhelk.basename=qa -jar build/libs/whelktool.jar --report ../../${TESTFOLDER}/reports/typenorm-$(date +%Y%m%d) --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
+time java -Xmx2G -Dxl.secret.properties=../secret.properties-${LIBRISENV} -Dtypenormalization=simple-types-algorithm -DaddCategory=false -Dnowhelk.datadir=../../${TESTFOLDER} -Dnowhelk.basename=qa -jar build/libs/whelktool.jar --report ../../${TESTFOLDER}/reports/without-category --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
 
 # 5. Compare the original works and instances files, qa-NORMALIZED-with-category.jsonl and qa-NORMALIZED.jsonl
