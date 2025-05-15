f82a80b4bbe957815f6028cc5ebe50421bd3663f
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2025-05-15 09:12:31 +0200

    Group edits on same day

--- whelktool/scripts/analysis/tg-50-cat-stats.groovy
+++ whelktool/scripts/analysis/tg-50-cat-stats.groovy
@@ -1,24 +1,26 @@
-import whelk.Whelk
 import whelk.history.History
 import whelk.util.Unicode
 
-PrintWriter byPath = getReportWriter("changes-by-path.txt")
-PrintWriter byVersion = getReportWriter("changes-by-version.txt")
-PrintWriter errors = getReportWriter("errors.txt")
-class C {
-    public static var CRUD = [ "@id": "https://id.kb.se/generator/crud" ]
-}
+byPath = getReportWriter("changes-by-path.txt")
+byVersion = getReportWriter("changes-by-version.txt")
+errors = getReportWriter("errors.txt")
 
-Whelk whelk = getWhelk()
+whelk = getWhelk()
 
-process = { bib ->
+void process(bib) {
     try {
         var shortId = bib.doc.getShortId()
         History history = new History(whelk.storage.loadDocumentHistory(shortId), whelk.jsonld)
         List changeSets = history.m_changeSetsMap.changeSets
+
+        var lastAgent = null
+        var lastDay = null
+        var editNo = 0
         int versionNo = 0
+        var edits = []
+
         for (var version : changeSets) {
-            if (version.tool != C.CRUD) {
+            if (version.tool != [ "@id": "https://id.kb.se/generator/crud" ]) {
                 continue
             }
 
@@ -29,37 +31,64 @@ process = { bib ->
                 continue
             }
 
-            var added = filteredPaths(version.addedPaths)
-            var removed = filteredPaths(version.removedPaths)
-            var modified = added.intersect(removed)
-            added -= modified
-            removed -= modified
+            String day = timestamp.split("T").first()
             String agent = version.agent["@id"]
-            var sigel = Unicode.stripPrefix(agent, "https://libris.kb.se/library/")
 
-            StringBuilder s = new StringBuilder()
-            var append = { operation, path ->
-                s.append([shortId, timestamp, year, versionNo, sigel, agent, operation, path].join('\t')).append("\n")
+            if(agent != lastAgent || day != lastDay) {
+                processEditSession(shortId, versionNo, edits)
+                edits = []
+                versionNo = editNo
             }
 
-            added.each { append("ADD", it)}
-            removed.each { append("REMOVE", it)}
-            modified.each { append("MODIFY", it)}
+            edits.add(version)
 
-            byPath.print(s.toString())
+            lastAgent = agent
+            lastDay = day
 
-            var allPaths = added + removed + modified
-            if (allPaths) {
-                byVersion.println([shortId, timestamp, year, versionNo, sigel, agent, allPaths.join(",")].join('\t'))
-            }
-
-            versionNo++
+            editNo++
         }
+        processEditSession(shortId, versionNo, edits)
     }
     catch (Exception e) {
         errors.println("Error in ${bib.doc.shortId}: ${e}")
         e.printStackTrace()
+    }
+}
+
+void processEditSession(shortId, versionNo, List edits) {
+    if(!edits) {
         return
+    }
+
+    println("-----\n" + edits.join('\n\n')+"-----" )
+
+    var added = edits.collectMany { filteredPaths(it.addedPaths) } as Set
+    var removed = edits.collectMany { filteredPaths(it.removedPaths) } as Set
+    var modified = added.intersect(removed)
+
+    var agent = edits.first().agent["@id"]
+    String timestamp = edits.last().date
+
+    added -= modified
+    removed -= modified
+
+    var sigel = Unicode.stripPrefix(agent, "https://libris.kb.se/library/")
+    String year = timestamp.split("-").first()
+
+    StringBuilder s = new StringBuilder()
+    var append = { operation, path ->
+        s.append([shortId, timestamp, year, versionNo, sigel, agent, operation, path].join('\t')).append("\n")
+    }
+
+    added.each { append("ADD", it)}
+    removed.each { append("REMOVE", it)}
+    modified.each { append("MODIFY", it)}
+
+    byPath.print(s.toString())
+
+    var allPaths = added + removed + modified
+    if (allPaths) {
+        byVersion.println([shortId, timestamp, year, versionNo, sigel, agent, allPaths.join(",")].join('\t'))
     }
 }
 
@@ -67,7 +96,8 @@ selectByCollection('bib') {
     process(it)
 }
 
-Set<String> filteredPaths(Collection<List<Object>> paths) {
+
+static Set<String> filteredPaths(Collection<List<Object>> paths) {
     paths.findResults {
         if (it.size() < 3 || it.get(0) != "@graph" || it.get(1) != 1) {
             return null;
