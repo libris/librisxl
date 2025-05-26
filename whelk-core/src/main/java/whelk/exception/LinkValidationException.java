63f618a3b1cb8cc8d2d518b6c699d1adba7f9447
Kalle Wåhlin <72360110+kwahlin@users.noreply.github.com>
2025-05-26 10:37:13 +0200

    Feature/lxl 4610 cleanup (#1567)
    * Remove useless check for outgoing links and clean up
    
    * Give more informative error message

--- whelk-core/src/main/java/whelk/exception/LinkValidationException.java
+++ whelk-core/src/main/java/whelk/exception/LinkValidationException.java
@@ -4,16 +4,4 @@ public class LinkValidationException extends RuntimeException {
     public LinkValidationException(String msg) {
         super(msg);
     }
-
-    public static class IncomingLinksException extends LinkValidationException {
-        public IncomingLinksException(String msg) {
-            super(msg);
-        }
-    }
-
-    public static class OutgoingLinksException extends LinkValidationException {
-        public OutgoingLinksException(String msg) {
-            super(msg);
-        }
-    }
 }
