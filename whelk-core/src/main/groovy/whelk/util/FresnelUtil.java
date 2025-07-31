9bb0ee5b15f782a9170b3bf6608d43161ccdbe22
Kalle Wåhlin <kalle.wahlin@kb.se>
2025-07-31 14:59:33 +0200

    fix(index): Avoid NPE from anonymous inverse property

--- whelk-core/src/main/groovy/whelk/util/FresnelUtil.java
+++ whelk-core/src/main/groovy/whelk/util/FresnelUtil.java
@@ -260,8 +260,6 @@ public class FresnelUtil {
                     if (thing.get(JsonLd.REVERSE_KEY) instanceof Map<?, ?> r && r.containsKey(i.name)) {
                         var v = r.get(i.name);
                         String inverseName = i.inverseName;
-                        if (inverseName == null)
-                            logger.error("Null inverse name for: {} / {}", i.name, i); // fall through into NPE, to not alter behaviour.
                         result.pick(Map.of(inverseName, v), new PropertyKey(inverseName));
                     }
                 }
@@ -668,7 +666,8 @@ public class FresnelUtil {
 
         private InverseProperty asInverseProperty(Object showProperty) {
             String p = (String) ((Map<?, ?>) showProperty).get("inverseOf");
-            return new InverseProperty(p, jsonLd.getInverseProperty(p));
+            String inverse = jsonLd.getInverseProperty(p);
+            return new InverseProperty(p, inverse == null ? JsonLd.REVERSE_KEY + "." + p : p);
         }
 
         // TODO
