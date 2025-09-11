802a928f841f4906d55ca8b91ff5a0be02933f56
Niklas Lindström <lindstream@gmail.com>
2025-09-11 11:56:01 +0200

    Make script nicer and increase result set limit

--- whelktool/scripts/cleanups/2025/09/add-agent-identifiers/chunkedjson.py
+++ whelktool/scripts/cleanups/2025/09/add-agent-identifiers/chunkedjson.py
@@ -12,38 +12,36 @@ with open(fname) as f:
     reader = csv.DictReader(f)
 
     for i, row in enumerate(reader):
-        (agent_iri, name, given, lifespan, alt_name, alt_given, id, idtype, work_title, orig_title, title, isbn) = [
-            row[key] for key in
-            ['agent', 'name', 'given', 'lifespan', 'alt_name', 'alt_given', 'id', 'idtype', 'work_title', 'orig_title', 'title', 'isbn']
-        ]
+        agent_iri = row['agent']
 
         agent = agents.setdefault(agent_iri, {})
-        if given:
-            agent['family'] = name
-            agent['given'] = given
-        elif name:
-            agent['name'] = name
+        if name := row['name']:
+            if given := row['given']:
+                agent['family'] = name
+                agent['given'] = given
+            else:
+                agent['name'] = name
 
-        if lifespan:
+        if lifespan := row['lifespan']:
             agent['lifespan'] = lifespan
 
-        if alt_name:
+        if alt_name := row['alt_name']:
             variant: dict[str, str] = {}
             variants = agent.setdefault('variant', []).append(variant)
-            if alt_given:
+            if alt_given := row['alt_given']:
                 variant['family'] = alt_name
                 variant['given'] = alt_given
             elif name:
                 variant['name'] = alt_name
 
-        if id:
-            idtype = idtype.removeprefix(KBV)
+        if id := row['id']:
+            idtype = row['idtype'].removeprefix(KBV)
             agent.setdefault('idMap', {}).setdefault(idtype, id)
 
-        for title in (work_title, orig_title, title):
-            if title:
+        for title_key in ('work_title', 'orig_title', 'title'):
+            if title := row[title_key]:
                 agent.setdefault('work', []).append(title)
-        if isbn:
+        if isbn := row['isbn']:
             agent.setdefault('isbn', []).append(isbn)
 
     json.dump(agents, sys.stdout, indent=2, ensure_ascii=False)
--- whelktool/scripts/cleanups/2025/09/add-agent-identifiers/get-candidates-for-isni-enrichment.sh
+++ whelktool/scripts/cleanups/2025/09/add-agent-identifiers/get-candidates-for-isni-enrichment.sh
@@ -2,5 +2,5 @@
 offset=${1:-0}
 curl -s https://libris.kb.se/sparql -HAccept:text/csv --data-urlencode query="
 $(cat get-candidates-for-isni-enrichment.rq)
-limit 1000 offset $offset
+limit 10000 offset $offset
 "
