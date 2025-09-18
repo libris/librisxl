661a7c15ae05928c8350c6be2b0f0597877ee6bf
Kalle Wåhlin <kalle.wahlin@kb.se>
2025-09-18 17:06:04 +0200

    Include namespace in disambiguation to prevent unnecessary conflicts

--- whelk-core/src/main/groovy/whelk/search2/Disambiguate.java
+++ whelk-core/src/main/groovy/whelk/search2/Disambiguate.java
@@ -14,6 +14,7 @@ import java.util.stream.Stream;
 
 import static whelk.JsonLd.LD_KEYS;
 import static whelk.JsonLd.Rdfs.RDF_TYPE;
+import static whelk.JsonLd.VOCAB_KEY;
 import static whelk.JsonLd.looksLikeIri;
 import static whelk.search2.QueryUtil.encodeUri;
 import static whelk.search2.VocabMappings.expandPrefixed;
@@ -24,15 +25,13 @@ public class Disambiguate {
     private final VocabMappings vocabMappings;
     private final Map<String, FilterAlias> filterAliasMappings;
 
-    private enum VocabTermType {
-        CLASS,
-        ENUM
-    }
+    private final List<String> nsPrecedenceOrder;
 
     public Disambiguate(VocabMappings vocabMappings, Collection<FilterAlias> appFilterAliases, Collection<FilterAlias.QueryDefinedAlias> queryFilterAliases, JsonLd jsonLd) {
         this.vocabMappings = vocabMappings;
         this.filterAliasMappings = getFilterAliasMappings(appFilterAliases, queryFilterAliases);
         this.jsonLd = jsonLd;
+        this.nsPrecedenceOrder = List.of("rdf", "librissearch", (String) jsonLd.context.get(VOCAB_KEY), "marc"); // FIXME
     }
 
     // For test only
@@ -41,40 +40,44 @@ public class Disambiguate {
         this.vocabMappings = vocabMappings;
         this.filterAliasMappings = getFilterAliasMappings(filterAliasMappings, List.of());
         this.jsonLd = jsonLd;
+        this.nsPrecedenceOrder = List.of("rdf", "librissearch", (String) jsonLd.context.get(VOCAB_KEY), "marc"); // FIXME
     }
 
     public Subpath mapKey(String key, int offset) {
-        Optional<String> mappedProperty = getMappedTerm(key, vocabMappings.propertyAliasMappings);
-        if (mappedProperty.isPresent()) {
-            if (mappedProperty.get().equals(RDF_TYPE)) {
-                return new Property.RdfType(jsonLd, new Key.RecognizedKey(key, offset));
+        for (String ns : nsPrecedenceOrder) {
+            Set<String> mappedProperties = vocabMappings.properties()
+                    .getOrDefault(key.toLowerCase(), Map.of())
+                    .getOrDefault(ns, Set.of());
+            if (mappedProperties.size() == 1) {
+                String p = getUnambiguous(mappedProperties);
+                if (RDF_TYPE.equals(p)) {
+                    return new Property.RdfType(jsonLd, new Key.RecognizedKey(key, offset));
+                }
+                return new Property(getUnambiguous(mappedProperties), jsonLd, new Key.RecognizedKey(key, offset));
             }
-            return new Property(mappedProperty.get(), jsonLd, new Key.RecognizedKey(key, offset));
-        }
-
-        Set<String> multipleMappedProperties = getMappedTermsForAmbiguous(key, vocabMappings.ambiguousPropertyAliases);
-        if (multipleMappedProperties.isEmpty()) {
-            // TODO: Look up all indexed keys starting with underscore?
-            if (LD_KEYS.contains(key) || key.startsWith("_")) {
-                return new Key.RecognizedKey(key, offset);
+            if (mappedProperties.size() > 1) {
+                // Ambiguous
+                Optional<String> equalPropertyKey = mappedProperties.stream().filter(key::equalsIgnoreCase).findFirst();
+                if (equalPropertyKey.isPresent()) {
+                    return new Property(equalPropertyKey.get(), jsonLd, new Key.RecognizedKey(key, offset));
+                }
+                Optional<Property> propertyWithCode = mappedProperties.stream()
+                        .map(pKey -> new Property(pKey, jsonLd, new Key.RecognizedKey(key, offset)))
+                        .filter(property -> property.definition().containsKey("librisQueryCode"))
+                        .findFirst();
+                if (propertyWithCode.isPresent()) {
+                    return propertyWithCode.get();
+                }
+                return new Key.AmbiguousKey(key, offset);
             }
-            return new Key.UnrecognizedKey(key, offset);
         }
 
-        Optional<String> equalPropertyKey = multipleMappedProperties.stream().filter(key::equalsIgnoreCase).findFirst();
-        if (equalPropertyKey.isPresent()) {
-            return new Property(equalPropertyKey.get(), jsonLd, new Key.RecognizedKey(key, offset));
+        // TODO: Get valid keys from ES index?
+        if (LD_KEYS.contains(key) || key.startsWith("_")) {
+            return new Key.RecognizedKey(key, offset);
         }
 
-        Optional<Property> propertyWithCode = multipleMappedProperties.stream()
-                .map(pKey -> new Property(pKey, jsonLd, new Key.RecognizedKey(key, offset)))
-                .filter(property -> property.definition().containsKey("librisQueryCode"))
-                .findFirst();
-        if (propertyWithCode.isPresent()) {
-            return propertyWithCode.get();
-        }
-
-        return new Key.AmbiguousKey(key, offset);
+        return new Key.UnrecognizedKey(key, offset);
     }
 
     public Optional<Value> mapValueForProperty(Property property, String value) {
@@ -97,14 +100,22 @@ public class Disambiguate {
             return Optional.ofNullable(DateTime.parse(value, token));
         }
         if (property.isType() || property.isVocabTerm()) {
-            Set<String> mappedTerms = mapToVocabTerm(value, property.isType() ? VocabTermType.CLASS : VocabTermType.ENUM);
-            return switch (mappedTerms.size()) {
-                case 0 -> Optional.of(token != null ? InvalidValue.forbidden(token) : InvalidValue.forbidden(value));
-                case 1 -> mappedTerms.stream().findFirst().map(term -> new VocabTerm(term, jsonLd.vocabIndex.get(term), token));
-                default -> mappedTerms.stream().filter(value::equalsIgnoreCase).findFirst()
-                        .map(v -> (Value) new VocabTerm(v, jsonLd.vocabIndex.get(v), token))
-                        .or(() -> Optional.of(token != null ? InvalidValue.ambiguous(token) : InvalidValue.ambiguous(value)));
-            };
+            for (String ns : nsPrecedenceOrder) {
+                var mappings = property.isType() ? vocabMappings.classes() : vocabMappings.enums();
+                Set<String> mappedClasses = mappings.getOrDefault(value.toLowerCase(), Map.of()).getOrDefault(ns, Set.of());
+                if (mappedClasses.size() == 1) {
+                    return mappedClasses.stream().findFirst().map(term -> new VocabTerm(term, jsonLd.vocabIndex.get(term), token));
+                }
+                if (mappedClasses.size() > 1) {
+                    // Ambiguous
+                    Optional<String> equalTermKey = mappedClasses.stream().filter(value::equalsIgnoreCase).findFirst();
+                    if (equalTermKey.isPresent()) {
+                        return equalTermKey.map(term -> new VocabTerm(term, jsonLd.vocabIndex.get(term), token));
+                    }
+                    return Optional.of(token != null ? InvalidValue.ambiguous(token) : InvalidValue.ambiguous(value));
+                }
+            }
+            return Optional.of(token != null ? InvalidValue.forbidden(token) : InvalidValue.forbidden(value));
         }
         if (property.isObjectProperty()) {
             String expanded = expandPrefixed(value);
@@ -141,26 +152,8 @@ public class Disambiguate {
         return new Property.TextQuery(jsonLd);
     }
 
-    private Set<String> mapToVocabTerm(String s, VocabTermType vocabTermType) {
-        Map<String, String> unambiguousMappings = switch (vocabTermType) {
-            case CLASS -> vocabMappings.classAliasMappings;
-            case ENUM -> vocabMappings.enumAliasMappings;
-        };
-        Map<String, Set<String>> ambiguousMappings = switch (vocabTermType) {
-            case CLASS -> vocabMappings.ambiguousClassAliases;
-            case ENUM -> vocabMappings.ambiguousEnumAliases;
-        };
-        return getMappedTerm(s, unambiguousMappings)
-                .map(Set::of)
-                .orElse(getMappedTermsForAmbiguous(s, ambiguousMappings));
-    }
-
-    private static Optional<String> getMappedTerm(String alias, Map<String, String> unambiguousMappings) {
-        return Optional.ofNullable(unambiguousMappings.get(alias.toLowerCase()));
-    }
-
-    private static Set<String> getMappedTermsForAmbiguous(String alias, Map<String, Set<String>> ambiguousMappings) {
-        return ambiguousMappings.getOrDefault(alias.toLowerCase(), Collections.emptySet());
+    private static String getUnambiguous(Set<String> mappedTerms) {
+        return mappedTerms.iterator().next();
     }
 
     private Map<String, FilterAlias> getFilterAliasMappings(Collection<FilterAlias> appFilterAliases, Collection<FilterAlias.QueryDefinedAlias> queryFilterAliases) {
