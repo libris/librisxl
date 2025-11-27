be193659323a53128b9272195a40233c24b43e78
Kai Pöykiö <kaipoy@xlbuild.kb.local>
2025-11-27 16:17:48 +0100

    remove xml/html entities from BOKR bibs

---
+++ whelktool/scripts/syslib/remove-html-entities-BOKR.groovy
@@ -0,0 +1,37 @@
+import org.apache.commons.lang3.StringEscapeUtils
+
+String qry = """
+collection = 'bib'
+and deleted = 'false'
+and data#>>'{@graph,0,descriptionCreator,@id}' = 'https://libris.kb.se/library/BOKR'
+"""
+
+def count = 0
+
+selectBySqlWhere(qry) { b ->
+
+	def item = b.getGraph()[1]
+
+	item.summary.each { summary ->
+			if ( summary.'@type' == 'Summary' ) {
+				//decode xml/html entities
+				if ( summary.label =~ /&[^ ]+;/ ) { 
+					if (summary.label instanceof List) {
+						summary.label = StringEscapeUtils.unescapeXml(StringEscapeUtils.unescapeHtml4(summary.label.join("\n")))
+					} else {
+						summary.label = StringEscapeUtils.unescapeXml(StringEscapeUtils.unescapeHtml4(summary.label))
+					}
+
+					//resave
+					b.scheduleSave(loud: true)
+
+					//println(b.getGraph()[1])
+					count++
+				}
+			}
+	}
+}
+
+println('Changed:')
+println(count)
+
