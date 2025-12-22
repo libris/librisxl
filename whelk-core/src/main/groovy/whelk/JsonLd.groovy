c9d9914219b22cf00c2944eb8bd7f9e86825ff57
Kalle Wåhlin <kalle.wahlin@kb.se>
2025-12-22 10:15:25 +0100

    WIP: Support fresnel:mergeProperties

--- whelk-core/src/main/groovy/whelk/JsonLd.groovy
+++ whelk-core/src/main/groovy/whelk/JsonLd.groovy
@@ -1161,7 +1161,7 @@ class JsonLd {
         }
     }
 
-    private static Object retainLinks(Object o, Set<String> preserveLinks) {
+    static Object retainLinks(Object o, Set<String> preserveLinks) {
         if (o instanceof Map) {
             if (preserveLinks.contains(o.get(ID_KEY))) {
                 return o.subMap([ID_KEY])
