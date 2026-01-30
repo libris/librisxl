db687a0df6b62ca2259374bbc45aa3410ae75da8
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2026-01-30 17:13:02 +0100

    Also fix ContentType in auth-deps.groovy

--- whelktool/scripts/cleanups/2025/11/auth-deps.groovy
+++ whelktool/scripts/cleanups/2025/11/auth-deps.groovy
@@ -6,3 +6,12 @@ Whelk whelk = getWhelk()
 selectByCollection('auth') { DocumentItem doc ->
     whelk.storage.recalculateDependencies(doc.doc)
 }
+
+String where = """
+        data #>> '{@graph,1,@type}' = 'ContentType' 
+        and deleted = false
+        """
+
+selectBySqlWhere(where) { DocumentItem doc ->
+    whelk.storage.recalculateDependencies(doc.doc)
+}
