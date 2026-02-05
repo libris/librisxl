1d08224880c122953204c4f25a8b146e4209daf8
Niklas Lindström <lindstream@gmail.com>
2026-02-05 18:54:22 +0100

    Adapt normalize test runner script
    - Use full (relative) folder as argument
    - Create empty works input file
    - Use "tailored" as the test basename (was "qa")

--- whelktool/scripts/typenormalization/normalize-tailored-examples-and-run-tests.sh
+++ whelktool/scripts/typenormalization/normalize-tailored-examples-and-run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# A testfolder located ../../ two levels up from whelktool, i.e. beside the librisxl repo
+# A testfolder (suggestion: ../../testdata, i.e. beside the librisxl repo)
 TESTFOLDER=$1
 TESTRECORDS=${2:-./scripts/typenormalization/tailored_examples.jsonl}
 
@@ -11,12 +11,18 @@ echo "Saving results to test folder: $TESTFOLDER"
 # Eun this from the whelktool folder
 # Zip the tailored examples file and save it in the parent folder of the librixl reop
 echo "Creating compressed tailored examples file..."
-gzip -c -k -f $TESTRECORDS > ../../$TESTFOLDER/qa-instances.jsonl.gz
+gzip -c -k -f $TESTRECORDS > $TESTFOLDER/tailored-instances.jsonl.gz
 
-# Run the typenormalization script on the tailored examples and save as qa-normalized.jsonl
+# Empty works file (all test works are unlinked, local works)
+echo -n '' | gzip > $TESTFOLDER/tailored-works.jsonl.gz
+
+echo "Rebuild whelktool"
+../gradlew jar
+
+# Run the typenormalization script on the tailored examples and save as tailored-normalized.jsonl
 echo "Running typenormalization on tailored examples..."
-time java -Xmx2G -Dxl.secret.properties=../secret.properties-dev2 -Dtypenormalization=simple-types-algorithm -Dnowhelk.datadir=../../$TESTFOLDER -Dnowhelk.basename=qa -jar build/libs/whelktool.jar --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
+time java -Xmx2G -Dxl.secret.properties=../secret.properties-dev2 -Dtypenormalization=simple-types-algorithm -Dnowhelk.datadir=$TESTFOLDER -Dnowhelk.basename=tailored -jar build/libs/whelktool.jar --dry-run --no-threads scripts/typenormalization/nowhelk.groovy
 
 # Run tests on the output
 echo "Running tests on normalized tailored examples..."
-python3 scripts/typenormalization/run_tailored_tests.py ../../$TESTFOLDER/qa-normalized.jsonl
+python3 scripts/typenormalization/run_tailored_tests.py $TESTFOLDER/tailored-normalized.jsonl
