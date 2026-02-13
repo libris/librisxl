1056984a1b198319877355ea48f5319d37fa1377
Niklas Lindström <lindstream@gmail.com>
2026-02-13 15:37:54 +0100

    Remove some other one-off scripts

--- whelktool/scripts/typenormalization/review_tests.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-with open(sys.argv[1]) as normalized_with_category:
-    records = []
-    for r in normalized_with_category:
-        records.append(json.loads(r))
-for n, rec in enumerate(records, 1):
-    print(f"\n{n}.\t", rec["@graph"][1]["instanceOf"]["hasTitle"][0]["mainTitle"])
