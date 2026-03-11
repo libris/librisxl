029f7dd358ed289a0aef1f85213783cd7863c096
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2026-03-11 17:13:29 +0100

    Add missing files.

---
+++ sru/src/test/groovy/whelk/sru/cql/CqlToXlSpec.groovy
@@ -0,0 +1,20 @@
+package whelk.sru.cql
+
+import spock.lang.Specification
+import sru.whelk.cql.cqlLexer
+import sru.whelk.cql.cqlParser
+
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.CharStreams;
+
+class CqlToXlSpec extends Specification {
+def "simple query"() {
+    given:
+    //String input = "AAA AND BBB"
+    String input = "dc.title any fish or/rel.combine=sum dc.creator any sanderson"
+    String output = Translation.translateCqlToXlQuery(input)
+
+    expect:
+    output == "good output"
+    }
+}
