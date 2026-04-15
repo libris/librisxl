f02f9915d884df81bda7f2a5c85d9a93beb934a9
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2026-04-15 16:46:20 +0200

    Revert "perf(DFS): remove unnecessary wrapper, intermediate Node, descend; use ArrayList instead of Stack (no need for synchronized here)"
    This reverts commit c22522cdb19380505dbb89264c84cd73cb56edfe.

--- whelk-core/src/main/groovy/whelk/util/DFS.java
+++ whelk-core/src/main/groovy/whelk/util/DFS.java
@@ -4,46 +4,56 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 // This is a straightforward port of the old groovy version in DocumentUtil. Can probably be improved
 class DFS {
-    private ArrayList<Object> path;
+    private Stack<Object> path;
     private DocumentUtil.Visitor visitor;
     private List<DocumentUtil.Operation> operations;
 
+    private record Node(Object value, Object keyOrIndex) {}
+
     public boolean traverse(final Object obj, DocumentUtil.Visitor visitor) {
         this.visitor = visitor;
-        path = new ArrayList<>();
+        path = new Stack<>();
         operations = new ArrayList<>();
 
         node(obj);
 
         Collections.reverse(operations);
-        for (DocumentUtil.Operation op : operations) {
+        for  (DocumentUtil.Operation op : operations) {
             op.perform(obj);
-        }
+        };
         return !operations.isEmpty();
     }
 
     private void node(Object obj) {
-        var op = visitor.visitElement(obj, path);
+        var op = visitor.visitElement(obj, Collections.unmodifiableList(path));
         if (op != null && !(op instanceof DocumentUtil.Nop)) {
             op.setPath(path);
             operations.add(op);
         }
 
         if (obj instanceof Map<?, ?> map) {
-            for (var e : map.entrySet()) {
-                path.add(e.getKey());
-                node(e.getValue());
-                path.removeLast();
-            }
+            var nodes = map.entrySet().stream()
+                    .map(e -> new Node(e.getValue(), e.getKey()))
+                    .toList();
+            descend(nodes);
         } else if (obj instanceof List<?> list) {
-            for (int i = 0; i < list.size(); i++) {
-                path.add(i);
-                node(list.get(i));
-                path.removeLast();
+            var nodes = new ArrayList<Node>(list.size());
+            for (int i = 0 ; i < list.size() ; i++) {
+                nodes.add(new Node(list.get(i), i));
             }
+            descend(nodes);
+        }
+    }
+
+    private void descend(List<Node> nodes) {
+        for (var n : nodes) {
+            path.push(n.keyOrIndex);
+            node(n.value);
+            path.pop();
         }
     }
 }
