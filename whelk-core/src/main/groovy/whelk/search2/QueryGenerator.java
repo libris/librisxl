60fe93d03a80e933e4bab45673421518475acca0
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2026-04-29 19:33:01 +0200

    feat(search): start remaking QueryGenerator

--- whelk-core/src/main/groovy/whelk/search2/QueryGenerator.java
+++ whelk-core/src/main/groovy/whelk/search2/QueryGenerator.java
@@ -10,57 +10,77 @@ import whelk.search2.querytree.Path;
 import whelk.search2.querytree.Selector;
 import whelk.search2.querytree.Token;
 import whelk.util.DocumentUtil;
+import whelk.util.FresnelUtil;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import static whelk.JsonLd.REVERSE_KEY;
 import static whelk.JsonLd.THING_KEY;
+import static whelk.JsonLd.WORK_KEY;
 
 public class QueryGenerator {
+
+    private static final List<List<Object>> PATHS = List.of(
+            List.of(THING_KEY, "classification", "*"),
+            List.of(THING_KEY, WORK_KEY, "classification", "*"),
+            List.of(THING_KEY, "publication", "*", "agent"),
+            List.of(THING_KEY, REVERSE_KEY, WORK_KEY, "*", "publication", "*", "agent")
+    );
+
     /*
     TODO
     We will have to revisit how we generate queries.
     This is just to have something to test in the frontend
      */
     public static void insertFindLinksOnBlankNodes(Object data, Whelk whelk) {
-        DocumentUtil.traverse(data, ( value, path) -> {
-            if (value instanceof Map<?, ?> map) {
-                var node = QueryUtil.castToStringObjectMap(map);
-                if (isBlank(node)) {
-                    var qPath = toQueryPath(path, whelk.jsonld);
+        for (var path : PATHS) {
+            var nodes = DocumentUtil.getAtPath(data, path, Collections.emptyList());
 
-                    if (!node.containsKey(JsonLd.Platform.COMPUTED_LABEL)) {
-                        return DocumentUtil.NOP;
-                    }
-
-                    // TODO
-                    var text = ((String) node.get(JsonLd.Platform.COMPUTED_LABEL)).split("·")[0].trim();
-                    var q = qPath.path().isEmpty()
-                            ? new FreeText(text)
-                            : new Condition(qPath, Operator.EQUALS, scopedFreeText(text));
-
-                    try {
-                        var url = QueryUtil.makeViewFindUrl(q.toQueryString(true), new QueryParams(Collections.emptyMap()));
-                        node.put("_find", Map.of(
-                                JsonLd.TYPE_KEY, "_Query",
-                                JsonLd.ID_KEY, url
-                        ));
-                    } catch (InvalidQueryException e) {
-                        throw new RuntimeException(e);
+            if (nodes instanceof List<?> l) {
+                for (var value : l) {
+                    if (value instanceof Map<?, ?> map) {
+                        var node = QueryUtil.castToStringObjectMap(map);
+                        if (isBlank(node)) {
+                            insert(path, node, whelk);
+                        }
                     }
                 }
             }
+        }
+    }
 
-            return DocumentUtil.NOP;
-        });
+    private static void insert(List<Object> path, Map<String, Object> node, Whelk whelk) {
+        var qPath = toQueryPath(path, whelk.jsonld);
+
+        var text = whelk.getFresnelUtil().asString(node, FresnelUtil.Lenses.SEARCH_NEEDLE);
+        if (text.isBlank()) {
+            return;
+        }
+
+        // TODO
+        var q = qPath.path().isEmpty()
+                ? new FreeText(text)
+                : new Condition(qPath, Operator.EQUALS, scopedFreeText(text));
+
+        try {
+            var url = QueryUtil.makeViewFindUrl(q.toQueryString(true), new QueryParams(Collections.emptyMap()));
+            node.put("_find", Map.of(
+                    JsonLd.TYPE_KEY, "_Query",
+                    JsonLd.ID_KEY, url
+            ));
+        } catch (InvalidQueryException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static Path toQueryPath(List<Object> docPath, JsonLd jsonLd) {
         return new Path(docPath.stream()
                 .filter(s -> !THING_KEY.equals(s))
                 .filter(s -> !(s instanceof Number))
+                .filter(s -> !"*".equals(s))
                 .filter(s -> !JsonLd.REVERSE_KEY.equals(s))
                 .filter(s -> !jsonLd.isIntegral(String.valueOf(s)))
                 .map(p -> new Key.UnrecognizedKey(new Token.Raw(String.valueOf(p), 0))).toList());
