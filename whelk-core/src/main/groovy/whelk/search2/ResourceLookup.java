2feff72f10b1cfff92a3c02c768c48783687eb97
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2026-05-04 17:59:44 +0200

    feat(search2): Add ExternalMapping for IntendedAudience

--- whelk-core/src/main/groovy/whelk/search2/ResourceLookup.java
+++ whelk-core/src/main/groovy/whelk/search2/ResourceLookup.java
@@ -295,11 +295,13 @@ public record ResourceLookup(VocabMappings vocabMappings, ExternalMappings exter
             return loadMappings(whelk);
         }
 
+        // TODO: get from vocab
         private static ExternalMappings loadMappings(Whelk whelk) {
             Map<String, Map<String, Map<String, Object>>> mappings = new HashMap<>();
             mappings.put("Library", loadMappingsForType("Library", List.of("sigel"), whelk));
             mappings.put("bibdb:Organization", loadMappingsForType("bibdb:Organization", List.of("code"), whelk));
             mappings.put("Country", loadMappingsForType("Country", List.of("code"), whelk));
+            mappings.put("IntendedAudience", loadMappingsForType("marc:AudienceType", List.of("code"), whelk));
             return new ExternalMappings(mappings);
         }
 
