8d949cd7b1781c762989a809720ba7611e5ee573
Lisa Sjögren <lisa.sjogren@kb.se>
2026-05-08 15:12:00 +0200

    Create script file with steps

---
+++ whelktool/scripts/cleanups/2026/05/fmt-548-replace-local-otherPhysicalFormat-with-linked-reproductionOf.groovy
@@ -0,0 +1,13 @@
+//Identifierar instanser som 
+//- pekar ömsesidigt på varandra via (otherPhysicalFormat>describedBy>controlNumber --> sameAs)
+//- har marc:displayText Originalversion Digitaliserad version (se specificering ovan)
+//
+//I den elektroniska instansen
+//
+//Lägg till reproductionOf med länk till den fysiska instansen
+//
+//Ta bort otherPhysicalFormat
+//
+//I den fysiska instansen
+//
+//Ta bort otherPhysicalFormat
