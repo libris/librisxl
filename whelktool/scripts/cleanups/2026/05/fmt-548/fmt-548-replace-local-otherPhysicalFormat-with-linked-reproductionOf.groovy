f8bffa00f14e5e3827fca80d422c4a16cf3245cf
Lisa Sjögren <lisa.sjogren@kb.se>
2026-05-12 16:58:52 +0200

    Create script that links instances based on list from notebook

---
+++ whelktool/scripts/cleanups/2026/05/fmt-548/fmt-548-replace-local-otherPhysicalFormat-with-linked-reproductionOf.groovy
@@ -0,0 +1,68 @@
+//Identifierar instanser som 
+//- pekar ömsesidigt på varandra via (otherPhysicalFormat>describedBy>controlNumber --> sameAs)
+//- har marc:displayText physicalversion Digitaliserad version (se specificering ovan)
+//
+
+// Set up logging
+def report = getReportWriter("report.tsv")
+
+// Read IDs
+var input = new File(scriptDir, 'mutual_matches_ids.tsv').readLines()
+
+List ids = []
+Map idMap = [:]
+
+// get header indexes
+def header = input[0].split("\t").toList()
+def physicalIndex = header.indexOf("instance_physical")
+def digitalIndex = header.indexOf("instance_digital")
+
+input.drop(1).each { line ->
+    def cols = line.split("\t")
+
+    def physicalIds = cols[physicalIndex]
+    def digitalIds = cols[digitalIndex]
+
+    ids << physicalIds
+
+    idMap.get(physicalIds, []) << digitalIds
+}
+
+// Fetch the phyiscal instance cfrom Libris
+selectByIds(ids) { physicalDoc ->
+
+    def physicalInstance = physicalDoc.graph[1]
+    def physicalShortId = physicalDoc.doc.shortId
+    def physicalId = physicalInstance["@id"]
+    
+    // Get id:s of digital reproductions of this resource from the ID map
+        selectByIds(idMap[physicalShortId]) { digitalDoc -> 
+
+            // Double check that local/old IDs match? maybe not necessary.
+
+            def digitalInstance = digitalDoc.graph[1]
+
+            if (digitalInstance.reproductionOf) {
+                def digitalId = digitalInstance["@id"]
+                report.println("$digitalId\talready has digitalReproduction. Skipping.")
+            }
+
+            else {
+
+                // Add "reproductionOf" with a link to the physical instance
+                digitalInstance.reproductionOf =  [
+                    "@id": physicalId
+                ]
+
+                // In the digital instance
+                // TODO Remove the property otherPhysicalFormat or a certain blank node?
+                digitalInstance.remove("otherPhysicalFormat")
+                
+                // In the physical instance
+                // TODO Remove the property otherPhysicalFormat or a certain blank node?
+                physicalInstance.remove("otherPhysicalFormat")
+            }
+
+    }
+
+}
