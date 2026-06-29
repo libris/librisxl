968a51bf44bb489ae6c48584fa56fd3fac00bfa5
Lisa Sjögren <lisa.sjogren@kb.se>
2026-06-29 18:23:37 +0200

    Look up by title, contributor and free text ISSN via API

--- whelktool/scripts/dataimports/shb/identify_matches.py
+++ whelktool/scripts/dataimports/shb/identify_matches.py
@@ -6,21 +6,32 @@ import argparse
 import json
 import unicodedata
 import re
+import requests
+import time
+
 from rapidfuzz import fuzz
 
-def prepare(instance: dict) -> dict:
+def prepare(entity: dict, match_counts: dict) -> dict:
 
-    try: 
-        has_title = instance.get("hasTitle", {})
-        normalized = {
-                "@id": instance["@id"],
-                "title": normalize(has_title.get("mainTitle", "")) + normalize(has_title.get("subtitle", "")),
-                "name": normalize(instance.get("responsibilityStatement", "")),
+    instance = entity["@graph"]["@graph"][1]
+    has_title = instance["hasTitle"]
+    responsibility_statement = instance.get("responsibilityStatement")
+
+    if has_title and responsibility_statement:
+        prepped = {
+            "@id": instance["@id"],
+            "full_title": f"{has_title.get("mainTitle", "")} {has_title.get("subtitle", "")}",
+            "responsibility_statement": responsibility_statement,
+            "part_issn": instance.get("partOf", {}).get("identifiedBy", {}).get("value", "")
             }
-        return normalized
-    except Exception as e:
-        print(f"{instance}\t{e}")
-        exit()
+        return prepped
+    else: 
+        if "insufficient" in match_counts:
+            match_counts["insufficient"] += 1
+        else:
+            match_counts["insufficient"] = 1
+        report.write(f"{instance["@id"]}\tInsufficient data - matching requires at least title and contributor\n")
+
 
 
 def compare(shbd: dict, match_record: dict):
@@ -61,42 +72,90 @@ def normalize(value: str):
     return value
 
 
+def find_matches(shbd_prepepd: dict, id_map, match_counts: dict):
+    '''
+    Given an SHBD record, finds matching records already in LIBRIS.
+    '''
+    headers = {"Accept": "application/ld+json"}
+
+    # Match on full title and contributor
+    query_string = f"title:{shbd_prepepd['full_title']} contributor:{shbd_prepepd['responsibility_statement']}* {shbd_prepepd['part_issn']}"
+
+    params = {"_q": query_string,
+          "_lens": "cards",
+          "limit": 50}
+
+    res = requests.get("http://libris-qa.kb.se/find?", params = params, headers=headers)
+    res.raise_for_status()
+
+    matches = res.json()["items"]
+
+    number_of_matches = len(matches)
+
+    if number_of_matches in match_counts:
+        match_counts[number_of_matches] += 1
+    else:
+        match_counts[number_of_matches] = 1
+
+    if matches:
+        id_with_matches = {shbd_prepepd["@id"]: [item["@id"] for item in res.json()["items"]]}
+        id_map.append(id_with_matches)
+        match_file.write(f"{shbd_prepepd["@id"]}\t{number_of_matches}\t{query_string}\t{json.dumps(id_with_matches)}\n")
+        return matches
+
 ### Main action ###
 
 argp = argparse.ArgumentParser()
-argp.add_argument("libris_file")
+#argp.add_argument("libris_file")
 argp.add_argument("shbd_file")
+argp.add_argument("match_file")
+argp.add_argument("report")
 args = argp.parse_args()
 
-good_matches = []
+start = time.time()
+
+id_map = []
+match_counts = {}
 
 # Create a simple dictionary of minimal match records from Libris
-with open(args.libris_file) as lf:
-    libris_prepepd_list: list = []
-    for line in lf:
-        libris_instance = json.loads(line)["@graph"]["@graph"][1]
-        libris_prepepd_list.append(prepare(libris_instance))
+#with open(args.libris_file) as lf:
+#    libris_prepepd_list: list = []
+#    for line in lf:
+#        libris_instance = json.loads(line)["@graph"]["@graph"][1]
+#        libris_prepepd_list.append(prepare(libris_instance))
 
-with open(args.shbd_file) as sf:
+with open(args.shbd_file, "r") as sf, open(args.match_file, "w") as match_file, open(args.report, "w") as report:
+    match_file.write("number_of_matches\tquery_string\tid_map\n")
+
+
     for idx, line in enumerate(sf):
-        matches = {}
-
-        if idx % 500 == 0:
-            print(f"Processing row {idx}")
-
-        shbd_instance = json.loads(line)
-        shbd_prepepd = prepare(shbd_instance)
-
-        for libris_prepped in libris_prepepd_list:
-            score = compare(libris_prepped, shbd_prepepd)
-            if score > 0.9:
-                matches[shbd_instance['@id']] = libris_prepped['@id']
         
-        if len(matches) == 1:
-            good_matches.append(good_matches)
-        elif len(matches) > 1:
-            print(f"Too many matches for {shbd_instance['@id']}: {matches}")
-        else:
-            print(f"No matches for {shbd_instance['@id']}: {matches}")
+        if idx % 100 == 0:
+            match_file.flush()
+            report.flush()
+            elapsed = idx / (time.time() - start)
+            elapsed_formatted = "{0:.4g}".format(elapsed)
+            if match_counts:
+                print(f"{idx + 1} records processed\t\t{elapsed_formatted} records/sec."
+            )
+                print(match_counts)
 
-    print(good_matches)
+
+        shbd_prepepd = prepare(json.loads(line), match_counts)
+
+        if shbd_prepepd:
+            matches = find_matches(shbd_prepepd, id_map, match_counts)
+
+        #for libris_prepped in libris_prepepd_list:
+        #    score = compare(libris_prepped, shbd_prepepd)
+        #    if score > 0.9:
+        #        matches[shbd_instance['@id']] = libris_prepped['@id']
+        #
+        #if len(matches) == 1:
+        #    good_matches.append(good_matches)
+        #elif len(matches) > 1:
+        #    print(f"Too many matches for {shbd_instance['@id']}: {matches}")
+        #else:
+        #    print(f"No matches for {shbd_instance['@id']}: {matches}")
+
+print(f"\nMatches:\n{match_counts}")
