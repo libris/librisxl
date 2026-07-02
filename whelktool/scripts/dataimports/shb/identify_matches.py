c79599ca5583c44c7463c70d5aa336df4ae69213
Lisa Sjögren <lisa.sjogren@kb.se>
2026-07-02 18:13:55 +0200

    Don't remove necessary colons in formatted query

--- whelktool/scripts/dataimports/shb/identify_matches.py
+++ whelktool/scripts/dataimports/shb/identify_matches.py
@@ -11,53 +11,57 @@ import time
 
 from rapidfuzz import fuzz
 
+
 def prepare(entity: dict, match_counts: dict) -> dict:
 
     instance = entity["@graph"]["@graph"][1]
     has_title = instance["hasTitle"]
-    full_title = f'{has_title.get("mainTitle", "")} {has_title.get("subtitle", "")}'
-    responsibility_statement = instance.get("responsibilityStatement")
+    full_title = f"{has_title.get('mainTitle', '')} {has_title.get('subtitle', '')}".replace(
+        ":", ""
+    )
+    responsibility_statement = instance.get("responsibilityStatement", "").replace(
+        ":", ""
+    )
 
-    if (has_title and responsibility_statement):
+    if has_title and responsibility_statement:
         prepped = {
             "@id": instance["@id"],
             "full_title": normalize(full_title),
             "responsibility_statement": responsibility_statement,
-            "part_of_issn": instance.get("partOf", {}).get("identifiedBy", {}).get("value", ""),
-            "issn_from_note": instance.get("issn_from_note", "")
-            }
+            "part_of_issn": instance.get("partOf", {})
+            .get("identifiedBy", {})
+            .get("value", ""),
+            "issn_from_note": instance.get("issn_from_note", ""),
+        }
         return prepped
-    else: 
+    else:
         if "insufficient" in match_counts:
             match_counts["insufficient"] += 1
         else:
             match_counts["insufficient"] = 1
-        report.write(f"{instance['@id']}\tTitle or contributor missing\t{json.dumps(instance)}\n")
-
+        report.write(
+            f"{instance['@id']}\tTitle or contributor missing\t{json.dumps(instance)}\n"
+        )
 
 
 def compare(shbd: dict, match_record: dict):
 
-    title_score = (
-        fuzz.ratio(shbd["title"], match_record["title"]) / 100
-    )
+    title_score = fuzz.ratio(shbd["title"], match_record["title"]) / 100
 
-    author_score = (
-        fuzz.ratio(shbd["name"], match_record["name"]) / 100
-    )
+    author_score = fuzz.ratio(shbd["name"], match_record["name"]) / 100
 
-    #year_score = 1.0 if shbd["year"] == match_record["year"] else 0.0
+    # year_score = 1.0 if shbd["year"] == match_record["year"] else 0.0
 
-    overall_score = 0.6 * title_score + 0.3 * author_score + 0.1# * year_score
+    overall_score = 0.6 * title_score + 0.3 * author_score + 0.1  # * year_score
 
     return overall_score
 
 
 def normalize(value: str):
     # Remove diacritics -- ??? too radical or useful with the OCR'd data?
-    #"".join(
+    # "".join(
     #    c for c in unicodedata.normalize("NFKD", value) if not unicodedata.combining(c)
-    #)
+    # )
 
     # Make lowercase
     value = value.lower()
@@ -75,20 +79,22 @@ def normalize(value: str):
 
 
 def find_matches(shbd_prepepd: dict, id_map, match_counts: dict):
-    '''
+    """
     Given an SHBD record, finds matching records already in LIBRIS.
-    '''
+    """
     headers = {"Accept": "application/ld+json"}
 
     # Match on full title and contributor
-    query_string = f"title:{shbd_prepepd['full_title']} contributor:{shbd_prepepd['responsibility_statement']}* {shbd_prepepd['part_of_issn']} {shbd_prepepd['issn_from_note']}".replace(":","")
+    query_string = f"title:{shbd_prepepd['full_title']} contributor:{shbd_prepepd['responsibility_statement']}* {shbd_prepepd['part_of_issn']} {shbd_prepepd['issn_from_note']}"
 
-    params = {"_q": query_string,
-          "_lens": "cards",
-          "_stats": "false", # Not needed
-          "limit": 50}
+    params = {
+        "_q": query_string,
+        "_lens": "cards",
+        "_stats": "false",  # Not needed
+        "limit": 50,
+    }
 
-    res = requests.get(f"{base_url}/find?", params = params, headers=headers)
+    res = requests.get(f"{base_url}/find?", params=params, headers=headers)
     res.raise_for_status()
     time.sleep(0.001)
 
@@ -102,15 +108,19 @@ def find_matches(shbd_prepepd: dict, id_map, match_counts: dict):
         match_counts[number_of_matches] = 1
 
     if matches:
-        id_with_matches = {shbd_prepepd['@id']: [item['@id'] for item in res.json()['items']]}
+        id_with_matches = {
+            shbd_prepepd["@id"]: [item["@id"] for item in res.json()["items"]]
+        }
         id_map.append(id_with_matches)
-        match_file.write(f"{shbd_prepepd['@id']}\t{number_of_matches}\t{query_string}\t{json.dump(id_with_matches)}\n")
+        match_file.write(
+            f"{shbd_prepepd['@id']}\t{number_of_matches}\t{query_string}\t{json.dumps(id_with_matches)}\n"
+        )
         return matches
+
 
 ### Main action ###
 
 argp = argparse.ArgumentParser()
-#argp.add_argument("libris_file")
 argp.add_argument("env")
 argp.add_argument("shbd_file")
 argp.add_argument("match_file")
@@ -128,43 +138,44 @@ base_url = f"http://libris{env_path}.kb.se"
 print(f"Getting started! Matching against records in {base_url}")
 
 # Create a simple dictionary of minimal match records from Libris
-#with open(args.libris_file) as lf:
+# with open(args.libris_file) as lf:
 #    libris_prepepd_list: list = []
 #    for line in lf:
 #        libris_instance = json.loads(line)["@graph"]["@graph"][1]
 #        libris_prepepd_list.append(prepare(libris_instance))
 
-with open(args.shbd_file, "r") as sf, open(args.match_file, "w") as match_file, open(args.report, "w", encoding="utf-8") as report:
+with open(args.shbd_file, "r") as sf, open(args.match_file, "w") as match_file, open(
+    args.report, "w", encoding="utf-8") as report:
     match_file.write("id\tnumber_of_matches\tquery_string\tid_map\n")
 
     for idx, line in enumerate(sf):
-        
+
         if idx % 500 == 0:
             match_file.flush()
             report.flush()
             elapsed = idx / (time.time() - start)
             elapsed_formatted = "{0:.4g}".format(elapsed)
             if match_counts:
-                print(f"{idx + 1} records processed\t\t{elapsed_formatted} records/sec."
-            )
+                print(
+                    f"{idx + 1} records processed\t\t{elapsed_formatted} records/sec."
+                )
                 print(match_counts)
-
 
         shbd_prepepd = prepare(json.loads(line), match_counts)
 
         if shbd_prepepd:
             matches = find_matches(shbd_prepepd, id_map, match_counts)
 
-        #for libris_prepped in libris_prepepd_list:
+        # for libris_prepped in libris_prepepd_list:
         #    score = compare(libris_prepped, shbd_prepepd)
         #    if score > 0.9:
         #        matches[shbd_instance['@id']] = libris_prepped['@id']
         #
-        #if len(matches) == 1:
+        # if len(matches) == 1:
         #    good_matches.append(good_matches)
-        #elif len(matches) > 1:
+        # elif len(matches) > 1:
         #    print(f"Too many matches for {shbd_instance['@id']}: {matches}")
-        #else:
+        # else:
         #    print(f"No matches for {shbd_instance['@id']}: {matches}")
 
 print(f"\nMatches:\n{match_counts}")
