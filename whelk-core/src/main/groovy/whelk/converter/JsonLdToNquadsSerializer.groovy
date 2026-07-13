ec9ec7c5013c6556014a39f9b443cf390a71dfe8
Niklas Lindström <lindstream@gmail.com>
2026-07-13 16:52:28 +0200

    Ensure blank node IDs are unique in nquads dataset

--- whelk-core/src/main/groovy/whelk/converter/JsonLdToNquadsSerializer.groovy
+++ whelk-core/src/main/groovy/whelk/converter/JsonLdToNquadsSerializer.groovy
@@ -2,12 +2,15 @@ package whelk.converter
 
 import groovy.transform.CompileStatic
 
+import org.apache.commons.codec.digest.DigestUtils
+
 import trld.platform.Output
 import trld.jsonld.Context
-import static trld.jsonld.Expansion.expansion
-import static trld.jsonld.Flattening.flatten
-import static trld.jsonld.Rdf.toRdfDataset
-import static trld.nq.Serializer.serialize
+import trld.jsonld.Expansion
+import trld.jsonld.Flattening
+import trld.jsonld.BNodes
+import trld.jsonld.Rdf
+import trld.jsonld.RdfDataset
 
 @CompileStatic
 class JsonLdToNquadsSerializer implements JsonLdToRdfSerializer {
@@ -28,9 +31,24 @@ class JsonLdToNquadsSerializer implements JsonLdToRdfSerializer {
     }
 
     void writeGraph(String id, Object data) {
-        data = ['@id': id, '@graph': data]
-        var rdfDataset = toRdfDataset(flatten(expansion(context, null, data, id)))
-        serialize(rdfDataset, out)
+        // Note: not enough:
+        // var rdfDataset = toRdfDataset(flatten(expansion(context, null, data, id)))
+        // ; need to ensure unique part bnode IDs in resulting output dataset:
+        var hash = DigestUtils.sha256Hex(id)
+        var pfx = "_:${hash}-" as String
+        var bnodes = new BNodes() {
+           String makeBnodeId(String identifier) {
+            return pfx + super.makeBnodeId(identifier).substring(2)
+           }
+        }
+        data = ['@id': id, '@graph': data] // to named graph
+        var flatExpandedData = Flattening.flatten(Expansion.expansion(context, null, data, id))
+        Map<String, Map<String, Object>> nodeMap = ['@default': [:]]
+        Flattening.makeNodeMap(bnodes, flatExpandedData, nodeMap)
+        var rdfDataset = new RdfDataset()
+        Rdf.jsonldToRdfDataset(nodeMap, rdfDataset, bnodes, null)
+
+        trld.nq.Serializer.serialize(rdfDataset, out)
     }
 
 }
