14b808e7daf610ca1ce7b9c6ca5c25f83c083cfc
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2026-07-16 20:09:06 +0200

    refactor(whelk-core.util): port Metrics to Java

--- whelk-core/src/main/groovy/whelk/util/Metrics.java
+++ whelk-core/src/main/groovy/whelk/util/Metrics.java
@@ -1,24 +1,24 @@
-package whelk.util
+package whelk.util;
 
-import io.prometheus.client.Counter
-import io.prometheus.client.Summary
-import io.prometheus.client.guava.cache.CacheMetricsCollector
+import io.prometheus.client.Counter;
+import io.prometheus.client.Summary;
+import io.prometheus.client.guava.cache.CacheMetricsCollector;
 
-class Metrics {
-    public static final CacheMetricsCollector cacheMetrics = new CacheMetricsCollector().register()
+public class Metrics {
+    public static final CacheMetricsCollector cacheMetrics = new CacheMetricsCollector().register();
 
-    static final Summary clientTimer = Summary.build()
+    public static final Summary clientTimer = Summary.build()
             .labelNames("target", "method")
             .quantile(0.5, 0.05)
             .quantile(0.95, 0.01)
             .quantile(0.99, 0.001)
             .name("client_requests_latency_seconds")
             .help("External request latency in seconds.")
-            .register()
+            .register();
 
-    static final Counter clientCounter = Counter.build()
+    public static final Counter clientCounter = Counter.build()
             .labelNames("target", "method", "status")
             .name("client_call_status")
             .help("External response status.")
-            .register()
+            .register();
 }
