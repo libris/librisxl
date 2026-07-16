d63c56a088a80b1af4bacf012abd2dc736f48a2e
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2026-07-16 20:07:30 +0200

    refactor(whelk-core.util): port Jackson to Java

--- whelk-core/src/main/groovy/whelk/util/Jackson.java
+++ whelk-core/src/main/groovy/whelk/util/Jackson.java
@@ -1,30 +1,32 @@
-package whelk.util
+package whelk.util;
 
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.core.JsonProcessingException
-import com.fasterxml.jackson.core.Version
-import com.fasterxml.jackson.databind.JsonSerializer
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializerProvider
-import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import groovy.lang.GString;
 
-class Jackson {
-    public static final ObjectMapper mapper = mapper()
-    
-    static ObjectMapper mapper() {
-        ObjectMapper mapper = new ObjectMapper()
-        def module = new SimpleModule(GStringSerializer.class.getSimpleName(), GStringSerializer.version)
-        module.addSerializer(GString, new GStringSerializer())
-        mapper.registerModule(module)
-        return mapper
+import java.io.IOException;
+
+public class Jackson {
+    public static final ObjectMapper mapper = mapper();
+
+    public static ObjectMapper mapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule(GStringSerializer.class.getSimpleName(), GStringSerializer.version);
+        module.addSerializer(GString.class, new GStringSerializer());
+        mapper.registerModule(module);
+        return mapper;
     }
 
-    static class GStringSerializer extends JsonSerializer<GString> {
-        static Version version = new Version(1, 0, 0, null, null, null)
+    public static class GStringSerializer extends JsonSerializer<GString> {
+        static final Version version = new Version(1, 0, 0, null, null, null);
 
         @Override
-        void serialize(GString value, JsonGenerator generator, SerializerProvider provider) throws IOException, JsonProcessingException {
-            generator.writeString(value.toString())
+        public void serialize(GString value, JsonGenerator generator, SerializerProvider provider) throws IOException {
+            generator.writeString(value.toString());
         }
     }
 }
