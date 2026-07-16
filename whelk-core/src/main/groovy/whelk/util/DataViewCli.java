9dfc6d6a3fa644860272d46f0aae0dea8fceb4f1
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2026-07-16 20:07:01 +0200

    refactor(whelk-core.util): port DataViewCli to Java

--- whelk-core/src/main/groovy/whelk/util/DataViewCli.java
+++ whelk-core/src/main/groovy/whelk/util/DataViewCli.java
@@ -1,60 +1,81 @@
-package whelk.util
+package whelk.util;
 
-import whelk.Document
-import whelk.Whelk
+import whelk.Document;
+import whelk.Whelk;
+import whelk.component.ElasticSearch;
 
-import static whelk.util.Jackson.mapper
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static whelk.util.Jackson.mapper;
 
 // TODO: factor out and use MarcFrameCli.addJsonLd to allow for local data.
-assert 'xl.secret.properties' in System.properties
+public class DataViewCli {
+    private static final List<String> COMMANDS = List.of("card", "chip", "embellish", "index");
 
-whelk = Whelk.createLoadedCoreWhelk()
-ld = whelk.jsonld
+    private static Whelk whelk;
 
-def COMMANDS = ['card', 'chip', 'embellish', 'index']
+    public static void main(String[] args) throws Exception {
+        if (System.getProperty("xl.secret.properties") == null) {
+            throw new IllegalStateException("Missing system property 'xl.secret.properties'");
+        }
 
-def view(cmd, ref) {
-    var file = new File(ref)
-    def data = file.exists() ? mapper.readValue(file, Map) : null
-    switch (cmd) {
-        // TODO: most useful if local data can be loaded (see TODO above).
-        //case 'card':
-        //    break
-        //case 'chip':
-        //    break
-        case 'embellish':
-            if (data) {
-              return ld.embellish(data)
-            } else {
-              var doc = whelk.getDocument(ref)
-              assert doc, "$ref is not found?"
-              System.err.println "$cmd: $doc.id (data is ${sizeOf(doc.data)} bytes)"
-              whelk.embellish(doc)
-              System.err.println "Done (data is now ${sizeOf(doc.data)} bytes)"
-              return doc.data
+        whelk = Whelk.createLoadedCoreWhelk();
+
+        String cmd = "card";
+        List<String> refs;
+
+        if (args.length > 1) {
+            cmd = args[0];
+            if (!COMMANDS.contains(cmd)) {
+                throw new IllegalArgumentException("Unknown command: " + cmd);
             }
-        case 'index':
-            def doc = new Document(data)
-            return whelk.elastic.getShapeForIndex(doc, whelk, null)
+            refs = Arrays.asList(args).subList(1, args.length);
+        } else {
+            refs = Arrays.asList(args);
+        }
+
+        for (String ref : refs) {
+            Object result = view(cmd, ref);
+            System.out.println(mapper.writeValueAsString(result));
+        }
     }
-}
 
-def sizeOf(data) {
-    return mapper.writeValueAsString(data).size()
-}
+    @SuppressWarnings("unchecked")
+    private static Object view(String cmd, String ref) throws Exception {
+        File file = new File(ref);
+        Map<String, Object> data = file.exists() ? mapper.readValue(file, Map.class) : null;
+        switch (cmd) {
+            // TODO: most useful if local data can be loaded (see TODO above).
+            //case 'card':
+            //    break
+            //case 'chip':
+            //    break
+            case "embellish":
+                if (data != null) {
+                    Document doc = new Document(data);
+                    whelk.embellish(doc);
+                    return doc.data;
+                } else {
+                    Document doc = whelk.getDocument(ref);
+                    if (doc == null) {
+                        throw new IllegalArgumentException(ref + " is not found?");
+                    }
+                    System.err.println(cmd + ": " + doc.getId() + " (data is " + sizeOf(doc.data) + " bytes)");
+                    whelk.embellish(doc);
+                    System.err.println("Done (data is now " + sizeOf(doc.data) + " bytes)");
+                    return doc.data;
+                }
+            case "index":
+                Document doc = new Document(data);
+                return ElasticSearch.getShapeForIndex(doc, whelk);
+        }
+        return null;
+    }
 
-List refs
-def cmd = "card"
-
-if (args.length > 1) {
-    cmd = args[0]
-    assert cmd in COMMANDS
-    refs = args[1..-1]
-} else {
-    refs = args[0..-1]
-}
-
-for (ref in refs) {
-    def result = view(cmd, ref)
-    println mapper.writeValueAsString(result)
+    private static int sizeOf(Object data) throws Exception {
+        return mapper.writeValueAsString(data).length();
+    }
 }
