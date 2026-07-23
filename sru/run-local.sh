91a2f2541f0ce20130e5bcf23692167a423213cf
Kai P <kai.poykio@kb.se>
2026-07-23 11:00:22 +0200

    secret as parameter

--- sru/run-local.sh
+++ sru/run-local.sh
@@ -1,1 +1,7 @@
-java -Xmx2G -Dxl.secret.properties=/appl/import/etc/secret.properties.prod -jar build/libs/sru.jar
+SECRET=$1
+
+if [ "$SECRET" != '' ]; then
+	java -Xmx2G -Dxl.secret.properties=$SECRET -jar build/libs/sru.jar
+else
+	echo Nope.
+fi
